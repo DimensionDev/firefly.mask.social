55541b7d88b83e63d5e43fd3aa9d7fedb8ac549b
guanbinrui <guanbinrui@dimension.im>
2023-09-25 21:13:11 +0800

    chore: prettier

--- src/app/components/GA.tsx
+++ src/app/components/GA.tsx
@@ -1,20 +1,17 @@
-import Script from "next/script";
+import Script from 'next/script';
 
 export function GA() {
-  return (
-    <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-5VSBSKJ6JM"
-      />
-      <Script id="ga">
-        {`
+    return (
+        <>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5VSBSKJ6JM" />
+            <Script id="ga">
+                {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', 'G-5VSBSKJ6JM');
                 `}
-      </Script>
-    </>
-  );
+            </Script>
+        </>
+    );
 }
