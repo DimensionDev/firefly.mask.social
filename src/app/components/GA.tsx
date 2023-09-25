d8715f4af124ae9ac9870e08c9d2cda39da14437
guanbinrui <guanbinrui@dimension.im>
2023-09-25 21:04:31 +0800

    chore: add GA

---
+++ src/app/components/GA.tsx
@@ -0,0 +1,20 @@
+import Script from "next/script";
+
+export function GA() {
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-5VSBSKJ6JM"
+      />
+      <Script id="ga">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-5VSBSKJ6JM');
+                `}
+      </Script>
+    </>
+  );
+}
