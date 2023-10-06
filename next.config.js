8ab6fea71864fc3abc45e997b55c51ff48336c57
guanbinrui <guanbinrui@dimension.im>
2023-10-06 20:37:26 +0800

    fix: http_proxy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'tailwindui.com'],
+        domains: ['images.unsplash.com', 'tailwindui.com', 'pbs.twimg.com'],
     },
 };
 
