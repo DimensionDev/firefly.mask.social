6d98f438b87394d778c01eee0af65774cef49b55
nuanyang233 <nuanyang233@gmail.com>
2023-10-31 16:48:22 +0800

    fix: build error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        dangerouslyAllowSVG: true,
         domains: ['images.unsplash.com', 'tailwindui.com', 'pbs.twimg.com'],
+    },
+    webpack: (config, context) => {
+        if (config.plugins) {
+            config.plugins.push(
+                new context.webpack.IgnorePlugin({
+                    resourceRegExp: /^(lokijs|pino-pretty|encoding)$/,
+                }),
+            );
+        }
+        return config;
     },
 };
 
