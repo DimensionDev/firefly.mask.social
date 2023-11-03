bff04a0d242a50958b350f9fdcc045c8a4dd2b44
Harlan Sun <1249413181@qq.com>
2023-11-03 17:53:14 +0800

    feat: update cast response

---
+++ src/providers/types/Farcaster.ts
@@ -0,0 +1,69 @@
+export interface CastsResponse {
+  result: CastsResult
+  next: Next
+}
+
+export interface CastsResult {
+  casts: Cast[]
+}
+
+export interface Cast {
+  hash: string
+  threadHash: string
+  author: Author
+  text: string
+  timestamp: number
+  replies: Replies
+  reactions: Reactions
+  recasts: Recasts
+  watches: Watches
+  recast: boolean
+  viewerContext: ViewerContext
+}
+
+export interface Author {
+  fid: number
+  username: string
+  displayName: string
+  pfp: Pfp
+  followerCount: number
+  followingCount: number
+}
+
+export interface Pfp {
+  url: string
+  verified: boolean
+}
+
+export interface Replies {
+  count: number
+}
+
+export interface Reactions {
+  count: number
+}
+
+export interface Recasts {
+  count: number
+  recasters: Recaster[]
+}
+
+export interface Recaster {
+  fid: number
+  username: string
+  displayName: string
+}
+
+export interface Watches {
+  count: number
+}
+
+export interface ViewerContext {
+  reacted: boolean
+  recast: boolean
+  watched: boolean
+}
+
+export interface Next {
+  cursor: string
+}
