df41804327a2d28fbfed331857b7a6926b6fedc8
guanbinrui <guanbinrui@dimension.im>
2023-11-07 00:11:45 +0800

    chore: prettier

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const WARPCAST_ROOT_URL = 'https://api.warpcast.com/v2';
 
-export const EMPTY_LIST = Object.freeze([]) as never[]
-export const EMPTY_OBJECT = Object.freeze({}) as Record<string, never>
+export const EMPTY_LIST = Object.freeze([]) as never[];
+export const EMPTY_OBJECT = Object.freeze({}) as Record<string, never>;
