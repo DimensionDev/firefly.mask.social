344653804a8e71ebac2ef4eb0efab2b86469c202
guanbinrui <guanbinrui@dimension.im>
2023-11-10 15:29:15 +0800

    refactor: migrate destroy method

--- src/providers/farcaster/Session.ts
+++ src/providers/farcaster/Session.ts
@@ -1,6 +1,3 @@
-import urlcat from 'urlcat';
-import { WARPCAST_ROOT_URL } from '@/constants';
-import { fetchJSON } from '@/helpers/fetchJSON';
 import { Session } from '@/providers/types/Session';
 import { BaseSession } from '@/providers/base/Session';
 import { Type } from '@/providers/types/SocialMedia';
@@ -19,29 +16,7 @@ export class FarcasterSession extends BaseSession implements Session {
         throw new Error('Not allowed');
     }
 
-    async destroy(): Promise<void> {
-        const response = await fetchJSON<{
-            result: {
-                success: boolean;
-            };
-        }>(urlcat(WARPCAST_ROOT_URL, '/auth'), {
-            method: 'DELETE',
-            headers: {
-                Authorization: `Bearer ${this.token}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                method: 'revokeToken',
-                params: {
-                    timestamp: this.timestamp,
-                },
-            }),
-        });
-
-        // indicate the session is destroyed
-        this.expiresAt = 0;
-
-        if (!response.result.success) throw new Error('Failed to destroy the session.');
-        return;
+    destroy(): Promise<void> {
+        throw new Error('Not allowed');
     }
 }
