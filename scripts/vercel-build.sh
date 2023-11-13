4c11c650cfae6eadc8191dac095b6c52d8634d50
guanbinrui <guanbinrui@dimension.im>
2023-11-14 02:30:49 +0800

    chore: prettier

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -8,12 +8,8 @@ cd src/maskbook
 # Install dependencies using pnpm
 pnpm install
 
-# Run Gulp task to apply polyfills using npx
-npx gulp polyfill
-npx gulp codegen
-
 # Return to the original directory
 cd -
 
 # Run Next.js build for the main project
-npx next build
+next build
