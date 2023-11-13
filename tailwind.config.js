073ea62a9d5a91def7e75f911eabbc0405ffd66e
guanbinrui <guanbinrui@dimension.im>
2023-11-13 16:42:39 +0800

    chore: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-import type { Config } from 'tailwindcss';
-
+/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -24,4 +23,4 @@ export default {
         },
     },
     plugins: [require('@tailwindcss/forms')],
-} satisfies Config;
+} 
