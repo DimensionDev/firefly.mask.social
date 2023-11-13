6c14873dcb9e26102a10a0d35eea16b25fcc7196
guanbinrui <guanbinrui@dimension.im>
2023-11-14 03:25:11 +0800

    fix: vercel build

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -6,10 +6,16 @@ npm i -g pnpm@8.10.4
 cd src/maskbook
 
 # Install dependencies using pnpm
-pnpm install
+NODE_ENV=development pnpm install
+
+npx gulp polyfill
+npx gulp codegen
 
 # Return to the original directory
 cd -
+
+# Patch packages
+npm run postinstall
 
 # Run Next.js build for the main project
 next build
