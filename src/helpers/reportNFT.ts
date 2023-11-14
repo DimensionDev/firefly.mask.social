68a804ac023d0bc3dbc1c7006848a2562434a9a4
guanbinrui <guanbinrui@mask.io>
2023-11-14 17:20:30 +0800

    chore: add related link

--- src/helpers/reportNFT.ts
+++ src/helpers/reportNFT.ts
@@ -4,5 +4,7 @@
  * @param {string} collectionId - The unique identifier of the NFT collection to be reported.
  */
 export function reportNFT(collectionId: string) {
+    // Learn more:
+    // https://www.notion.so/mask/v1-misc-reportNFT-872f7736bc2d4ba7bdb243d03c6620d0
     throw new Error('Not implemented');
 }
