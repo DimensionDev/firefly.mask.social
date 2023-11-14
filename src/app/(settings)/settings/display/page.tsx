02118f83e9948fcee8ec4185c0bbbb429410ed82
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-11-15 03:38:21 +0800

    chore: prettier tailwind (#44)
    * chore: install prettier-plugin-tailwindcss
    
    * chore: sort class

--- src/app/(settings)/settings/display/page.tsx
+++ src/app/(settings)/settings/display/page.tsx
@@ -1,18 +1,18 @@
 export default function Display() {
     return (
-        <div className="flex flex-col w-full p-[24px] items-center">
-            <div className=" text-main text-[18px] font-bold leading-[24px] w-full gap-[24px]">Display</div>
-            <button className="w-[250px] h-[60px] px-3 bg-white rounded-lg border border-neutral-900 my-[12px] justify-center items-center gap-5 inline-flex">
-                <div className="text-slate-950 text-sm font-bold font-['Helvetica'] leading-[18px]">Follow System</div>
+        <div className="flex w-full flex-col items-center p-[24px]">
+            <div className=" w-full gap-[24px] text-[18px] font-bold leading-[24px] text-main">Display</div>
+            <button className="my-[12px] inline-flex h-[60px] w-[250px] items-center justify-center gap-5 rounded-lg border border-neutral-900 bg-white px-3">
+                <div className="font-['Helvetica'] text-sm font-bold leading-[18px] text-slate-950">Follow System</div>
             </button>
-            <button className="w-[250px] h-[60px] px-3 bg-white rounded-lg border border-neutral-900 my-[12px] justify-center items-center gap-5 inline-flex">
-                <div className="w-2 h-2 relative shadow">
-                    <div className="w-2 h-2 left-0 top-0 absolute bg-green-500 rounded-full" />
+            <button className="my-[12px] inline-flex h-[60px] w-[250px] items-center justify-center gap-5 rounded-lg border border-neutral-900 bg-white px-3">
+                <div className="relative h-2 w-2 shadow">
+                    <div className="absolute left-0 top-0 h-2 w-2 rounded-full bg-green-500" />
                 </div>
-                <div className="text-slate-950 text-sm font-bold font-['Helvetica'] leading-[18px]">Light mode</div>
+                <div className="font-['Helvetica'] text-sm font-bold leading-[18px] text-slate-950">Light mode</div>
             </button>
-            <button className="w-[250px] h-[60px] px-3 bg-slate-950 rounded-lg border border-white justify-center items-center gap-4 inline-flex">
-                <div className="text-white text-sm font-bold font-['Helvetica'] leading-[18px]">Dark mode</div>
+            <button className="inline-flex h-[60px] w-[250px] items-center justify-center gap-4 rounded-lg border border-white bg-slate-950 px-3">
+                <div className="font-['Helvetica'] text-sm font-bold leading-[18px] text-white">Dark mode</div>
             </button>
         </div>
     );
