359c04027630c2c87396e1756425b0cbf3de2c41
guanbinrui <guanbinrui@mask.io>
2023-11-14 16:37:48 +0800

    chore: add placeholder for misc APIs

---
+++ src/helpers/reportNFT.ts
@@ -0,0 +1,8 @@
+/**
+ * Reports a scam NFT collection based on the provided collectionId.
+ *
+ * @param {string} collectionId - The unique identifier of the NFT collection to be reported.
+ */
+export function reportNFT(collectionId: string) {
+    throw new Error('Not implemented');
+}
