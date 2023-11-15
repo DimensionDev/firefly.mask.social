f3a90748af35aaf582b404f737f6bfa49ccff66b
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-11-15 21:11:00 +0800

    chore: npm run test (#48)
    * chore: npm run test
    
    * fix: lint errors
    
    * chore: vitest setup
    
    * chore: tsc for vite
    
    * chore: compile ts files
    
    * chore: setup
    
    * fix: remove vite.config.js
    
    * fix: env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vitest/config';
 
+function createURL(pathToFile: string) {
+    return new URL(pathToFile, import.meta.url).toString();
+}
+
 export default defineConfig({
     test: {
         include: ['./tests/**/*.ts'],
+        exclude: ['./tests/**/*.d.ts'],
         alias: {
-            '@': './src',
+            '@': createURL('./src'),
         },
+        setupFiles: ['./setups/index.ts'],
     },
 });
