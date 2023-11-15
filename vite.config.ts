41bc8af90a28464fdabbaa8f1d25d77412a1c9a9
guanbinrui <guanbinrui@mask.io>
2023-11-15 14:35:54 +0800

    chore: npm run test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vitest/config';
 
+function createURL(pathToFile: string) {
+    return new URL(pathToFile, import.meta.url).toString();
+}
+
 export default defineConfig({
     test: {
         include: ['./tests/**/*.ts'],
         alias: {
-            '@': './src',
+            '@': createURL('./src'),
         },
     },
 });
