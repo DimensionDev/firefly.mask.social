51bcd5a9e9da8bc8a19cff71fa2b797d31974143
nuanyang233 <nuanyang233@gmail.com>
2023-11-18 05:22:01 -0800

    feat: init react-query (#52)
    * feat: init react-query
    
    * fix: ci
    
    * chore: prettier

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Image } from '@/esm/Image.js';
+
+export default function Loading() {
+    return (
+        <div className="flex min-h-[500px] items-center justify-center">
+            <Image src="/svg/loading.svg" className="animate-spin" width={24} height={24} alt="loading" />
+        </div>
+    );
+}
