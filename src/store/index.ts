a659d5e3cec0e17e09e662e56eca2d9b0120d6d8
Harlan Sun <1249413181@qq.com>
2023-11-24 10:50:26 +0800

    fix: spell

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-export * from './useAccountPresistStore.js';
+export * from './useAccountPersistStore.js';
 export { useGlobalState } from './useGlobalStore.js';
 export { useImpressionsStore } from './useImpressionsStore.js';
 export { hydrateLeafwatchViewerId, useLeafwatchPersistStore } from './useLeafwatchPersistStore.js';
