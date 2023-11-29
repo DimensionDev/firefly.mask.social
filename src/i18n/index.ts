2ffc67a9b5230092156d0f362b0cad7699f70cd0
guanbinrui <guanbinrui@mask.io>
2023-11-29 19:10:17 +0800

    fix: messages

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,17 @@
 import { i18n, type Messages } from '@lingui/core';
 import dayjs from 'dayjs';
 
+// @ts-ignore
+import * as EN from '@/locales/en/messages.mjs';
+
+// @ts-ignore
+import * as PSEUDO from '@/locales/pseudo/messages.mjs';
+
 import { IS_PREVIEW, IS_PRODUCTION, LS_LOCALE_KEY } from '@/constants/index.js';
 
 const locales: Record<string, Messages> = {
-    // @ts-ignore
-    en: await import(`@/locales/en/messages.mjs`),
-    // @ts-ignore
-    pseudo: await import(`@/locales/pseudo/messages.mjs`),
+    en: EN.messages,
+    pseudo: PSEUDO.messages,
 };
 
 export const supportedLocales: Record<string, string> = {
