e98fc774b23d85e2f75e5386fdaf14289380562c
Harlan Sun <1249413181@qq.com>
2023-12-01 17:32:26 +0800

    feat: add communities

--- src/app/(settings)/settings/communities/page.tsx
+++ src/app/(settings)/settings/communities/page.tsx
@@ -4,13 +4,51 @@ import { t, Trans } from '@lingui/macro';
 
 import { AccountCard } from './AccountCard/index.js';
 
-const communities = [
+const fireflyCommunities = [
     {
         content: t`Follow @thefireflyapp on X`,
-        link: 'https://twitter.com/thefireflyapp',
+        link: 'https://twitter.com/intent/user?screen_name=thefireflyapp',
         logo: '/svg/x-light.svg',
     },
+    {
+        content: t`Visit firefly.land`,
+        link: 'https://firefly.land',
+        logo: '/svg/fireflyRound.svg'
+    },
+    {
+        content: t`Join our Discord`,
+        link: 'https://discord.com/invite/pufMbBGQZN',
+        logo: '/svg/discord.svg'
+    },
+    {
+        content: t`Join our Telegram`,
+        link: 'https://t.me/+mz9T_4YOYhoyYmYx',
+        logo: '/svg/telegram.svg'
+    }
 ];
+
+const maskCommunities = [
+    {
+        content: t`Follow @realMaskNetwork on X`,
+        link: 'https://twitter.com/intent/user?screen_name=realMaskNetwork',
+        logo: '/svg/x-light.svg',
+    },
+    {
+        content: t`Visit mask.io`,
+        link: 'https://mask.io',
+        logo: '/svg/maskRound.svg'
+    },
+    {
+        content: t`Join our Discord`,
+        link: 'https://discord.com/invite/4SVXvj7',
+        logo: '/svg/discord.svg'
+    },
+    {
+        content: t`Join our Telegram`,
+        link: 'https://t.me/maskbook_group#telegram',
+        logo: '/svg/telegram.svg'
+    }
+]
 
 export default function Connected() {
     return (
@@ -25,7 +63,15 @@ export default function Connected() {
                     <Trans>Firefly</Trans>
                 </span>
             </div>
-            {communities.map(({ content, link, logo }) => (
+            {fireflyCommunities.map(({ content, link, logo }) => (
+                <AccountCard key={link} content={content} link={link} logo={logo} />
+            ))}
+            <div className="flex w-full items-center justify-between">
+                <span className="text-base font-bold leading-[18px] text-main">
+                    <Trans>Mask Network</Trans>
+                </span>
+            </div>
+            {maskCommunities.map(({ content, link, logo }) => (
                 <AccountCard key={link} content={content} link={link} logo={logo} />
             ))}
         </div>
