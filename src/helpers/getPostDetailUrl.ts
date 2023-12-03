9caac9f278a57f62092a5e3290c30e5b89069cfa
UncleBill <billbill290@gmail.com>
2023-12-03 22:02:55 +0800

    fix: don't wrap the whole post card with link but the content only (#126)
    * fix: don't wrap the whole post card with link but the content only
    
    * fix: link
    
    ---------
    
    Co-authored-by: guanbinrui <guanbinrui@dimension.im>

--- src/helpers/getPostDetailUrl.ts
+++ src/helpers/getPostDetailUrl.ts
@@ -3,6 +3,5 @@ import urlcat from 'urlcat';
 import type { SocialPlatform } from '@/constants/enum.js';
 
 export function getPostDetailUrl(id: string, source: SocialPlatform) {
-    if (typeof window === 'undefined') return '';
-    return urlcat(`${location.origin}/detail/:platform/:id`, { platform: source.toLowerCase(), id });
+    return urlcat(`/detail/:platform/:id`, { platform: source.toLowerCase(), id });
 }
