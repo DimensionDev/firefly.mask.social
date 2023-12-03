bd496b3a509b785291f2328a63e91fc8818f9b1b
guanbinrui <guanbinrui@dimension.im>
2023-12-03 22:11:47 +0800

    refactor: createPageTitle

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,10 +1,11 @@
 import type React from 'react';
 
 import { SITE_NAME } from '@/constants/index.js';
+import { createPageTitle } from '@/helpers/createPageTitle.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 
 export const metadata = createSiteMetadata({
-    title: `Profile • ${SITE_NAME}`,
+    title: createPageTitle('Profile'),
 });
 
 export default function DetailLayout({ children }: { children: React.ReactNode }) {
