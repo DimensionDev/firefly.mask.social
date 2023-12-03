73504134d0f21ad50f0e98f7a788681471b85c21
guanbinrui <guanbinrui@dimension.im>
2023-12-03 22:14:19 +0800

    fix: lint errors

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,6 +1,5 @@
 import type React from 'react';
 
-import { SITE_NAME } from '@/constants/index.js';
 import { createPageTitle } from '@/helpers/createPageTitle.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 
