823fa44a55345b24e074b4b24c08506c7d97a087
guanbinrui <guanbinrui@dimension.im>
2023-12-04 14:37:50 +0800

    chore: regexp

--- src/helpers/getPostPayload.ts
+++ src/helpers/getPostPayload.ts
@@ -1,6 +1,7 @@
 export function getPostPayload(raw: string | undefined): [string, '1' | '2'] | undefined {
-    const matched = raw?.match(/(?:.*)PostData_([v1|v2])=(.*)/);
+    const matched = raw?.match(/(?:.*)PostData_(v1|v2)=(.*)/);
     if (!matched) return;
+
     const [, version, payload] = matched;
 
     if (version === 'v1') return [payload, '1'];
