b26e5aaf1e7d4db82091b7a735fb02cbf6a9bc1d
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2023-12-04 14:13:56 +0800

    refactor: naming icons (#136)
    * refactor: naming icons
    
    * refactor: naming icons
    
    * refactor: renaming

--- src/components/compose/ComposeAction.tsx
+++ src/components/compose/ComposeAction.tsx
@@ -7,7 +7,7 @@ import { type ChangeEvent, type Dispatch, type SetStateAction, useCallback, useR
 
 import AtIcon from '@/assets/at.svg';
 import GalleryIcon from '@/assets/gallery.svg';
-import NumberSignIcon from '@/assets/numberSign.svg';
+import NumberSignIcon from '@/assets/number-sign.svg';
 import PostBy from '@/components/compose/PostBy.js';
 import ReplyRestriction from '@/components/compose/ReplyRestriction.js';
 import uploadToIPFS, { type IPFSResponse } from '@/services/uploadToIPFS.js';
