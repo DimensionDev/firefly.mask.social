937505806fa55c01102f2b6a0840b3c7c190f560
guanbinrui <guanbinrui@dimension.im>
2023-12-04 10:21:11 +0800

    chore: i18n

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,10 +1,11 @@
+import { t } from '@lingui/macro';
 import type React from 'react';
 
 import { createPageTitle } from '@/helpers/createPageTitle.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 
 export const metadata = createSiteMetadata({
-    title: createPageTitle('Profile'),
+    title: createPageTitle(t`Profile`),
 });
 
 export default function DetailLayout({ children }: { children: React.ReactNode }) {
--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,10 +1,11 @@
+import { t } from '@lingui/macro';
 import type React from 'react';
 
 import { createPageTitle } from '@/helpers/createPageTitle.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 
 export const metadata = createSiteMetadata({
-    title: createPageTitle('Search'),
+    title: createPageTitle(t`Search`),
 });
 
 export default function DetailLayout({ children }: { children: React.ReactNode }) {
