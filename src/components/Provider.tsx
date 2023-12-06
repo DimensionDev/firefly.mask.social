6a549740f6de866ebcdfd69df9ebfdfe0ebb0c31
guanbinrui <guanbinrui@dimension.im>
2023-12-06 18:23:36 +0800

    chore: rollup providers

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -58,27 +58,25 @@ export function Providers(props: { children: React.ReactNode }) {
 
     return (
         <I18nProvider i18n={i18n}>
-            <DarkModeContext.Provider value={darkModeContext}>
-                <SnackbarProvider
-                    maxSnack={30}
-                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-                    autoHideDuration={3000}
-                >
-                    {/* wagmi depends @tanstack/react-query@4.29.23 */}
-                    <WagmiProvider>
-                        {/* livepeer depends @tanstack/react-query@4.36.1 */}
-                        <LivepeerConfig client={livepeerClient}>
-                            {/* We are using @tanstack/react-query@5.8.7 */}
-                            <QueryClientProvider client={queryClient}>
-                                <ReactQueryStreamedHydration>{props.children}</ReactQueryStreamedHydration>
-                                {process.env.NODE_ENV === 'development' ? (
-                                    <ReactQueryDevtools initialIsOpen={false} />
-                                ) : null}
-                            </QueryClientProvider>
-                        </LivepeerConfig>
-                    </WagmiProvider>
-                </SnackbarProvider>
-            </DarkModeContext.Provider>
+            {/* We are using @tanstack/react-query@5.8.7 */}
+            <QueryClientProvider client={queryClient}>
+                <ReactQueryStreamedHydration>
+                    <DarkModeContext.Provider value={darkModeContext}>
+                        <SnackbarProvider
+                            maxSnack={30}
+                            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                            autoHideDuration={3000}
+                        >
+                            {/* wagmi depends @tanstack/react-query@4.29.23 */}
+                            <WagmiProvider>
+                                {/* livepeer depends @tanstack/react-query@4.36.1 */}
+                                <LivepeerConfig client={livepeerClient}>{props.children}</LivepeerConfig>
+                            </WagmiProvider>
+                        </SnackbarProvider>
+                    </DarkModeContext.Provider>
+                </ReactQueryStreamedHydration>
+                {process.env.NODE_ENV === 'development' ? <ReactQueryDevtools initialIsOpen={false} /> : null}
+            </QueryClientProvider>
         </I18nProvider>
     );
 }
