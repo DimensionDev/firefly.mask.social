8e5240c61d8f6e8d7f43a6996076e745bf5e57d0
guanbinrui <guanbinrui@dimension.im>
2023-12-06 17:30:21 +0800

    chore: add comments for setup-packages.sh

--- scripts/setup-packages.sh
+++ scripts/setup-packages.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#
+# Description: This script accepts three parameters - source node_modules pathname,
+# destination node_modules pathname, and folder name prefix. It finds all folders in
+# the source/.pnpm/ directory whose filename starts with the specified prefix, deletes
+# those folders, and creates symbolic links to corresponding folders in the
+# dest/.pnpm/ directory.
+#
+# Usage: ./script.sh <source_node_modules_pathname> <dest_node_modules_pathname> <folder_name_prefix>
+#
+# Example: ./script.sh /path/to/source_node_modules /path/to/dest_node_modules '@tanstack+'
 
 # Check if correct number of arguments are provided
 if [ "$#" -ne 3 ]; then
@@ -6,9 +16,9 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-source_path="$1"
-dest_path="$2"
-name_prefix="$3"
+source_path="$1"      # Source node_modules pathname
+dest_path="$2"        # Destination node_modules pathname
+name_prefix="$3"      # Folder name prefix
 
 # Check if source directory exists
 if [ ! -d "$source_path" ]; then
@@ -26,13 +36,13 @@ fi
 find "$source_path/.pnpm" -type d -name "$name_prefix"* | while read -r folder; do
     # Extract the folder name without path
     folder_name=$(basename "$folder")
-
+    
     # Delete the folder in the source directory
     rm -rf "$folder"
-
+    
     # Create a symbolic link to the destination directory
     ln -s "$dest_path/.pnpm/$folder_name" "$folder"
-
+    
     echo "Soft link created for $folder_name"
 done
 
