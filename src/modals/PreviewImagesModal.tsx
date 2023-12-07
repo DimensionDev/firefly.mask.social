e7b06f4cc933be87397e852665b17970650bb7fb
UncleBill <billbill290@gmail.com>
2023-12-07 12:19:16 +0800

    fix: setting ui issues (#150)
    * fix: mf-5721 policy links
    
    * fix: mf-5719 firefly round icon
    
    * fix: countdown circle svg props
    
    * Fix: mf-5733 image size

--- src/modals/PreviewImagesModal.tsx
+++ src/modals/PreviewImagesModal.tsx
@@ -81,8 +81,7 @@ export const PreviewImagesModal = forwardRef<SingletonModalRefCreator<PreviewIma
                                     <Image
                                         src={state ?? current}
                                         alt={state ?? current}
-                                        width={1000}
-                                        height={1000}
+                                        width="auto"
                                         className="max-h-[calc(100vh-110px)] max-w-full"
                                     />
 
