cc9bfc48aec18579ceac64c04727a191777e5c04
Harlan Sun <1249413181@qq.com>
2023-12-09 15:33:05 +0800

    fix: revert getWalletClientRequired

--- src/helpers/getWalletClientRequired.ts
+++ src/helpers/getWalletClientRequired.ts
@@ -1,6 +1,8 @@
 import { t } from '@lingui/macro';
 import { getWalletClient, type GetWalletClientArgs, type GetWalletClientResult } from 'wagmi/actions';
 
+import { chains } from '@/configs/wagmiClient.js';
+import { ChainModalRef, ConnectWalletModalRef } from '@/modals/controls.js';
 import { ConnectWalletModalRef } from '@/modals/controls.js';
 
 export async function getWalletClientRequired(
@@ -11,6 +13,16 @@ export async function getWalletClientRequired(
 
     const secondTryResult = firstTryResult ?? (await getWalletClient(args));
     if (!secondTryResult) throw new Error(t`No wallet client found`);
+
+    if (args?.chainId && args.chainId !== secondTryResult.chain.id) {
+        await ChainModalRef.openAndWaitForClose();
+        if (args?.chainId !== secondTryResult.chain.id)
+            throw new Error(
+                t`Please switch to the ${
+                    chains.find((x) => x.id === args?.chainId)?.name ?? 'correct'
+                } network in your wallet (${secondTryResult.chain.name})}`,
+            );
+    }
 
     return secondTryResult;
 }
