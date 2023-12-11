a93ec4bedc67f9b262e69a219316bde5534fb128
unclebill <billbill290@gmail.com>
2023-12-11 15:48:35 +0800

    fix: adjust search bar behavior

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -32,10 +32,10 @@ const SearchBar = memo(function SearchBar(props: SearchBarProps) {
     const params = useSearchParams();
     const { isDarkMode } = useDarkMode();
     const { currentSocialPlatform } = useGlobalState();
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [showDropdown, setShowDropdown] = useState(false);
 
     const isSearchPage = pathname === '/search';
-
-    const dropdownRef = useRef(null);
     const { updateSearchText } = useSearchStore();
     const queryKeyword = params.get('q') || '';
     const [searchText, setSearchText] = useState(queryKeyword);
@@ -46,7 +46,10 @@ const SearchBar = memo(function SearchBar(props: SearchBarProps) {
         [debouncedSearchText, histories],
     );
 
-    useOnClickOutside(dropdownRef, () => setSearchText(''));
+    const rootRef = useRef(null);
+    useOnClickOutside(rootRef, () => {
+        setShowDropdown(false);
+    });
 
     const { data: profiles, isLoading } = useQuery({
         queryKey: ['searchText', debouncedSearchText, currentSocialPlatform],
@@ -74,18 +77,19 @@ const SearchBar = memo(function SearchBar(props: SearchBarProps) {
         setSearchText(searchText);
     };
 
-    const handleSubmit = (evt: ChangeEvent<HTMLFormElement>) => {
-        evt.preventDefault();
-        addRecord(searchText);
+    const selectKeyword = (keyword: string) => {
+        addRecord(keyword);
+        setSearchText(keyword);
 
         if (isSearchPage) {
-            router.push(`/search?q=${searchText}&type=${params.get('type') ?? SearchType.Profiles}`);
+            router.push(`/search?q=${keyword}&type=${params.get('type') ?? SearchType.Profiles}`);
         } else {
-            router.push(`/search?q=${searchText}&type=${SearchType.Profiles}`);
+            router.push(`/search?q=${keyword}&type=${SearchType.Profiles}`);
         }
+        setShowDropdown(false);
 
         // submit the final search text
-        updateSearchText(searchText);
+        updateSearchText(keyword);
     };
 
     if (props.source === 'header' && !isSearchPage) return null;
@@ -94,17 +98,22 @@ const SearchBar = memo(function SearchBar(props: SearchBarProps) {
     return (
         <div
             className={classNames('flex items-center px-4 pt-6', {
-                'pl-0': props.source === 'secondary',
-                'pr-0': props.source === 'secondary',
-                'pb-5': props.source === 'secondary',
+                'pb-5 pl-0 pr-0': props.source === 'secondary',
             })}
+            ref={rootRef}
         >
             {isSearchPage && props.source === 'header' ? (
                 <LeftArrowIcon width={24} height={24} className="mr-7 cursor-pointer" onClick={() => router.back()} />
             ) : null}
             <div className="relative flex flex-grow items-center rounded-xl bg-lightBg px-3 text-main">
                 <SearchIcon width={18} height={18} />
-                <form className="w-full flex-1" onSubmit={handleSubmit}>
+                <form
+                    className="w-full flex-1"
+                    onSubmit={(evt) => {
+                        evt.preventDefault();
+                        selectKeyword(searchText);
+                    }}
+                >
                     <label className="flex w-full items-center" htmlFor="search">
                         <input
                             type="search"
@@ -113,7 +122,9 @@ const SearchBar = memo(function SearchBar(props: SearchBarProps) {
                             value={searchText}
                             className=" w-full border-0 bg-transparent py-2 text-[10px] placeholder-secondary focus:border-0 focus:outline-0 focus:ring-0 sm:text-sm sm:leading-6"
                             placeholder={t`Search…`}
+                            ref={inputRef}
                             onChange={handleInputChange}
+                            onFocus={() => setShowDropdown(true)}
                         />
                         <CloseIcon
                             className={classNames('cursor-pointer', searchText ? 'visible' : 'invisible')}
@@ -123,121 +134,124 @@ const SearchBar = memo(function SearchBar(props: SearchBarProps) {
                                 evt.preventDefault();
                                 evt.stopPropagation();
                                 setSearchText('');
+                                inputRef.current?.focus();
                             }}
                         />
                     </label>
                 </form>
-                {searchText.length > 0 ? (
-                    <div
-                        className="absolute inset-x-0 top-[40px] z-[1000] mt-2 flex w-full flex-col bg-white dark:bg-black"
-                        ref={dropdownRef}
-                    >
-                        <div className="rounded-2xl shadow-[0px_4px_30px_0px_rgba(0,0,0,0.10)]">
-                            {matchedHistories.length ? (
-                                <>
-                                    <h2 className=" flex p-3 pb-2 text-xs">
-                                        <Trans>Recent</Trans>
-                                        <button className="ml-auto font-bold text-[#246BFD]" onClick={clearAll}>
-                                            Clear All
-                                        </button>
-                                    </h2>
-                                    <ul className="mx-4 my-4">
-                                        {histories.map((history) => (
-                                            <li key={history} className="flex cursor-pointer items-center">
-                                                <SearchIcon width={18} height={18} />
-                                                <span className="color-main ml-4 text-ellipsis py-2">{history}</span>
-                                                <button onClick={() => removeRecord(history)} className="ml-auto">
-                                                    <CloseIcon width={16} height={16} />
-                                                </button>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </>
-                            ) : null}
-
-                            <div
-                                className=" flex cursor-pointer items-center px-4 py-4 text-left hover:bg-bg"
-                                // @ts-ignore
-                                onClick={handleSubmit}
-                            >
-                                <SearchIcon width={18} height={18} />
-                                <span className=" ml-4">{searchText}</span>
-                            </div>
-                            <h2 className=" p-3 pb-2 text-xs">
-                                {currentSocialPlatform === SocialPlatform.Lens ? (
-                                    <Trans>Publications</Trans>
-                                ) : currentSocialPlatform === SocialPlatform.Farcaster ? (
-                                    <Trans>Casts</Trans>
-                                ) : (
-                                    <Trans>Posts</Trans>
-                                )}
-                            </h2>
-
-                            <div
-                                className=" flex cursor-pointer items-center px-4 py-4 text-left hover:bg-bg"
-                                // @ts-ignore
-                                onClick={handleSubmit}
-                            >
-                                <SearchIcon width={18} height={18} />
-                                <span className=" ml-4">{searchText}</span>
-                            </div>
-
-                            {isLoading || profiles?.data ? (
-                                <>
-                                    <hr className=" border-b border-t-0 border-line" />
-                                    <h2 className=" p-3 pb-2 text-xs">
-                                        <Trans>Profiles</Trans>
-                                    </h2>
-                                </>
-                            ) : null}
-
-                            {isLoading ? (
-                                <div className="flex flex-col items-center space-y-2 px-4 pb-5 pt-2 text-center text-sm font-bold">
-                                    <LoadingIcon className="animate-spin" width={24} height={24} />
-                                    <div className="text-bold">{t`Searching users`}</div>
-                                </div>
-                            ) : profiles?.data.length === 0 ? (
-                                <div className="space-y-2 px-4 py-4 text-center text-sm font-bold">
-                                    <div className="text-bold">{t`No matching users`}</div>
-                                </div>
-                            ) : profiles?.data.length ? (
-                                <div className="cursor-pointer py-2">
-                                    {profiles.data.slice(0, 10).map((user) => (
-                                        <div
-                                            key={user.handle}
-                                            className="space-y-2 px-4 py-2 text-center text-sm font-bold hover:bg-bg"
-                                            onClick={(evt) => {
-                                                router.push(`/profile/${user.handle}`);
-                                                setSearchText('');
+                {showDropdown ? (
+                    <div className="absolute inset-x-0 top-[40px] z-[1000] mt-2 flex w-full flex-col overflow-hidden rounded-2xl bg-white shadow-[0_4px_30px_0_rgba(0,0,0,0.10)] dark:bg-black">
+                        {matchedHistories.length && !searchText ? (
+                            <>
+                                <h2 className=" flex p-3 pb-2 text-xs">
+                                    <Trans>Recent</Trans>
+                                    <button className="ml-auto font-bold text-[#246BFD]" onClick={clearAll}>
+                                        Clear All
+                                    </button>
+                                </h2>
+                                <ul className="my-4">
+                                    {histories.map((history) => (
+                                        <li
+                                            key={history}
+                                            className="flex cursor-pointer items-center px-4 hover:bg-bg"
+                                            onClick={() => {
+                                                selectKeyword(history);
                                             }}
                                         >
-                                            <div className="flex flex-row items-center">
-                                                <Image
-                                                    className="mr-[10px] h-10 w-10 rounded-full"
-                                                    loading="lazy"
-                                                    src={user.pfp}
-                                                    fallback={
-                                                        isDarkMode
-                                                            ? '/image/firefly-dark-avatar.png'
-                                                            : '/image/firefly-light-avatar.png'
-                                                    }
-                                                    width={40}
-                                                    height={40}
-                                                    alt={user.displayName}
-                                                />
-                                                <div className="flex-1 text-left">
-                                                    <div className="flex">
-                                                        <span className="mr-1">{user.displayName}</span>
-                                                        <SourceIcon source={user.source} />
-                                                    </div>
-                                                    <div className=" font-normal text-secondary">@{user.handle}</div>
+                                            <SearchIcon width={18} height={18} />
+                                            <span className="color-main ml-4 text-ellipsis py-2">{history}</span>
+                                            <button
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    removeRecord(history);
+                                                }}
+                                                className="ml-auto"
+                                            >
+                                                <CloseIcon width={16} height={16} />
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </>
+                        ) : null}
+
+                        {searchText ? (
+                            <>
+                                <h2 className=" p-3 pb-2 text-xs">
+                                    {currentSocialPlatform === SocialPlatform.Lens ? (
+                                        <Trans>Publications</Trans>
+                                    ) : currentSocialPlatform === SocialPlatform.Farcaster ? (
+                                        <Trans>Casts</Trans>
+                                    ) : (
+                                        <Trans>Posts</Trans>
+                                    )}
+                                </h2>
+
+                                <div
+                                    className=" flex cursor-pointer items-center px-4 py-4 text-left hover:bg-bg"
+                                    onClick={() => selectKeyword(searchText)}
+                                >
+                                    <SearchIcon width={18} height={18} />
+                                    <span className=" ml-4">{searchText}</span>
+                                </div>
+                            </>
+                        ) : null}
+
+                        {isLoading || profiles?.data ? (
+                            <>
+                                <hr className=" border-b border-t-0 border-line" />
+                                <h2 className=" p-3 pb-2 text-xs">
+                                    <Trans>Profiles</Trans>
+                                </h2>
+                            </>
+                        ) : null}
+
+                        {isLoading ? (
+                            <div className="flex flex-col items-center space-y-2 px-4 pb-5 pt-2 text-center text-sm font-bold">
+                                <LoadingIcon className="animate-spin" width={24} height={24} />
+                                <div className="text-bold">{t`Searching users`}</div>
+                            </div>
+                        ) : profiles?.data.length === 0 ? (
+                            <div className="space-y-2 px-4 py-4 text-center text-sm font-bold">
+                                <div className="text-bold">{t`No matching users`}</div>
+                            </div>
+                        ) : profiles?.data.length ? (
+                            <div className="cursor-pointer py-2">
+                                {profiles.data.slice(0, 10).map((user) => (
+                                    <div
+                                        key={user.handle}
+                                        className="space-y-2 px-4 py-2 text-center text-sm font-bold hover:bg-bg"
+                                        onClick={(evt) => {
+                                            router.push(`/profile/${user.handle}`);
+                                            setSearchText('');
+                                        }}
+                                    >
+                                        <div className="flex flex-row items-center">
+                                            <Image
+                                                className="mr-[10px] h-10 w-10 rounded-full"
+                                                loading="lazy"
+                                                src={user.pfp}
+                                                fallback={
+                                                    isDarkMode
+                                                        ? '/image/firefly-dark-avatar.png'
+                                                        : '/image/firefly-light-avatar.png'
+                                                }
+                                                width={40}
+                                                height={40}
+                                                alt={user.displayName}
+                                            />
+                                            <div className="flex-1 text-left">
+                                                <div className="flex">
+                                                    <span className="mr-1">{user.displayName}</span>
+                                                    <SourceIcon source={user.source} />
                                                 </div>
+                                                <div className=" font-normal text-secondary">@{user.handle}</div>
                                             </div>
                                         </div>
-                                    ))}
-                                </div>
-                            ) : null}
-                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : null}
                     </div>
                 ) : null}
             </div>
