4e4976314fe6dbae05b2ea0f1f6795c9fffe1151
beyond009 <beyond009@users.noreply.github.com>
2023-12-11 05:31:28 +0000

    chore: prettier

--- src/components/Login/LoginLens.tsx
+++ src/components/Login/LoginLens.tsx
@@ -20,7 +20,7 @@ import { LensSocialMediaProvider } from '@/providers/lens/SocialMedia.js';
 import type { Profile } from '@/providers/types/SocialMedia.js';
 import { useLensStateStore } from '@/store/useLensStore.js';
 
-interface LoginLensProps { }
+interface LoginLensProps {}
 
 export function LoginLens(props: LoginLensProps) {
     const [selected, setSelected] = useState<Profile>();
@@ -104,13 +104,15 @@ export function LoginLens(props: LoginLensProps) {
                                     <Switch checked={signless} onChange={setSignless}>
                                         {({ checked }) => (
                                             <button
-                                                className={`${checked ? 'bg-success' : 'bg-gray-200'
-                                                    } relative inline-flex h-[22px] w-[43px] items-center rounded-full`}
+                                                className={`${
+                                                    checked ? 'bg-success' : 'bg-gray-200'
+                                                } relative inline-flex h-[22px] w-[43px] items-center rounded-full`}
                                             >
                                                 <span className="sr-only">Enable signless</span>
                                                 <span
-                                                    className={`${checked ? 'translate-x-6' : 'translate-x-1'
-                                                        } inline-block h-3 w-3 transform rounded-full bg-white transition`}
+                                                    className={`${
+                                                        checked ? 'translate-x-6' : 'translate-x-1'
+                                                    } inline-block h-3 w-3 transform rounded-full bg-white transition`}
                                                 />
                                             </button>
                                         )}
