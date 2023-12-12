97bd985b32441f7c3fc520d46191a6d0ac1f3b1a
unclebill <billbill290@gmail.com>
2023-12-12 10:57:57 +0800

    fix: farcaster handle

--- src/components/Search/ProfileInList.tsx
+++ src/components/Search/ProfileInList.tsx
@@ -1,6 +1,7 @@
 import { Image } from '@/components/Image.js';
 import FollowButton from '@/components/Profile/FollowButton.js';
 import { SourceIcon } from '@/components/SourceIcon.js';
+import { SocialPlatform } from '@/constants/enum.js';
 import { Link } from '@/esm/Link.js';
 import { useDarkMode } from '@/hooks/useDarkMode.js';
 import type { Profile } from '@/providers/types/SocialMedia.js';
@@ -14,10 +15,12 @@ export function ProfileInList(props: ProfileInListProps) {
     const { profile } = props;
 
     const { isDarkMode } = useDarkMode();
+    const isLens = profile.source === SocialPlatform.Lens;
+    const handle = isLens ? profile.handle : profile.nickname;
 
     return (
-        <div className="flex-start flex cursor-pointer px-4 py-6 hover:bg-bg">
-            <Link className="flex-start flex flex-1" href={`/profile/${profile.handle}`}>
+        <div className="flex-start flex cursor-pointer border-b border-secondaryLine px-4 py-6 hover:bg-bg dark:border-line">
+            <Link className="flex-start flex flex-1" href={`/profile/${handle}`}>
                 <Image
                     loading="lazy"
                     className="mr-3 h-[78px] w-[78px] rounded-full border"
@@ -33,7 +36,7 @@ export function ProfileInList(props: ProfileInListProps) {
                         <span className="mr-2 text-xl">{profile.displayName}</span>
                         <SourceIcon source={profile.source} />
                     </span>
-                    {profile.handle ? <span className="text-sm text-secondary">@{profile.handle}</span> : null}
+                    {handle ? <span className="text-sm text-secondary">@{handle}</span> : null}
                     {profile.bio ? <span className="mt-1.5 line-clamp-1 text-sm">{profile.bio}</span> : null}
                 </div>
             </Link>
