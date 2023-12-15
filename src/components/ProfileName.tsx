348d5f54092fd197f23fcf55f9a7bc843f382ddd
guanbinrui <guanbinrui@dimension.im>
2023-12-15 18:55:21 +0800

    refactor: remove unused code

--- src/components/ProfileName.tsx
+++ src/components/ProfileName.tsx
@@ -8,7 +8,7 @@ export function ProfileName(props: ProfileNameProps) {
     const { profile } = props;
     return (
         <div className="inline-flex shrink grow basis-0 flex-col items-start justify-center">
-            <div className=" flex items-center text-base font-medium text-main">{profile.displayName}</div>
+            <div className=" text-base font-medium text-main">{profile.displayName}</div>
             <div className=" text-[15px] font-normal text-neutral-500">@{profile.handle}</div>
         </div>
     );
