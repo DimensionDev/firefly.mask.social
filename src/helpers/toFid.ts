609c3cf813173cdf2923528d28fbacb3de551421
unclebill <billbill290@gmail.com>
2023-12-19 17:36:42 +0800

    fix: fid should be even length

--- src/helpers/toFid.ts
+++ src/helpers/toFid.ts
@@ -2,5 +2,6 @@ import { toInteger } from 'lodash-es';
 import { numberToHex } from 'viem';
 
 export function toFid(profileId: string) {
-    return numberToHex(toInteger(profileId));
+    // fid should be even length
+    return numberToHex(toInteger(profileId), { size: 32 });
 }
