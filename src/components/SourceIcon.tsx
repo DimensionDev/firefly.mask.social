fc84300ade1d351848061865d467b70beead0295
unclebill <billbill290@gmail.com>
2023-12-19 18:11:51 +0800

    fix: dynamic class might not be generated at build time

--- src/components/SourceIcon.tsx
+++ src/components/SourceIcon.tsx
@@ -3,33 +3,23 @@ import { safeUnreachable } from '@masknet/kit';
 import FarcasterIcon from '@/assets/farcaster.svg';
 import LensIcon from '@/assets/lens.svg';
 import { SocialPlatform } from '@/constants/enum.js';
-import { classNames } from '@/helpers/classNames.js';
 
 interface SourceIconProps extends React.SVGProps<SVGSVGElement> {
     size?: number;
     source: SocialPlatform;
 }
 
-export function SourceIcon({ source, size = 20, className = '', ...props }: SourceIconProps) {
+export function SourceIcon({ source, size = 20, ...props }: SourceIconProps) {
+    const style = {
+        height: size,
+        width: size,
+        ...props.style,
+    };
     switch (source) {
         case SocialPlatform.Lens:
-            return (
-                <LensIcon
-                    {...props}
-                    className={classNames(` h-[${size}px] w-[${size}px]`, className)}
-                    width={size}
-                    height={size}
-                />
-            );
+            return <LensIcon {...props} style={style} width={size} height={size} />;
         case SocialPlatform.Farcaster:
-            return (
-                <FarcasterIcon
-                    {...props}
-                    className={classNames(` h-[${size}px] w-[${size}px]`, className)}
-                    width={size}
-                    height={size}
-                />
-            );
+            return <FarcasterIcon {...props} style={style} width={size} height={size} />;
         default:
             safeUnreachable(source);
             return null;
