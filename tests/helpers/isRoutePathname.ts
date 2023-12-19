f34f3298c11171a878fc9c9e0177e9ba0b2ec316
guanbinrui <guanbinrui@dimension.im>
2023-12-20 02:00:26 +0800

    refactor: test cases

--- tests/helpers/isRoutePathname.ts
+++ tests/helpers/isRoutePathname.ts
@@ -4,28 +4,20 @@ import { isRoutePathname } from '@/helpers/isRoutePathname.js';
 
 describe('isRoutePathname', () => {
     test('should correctly check pathname matching based on criteria', () => {
-        // Identical pathnames
-        const result1 = isRoutePathname('/users/123', '/users/123/');
-        expect(result1).toBe(true);
+        const cases = [
+            ['/users/123', '/users/123', true],
+            ['/users/123', '/users/123/', true],
+            ['/posts/456', '/users/456/', false],
+            ['/articles/789', '/articles/', true],
+            ['/categories/abc/123', '/categories', true],
+            ['/categories/abc/123', '/categories/', true],
+            ['/settings//', '/settings', false],
+            ['/settings', '/settings//', false],
+        ] as Array<[`/${string}`, `/${string}`, boolean]>;
 
-        // Literal matching
-        const result2 = isRoutePathname('/users/123', '/users/123');
-        expect(result2).toBe(true);
-
-        // Different pathnames
-        const result3 = isRoutePathname('/posts/456', '/users/456/');
-        expect(result3).toBe(false);
-
-        // Partial matching
-        const result4 = isRoutePathname('/articles/789', '/articles/');
-        expect(result4).toBe(true);
-
-        // Unequal length
-        const result5 = isRoutePathname('/categories/abc/123', '/categories/');
-        expect(result5).toBe(true);
-
-        // Unequal length without ending /
-        const result6 = isRoutePathname('/categories/abc/123', '/categories');
-        expect(result6).toBe(true);
+        cases.forEach(([pathname1, pathname2, expectedResult]) => {
+            const result = isRoutePathname(pathname1, pathname2);
+            expect(result).toBe(expectedResult);
+        });
     });
 });
