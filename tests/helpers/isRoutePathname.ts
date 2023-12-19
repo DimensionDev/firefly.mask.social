8328c75ecb9624b5bf8cbce3a7740e6d4b49a9e1
guanbinrui <guanbinrui@dimension.im>
2023-12-20 01:55:13 +0800

    fix: test case

--- tests/helpers/isRoutePathname.ts
+++ tests/helpers/isRoutePathname.ts
@@ -4,24 +4,28 @@ import { isRoutePathname } from '@/helpers/isRoutePathname.js';
 
 describe('isRoutePathname', () => {
     test('should correctly check pathname matching based on criteria', () => {
-        // Test case 1: Identical pathnames
+        // Identical pathnames
         const result1 = isRoutePathname('/users/123', '/users/123/');
         expect(result1).toBe(true);
 
-        // Test case 2: Literal matching
+        // Literal matching
         const result2 = isRoutePathname('/users/123', '/users/123');
         expect(result2).toBe(true);
 
-        // Test case 3: Different pathnames
+        // Different pathnames
         const result3 = isRoutePathname('/posts/456', '/users/456/');
         expect(result3).toBe(false);
 
-        // Test case 4: Partial matching
+        // Partial matching
         const result4 = isRoutePathname('/articles/789', '/articles/');
         expect(result4).toBe(true);
 
-        // Test case 5: Unequal length
+        // Unequal length
         const result5 = isRoutePathname('/categories/abc/123', '/categories/');
-        expect(result5).toBe(false);
+        expect(result5).toBe(true);
+
+        // Unequal length without ending /
+        const result6 = isRoutePathname('/categories/abc/123', '/categories');
+        expect(result6).toBe(true);
     });
 });
