2be2333fdba78753eacfac9fd54109d9f957367a
guanbinrui <guanbinrui@dimension.im>
2023-12-20 22:13:09 +0800

    chore: build the first connection

--- src/mask/custom-elements.ts
+++ src/mask/custom-elements.ts
@@ -14,6 +14,9 @@ import '@/mask/custom-elements/DecryptedPost.js';
 
 import { setupBuildInfoManually } from '@masknet/flags/build-info';
 import { setPluginDebuggerMessages } from '@/mask/message-host/index.js';
+import { EVMWeb3 } from '@masknet/web3-providers';
+import { ChainId, ProviderType } from '@masknet/web3-shared-evm';
+import { getAccount, getNetwork } from 'wagmi/actions';
 
 setupBuildInfoManually({
     channel:
@@ -29,4 +32,16 @@ if (process.env.NODE_ENV === 'development') {
     await import('@masknet/plugin-debugger/messages').then((module) =>
         setPluginDebuggerMessages(module.PluginDebuggerMessages),
     );
+}
+
+// use custom event provider if a wallet connected to the wagmi client
+// if no wallet connected, the rest connection will be monitored in custom-event-provider.ts
+const account = getAccount();
+
+if (account.isConnected) {
+    const network = getNetwork();
+    EVMWeb3.connect({
+        chainId: network.chain?.id ?? ChainId.Mainnet,
+        providerType: ProviderType.CustomEvent,
+    });
 }
