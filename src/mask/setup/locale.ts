38df8b7ec76c69558c0d54bb5bf774d5e2cdf6ec
unclebill <billbill290@gmail.com>
2023-12-20 17:08:52 +0800

    fix: mf-5832 mf-5858 add share-ui locales

--- src/mask/setup/locale.ts
+++ src/mask/setup/locale.ts
@@ -3,6 +3,9 @@ import { i18NextInstance } from '@masknet/shared-base';
 import { addShareBaseI18N } from '@masknet/shared-base-ui';
 import { initReactI18next } from 'react-i18next';
 
+import { addMaskI18N } from '@/maskbook/packages/mask/shared-ui/locales/languages.js';
+
+addMaskI18N(i18NextInstance);
 initReactI18next.init(i18NextInstance);
 addSharedI18N(i18NextInstance);
 addShareBaseI18N(i18NextInstance);
