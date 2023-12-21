30797f42601259d3a38a2e4fa53c41e5c4ca0338
guanbinrui <guanbinrui@mask.io>
2023-12-21 23:59:27 +0800

    fix: not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,21 @@
-'use client';
-
-import { Trans } from '@lingui/macro';
+import { t } from '@lingui/macro';
 
 import { IfPathname } from '@/components/IfPathname.js';
 import { AsideSearchBar } from '@/components/Search/SearchBar.js';
 import { SearchFilter } from '@/components/Search/SearchFilter.js';
 import { Image } from '@/esm/Image.js';
+import { getLocaleFromCookies } from '@/helpers/getLocaleFromCookies.js';
+import { setLocale } from '@/i18n/index.js';
 
 export default function NotFound() {
+    setLocale(getLocaleFromCookies());
+
     return (
         <>
             <main className="max-w-[888px] flex-[1_1_100%] border-r border-line pl-[289px]">
                 <div className="flex h-screen flex-col items-center justify-center">
-                    <Image
-                        src="/image/radar.png"
-                        width={200}
-                        height={106}
-                        alt="Something went wrong, Please try again."
-                    />
-                    <div className="mt-11 text-sm font-bold">
-                        <Trans>The page could be found.</Trans>
-                    </div>
+                    <Image src="/image/radar.png" width={200} height={106} alt="Page not found. Please try again." />
+                    <div className="mt-11 text-sm font-bold">{t`The page could not be found.`}</div>
                 </div>
             </main>
             <aside className=" sticky top-0 z-[1] h-full w-96 px-4 lg:block">
@@ -30,9 +25,6 @@ export default function NotFound() {
                 <IfPathname isOneOf={['/search']}>
                     <SearchFilter />
                 </IfPathname>
-
-                <mask-calendar-widget />
-                <mask-page-inspector />
             </aside>
         </>
     );
