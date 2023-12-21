94ef820c25e82918eacdcdb5452749a2b5b95f3f
guanbinrui <guanbinrui@mask.io>
2023-12-21 23:41:02 +0800

    Revert "fix: lingui title"
    This reverts commit 4951ce266fefe74a1fdcdd8a243577850db96a4a.

--- src/app/(normal)/notifications/layout.tsx
+++ src/app/(normal)/notifications/layout.tsx
@@ -4,10 +4,8 @@ import type React from 'react';
 import { createPageTitle } from '@/helpers/createPageTitle.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 
-const title = t`Notifications`;
-
 export const metadata = createSiteMetadata({
-    title: createPageTitle(title),
+    title: createPageTitle(t`Notifications`),
 });
 
 export default function DetailLayout({ children }: { children: React.ReactNode }) {
