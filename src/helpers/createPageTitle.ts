7e4ce7a6b5532866b73d091cebeef6ad9413324f
guanbinrui <guanbinrui@mask.io>
2023-12-22 01:04:17 +0800

    fix: i18n title

--- src/helpers/createPageTitle.ts
+++ src/helpers/createPageTitle.ts
@@ -1,5 +1,8 @@
 import { SITE_NAME } from '@/constants/index.js';
+import { getLocaleFromCookies } from '@/helpers/getLocaleFromCookies.js';
+import { setLocale } from '@/i18n/index.js';
 
 export function createPageTitle(title: string) {
+    setLocale(getLocaleFromCookies());
     return `${title} • ${SITE_NAME}`;
 }
