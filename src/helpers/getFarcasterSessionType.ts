15d7ae8fc0452b5a4ca7067a11722a94177e6e18
beyond009 <beyond009@users.noreply.github.com>
2023-12-22 08:46:53 +0000

    chore: prettier

--- src/helpers/getFarcasterSessionType.ts
+++ src/helpers/getFarcasterSessionType.ts
@@ -3,7 +3,7 @@ import { WarpcastSession } from '@/providers/warpcast/Session.js';
 
 export function getFarcasterSessionType() {
     const session = warpcastClient.getSession();
-    const noSession = !session
+    const noSession = !session;
     const isCustodyWallet = WarpcastSession.isCustodyWallet(session);
     const isGrantByPermission = WarpcastSession.isGrantByPermission(session);
     return { isCustodyWallet, isGrantByPermission, noSession };
