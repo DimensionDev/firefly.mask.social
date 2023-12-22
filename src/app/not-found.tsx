04cb0f1169f7ebaf4b18f82797b4d594497096a3
guanbinrui <guanbinrui@mask.io>
2023-12-22 13:39:31 +0800

    fix: eslint

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,8 +4,8 @@ import { IfPathname } from '@/components/IfPathname.js';
 import { AsideSearchBar } from '@/components/Search/SearchBar.js';
 import { SearchFilter } from '@/components/Search/SearchFilter.js';
 import { Image } from '@/esm/Image.js';
-import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 import { createPageTitle } from '@/helpers/createPageTitle.js';
+import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 
 export const metadata = createSiteMetadata({
     title: createPageTitle(t`Page not found`),
