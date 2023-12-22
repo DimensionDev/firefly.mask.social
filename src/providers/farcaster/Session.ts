64ea877609d669cf41175bd4845f57751dd58994
unclebill <billbill290@gmail.com>
2023-12-22 22:21:54 +0800

    fix: mf-5881 post to farcaster with custody wallet

--- src/providers/farcaster/Session.ts
+++ src/providers/farcaster/Session.ts
@@ -52,11 +52,11 @@ export class FarcasterSession extends BaseSession implements Session {
 
     static isGrantByPermission(session: Session | null): session is FarcasterSession & { signerRequestToken: string } {
         if (!session) return false;
-        return session.type === SessionType.Farcaster && !(session as FarcasterSession).signerRequestToken;
+        return session.type === SessionType.Farcaster && !!(session as FarcasterSession).signerRequestToken;
     }
 
     static isCustodyWallet(session: Session | null): session is FarcasterSession & { signerRequestToken: undefined } {
         if (!session) return false;
-        return session.type === SessionType.Farcaster && !!(session as FarcasterSession).signerRequestToken;
+        return session.type === SessionType.Farcaster && !(session as FarcasterSession).signerRequestToken;
     }
 }
