3024efa6709b23a5119366e563993fc15ef54cbc
guanbinrui <guanbinrui@users.noreply.github.com>
2023-12-22 14:49:34 +0000

    chore: prettier

--- src/mask/setup/overwrite-modals.ts
+++ src/mask/setup/overwrite-modals.ts
@@ -1,12 +1,12 @@
 import { EVMWeb3 } from '@masknet/web3-providers';
 import { ProviderType } from '@masknet/web3-shared-evm';
 
-import { SelectProviderModal } from "@/maskbook/packages/shared/src/index.js";
+import { SelectProviderModal } from '@/maskbook/packages/shared/src/index.js';
 
 SelectProviderModal.__unsafe_overwrite_methods__({
     async open() {
         await EVMWeb3.connect({
             providerType: ProviderType.CustomEvent,
         });
-    }
-})
+    },
+});
--- src/modals/hasRedPacketPayload.ts
+++ src/modals/hasRedPacketPayload.ts
@@ -1,6 +1,6 @@
 import { RedPacketMetaKey } from '@masknet/plugin-redpacket';
-import type { TypedMessage } from "@masknet/typed-message";
+import type { TypedMessage } from '@masknet/typed-message';
 
 export function hasRedPacketPayload(message?: TypedMessage | null) {
-    return message?.meta?.has(RedPacketMetaKey)
+    return message?.meta?.has(RedPacketMetaKey);
 }
