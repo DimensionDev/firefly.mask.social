8684be0d57eb6a5452995156e090d7f27e1f9bc6
unclebill <billbill290@gmail.com>
2023-12-23 16:52:16 +0800

    fixup! fix: mf-5888 the remove button for image (#266)

--- src/components/Compose/ComposeVideo.tsx
+++ src/components/Compose/ComposeVideo.tsx
@@ -2,7 +2,7 @@ import { t } from '@lingui/macro';
 import { useMemo } from 'react';
 
 import CloseIcon from '@/assets/close.svg';
-import { Tooltip } from '@/components/Tooltip.js';
+import { Tippy } from '@/esm/Tippy.js';
 import { useComposeStateStore } from '@/store/useComposeStore.js';
 
 export default function ComposeVideo() {
@@ -14,11 +14,15 @@ export default function ComposeVideo() {
     return (
         <div className=" relative mt-3 overflow-hidden rounded-2xl">
             <video controls src={blobURL} />
-            <div className=" absolute right-2 top-2 z-50 h-[18px] w-[18px]">
-                <Tooltip content={t`Remove`} placement="top">
-                    <CloseIcon className=" cursor-pointer" width={18} height={18} onClick={() => updateVideo(null)} />
-                </Tooltip>
-            </div>
+            <Tippy content={<span>{t`Remove`}</span>} placement="top">
+                <div
+                    className="radius-8 absolute right-1 top-1 z-50 inline-flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-gray-500 hover:bg-opacity-70"
+                    onClick={() => updateVideo(null)}
+                    role="button"
+                >
+                    <CloseIcon width={18} height={18} color="#fff" />
+                </div>
+            </Tippy>
         </div>
     );
 }
