d439172b7054da206d300c302c206cb018759b49
nuanyang233 <nuanyang233@gmail.com>
2023-12-24 03:15:28 +0800

    fix: global font

--- src/components/MobileFallback.tsx
+++ src/components/MobileFallback.tsx
@@ -12,11 +12,11 @@ export function MobileFallback() {
     const { isDarkMode } = useDarkMode();
 
     return (
-        <div className="md:hidden">
+        <div className="flex h-screen flex-col md:hidden">
             <div className="px-4">
                 {!isDarkMode ? <LightLogo width={134} height={64} /> : <DarkLogo width={134} height={64} />}
             </div>
-            <div className="flex h-screen flex-col items-center justify-center">
+            <div className="flex flex-1 flex-col items-center justify-center">
                 <Image src="/image/radar.png" width={200} height={106} alt="Please open this link on PC explore." />
                 <div className="mt-11 text-sm font-bold">{t`Please open this link on PC explore.`}</div>
                 <Link href="https://firefly.mask.social" className="mt-11 text-[#1C68F3] underline">
