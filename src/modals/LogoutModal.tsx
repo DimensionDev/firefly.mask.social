3cc381ca37eb66630886e916f08d077e769d8bc4
Harlan Sun <1249413181@qq.com>
2023-12-24 12:21:38 +0700

    chore: review

--- src/modals/LogoutModal.tsx
+++ src/modals/LogoutModal.tsx
@@ -41,8 +41,8 @@ export const LogoutModal = forwardRef<SingletonModalRefCreator<LogoutModalProps 
         return !source
             ? lensProfiles.concat(farcasterProfiles)
             : source === SocialPlatform.Lens
-              ? lensProfiles
-              : farcasterProfiles;
+                ? lensProfiles
+                : farcasterProfiles;
     }, [lensProfiles, farcasterProfiles, source, profile]);
 
     return (
@@ -83,9 +83,9 @@ export const LogoutModal = forwardRef<SingletonModalRefCreator<LogoutModalProps 
                                 </div>
                                 <div className="flex flex-col gap-[12px] p-[24px]">
                                     <div className="text-[15px] font-medium leading-normal text-lightMain">
-                                        <Trans>{`Confirm to log out ${
-                                            profiles.length > 1 ? 'these accounts' : 'this account'
-                                        }?`}</Trans>
+                                        {profiles.length > 1 ?
+                                            <Trans>Confirm to log out these accounts?</Trans> : <Trans>Confirm to log out this account?</Trans>
+                                        }
                                     </div>
                                     {profiles.map((profile) => (
                                         <div
