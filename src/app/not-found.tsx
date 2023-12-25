d47094bddac2b882af8ae7165896fecf72726128
nuanyang233 <nuanyang233@gmail.com>
2023-12-24 19:44:50 -0800

    fix: timeline width (#287)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ export const metadata = createSiteMetadata({
 export default function NotFound() {
     return (
         <>
-            <main className="max-w-[888px] flex-[1_1_100%] border-r border-line pl-[289px]">
+            <main className="w-[888px] flex-[1_1_100%] border-r border-line pl-[289px]">
                 <div className="flex h-screen flex-col items-center justify-center">
                     <Image src="/image/radar.png" width={200} height={106} alt="Page not found. Please try again." />
                     <div className="mt-11 text-sm font-bold">{t`The page could not be found.`}</div>
