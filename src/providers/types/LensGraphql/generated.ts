b0754439da07c13181d245cfb9bd238b3a2d2ad5
beyond009 <beyond009@users.noreply.github.com>
2024-01-04 07:24:54 +0000

    chore: prettier

--- src/providers/types/LensGraphql/generated.ts
+++ src/providers/types/LensGraphql/generated.ts
@@ -1,7 +1,7 @@
 /* cspell:disable */
 
-import { gql } from '@apollo/client'
-import * as Apollo from '@apollo/client'
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
