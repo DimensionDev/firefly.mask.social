96d9b66a6fd17cad0d010f3bd95111b467ded362
Harlan Sun <1249413181@qq.com>
2024-01-05 18:19:50 +0800

    chore: remove unused

--- src/providers/types/LensGraphql/profileManagers.ts
+++ src/providers/types/LensGraphql/profileManagers.ts
@@ -11,13 +11,8 @@ import {
     LimitType,
     MetadataAttributeType,
     NetworkAddressFieldsFragmentDoc,
-    ProfileFieldsFragmentDoc,
     ProfileMetadataFieldsFragmentDoc,
     ProfileOperationsFieldsFragmentDoc,
-    ProfilesDocument,
-    type ProfilesQuery,
-    type ProfilesQueryVariables,
-    type ProfilesRequest,
     type Scalars,
 } from './generated.js';
 
@@ -30,95 +25,6 @@ const apolloClient = new ApolloClient({
 
 export type ProfilesManagedQuery = {
     __typename?: 'Query';
-    profilesManaged: {
-        __typename?: 'PaginatedProfileResult';
-        items: Array<{
-            __typename?: 'Profile';
-            id: any;
-            handle?: {
-                __typename?: 'HandleInfo';
-                fullHandle: any;
-                localName: string;
-                suggestedFormatted: {
-                    __typename?: 'SuggestedFormattedHandle';
-                    localName: string;
-                };
-                linkedTo?: { __typename?: 'HandleLinkedTo'; nftTokenId: any } | null;
-            } | null;
-            ownedBy: { __typename?: 'NetworkAddress'; address: any; chainId: any };
-            operations: {
-                __typename?: 'ProfileOperations';
-                id: any;
-                isBlockedByMe: {
-                    __typename?: 'OptimisticStatusResult';
-                    value: boolean;
-                };
-                isFollowedByMe: {
-                    __typename?: 'OptimisticStatusResult';
-                    value: boolean;
-                };
-                isFollowingMe: {
-                    __typename?: 'OptimisticStatusResult';
-                    value: boolean;
-                };
-            };
-            metadata?: {
-                __typename?: 'ProfileMetadata';
-                displayName?: string | null;
-                bio?: any | null;
-                rawURI: any;
-                appId?: any | null;
-                picture?:
-                    | {
-                          __typename?: 'ImageSet';
-                          optimized?: { __typename?: 'Image'; uri: any } | null;
-                      }
-                    | {
-                          __typename?: 'NftImage';
-                          image: {
-                              __typename?: 'ImageSet';
-                              optimized?: { __typename?: 'Image'; uri: any } | null;
-                          };
-                      }
-                    | null;
-                coverPicture?: {
-                    __typename?: 'ImageSet';
-                    optimized?: { __typename?: 'Image'; uri: any } | null;
-                } | null;
-                attributes?: Array<{
-                    __typename?: 'MetadataAttribute';
-                    type: MetadataAttributeType;
-                    key: string;
-                    value: string;
-                }> | null;
-            } | null;
-            followModule?:
-                | {
-                      __typename?: 'FeeFollowModuleSettings';
-                      type: FollowModuleType;
-                      recipient: any;
-                      amount: {
-                          __typename?: 'Amount';
-                          value: string;
-                          asset: {
-                              __typename?: 'Erc20';
-                              name: string;
-                              symbol: string;
-                              decimals: number;
-                              contract: {
-                                  __typename?: 'NetworkAddress';
-                                  address: any;
-                                  chainId: any;
-                              };
-                          };
-                      };
-                  }
-                | { __typename?: 'RevertFollowModuleSettings'; type: FollowModuleType }
-                | { __typename?: 'UnknownFollowModuleSettings'; type: FollowModuleType }
-                | null;
-        }>;
-        pageInfo: { __typename?: 'PaginatedResultInfo'; next?: any | null };
-    };
     lastLoggedInProfile?: {
         __typename?: 'Profile';
         id: any;
@@ -260,161 +166,12 @@ export type LastLoggedInProfileRequest = {
     for: Scalars['EvmAddress']['input'];
 };
 
-export type CurrentProfileQuery = {
-    __typename?: 'Query';
-    profile?: {
-        __typename?: 'Profile';
-        id: any;
-        signless: boolean;
-        sponsor: boolean;
-        createdAt: any;
-        interests: string[];
-        invitesLeft: number;
-        handle?: {
-            __typename?: 'HandleInfo';
-            fullHandle: any;
-            localName: string;
-            suggestedFormatted: {
-                __typename?: 'SuggestedFormattedHandle';
-                localName: string;
-            };
-            linkedTo?: { __typename?: 'HandleLinkedTo'; nftTokenId: any } | null;
-        } | null;
-        ownedBy: { __typename?: 'NetworkAddress'; address: any; chainId: any };
-        stats: {
-            __typename?: 'ProfileStats';
-            id: any;
-            followers: number;
-            following: number;
-            comments: number;
-            posts: number;
-            mirrors: number;
-            quotes: number;
-        };
-        operations: {
-            __typename?: 'ProfileOperations';
-            id: any;
-            isBlockedByMe: {
-                __typename?: 'OptimisticStatusResult';
-                value: boolean;
-            };
-            isFollowedByMe: {
-                __typename?: 'OptimisticStatusResult';
-                value: boolean;
-            };
-            isFollowingMe: {
-                __typename?: 'OptimisticStatusResult';
-                value: boolean;
-            };
-        };
-        invitedBy?: { __typename?: 'Profile'; id: any } | null;
-        onchainIdentity: {
-            __typename?: 'ProfileOnchainIdentity';
-            proofOfHumanity: boolean;
-            ens?: { __typename?: 'EnsOnchainIdentity'; name?: any | null } | null;
-            sybilDotOrg: {
-                __typename?: 'SybilDotOrgIdentity';
-                verified: boolean;
-                source?: {
-                    __typename?: 'SybilDotOrgIdentitySource';
-                    twitter: {
-                        __typename?: 'SybilDotOrgTwitterIdentity';
-                        handle?: string | null;
-                    };
-                } | null;
-            };
-            worldcoin: { __typename?: 'WorldcoinIdentity'; isHuman: boolean };
-        };
-        followNftAddress?: {
-            __typename?: 'NetworkAddress';
-            address: any;
-            chainId: any;
-        } | null;
-        metadata?: {
-            __typename?: 'ProfileMetadata';
-            displayName?: string | null;
-            bio?: any | null;
-            rawURI: any;
-            appId?: any | null;
-            picture?:
-                | {
-                      __typename?: 'ImageSet';
-                      optimized?: { __typename?: 'Image'; uri: any } | null;
-                  }
-                | {
-                      __typename?: 'NftImage';
-                      image: {
-                          __typename?: 'ImageSet';
-                          optimized?: { __typename?: 'Image'; uri: any } | null;
-                      };
-                  }
-                | null;
-            coverPicture?: {
-                __typename?: 'ImageSet';
-                optimized?: { __typename?: 'Image'; uri: any } | null;
-            } | null;
-            attributes?: Array<{
-                __typename?: 'MetadataAttribute';
-                type: MetadataAttributeType;
-                key: string;
-                value: string;
-            }> | null;
-        } | null;
-        followModule?:
-            | {
-                  __typename?: 'FeeFollowModuleSettings';
-                  type: FollowModuleType;
-                  recipient: any;
-                  amount: {
-                      __typename?: 'Amount';
-                      value: string;
-                      asset: {
-                          __typename?: 'Erc20';
-                          name: string;
-                          symbol: string;
-                          decimals: number;
-                          contract: {
-                              __typename?: 'NetworkAddress';
-                              address: any;
-                              chainId: any;
-                          };
-                      };
-                  };
-              }
-            | { __typename?: 'RevertFollowModuleSettings'; type: FollowModuleType }
-            | { __typename?: 'UnknownFollowModuleSettings'; type: FollowModuleType }
-            | null;
-        txHash: any | null;
-    } | null;
-    userSigNonces: { __typename?: 'UserSigNonces'; lensHubOnchainSigNonce: any };
-};
-
 export type ProfileRequest = {
     /** The handle for profile you want to fetch - namespace/localname */
     forHandle?: InputMaybe<Scalars['Handle']['input']>;
     /** The profile you want to fetch */
     forProfileId?: InputMaybe<Scalars['ProfileId']['input']>;
 };
-
-export type CurrentProfileQueryVariables = Exact<{
-    request: ProfileRequest;
-}>;
-
-export const CurrentProfileDocument = gql`
-    query CurrentProfile($request: ProfileRequest!) {
-        profile(request: $request) {
-            ...ProfileFields
-            guardian {
-                protected
-                cooldownEndsOn
-            }
-        }
-        userSigNonces {
-            lensHubOnchainSigNonce
-        }
-    }
-    ${ProfileFieldsFragmentDoc}
-`;
 
 export const profilesManagedQuery = (request: ProfilesManagedRequest | LastLoggedInProfileRequest) => {
     return apolloClient.query<ProfilesManagedQuery, ProfilesManagedQueryVariables>({
@@ -427,20 +184,3 @@ export const profilesManagedQuery = (request: ProfilesManagedRequest | LastLogge
     });
 };
 
-export const profileQuery = (request: ProfilesRequest) => {
-    return apolloClient.query<ProfilesQuery, ProfilesQueryVariables>({
-        query: ProfilesDocument,
-        variables: {
-            request,
-        },
-    });
-};
-
-export const currentProfileQuery = (request: ProfileRequest) => {
-    return apolloClient.query<CurrentProfileQuery, CurrentProfileQueryVariables>({
-        query: CurrentProfileDocument,
-        variables: {
-            request,
-        },
-    });
-};
