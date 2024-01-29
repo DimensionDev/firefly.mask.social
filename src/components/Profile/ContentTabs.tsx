c3c05633904762e6b44d91daf7ce94f4feef8ce0
guanbinrui <guanbinrui@dimension.im>
2024-01-29 16:22:05 +0800

    fix: hide collect tab for fc

--- src/components/Profile/ContentTabs.tsx
+++ src/components/Profile/ContentTabs.tsx
@@ -4,7 +4,7 @@ import { Suspense, useState } from 'react';
 import Loading from '@/components/Loading.js';
 import ContentCollected from '@/components/Profile/ContentCollected.js';
 import ContentFeed from '@/components/Profile/ContentFeed.js';
-import type { SocialPlatform } from '@/constants/enum.js';
+import { SocialPlatform } from '@/constants/enum.js';
 import { classNames } from '@/helpers/classNames.js';
 
 enum TabEnum {
@@ -22,25 +22,30 @@ export default function ContentTabs({ profileId, source }: ContentTabsProps) {
     return (
         <>
             <div className=" flex gap-5 border-b border-lightLineSecond px-5 dark:border-line">
-                {Object.values(TabEnum).map((tabName) => (
-                    <div key={tabName} className=" flex flex-col">
-                        <button
-                            className={classNames(
-                                ' flex h-[46px] items-center px-[14px] font-extrabold transition-all',
-                                tab === tabName ? ' text-main' : ' text-third hover:text-main',
-                            )}
-                            onClick={() => setTab(tabName)}
-                        >
-                            {tabName === TabEnum.Feed ? <Trans>Feed</Trans> : <Trans>Collected</Trans>}
-                        </button>
-                        <span
-                            className={classNames(
-                                ' h-1 w-full rounded-full bg-[#9250FF] transition-all',
-                                tab !== tabName ? ' hidden' : '',
-                            )}
-                        />
-                    </div>
-                ))}
+                {Object.values(TabEnum)
+                    .filter((x) => {
+                        if (source === SocialPlatform.Farcaster) return x !== TabEnum.Collected;
+                        return true;
+                    })
+                    .map((tabName) => (
+                        <div key={tabName} className=" flex flex-col">
+                            <button
+                                className={classNames(
+                                    ' flex h-[46px] items-center px-[14px] font-extrabold transition-all',
+                                    tab === tabName ? ' text-main' : ' text-third hover:text-main',
+                                )}
+                                onClick={() => setTab(tabName)}
+                            >
+                                {tabName === TabEnum.Feed ? <Trans>Feed</Trans> : <Trans>Collected</Trans>}
+                            </button>
+                            <span
+                                className={classNames(
+                                    ' h-1 w-full rounded-full bg-[#9250FF] transition-all',
+                                    tab !== tabName ? ' hidden' : '',
+                                )}
+                            />
+                        </div>
+                    ))}
             </div>
 
             {tab === TabEnum.Feed && (
@@ -49,7 +54,7 @@ export default function ContentTabs({ profileId, source }: ContentTabsProps) {
                 </Suspense>
             )}
 
-            {tab === TabEnum.Collected && (
+            {tab === TabEnum.Collected && source !== SocialPlatform.Farcaster && (
                 <Suspense fallback={<Loading />}>
                     <ContentCollected source={source} profileId={profileId} />
                 </Suspense>
