2e6a46c1f514c7ede43078b144c9fc05faf29fd3
guanbinrui <guanbinrui@users.noreply.github.com>
2024-02-02 13:44:07 +0000

    chore: prettier

--- src/helpers/getSnackbarMessageFromError.tsx
+++ src/helpers/getSnackbarMessageFromError.tsx
@@ -3,9 +3,9 @@ import type { SnackbarMessage } from 'notistack';
 
 /**
  * Get a snackbar message from an error.
- * @param error 
+ * @param error
  * @param fallback the fallback message if error is not an instance of Error
- * @returns 
+ * @returns
  */
 export function getSnackbarMessageFromError(error: unknown, fallback: string): SnackbarMessage {
     return error instanceof Error ? (
