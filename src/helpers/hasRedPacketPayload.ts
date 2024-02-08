f132c53595e2137a52e2718bad12f38123c13c98
guanbinrui <guanbinrui@users.noreply.github.com>
2024-02-08 07:57:29 +0000

    chore: prettier

--- src/helpers/hasRedPacketPayload.ts
+++ src/helpers/hasRedPacketPayload.ts
@@ -1,5 +1,5 @@
-import { RedPacketMetaKey } from "@masknet/plugin-redpacket";
-import type { TypedMessage } from "@masknet/typed-message";
+import { RedPacketMetaKey } from '@masknet/plugin-redpacket';
+import type { TypedMessage } from '@masknet/typed-message';
 
 export function hasRedPacketPayload(message: TypedMessage | null) {
     return message?.meta?.has(RedPacketMetaKey);
