e8978df21f2cff4219fee29a49092221f552404d
guanbinrui <guanbinrui@dimension.im>
2024-02-08 17:00:45 +0800

    chore: support GET

--- src/app/api/hubble/route.ts
+++ src/app/api/hubble/route.ts
@@ -1,15 +1,23 @@
 import urlcat from 'urlcat';
 
-export function POST(request: Request) {
+const handleRequest = (request: Request) => {
     const u = new URL(request.url);
 
     const newHeaders = new Headers(request.headers);
     if (process.env.HUBBLE_TOKEN) newHeaders.set('api_token', process.env.HUBBLE_TOKEN);
 
     return fetch(
-        new Request(urlcat(process.env.HUBBLE_URL, u.pathname), {
+        new Request(urlcat(process.env.HUBBLE_URL, u.pathname + u.search), {
             ...request,
             headers: newHeaders,
         }),
     );
+}
+
+export function GET(request: Request) {
+    return handleRequest(request)
+}
+
+export function POST(request: Request) {
+    return handleRequest(request)
 }
