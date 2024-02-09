3eaea31da6966c5c597b71e7805761d4eb00c689
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-02-10 01:40:37 +0800

    fix: message coding (#438)
    * fix: message coding
    
    * fix: sig

--- src/helpers/encodeMessageData.ts
+++ src/helpers/encodeMessageData.ts
@@ -1,5 +1,6 @@
 import { FarcasterNetwork, Message, MessageData, NobleEd25519Signer } from '@farcaster/core';
 import { blake3 } from '@noble/hashes/blake3';
+import { bytesToHex } from '@noble/hashes/utils';
 import { toBytes } from 'viem';
 
 import { farcasterClient } from '@/configs/farcasterClient.js';
@@ -14,23 +15,32 @@ export async function encodeMessageData(
     const privateKey = withPrivateKey || token;
     const signer = new NobleEd25519Signer(toBytes(privateKey));
 
+    const fid = Number.parseInt(profileId, 10);
+
     // @ts-ignore timestamp is not needed
     const messageData: MessageData = {
-        ...withMessageData(Number.parseInt(profileId, 10)),
-        fid: Number.parseInt(profileId, 10),
+        ...withMessageData(fid),
+        fid,
         network: FarcasterNetwork.MAINNET,
     };
+    const messageDataBytes = MessageData.encode(messageData).finish();
+    const messageDataHash = blake3(messageDataBytes, { dkLen: 20 });
+
     const message = await withMessage(messageData, signer);
     const messageBytes = Message.encode(message).finish();
-    const messageHash = blake3(messageBytes, { dkLen: 20 });
+
+    const signerKeyResult = await signer.getSignerKey();
+    const signatureResult = await signer.signMessageHash(messageDataHash);
+
+    if (signerKeyResult.isErr() || signatureResult.isErr()) {
+        throw new Error('Invalid signer key or signature.');
+    }
 
     return {
-        signer: `0x${Buffer.from((await signer.getSignerKey())._unsafeUnwrap()).toString('hex')}`,
+        signer: `0x${bytesToHex(signerKeyResult.value)}`,
         messageBytes: Buffer.from(messageBytes),
         messageData,
-        messageHash: `0x${Buffer.from(messageHash).toString('hex')}`,
-        messageSignature: `0x${Buffer.from((await signer.signMessageHash(messageHash))._unsafeUnwrap()).toString(
-            'hex',
-        )}`,
+        messageDataHash: `0x${bytesToHex(messageDataHash)}`,
+        messageDataSignature: `0x${bytesToHex(signatureResult.value)}`,
     } as const;
 }
