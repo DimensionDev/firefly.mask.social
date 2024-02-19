60db8549da3c8a9435a6367b9487f13f75b5bcda
guanbinrui <guanbinrui@dimension.im>
2024-02-19 17:14:11 +0800

    fix: farcaster mention text

--- src/components/Markup/MarkupLink/MentionLink.tsx
+++ src/components/Markup/MarkupLink/MentionLink.tsx
@@ -3,13 +3,12 @@
 import { memo } from 'react';
 
 import { Link } from '@/esm/Link.js';
-import { getLensHandleFromMentionTitle } from '@/helpers/getLensHandleFromMentionTitle.js';
 
-export const MentionLink = memo<{ title: string; link: string }>(function MentionLink({ title, link }) {
-    if (!title) return null;
+export const MentionLink = memo<{ handle: string; link: string }>(function MentionLink({ handle, link }) {
+    if (!handle) return null;
     return (
         <Link href={link} className="text-link" onClick={(event) => event.stopPropagation()}>
-            @{getLensHandleFromMentionTitle(title)}
+            @{handle}
         </Link>
     );
 });
