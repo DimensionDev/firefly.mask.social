2dbaedc99f905402eb38e956e46549fd64a49270
guanbinrui <guanbinrui@dimension.im>
2024-02-19 16:00:20 +0800

    refactor: comment

--- src/helpers/getLensHandleFromMentionTitle.ts
+++ src/helpers/getLensHandleFromMentionTitle.ts
@@ -1,3 +1,11 @@
+/**
+ * Examples in the returned post:
+ * - @lens/handle
+ * - @handle.lens
+ *
+ * @param mentionTitle
+ * @returns
+ */
 export function getLensHandleFromMentionTitle(mentionTitle: string) {
     return mentionTitle.replace(/^@lens\//i, '').replace(/^@([^\s]+)\.lens/i, '$1');
 }
