a32adef466ced7256bb6b42f11ddbb12de05a57e
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-02-26 19:59:01 +0800

    refactor: fw 413 cors (#515)
    * refactor: code stlye
    
    * refactor: code stlye
    
    * refactor: update comment
    
    * refactor: comment
    
    * refactor: comment
    
    * chore: untilImageLoaded
    
    * chore: remove replacement

--- src/helpers/readImageBlob.ts
+++ src/helpers/readImageBlob.ts
@@ -1,17 +1,37 @@
-export async function readImageBlob(img: HTMLImageElement) {
+function untilImageLoaded(img: HTMLImageElement) {
+    return new Promise<HTMLImageElement>((resolve, reject) => {
+        if (img.complete) {
+            resolve(img);
+        } else {
+            img.onload = () => resolve(img);
+            img.onerror = () => reject(new Error('Failed to load image'));
+        }
+    });
+}
+
+/**
+ * Read an image blob from an image element
+ * @param img
+ * @returns
+ */
+export async function readImageBlob(img: HTMLImageElement, mimeType = 'image/jpeg') {
     const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return null;
+
     canvas.width = img.naturalWidth;
     canvas.height = img.naturalHeight;
     canvas.style.display = 'none';
-    document.body.appendChild(canvas);
-    const ctx = canvas.getContext('2d');
-    if (!ctx) return null;
-    ctx.drawImage(img, 0, 0);
+
     try {
-        const blob = await new Promise<Blob | null>((resolve) => {
-            canvas.toBlob(resolve, 'image/jpeg', 1);
+        await untilImageLoaded(img);
+
+        document.body.appendChild(canvas);
+        ctx.drawImage(img, 0, 0);
+
+        return new Promise<Blob | null>((resolve) => {
+            canvas.toBlob(resolve, mimeType, 1);
         });
-        return blob;
     } catch {
         return null;
     } finally {
