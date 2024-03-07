1b998e685118a36054e23017ecb5b19911d739c2
guanbinrui <guanbinrui@mask.io>
2024-03-07 17:58:17 +0800

    fix: modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -25,22 +25,22 @@ export function Modal({ open, onClose, children }: ModalProps) {
                 >
                     <div className="fixed inset-0 bg-main/25 bg-opacity-30" />
                 </Transition.Child>
-            </Dialog>
-            <div className="fixed inset-0 overflow-y-auto">
-                <div className=" flex min-h-full items-center justify-center p-4 text-center">
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0 scale-95"
-                        enterTo="opacity-100 scale-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100 scale-100"
-                        leaveTo="opacity-0 scale-95"
-                    >
-                        {children}
-                    </Transition.Child>
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className=" flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            {children}
+                        </Transition.Child>
+                    </div>
                 </div>
-            </div>
+            </Dialog>
         </Transition>
     );
 }
