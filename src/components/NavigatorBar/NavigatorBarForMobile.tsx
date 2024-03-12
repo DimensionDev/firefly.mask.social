4b78e07ea918c5a61dd669963d190622693558d3
guanbinrui <guanbinrui@dimension.im>
2024-03-12 23:41:02 +0800

    chore: impl account icons

--- src/components/NavigatorBar/NavigatorBarForMobile.tsx
+++ src/components/NavigatorBar/NavigatorBarForMobile.tsx
@@ -3,19 +3,38 @@ import { memo } from 'react';
 import FireflyIcon from '@/assets/firefly.svg';
 import MagnifierIcon from '@/assets/magnifier.svg';
 import MenuIcon from '@/assets/menu.svg';
+import { ProfileAvatar } from '@/components/ProfileAvatar.js';
+import { useFarcasterStateStore, useLensStateStore } from '@/store/useProfileStore.js';
 
 export const NavigatorBarForMobile = memo(function NavigatorBarForMobile() {
+    const lensProfile = useLensStateStore.use.currentProfile?.();
+    const farcasterProfile = useFarcasterStateStore.use.currentProfile?.();
+
     return (
         <header className=" flex items-center px-4 py-[7px] text-main">
-            <button className=" flex h-[30px] w-[30x] items-center justify-center">
-                <MenuIcon />
-            </button>
+            <div className=" flex h-[30px] w-[30px]">
+                {farcasterProfile ? (
+                    <div className=" relative z-10">
+                        <ProfileAvatar size={30} profile={farcasterProfile} enableSourceIcon={false} />
+                    </div>
+                ) : lensProfile ? (
+                    <div className=" relative z-10">
+                        <ProfileAvatar size={30} profile={lensProfile} enableSourceIcon={false} />
+                    </div>
+                ) : null}
+                {lensProfile ? (
+                    <div className=" relative left-[20px]">
+                        <ProfileAvatar size={30} profile={lensProfile} enableSourceIcon={false} />
+                    </div>
+                ) : null}
+                {farcasterProfile || lensProfile ? null : <MenuIcon />}
+            </div>
             <h1 className=" flex flex-1 justify-center">
                 <FireflyIcon />
             </h1>
-            <button className=" flex h-[30px] w-[30x] items-center justify-center">
+            <div className=" flex h-[30px] w-[30px]">
                 <MagnifierIcon />
-            </button>
+            </div>
         </header>
     );
 });
