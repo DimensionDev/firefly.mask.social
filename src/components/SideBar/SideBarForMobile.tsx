d733bcfc0fdd8fb8621ce5754a2be1cae8637420
guanbinrui <guanbinrui@dimension.im>
2024-03-13 23:39:07 +0800

    chore: add <SideBarForMobile />

--- src/components/SideBar/SideBarForMobile.tsx
+++ src/components/SideBar/SideBarForMobile.tsx
@@ -1,5 +1,11 @@
 import { memo } from 'react';
 
+import { Menu } from '@/components/SideBar/Menu.js';
+
 export const SideBarForMobile = memo(function SideBarForMobile() {
-    return null;
+    return (
+        <div className=" fixed inset-0 right-[35%] z-50 bg-white dark:bg-black ">
+            <Menu />
+        </div>
+    );
 });
