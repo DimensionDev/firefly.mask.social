ee9c89e2ccfb6b3fc11b0748a65424297db22c10
guanbinrui <guanbinrui@mask.io>
2024-03-14 15:00:25 +0800

    refactor: settings for mobile

--- src/app/(settings)/settings/layout.tsx
+++ src/app/(settings)/settings/layout.tsx
@@ -2,9 +2,12 @@ import { SettingsList } from '@/app/(settings)/components/SettingsList.js';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <main className="flex min-h-screen w-full flex-1 pl-0 md:pl-[61px] lg:pl-72">
-            <SettingsList />
-            {children}
-        </main>
+        <>
+            <main className="hidden min-h-screen w-full flex-1 pl-0 md:flex md:pl-[61px] lg:pl-72">
+                <SettingsList />
+                {children}
+            </main>
+            <main className=" flex min-h-screen w-full md:hidden">{children}</main>
+        </>
     );
 }
--- src/app/(settings)/settings/page.tsx
+++ src/app/(settings)/settings/page.tsx
@@ -1,8 +1,21 @@
-import { redirect } from 'next/navigation.js';
+'use client';
+
+import { redirect, useRouter } from 'next/navigation.js';
+import { useMount } from 'react-use';
 import urlcat from 'urlcat';
 
+import { SettingsList } from '@/app/(settings)/components/SettingsList.js';
 import { PageRoutes } from '@/constants/enum.js';
+import { useIsSmall } from '@/hooks/useMediaQuery.js';
 
-export default async function Settings() {
-    redirect(urlcat(PageRoutes.Settings, '/display'));
+export default function Settings() {
+    const isSmall = useIsSmall('max');
+
+    if (!isSmall) return null;
+
+    return (
+        <div className=" flex min-h-screen w-full">
+            <SettingsList />
+        </div>
+    );
 }
