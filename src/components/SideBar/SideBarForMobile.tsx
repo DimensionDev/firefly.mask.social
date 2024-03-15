a9546b836ff55cb06f14a093a026b81bbb923614
guanbinrui <guanbinrui@mask.io>
2024-03-15 14:23:37 +0800

    refactor: close button

--- src/components/SideBar/SideBarForMobile.tsx
+++ src/components/SideBar/SideBarForMobile.tsx
@@ -50,26 +50,6 @@ export const SideBarForMobile = memo(function SideBarForMobile() {
                         leaveTo="-translate-x-full"
                     >
                         <Dialog.Panel className="relative mr-16 flex w-full max-w-[280px] flex-1">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-in-out duration-300"
-                                enterFrom="opacity-0"
-                                enterTo="opacity-100"
-                                leave="ease-in-out duration-300"
-                                leaveFrom="opacity-100"
-                                leaveTo="opacity-0"
-                            >
-                                <div className="absolute left-full top-0 flex w-16 justify-center pt-4">
-                                    <button
-                                        type="button"
-                                        className="-m-2.5 p-2.5"
-                                        onClick={() => updateSidebarOpen(false)}
-                                    >
-                                        <span className="sr-only">Close sidebar</span>
-                                        <CloseIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                                    </button>
-                                </div>
-                            </Transition.Child>
                             <div className=" fixed inset-0 z-50 flex w-[280px] flex-col bg-white dark:bg-black ">
                                 <div className="flex grow flex-col gap-y-5 border-r border-line px-3 lg:px-6">
                                     <div className="flex h-16 shrink-0 items-center lg:px-4">
@@ -84,6 +64,26 @@ export const SideBarForMobile = memo(function SideBarForMobile() {
                                     <Menu />
                                 </div>
                             </div>
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-in-out duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in-out duration-300"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <div className="absolute right-0 top-0 z-50 flex w-16 justify-center pt-4">
+                                    <button
+                                        type="button"
+                                        className="-m-2.5 p-2.5"
+                                        onClick={() => updateSidebarOpen(false)}
+                                    >
+                                        <span className="sr-only">Close sidebar</span>
+                                        <CloseIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                                    </button>
+                                </div>
+                            </Transition.Child>
                         </Dialog.Panel>
                     </Transition.Child>
                 </div>
