420b934d5131590359ba20c07d52f555c2474320
guanbinrui <guanbinrui@dimension.im>
2024-03-18 17:48:44 +0800

    refactor: adjust style for mobile

--- src/components/Login/ProfileAvatarInteractive.tsx
+++ src/components/Login/ProfileAvatarInteractive.tsx
@@ -1,17 +1,21 @@
 'use client';
 
+import { delay } from '@masknet/kit';
+
 import { ProfileSettings } from '@/components/Login/ProfileSettings.js';
 import { ProfileAvatar } from '@/components/ProfileAvatar.js';
 import { Tippy } from '@/esm/Tippy.js';
 import { useIsMedium } from '@/hooks/useMediaQuery.js';
 import { DraggablePopoverRef } from '@/modals/controls.js';
 import type { Profile } from '@/providers/types/SocialMedia.js';
+import { useNavigatorState } from '@/store/useNavigatorStore.js';
 
 interface ProfileAvatarInteractiveProps {
     profile: Profile;
 }
 
 export function ProfileAvatarInteractive({ profile }: ProfileAvatarInteractiveProps) {
+    const { updateSidebarOpen } = useNavigatorState();
     const isMedium = useIsMedium();
 
     return isMedium ? (
@@ -32,11 +36,13 @@ export function ProfileAvatarInteractive({ profile }: ProfileAvatarInteractivePr
     ) : (
         <div
             className=" flex justify-center"
-            onClick={() =>
+            onClick={async () => {
+                updateSidebarOpen(false);
+                await delay(300);
                 DraggablePopoverRef.open({
                     content: <ProfileSettings source={profile.source} />,
-                })
-            }
+                });
+            }}
         >
             <ProfileAvatar profile={profile} clickable />
         </div>
