e5eec8a9ccd1f04c29ee30d69e9e8325ebbca569
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-03-22 18:21:20 +0800

    fix: ui (#618)

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -39,11 +39,11 @@ export function Modal({ backdrop = true, open, onClose, children }: ModalProps) 
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
-                            enterFrom="opacity-0 scale-95"
-                            enterTo="opacity-100 scale-100"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
                             leave="ease-in duration-200"
-                            leaveFrom="opacity-100 scale-100"
-                            leaveTo="opacity-0 scale-95"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
                         >
                             {children}
                         </Transition.Child>
