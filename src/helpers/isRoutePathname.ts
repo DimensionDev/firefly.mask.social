2915bd0e6be82742439b27fe2425365ae1577e4d
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-03-26 11:45:40 +0800

    fix: navigation header (#627)
    * fix: add navigation header for own profile page
    
    * chore: lingui
    
    * chore: set the default parameter

--- src/helpers/isRoutePathname.ts
+++ src/helpers/isRoutePathname.ts
@@ -1,4 +1,4 @@
-export function isRoutePathname(pathname: string, routePathname: `/${string}`) {
+export function isRoutePathname(pathname: string, routePathname: `/${string}`, exact = false) {
     // Check if both pathnames start with '/'
     if (!pathname.startsWith('/') || !routePathname.startsWith('/')) {
         return false;
@@ -7,6 +7,11 @@ export function isRoutePathname(pathname: string, routePathname: `/${string}`) {
     // Remove trailing '/' from, if present and split the pathnames into parts
     const pathnameParts = pathname.replace(/\/$/, '').split('/');
     const routePathnameParts = routePathname.replace(/\/$/, '').split('/');
+
+    // In exact mode, check if the number of parts in both pathnames are equal
+    if (exact && routePathnameParts.length !== pathnameParts.length) {
+        return false;
+    }
 
     // Check if the number of parts in routePathname is less than or equal to pathname
     if (routePathnameParts.length > pathnameParts.length) {
