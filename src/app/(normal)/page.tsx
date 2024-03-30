b8b800092e9a4e37b2fc96b45ff9908278cc264b
nuanyang233 <nuanyang233@gmail.com>
2024-03-30 22:55:47 +0800

    fix: set homepage to client component

--- src/app/(normal)/page.tsx
+++ src/app/(normal)/page.tsx
@@ -1,25 +1,13 @@
-import { createIndicator } from '@masknet/shared-base';
-import { unstable_cache } from 'next/cache.js';
+'use client';
 
-import { discoverPosts } from '@/app/(normal)/helpers/discoverPosts.js';
 import { Home } from '@/app/(normal)/pages/Home.js';
 import { SocialPlatform, SourceInURL } from '@/constants/enum.js';
 import { resolveSocialPlatform } from '@/helpers/resolveSocialPlatform.js';
 import type { SearchParams } from '@/types/index.js';
 
-const fetchPosts = unstable_cache(discoverPosts, ['discoverPosts'], {
-    revalidate: 60 * 3,
-});
-
-export default async function Page({ searchParams }: { searchParams: SearchParams }) {
-    try {
-        const source = searchParams.source
-            ? resolveSocialPlatform(searchParams.source as SourceInURL)
-            : SocialPlatform.Farcaster;
-        const posts = await fetchPosts(source, createIndicator(undefined, ''));
-
-        return <Home source={source} pageable={posts} />;
-    } catch {
-        return <Home source={SocialPlatform.Farcaster} />;
-    }
+export default function Page({ searchParams }: { searchParams: SearchParams }) {
+    const source = searchParams.source
+        ? resolveSocialPlatform(searchParams.source as SourceInURL)
+        : SocialPlatform.Farcaster;
+    return <Home source={source} />;
 }
