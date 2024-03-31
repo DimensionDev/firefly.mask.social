8f0588fdc72783c4e09d8084ea5e77694660eca9
guanbinrui <guanbinrui@dimension.im>
2024-04-01 01:35:30 +0800

    refactor: getStampAvatarByProfileId

--- src/helpers/getStampAvatarByProfileId.ts
+++ src/helpers/getStampAvatarByProfileId.ts
@@ -4,14 +4,14 @@ import urlcat from 'urlcat';
 import { SocialPlatform } from '@/constants/enum.js';
 import { FIREFLY_STAMP_URL } from '@/constants/index.js';
 
-export function getStampAvatarByProfileId(source: SocialPlatform, id: string) {
+export function getStampAvatarByProfileId(source: SocialPlatform, profileId: string) {
     switch (source) {
         case SocialPlatform.Lens:
-            return urlcat(FIREFLY_STAMP_URL, '/lens/:id', { id });
+            return urlcat(FIREFLY_STAMP_URL, '/lens/:id', { id: profileId });
         case SocialPlatform.Farcaster:
-            return urlcat(FIREFLY_STAMP_URL, '/farcaster/:id', { id });
+            return urlcat(FIREFLY_STAMP_URL, '/farcaster/:id', { id: profileId });
         default:
             safeUnreachable(source);
-            return urlcat(FIREFLY_STAMP_URL, '/:id', { id });
+            return '';
     }
 }
