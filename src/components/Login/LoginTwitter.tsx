8ed9a996e361f9dda62a778e0c5c6e00fb86e065
guanbinrui <guanbinrui@dimension.im>
2024-04-02 11:20:31 +0800

    refactor: login twitter

---
+++ src/components/Login/LoginTwitter.tsx
@@ -0,0 +1,20 @@
+import { Trans } from '@lingui/macro';
+import { signIn } from 'next-auth/react';
+import { useEffectOnce } from 'react-use';
+
+import LoadingIcon from '@/assets/loading.svg';
+
+export function LoginTwitter() {
+    useEffectOnce(() => {
+        signIn('twitter');
+    });
+
+    return (
+        <div className="flex min-h-[200px] w-full flex-col items-center justify-center gap-4 p-4">
+            <LoadingIcon className="animate-spin" width={24} height={24} />
+            <div className=" mt-2 text-center text-sm leading-[16px] text-lightSecond">
+                <Trans>Please confirm the login with X.</Trans>
+            </div>
+        </div>
+    );
+}
