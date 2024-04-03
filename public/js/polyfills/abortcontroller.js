c326be32a34a43610adb03854fc76d81ae1d457f
nuanyang233 <nuanyang233@gmail.com>
2024-04-03 17:23:30 +0800

    fix: polyfill

--- public/js/polyfills/abortcontroller.js
+++ public/js/polyfills/abortcontroller.js
@@ -1,21 +1,28 @@
 ;(() => {
-    if (!AbortSignal.prototype.timeout) {
-        AbortSignal.prototype.timeout = function(ms) {
-          const controller = new AbortController();
-          const timer = setTimeout(() => controller.abort(), ms);
-      
-          if (this.aborted) {
-            clearTimeout(timer);
-            controller.abort();
-          } else {
-            this.addEventListener('abort', () => {
-              clearTimeout(timer);
-              controller.abort();
-            }, {once: true});
-          }
-      
-          return controller.signal;
+    'use strict';
+    if (typeof AbortController === 'function' && typeof AbortSignal === 'function') {
+        if (!('timeout' in AbortSignal)) {
+            AbortSignal.timeout = function(ms) {
+                if (typeof ms !== 'number') {
+                    throw new TypeError('The timeout duration must be a number');
+                }
+                if (ms <= 0) {
+                    throw new RangeError('The timeout duration must be a positive number');
+                }
+
+                const controller = new AbortController();
+                const timer = setTimeout(() => {
+                    controller.abort();
+                }, ms);
+
+                controller.signal.addEventListener('abort', () => {
+                    clearTimeout(timer);
+                }, { once: true });
+
+                return controller.signal;
+            };
         }
-      }
+    } else {
+        console.warn('AbortController or AbortSignal is not supported in this environment');
+    }
 })();
-null;
