37e5a6590fd9c86ba938e4e2edaa8e223207327e
SunWukong <swkatmask@gmail.com>
2024-04-03 17:56:07 +0800

    fix: use Avatar in LoginLens

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,7 @@
+import { useQuery } from '@tanstack/react-query';
 import { compact } from 'lodash-es';
 import type { ImageProps as NextImageProps } from 'next/image.js';
 import { memo } from 'react';
-import { useAsync } from 'react-use';
 
 import { Image as NextImage } from '@/esm/Image.js';
 import { classNames } from '@/helpers/classNames.js';
@@ -19,10 +19,10 @@ interface Props extends NextImageProps {
 export const Avatar = memo(function Avatar({ src, size, className, fallbackUrl, ...rest }: Props) {
     const { isDarkMode } = useDarkMode();
 
-    const { value: url, loading } = useAsync(
-        async () => resolveFirstAvailableUrl(compact([resolveImgurUrl(resolveAvatarFallbackUrl(src)), fallbackUrl])),
-        [src, fallbackUrl],
-    );
+    const { data: url, isLoading: loading } = useQuery({
+        queryKey: ['avatar', src, fallbackUrl],
+        queryFn: () => resolveFirstAvailableUrl(compact([resolveImgurUrl(resolveAvatarFallbackUrl(src)), fallbackUrl])),
+    });
 
     const defaultFallbackUrl = isDarkMode ? '/image/firefly-dark-avatar.png' : '/image/firefly-light-avatar.png';
 
