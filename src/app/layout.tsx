f03e54f43a72f869ff115000ce43c09e12f63bc1
nuanyang233 <nuanyang233@gmail.com>
2024-04-03 13:28:08 +0800

    feat: add insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { Inter } from 'next/font/google';
 import { ScrollRestorer } from 'next-scroll-restorer';
@@ -51,6 +52,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     <Modals />
                 </Providers>
                 <GA />
+                <SpeedInsights />
                 <BeforeUnload />
             </body>
             <ScrollRestorer />
