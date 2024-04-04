33b8836944e97992e229c34a22c9a609ab61a65f
nuanyang233 <nuanyang233@gmail.com>
2024-04-05 00:45:40 +0800

    chore: comment

--- src/helpers/waitUntilComplete.ts
+++ src/helpers/waitUntilComplete.ts
@@ -2,8 +2,8 @@ import { type LensClient, LensTransactionStatusType } from '@lens-protocol/clien
 
 /**
  * Waits a transaction to complete (for lens only).
- * @param client
- * @param hash
+ * @param client A lens client instance
+ * @param hash lens transaction hash
  * @returns
  */
 export async function waitUntilComplete(client: LensClient, hash: string | null) {
