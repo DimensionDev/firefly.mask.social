ab9fcb230d13b5ba97f846988c1dc232a7b02b49
guanbinrui <guanbinrui@dimension.im>
2024-04-04 16:37:35 +0800

    fix: isPublishedPost

--- src/helpers/isPublishedPost.ts
+++ src/helpers/isPublishedPost.ts
@@ -3,5 +3,5 @@ import type { CompositePost } from '@/store/useComposeStore.js';
 import type { ComposeType } from '@/types/compose.js';
 
 export function isPublishedPost(type: ComposeType, compositePost: CompositePost) {
-    return SORTED_SOURCES.every((x) => compositePost.availableSources.includes(x) && compositePost.postId[x]);
+    return SORTED_SOURCES.every((x) => (compositePost.availableSources.includes(x) ? compositePost.postId[x] : true));
 }
