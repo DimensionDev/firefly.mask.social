60d03e757b19e8f25575281682180950b6d62e1e
guanbinrui <guanbinrui@dimension.im>
2024-04-11 10:46:30 +0800

    fix: type

--- src/helpers/getFarcasterSessionType.ts
+++ src/helpers/getFarcasterSessionType.ts
@@ -2,8 +2,7 @@ import { farcasterClient } from '@/configs/farcasterClient.js';
 import { FarcasterSession } from '@/providers/farcaster/Session.js';
 
 export function getFarcasterSessionType() {
-    const session = farcasterClient.session();
-    const isCustodyWallet = FarcasterSession.isCustodyWallet(session);
-    const isGrantByPermission = FarcasterSession.isGrantByPermission(session);
+    const isCustodyWallet = FarcasterSession.isCustodyWallet(farcasterClient.session);
+    const isGrantByPermission = FarcasterSession.isGrantByPermission(farcasterClient.session);
     return { isCustodyWallet, isGrantByPermission };
 }
