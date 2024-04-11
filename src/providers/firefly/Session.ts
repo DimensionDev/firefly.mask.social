ae2830806d60adf703669e7801b83b2f46ede5c9
guanbinrui <guanbinrui@dimension.im>
2024-04-11 19:10:10 +0800

    chore: FireflySession.from

--- src/providers/firefly/Session.ts
+++ src/providers/firefly/Session.ts
@@ -1,3 +1,5 @@
+import { safeUnreachable } from '@masknet/kit';
+
 import { BaseSession } from '@/providers/base/Session.js';
 import type { Session } from '@/providers/types/Session.js';
 import { SessionType } from '@/providers/types/SocialMedia.js';
@@ -13,5 +15,21 @@ export class FireflySession extends BaseSession implements Session {
 
     override async destroy(): Promise<void> {
         throw new Error('Not allowed');
+    }
+
+    static async from(session: Session): Promise<FireflySession> {
+        switch (session.type) {
+            case SessionType.Lens:
+                throw new Error('Not implemented yet');
+            case SessionType.Farcaster:
+                throw new Error('Not implemented yet');
+            case SessionType.Firefly:
+                throw new Error('Not allowed');
+            case SessionType.Twitter:
+                throw new Error('Not allowed');
+            default:
+                safeUnreachable(session.type);
+                throw new Error(`Unknown session type: ${session.type}`);
+        }
     }
 }
