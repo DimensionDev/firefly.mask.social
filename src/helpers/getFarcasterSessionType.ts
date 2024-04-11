28d1fe57430bb2c730d9fe10e7b581913a4a869c
guanbinrui <guanbinrui@dimension.im>
2024-04-11 10:49:50 +0800

    refactor: code style

--- src/helpers/getFarcasterSessionType.ts
+++ src/helpers/getFarcasterSessionType.ts
@@ -2,7 +2,9 @@ import { farcasterClient } from '@/configs/farcasterClient.js';
 import { FarcasterSession } from '@/providers/farcaster/Session.js';
 
 export function getFarcasterSessionType() {
-    const isCustodyWallet = FarcasterSession.isCustodyWallet(farcasterClient.session);
-    const isGrantByPermission = FarcasterSession.isGrantByPermission(farcasterClient.session);
-    return { isCustodyWallet, isGrantByPermission };
+    return farcasterClient.withSession((session) => {
+        const isCustodyWallet = FarcasterSession.isCustodyWallet(session);
+        const isGrantByPermission = FarcasterSession.isGrantByPermission(session);
+        return { isCustodyWallet, isGrantByPermission };
+    });
 }
