b8854661341364931973db142080e1840276c0cb
Harlan Sun <1249413181@qq.com>
2024-04-18 10:27:40 +0800

    fix: twitter media

--- src/services/uploadToTwitter.ts
+++ src/services/uploadToTwitter.ts
@@ -1,9 +1,31 @@
-import { Twitter } from '@masknet/web3-providers';
+import { TwitterApi } from "twitter-api-v2";
+
+function fileToBuffer(file: File): Promise<ArrayBuffer> {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            resolve(reader.result as ArrayBuffer);
+        };
+
+        reader.onerror = error => {
+            reject(error);
+        };
+
+        reader.readAsArrayBuffer(file);
+    });
+}
 
 export async function uploadToTwitter(files: File[]) {
-    const medias = await Promise.all(files.map((x) => Twitter.uploadMedia(x)));
+    const Twitter = new TwitterApi()
+
+    const medias = await Promise.all(files.map(async (x) => {
+        const buffer = await fileToBuffer(x)
+        return await Twitter.v1.uploadMedia(Buffer.from(buffer), {type: x.type})
+    }));
     return medias.map((x, i) => ({
-        ...x,
+        media_id: Number(x),
+        media_id_string: x,
         file: files[i],
     }));
 }
