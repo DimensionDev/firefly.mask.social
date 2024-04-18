637590e5c4eb6fd3220538f8cbee2c050492a746
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-04-18 16:15:46 +0800

    chore: disable check & auto complete (#802)
    * chore: disable check & auto complete
    
    * chore: add type

--- src/components/Frame/Input.tsx
+++ src/components/Frame/Input.tsx
@@ -13,6 +13,8 @@ export const Input = forwardRef<HTMLInputElement, Props>(function FrameInput({ i
             ref={ref}
             className="w-full rounded-md border border-line bg-white px-2 py-1.5 dark:bg-darkBottom dark:text-white"
             type="text"
+            autoComplete="off"
+            spellCheck="false"
             placeholder={input.label || t`Type something here...`}
         />
     );
