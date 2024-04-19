6b20c2921d57248fd648500549f1f49bf43977fa
nuanyang233 <nuanyang233@gmail.com>
2024-04-19 15:42:08 +0800

    fix: missing asset in farcaster

--- src/helpers/getResourceType.ts
+++ src/helpers/getResourceType.ts
@@ -2,29 +2,29 @@ import { canParseURL } from '@/helpers/canParseURL.js';
 
 export function getResourceType(urlString: string) {
     let fileExtension;
-
+    let parsedUrl;
     if (typeof window !== 'undefined') {
         if (!canParseURL(urlString)) return;
-        const parsedUrl = new URL(urlString);
-        // TODO Temporary solution for https://mask.atlassian.net/browse/FW-755
-        // cspell:ignore imagedelivery
-        if (parsedUrl.host === 'imagedelivery.net') {
-            return 'Image';
-        }
+        parsedUrl = new URL(urlString);
         fileExtension = parsedUrl.pathname.split('.').pop()?.toLowerCase();
     } else if (typeof require === 'function') {
         const path = require('path');
         const url = require('url');
 
-        const parsedUrl = url.parse(urlString);
+        parsedUrl = url.parse(urlString);
         fileExtension = path.extname(parsedUrl.pathname).slice(1).toLowerCase();
     } else {
         throw new Error('Unsupported environment');
     }
 
+    // TODO Temporary solution for https://mask.atlassian.net/browse/FW-755
+    if (['imagedelivery.net'].includes(parsedUrl.hostname)) {
+        return 'Image';
+    }
+
     if (['jpg', 'jpeg', 'png', 'gif'].includes(fileExtension)) {
         return 'Image';
-    } else if (['mp4', 'webm', 'ogg'].includes(fileExtension)) {
+    } else if (['mp4', 'webm', 'ogg', 'm3u8'].includes(fileExtension)) {
         return 'Video';
     } else if (['mp3'].includes(fileExtension)) {
         return 'Audio';
