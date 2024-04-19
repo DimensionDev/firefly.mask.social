097f2495952d765138f30a1ecc4060b9ed87b773
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-04-19 18:23:35 +0800

    fix: not available chars (#826)

--- src/helpers/getCurrentPostLimits.ts
+++ src/helpers/getCurrentPostLimits.ts
@@ -1,7 +1,13 @@
-import type { SocialPlatform } from '@/constants/enum.js';
+import { SocialPlatform } from '@/constants/enum.js';
 import { DANGER_CHAR_LIMIT, MAX_CHAR_SIZE_PER_POST, SAFE_CHAR_LIMIT } from '@/constants/index.js';
 
 export function getCurrentPostLimits(availableSources: SocialPlatform[]) {
+    if (!availableSources.length)
+        return {
+            MAX_CHAR_SIZE_PER_POST: MAX_CHAR_SIZE_PER_POST[SocialPlatform.Farcaster],
+            DANGER_CHAR_LIMIT: DANGER_CHAR_LIMIT[SocialPlatform.Farcaster],
+            SAFE_CHAR_LIMIT: SAFE_CHAR_LIMIT[SocialPlatform.Farcaster],
+        };
     return {
         MAX_CHAR_SIZE_PER_POST: Math.min(...availableSources.map((x) => MAX_CHAR_SIZE_PER_POST[x])),
         DANGER_CHAR_LIMIT: Math.min(...availableSources.map((x) => DANGER_CHAR_LIMIT[x])),
