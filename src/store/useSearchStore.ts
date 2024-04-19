8f158ab26530d11e7ca6d324712b2f685f77e895
guanbinrui <guanbinrui@dimension.im>
2024-04-19 14:13:17 +0800

    fix: remove keyword

--- src/store/useSearchStore.ts
+++ src/store/useSearchStore.ts
@@ -7,20 +7,13 @@ import { SearchType } from '@/constants/enum.js';
 import { createSelectors } from '@/helpers/createSelector.js';
 
 interface SearchTypeState {
-    searchKeyword: string | undefined;
     searchType: SearchType | undefined;
-    updateSearchKeyword: (keyword: string) => void;
     updateSearchType: (type: SearchType) => void;
 }
 
 const useSearchStateBase = create<SearchTypeState, [['zustand/immer', never]]>(
     immer((set) => ({
-        searchKeyword: undefined,
         searchType: undefined,
-        updateSearchKeyword: (keyword: string) =>
-            set((state) => {
-                state.searchKeyword = keyword;
-            }),
         updateSearchType: (type: SearchType) =>
             set((state) => {
                 state.searchType = type;
@@ -38,7 +31,7 @@ export interface SearchState {
 export function useSearchStateStore() {
     const params = useSearchParams();
     const router = useRouter();
-    const { searchKeyword, searchType, updateSearchKeyword, updateSearchType } = useStore();
+    const { searchType, updateSearchType } = useStore();
 
     const updateState = useCallback(
         (state: SearchState, replace?: boolean) => {
@@ -46,7 +39,6 @@ export function useSearchStateStore() {
 
             if (state.q) {
                 newParams.set('q', state.q);
-                updateSearchKeyword(state.q);
             }
             if (state.type) {
                 newParams.set('type', state.type);
@@ -60,15 +52,14 @@ export function useSearchStateStore() {
             if (replace) router.replace(url);
             else router.push(url);
         },
-        [params, router, updateSearchKeyword, updateSearchType],
+        [params, router, updateSearchType],
     );
 
     return {
         // use ?? means '' is valid value, it was used when clear the search input
-        searchKeyword: searchKeyword ?? (params.get('q') || ''),
-        searchType: searchType || (params.get('type') as SearchType) || SearchType.Users,
+        searchKeyword: params.get('q') || '',
+        searchType: searchType || (params.get('type') as SearchType) || SearchType.Posts,
         updateState,
-        updateSearchKeyword,
         updateSearchType,
     };
 }
