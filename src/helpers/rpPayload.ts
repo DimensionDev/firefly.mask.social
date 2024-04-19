ae816be65037c09e54d0714c960b1c25b42d6473
guanbinrui <guanbinrui@dimension.im>
2024-04-19 14:48:46 +0800

    refactor: code style

--- src/helpers/rpPayload.ts
+++ src/helpers/rpPayload.ts
@@ -1,20 +1,22 @@
 import { RedPacketMetaKey } from '@masknet/plugin-redpacket';
 import type { TypedMessage } from '@masknet/typed-message';
 
+const RP_ENCRYPED_KEY = `${RedPacketMetaKey}:encrypted`;
+
 export function hasRpPayload(message: TypedMessage | null) {
     return message?.meta?.has(RedPacketMetaKey);
 }
 
 export function isRpEncryped(message: TypedMessage | null) {
-    if (hasRpPayload(message)) return message?.meta?.get(`${RedPacketMetaKey}:encrypted`) === true;
+    if (hasRpPayload(message)) return message?.meta?.get(RP_ENCRYPED_KEY) === true;
     return false;
 }
 
-export function updateRpEncrypted<T extends TypedMessage>(message: T | null): T | null {
+export function updateRpEncrypted<T extends TypedMessage>(message: T | null, encryped = true): T | null {
     if (hasRpPayload(message) && message?.meta) {
         return {
             ...message,
-            meta: new Map([[`${RedPacketMetaKey}:encryped`, true], ...(message?.meta?.entries() ?? [])]),
+            meta: new Map([[RP_ENCRYPED_KEY, encryped], ...(message?.meta?.entries() ?? [])]),
         };
     }
     return message;
