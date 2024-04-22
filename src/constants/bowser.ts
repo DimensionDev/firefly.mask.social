ccd0bcd57430610445cbab93dc1d0d85843794ad
nuanyang233 <nuanyang233@gmail.com>
2024-04-22 17:31:44 +0800

    fix: replace bowser (#860)

--- src/constants/bowser.ts
+++ src/constants/bowser.ts
@@ -1,6 +1,6 @@
 'use client';
 
-const bowser = typeof window !== 'undefined' ? window.bowser.getParser(window.navigator.userAgent) : undefined;
+const ua = typeof window !== 'undefined' ? window.navigator.userAgent : undefined;
 
-export const IS_SAFARI = !!bowser?.is('safari');
-export const IS_APPLE = !!bowser?.is('apple');
+export const IS_SAFARI = ua?.toLowerCase().includes('safari');
+export const IS_APPLE = ua?.toLowerCase().includes('apple');
