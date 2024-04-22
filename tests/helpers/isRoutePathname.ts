53e80511aba4e79595c282a951f96288fb5b0814
nuanyang233 <nuanyang233@gmail.com>
2024-04-22 17:39:51 +0800

    fix: test case

--- tests/helpers/isRoutePathname.ts
+++ tests/helpers/isRoutePathname.ts
@@ -5,24 +5,24 @@ import { isRoutePathname } from '@/helpers/isRoutePathname.js';
 describe('isRoutePathname', () => {
     test('should correctly check pathname matching based on criteria', () => {
         const cases = [
-            ['users/123', '/users/123', false],
-            ['/users/123', 'users/123', false],
-            ['/users/123', '/users/123', true],
-            ['/users/123', '/users/123/', true],
-            ['/posts/456', '/users/456/', false],
-            ['/articles/789', '/articles/', true],
-            ['/categories/abc/123', '/categories', true],
-            ['/categories/abc/123', '/categories/', true],
-            ['/settings//', '/settings', true],
-            ['/settings', '/settings//', false],
-            ['/post/:detail', '/post/123', true],
-            ['/post/:detail', '/post', false],
-            ['/post/:detail', '/post/123/photos/123', false],
-            ['/post/:detail/photos/:id', '/post/123/photos/123', true],
-        ] as Array<[`/${string}`, `/${string}`, boolean]>;
+            ['users/123', '/users/123', false, false],
+            ['/users/123', 'users/123', false, false],
+            ['/users/123', '/users/123', false, true],
+            ['/users/123', '/users/123/', false, true],
+            ['/posts/456', '/users/456/', false, false],
+            ['/articles/789', '/articles/', false, true],
+            ['/categories/abc/123', '/categories', false, true],
+            ['/categories/abc/123', '/categories/', false, true],
+            ['/settings//', '/settings', false, true],
+            ['/settings', '/settings//', false, false],
+            ['/post/123', '/post/:detail', false, true],
+            ['/post', '/post/:detail', false, false],
+            ['/post/123/photos/123', '/post/:detail', true, false],
+            ['/post/123/photos/123', '/post/:detail/photos/:id', true, true],
+        ] as Array<[`/${string}`, `/${string}`, boolean, boolean]>;
 
-        cases.forEach(([pathname1, pathname2, expectedResult]) => {
-            const result = isRoutePathname(pathname1, pathname2);
+        cases.forEach(([pathname1, pathname2, exact, expectedResult]) => {
+            const result = isRoutePathname(pathname1, pathname2, exact);
             expect(result).toBe(expectedResult);
         });
     });
