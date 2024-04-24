c6be1a1a3528f9ba41ba447b1367f12d1fe7063b
guanbinrui <guanbinrui@dimension.im>
2024-04-24 20:23:13 +0800

    chore: UI

--- src/modals/SessionRecoveryModal.tsx
+++ src/modals/SessionRecoveryModal.tsx
@@ -1,24 +1,34 @@
 import { t } from '@lingui/macro';
 import type { SingletonModalRefCreator } from '@masknet/shared-base';
-import { forwardRef } from 'react';
+import { forwardRef, useEffect } from 'react';
 import { useAsyncFn } from 'react-use';
 
 import { ConfirmModalRef } from '@/modals/controls.js';
+import type { Session } from '@/providers/types/Session.js';
+import { useSyncSessionStore } from '@/store/useSyncSessionStore.js';
 
 export const SessionRecoveryModal = forwardRef<SingletonModalRefCreator>(function SessionRecoveryModal(_, ref) {
-    const [state, onSessionRecovery] = useAsyncFn(async () => {
+    const { synced } = useSyncSessionStore();
+
+    const [state, onSessionRecovery] = useAsyncFn(async (sessions: Session[]) => {
+        if (!sessions.length) return;
+
         const confirmed = await ConfirmModalRef.openAndWaitForClose({
             title: t`Device Logged In`,
             content: (
                 <div>
-                    <p>{t`One click to connect your account status`}</p>
+                    <p className=" text-main">{t`One click to connect your account status`}</p>
                 </div>
             ),
         });
 
-        if (confirmed) {
-            // do something
-        }
+        if (!confirmed) return;
     }, []);
+
+    useEffect(() => {
+        onSessionRecovery(synced);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [synced]);
+
     return null;
 });
