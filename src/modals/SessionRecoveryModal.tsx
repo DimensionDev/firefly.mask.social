478475f2aa9a40268a39fc6acd93b1acbd1d00da
guanbinrui <guanbinrui@mask.io>
2024-04-24 09:24:49 +0800

    chore: SessionRecoveryModal

---
+++ src/modals/SessionRecoveryModal.tsx
@@ -0,0 +1,7 @@
+import { forwardRef } from 'react';
+
+import type { SingletonModalRefCreator } from '@masknet/shared-base';
+
+export const SessionRecoveryModal = forwardRef<SingletonModalRefCreator>(function SessionRecoveryModal(_, ref) {
+    return null;
+});
