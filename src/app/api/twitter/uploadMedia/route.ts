b5ee89015e1daa5b5b11321160331db2e22a8379
Harlan Sun <33143119+beyond009@users.noreply.github.com>
2024-04-25 11:46:26 +0800

    fix: twitter upload media (#867)
    * fix: twitter upload media
    
    * chore: sync submodule
    
    * chore: prettier
    
    * fix: server error
    
    * fix: server error
    
    * fix: type error
    
    * chore: prettier
    
    * fix: ci
    
    ---------
    
    Co-authored-by: beyond009 <beyond009@users.noreply.github.com>
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>
    Co-authored-by: guanbinrui <guanbinrui@mask.io>

---
+++ src/app/api/twitter/uploadMedia/route.ts
@@ -0,0 +1,21 @@
+import { StatusCodes } from 'http-status-codes';
+import { NextRequest } from 'next/server.js';
+import { TwitterApi } from 'twitter-api-v2';
+
+import { createErrorResponseJSON } from '@/helpers/createErrorResponseJSON.js';
+import { createSuccessResponseJSON } from '@/helpers/createSuccessResponseJSON.js';
+
+export async function POST(request: NextRequest) {
+    try {
+        const client = new TwitterApi();
+        const formData = await request.formData();
+        const file = formData.get('file') as File;
+        const buffer = Buffer.from(await file.arrayBuffer());
+        const res = client.v1.uploadMedia(buffer, { type: file.type });
+        return createSuccessResponseJSON({ media_id: res }, { status: StatusCodes.OK });
+    } catch (error) {
+        return createErrorResponseJSON(error instanceof Error ? error.message : 'Internal Server Error', {
+            status: StatusCodes.INTERNAL_SERVER_ERROR,
+        });
+    }
+}
--- src/services/uploadToTwitter.ts
+++ src/services/uploadToTwitter.ts
@@ -1,7 +1,18 @@
-import { Twitter } from '@masknet/web3-providers';
+import { TwitterBaseAPI } from '@masknet/web3-providers/types';
+
+import { fetchJSON } from '@/helpers/fetchJSON.js';
 
 export async function uploadToTwitter(files: File[]) {
-    const medias = await Promise.all(files.map((x) => Twitter.uploadMedia(x)));
+    const medias = await Promise.all(
+        files.map((x) => {
+            const formData = new FormData();
+            formData.append('file', x);
+            return fetchJSON<TwitterBaseAPI.MediaResponse>('/api/twitter/uploadMedia', {
+                method: 'POST',
+                body: formData,
+            });
+        }),
+    );
     return medias.map((x, i) => ({
         ...x,
         file: files[i],
