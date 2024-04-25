da99ca75c159787452e366236521ff9c260e20fe
Harlan Sun <1249413181@qq.com>
2024-04-25 10:26:49 +0800

    fix: twitter upload media

---
+++ src/app/api/twitter/uploadMedia/route.ts
@@ -0,0 +1,19 @@
+import { StatusCodes } from 'http-status-codes';
+import { NextRequest } from 'next/server.js';
+
+import { createErrorResponseJSON } from '@/helpers/createErrorResponseJSON.js';
+import { createSuccessResponseJSON } from '@/helpers/createSuccessResponseJSON.js';
+import { Twitter } from '@masknet/web3-providers';
+
+export async function POST(request: NextRequest) {
+    try {
+        const formData = await request.formData()
+        const file = formData.get("file") as File;
+        const res = Twitter.uploadMedia(file);
+        return createSuccessResponseJSON(res, { status: StatusCodes.OK });
+    } catch (error) {
+        return createErrorResponseJSON(error instanceof Error ? error.message : 'Internal Server Error', {
+            status: StatusCodes.INTERNAL_SERVER_ERROR,
+        });
+    }
+}
--- src/services/uploadToTwitter.ts
+++ src/services/uploadToTwitter.ts
@@ -1,7 +1,15 @@
-import { Twitter } from '@masknet/web3-providers';
+import { fetchJSON } from '@/helpers/fetchJSON.js';
+import { TwitterBaseAPI } from '@/maskbook/packages/web3-providers/src/entry-types.js';
 
 export async function uploadToTwitter(files: File[]) {
-    const medias = await Promise.all(files.map((x) => Twitter.uploadMedia(x)));
+    const medias = await Promise.all(files.map((x) => {
+        const formData = new FormData();
+        formData.append('file', x);
+        return fetchJSON<TwitterBaseAPI.MediaResponse>('/api/twitter/uploadMedia', {
+            method: 'POST',
+            body: formData,
+        });
+    }));
     return medias.map((x, i) => ({
         ...x,
         file: files[i],
