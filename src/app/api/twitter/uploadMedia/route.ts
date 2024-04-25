3ee82fac84cd01f84b8054a44054e8290cc04aea
Harlan Sun <1249413181@qq.com>
2024-04-25 19:51:47 +0800

    fix: type

--- src/app/api/twitter/uploadMedia/route.ts
+++ src/app/api/twitter/uploadMedia/route.ts
@@ -6,8 +6,6 @@ import { createErrorResponseJSON } from '@/helpers/createErrorResponseJSON.js';
 import { createSuccessResponseJSON } from '@/helpers/createSuccessResponseJSON.js';
 import { createTwitterClientV2 } from '@/helpers/createTwitterClientV2.js';
 
-export const config = { runtime: 'experimental-edge' };
-
 export async function POST(request: NextRequest) {
     try {
         const client = await createTwitterClientV2(request);
