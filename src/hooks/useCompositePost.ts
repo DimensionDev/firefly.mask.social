9b036b34296ef59355e6f48c3c612e7d3fe93ad0
guanbinrui <guanbinrui@mask.io>
2024-04-26 17:26:25 +0800

    fix: warning

--- src/hooks/useCompositePost.ts
+++ src/hooks/useCompositePost.ts
@@ -6,5 +6,6 @@ import { useComposeStateStore } from '@/store/useComposeStore.js';
 export function useCompositePost(id?: string) {
     const { cursor, posts } = useComposeStateStore();
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     return useMemo(() => getCompositePost(id ?? cursor)!, [id, cursor, posts]);
 }
