b4a70551835eeb85d133bdf673d716caaf681a39
LeifXu <LeifXu@users.noreply.github.com>
2024-04-28 08:22:07 +0000

    chore: prettier

--- src/services/uploadToS3.ts
+++ src/services/uploadToS3.ts
@@ -1,10 +1,10 @@
 export async function uploadToS3(file: File): Promise<string> {
-    const formData = new FormData()
-    formData.append('file', file)
+    const formData = new FormData();
+    formData.append('file', file);
     const response = await fetch('/api/s3', {
         method: 'PUT',
         body: formData,
-    })
-    const data = await response.json() as { link: string }
-    return data.link
+    });
+    const data = (await response.json()) as { link: string };
+    return data.link;
 }
