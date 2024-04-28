bee9c3806e6a7413a21464368375cd5bfce35f7e
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-04-28 21:38:24 +0800

    refactor: code style (#904)

--- src/services/uploadToS3.ts
+++ src/services/uploadToS3.ts
@@ -1,10 +1,13 @@
+import { fetchJSON } from '@/helpers/fetchJSON.js';
+import type { ResponseJSON } from '@/types/index.js';
+
 export async function uploadToS3(file: File): Promise<string> {
     const formData = new FormData();
     formData.append('file', file);
-    const response = await fetch('/api/s3', {
+    const response = await fetchJSON<ResponseJSON<{ link: string }>>('/api/s3', {
         method: 'PUT',
         body: formData,
     });
-    const data = (await response.json()) as { link: string };
-    return data.link;
+    if (!response.success) throw new Error(response.error.message);
+    return response.data.link;
 }
