803168360935493a89bed69bbe763a3d26d43a7e
LeifXu <167842553+LeifXu@users.noreply.github.com>
2024-04-29 16:29:36 +0800

    fix: upload file to s3 without default content type (#914)

--- src/services/uploadToS3.ts
+++ src/services/uploadToS3.ts
@@ -4,10 +4,16 @@ import type { ResponseJSON } from '@/types/index.js';
 export async function uploadToS3(file: File): Promise<string> {
     const formData = new FormData();
     formData.append('file', file);
-    const response = await fetchJSON<ResponseJSON<{ link: string }>>('/api/s3', {
-        method: 'PUT',
-        body: formData,
-    });
+    const response = await fetchJSON<ResponseJSON<{ link: string }>>(
+        '/api/s3',
+        {
+            method: 'PUT',
+            body: formData,
+        },
+        {
+            noDefaultContentType: true,
+        },
+    );
     if (!response.success) throw new Error(response.error.message);
     return response.data.link;
 }
