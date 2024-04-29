f3c336f56386c88a838147015dc4917725500afb
LeifXu <LeifXu@users.noreply.github.com>
2024-04-29 08:18:14 +0000

    chore: prettier

--- src/services/uploadToS3.ts
+++ src/services/uploadToS3.ts
@@ -4,12 +4,16 @@ import type { ResponseJSON } from '@/types/index.js';
 export async function uploadToS3(file: File): Promise<string> {
     const formData = new FormData();
     formData.append('file', file);
-    const response = await fetchJSON<ResponseJSON<{ link: string }>>('/api/s3', {
-        method: 'PUT',
-        body: formData,
-    }, {
-        noDefaultContentType: true
-    });
+    const response = await fetchJSON<ResponseJSON<{ link: string }>>(
+        '/api/s3',
+        {
+            method: 'PUT',
+            body: formData,
+        },
+        {
+            noDefaultContentType: true,
+        },
+    );
     if (!response.success) throw new Error(response.error.message);
     return response.data.link;
 }
