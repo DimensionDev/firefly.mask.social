06395efd39786ee92ef710f39de5f8b732363064
guanbinrui <guanbinrui@users.noreply.github.com>
2024-04-30 09:36:09 +0000

    chore: prettier

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,14 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://4162665492eff33477d22a63cf6585d9@o1338713.ingest.us.sentry.io/4507174642843648",
+    dsn: 'https://4162665492eff33477d22a63cf6585d9@o1338713.ingest.us.sentry.io/4507174642843648',
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 });
--- src/app/global-error.jsx
+++ src/app/global-error.jsx
@@ -1,19 +1,19 @@
-"use client";
+'use client';
 
-import * as Sentry from "@sentry/nextjs";
-import Error from "next/error";
-import { useEffect } from "react";
+import * as Sentry from '@sentry/nextjs';
+import Error from 'next/error';
+import { useEffect } from 'react';
 
 export default function GlobalError({ error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+    useEffect(() => {
+        Sentry.captureException(error);
+    }, [error]);
 
-  return (
-    <html>
-      <body>
-        <Error />
-      </body>
-    </html>
-  );
+    return (
+        <html>
+            <body>
+                <Error />
+            </body>
+        </html>
+    );
 }
