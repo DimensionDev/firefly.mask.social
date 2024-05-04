6aa614877eebc0f51eb120ecf8f8927eae043f10
nuanyang233 <nuanyang233@gmail.com>
2024-05-04 16:03:46 +0800

    fix: hidden footer when list initial render

--- src/components/ListInPage.tsx
+++ src/components/ListInPage.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { UseSuspenseInfiniteQueryResult } from '@tanstack/react-query';
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import type { Components } from 'react-virtuoso';
 
 import { NoResultsFallback, type NoResultsFallbackProps } from '@/components/NoResultsFallback.js';
@@ -26,6 +26,7 @@ export function ListInPage<T = unknown>({
     VirtualListProps,
     NoResultsFallbackProps,
 }: ListInPageProps<T>) {
+    const itemsRendered = useRef(false);
     const currentSource = useGlobalState.use.currentSource();
     const isLogin = useIsLogin(currentSource);
 
@@ -56,6 +57,7 @@ export function ListInPage<T = unknown>({
         hasNextPage,
         fetchNextPage,
         isFetching,
+        itemsRendered: itemsRendered.current,
         ...(VirtualListProps?.context ?? {}),
     };
 
@@ -64,6 +66,11 @@ export function ListInPage<T = unknown>({
             useWindowScroll
             data={data}
             endReached={onEndReached}
+            itemSize={(el: HTMLElement) => {
+                if (!itemsRendered.current) itemsRendered.current = true;
+
+                return el.getBoundingClientRect().height;
+            }}
             {...VirtualListProps}
             context={Context}
             components={Components}
--- src/components/VirtualList/VirtualListFooter.tsx
+++ src/components/VirtualList/VirtualListFooter.tsx
@@ -9,6 +9,7 @@ interface VirtualListFooterProps {
         hasNextPage?: boolean;
         fetchNextPage?: () => Promise<void>;
         isFetching?: boolean;
+        itemsRendered: boolean;
     };
 }
 export const VirtualListFooter = memo<VirtualListFooterProps>(function VirtualListFooter({ context }) {
@@ -32,6 +33,9 @@ export const VirtualListFooter = memo<VirtualListFooterProps>(function VirtualLi
                 <Trans>You&apos;ve hit rock bottom.</Trans>
             </div>
         );
+
+    if (!context.itemsRendered) return null;
+    
     return (
         <div className="flex items-center justify-center p-2" ref={observe}>
             <LoadingIcon width={16} height={16} className="animate-spin" />
