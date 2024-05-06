20d5d905420076a6a48e751860cb9d067c6e27e0
LeifXu <167842553+LeifXu@users.noreply.github.com>
2024-05-06 15:50:57 +0800

    feat: twitter timeline and profile (#996)
    * feat: add the user timeline api from `twitter`
    
    * chore: prettier
    
    * feat: hidden views when source is  twitter post
    
    * feat: X api Paging and parameter validate
    
    * feat: add some twitter api
    
    * chore: prettier
    
    * feat: hidden twitter tab
    
    * chore: prettier
    
    * feat: update text `src/components/Actions/Mirrors.tsx`
    
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>
    
    * chore: lingui extract
    
    * feat: twitter api error handle
    
    * chore: prettier
    
    * fix: adjust code about find root post
    
    * fix: fix twitter post root error
    
    * fix: remove console
    
    * chore: prettier
    
    * feat: add words `Paginable` `unretweet` to `cspell.json`
    
    ---------
    
    Co-authored-by: LeifXu <LeifXu@users.noreply.github.com>
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>

---
+++ src/helpers/withRequestErrorHandler.ts
@@ -0,0 +1,40 @@
+import { StatusCodes } from 'http-status-codes';
+import type { NextRequest } from 'next/server.js';
+import { ZodError } from 'zod';
+
+import { createErrorResponseJSON } from '@/helpers/createErrorResponseJSON.js';
+import { handleZodErrorMessage } from '@/helpers/handleZodErrorMessage.js';
+
+export class UnauthorizedError extends Error {
+    constructor(message?: string) {
+        super(message ?? 'Unauthorized');
+    }
+}
+
+export function withRequestErrorHandler(options?: { throwError?: boolean }) {
+    const { throwError = false } = options ?? {};
+    return (handler: (request: NextRequest, ...other: any[]) => Promise<Response>) => {
+        return async (request: NextRequest, ...other: any[]) => {
+            try {
+                return await handler(request, ...other);
+            } catch (error) {
+                if (error instanceof ZodError) {
+                    return createErrorResponseJSON(handleZodErrorMessage(error), {
+                        status: StatusCodes.BAD_REQUEST,
+                    });
+                }
+                if (error instanceof UnauthorizedError) {
+                    return createErrorResponseJSON(error.message, {
+                        status: StatusCodes.UNAUTHORIZED,
+                    });
+                }
+                if (!throwError) {
+                    return createErrorResponseJSON(error instanceof Error ? error.message : 'Internal Server Error', {
+                        status: StatusCodes.INTERNAL_SERVER_ERROR,
+                    });
+                }
+                throw error;
+            }
+        };
+    };
+}
