9c14587a02c7c5aa4343d7f63abd8edda2cfac7a
guanbinrui <guanbinrui@mask.io>
2024-05-09 01:20:24 +0800

    Revert "refactor: remove space between menu items"
    This reverts commit 08f1d96a902314940c9cacd0f83c4df51bb9035c.

--- src/components/Actions/ReportUserButton.tsx
+++ src/components/Actions/ReportUserButton.tsx
@@ -22,7 +22,7 @@ export const ReportUserButton = forwardRef<HTMLButtonElement, Props>(function Re
 ) {
     return (
         <ClickableButton
-            className={classNames('flex cursor-pointer items-center p-4 hover:bg-bg', className)}
+            className={classNames('flex cursor-pointer items-center space-x-2 p-4 hover:bg-bg', className)}
             {...rest}
             onClick={async () => {
                 rest.onClick?.();
