00436f0ef6134830e9172f6aa39ffbd93897a0c9
guanbinrui <guanbinrui@mask.io>
2024-05-09 00:02:22 +0800

    Revert "refactor: switch position"
    This reverts commit e36261466c507a1c1462bd4c8056a24bf291bfe3.

--- src/app/(normal)/layout.tsx
+++ src/app/(normal)/layout.tsx
@@ -73,13 +73,12 @@ export default function Layout({ children, modal }: { children: React.ReactNode;
                 <IfPathname isOneOf={['/search']}>
                     <SearchFilter />
                 </IfPathname>
-
                 <div className="mt-6">
-                    <mask-calendar-widget />
+                    <SuggestedChannels source={Source.Farcaster} />
                 </div>
 
                 <div className="mt-6">
-                    <SuggestedChannels source={Source.Farcaster} />
+                    <mask-calendar-widget />
                 </div>
             </aside>
             <ComposeButton />
