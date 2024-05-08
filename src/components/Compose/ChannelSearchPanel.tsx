56d3f98842309a7cf07615b0037e983ae2a9a481
guanbinrui <guanbinrui@mask.io>
2024-05-08 11:47:46 +0800

    fix: search box

--- src/components/Compose/ChannelSearchPanel.tsx
+++ src/components/Compose/ChannelSearchPanel.tsx
@@ -2,13 +2,12 @@ import { Popover, Transition } from '@headlessui/react';
 import { Trans } from '@lingui/macro';
 import { Fragment, useState } from 'react';
 
+import DisableNoIcon from '@/assets/disable-no.svg';
 import LoadingIcon from '@/assets/loading.svg';
-import RadioDisableNoIcon from '@/assets/radio.disable-no.svg';
 import SearchIcon from '@/assets/search.svg';
 import YesIcon from '@/assets/yes.svg';
 import { Avatar } from '@/components/Avatar.js';
 import { SearchInput } from '@/components/Search/SearchInput.js';
-import { SOURCES_WITH_CHANNEL_SUPPORT } from '@/constants/channel.js';
 import { classNames } from '@/helpers/classNames.js';
 import { isSameChannel } from '@/helpers/isSameChannel.js';
 import { useCompositePost } from '@/hooks/useCompositePost.js';
@@ -22,51 +21,69 @@ export function ChannelSearchPanel() {
 
     const { data, isLoading, isError } = useSearchChannels(inputText);
 
-    const ListBox = isLoading ? (
-        <div className="m-auto">
-            <LoadingIcon className="animate-spin" width={24} height={24} />
+    const InputBox = (
+        <div className="relative flex flex-grow items-center rounded-xl bg-lightBg px-3 text-main">
+            <SearchIcon width={18} height={18} className="shrink-0" />
+            <SearchInput
+                value={inputText}
+                onChange={(ev) => setInputText(ev.target.value)}
+                onClear={() => setInputText('')}
+            />
         </div>
-    ) : isError ? (
-        <div className="m-auto">
-            <Trans>Something went wrong. Please try again.</Trans>
+    );
+    const ListBox = (
+        <div className=" flex max-h-[280px] flex-col gap-2 overflow-auto px-1">
+            {isLoading ? (
+                <div className="m-auto flex h-[100px] items-center justify-center text-center text-sm text-main">
+                    <LoadingIcon className="animate-spin" width={24} height={24} />
+                </div>
+            ) : isError ? (
+                <div className="m-auto flex h-[100px] items-center justify-center text-center text-sm text-main">
+                    <Trans>Something went wrong. Please try again.</Trans>
+                </div>
+            ) : !data?.length ? (
+                <div className="m-auto flex h-[100px] items-center justify-center text-center text-sm text-main">
+                    <Trans>There is no data available for display.</Trans>
+                </div>
+            ) : (
+                data?.map((channel) => {
+                    const isSelected = isSameChannel(channel, selectedChannel[channel.source]);
+                    return (
+                        <Fragment key={channel.id}>
+                            <div
+                                className="flex h-[32px] cursor-pointer items-center justify-between"
+                                onClick={() => {
+                                    if (!isSelected) {
+                                        updateChannel(channel.source, channel);
+                                    }
+                                }}
+                            >
+                                <div className="flex h-[24px] items-center gap-2">
+                                    <Avatar
+                                        className="mr-3 shrink-0 rounded-full border "
+                                        src={channel.imageUrl}
+                                        size={24}
+                                        alt={channel.name}
+                                    />
+                                    <span
+                                        className={classNames('font-bold', isSelected ? 'text-main' : 'text-secondary')}
+                                    >
+                                        {channel.name}
+                                    </span>
+                                </div>
+                                {isSelected ? (
+                                    <YesIcon width={40} height={40} />
+                                ) : (
+                                    <div className="flex h-10 w-10 items-center justify-center">
+                                        <DisableNoIcon width={20} height={20} />
+                                    </div>
+                                )}
+                            </div>
+                        </Fragment>
+                    );
+                })
+            )}
         </div>
-    ) : !data?.length ? (
-        <div className="m-auto">
-            <Trans>There is no data available for display.</Trans>
-        </div>
-    ) : (
-        data?.map((channel) => {
-            const isSelected = isSameChannel(channel, selectedChannel[channel.source]);
-            return (
-                <Fragment key={channel.id}>
-                    <div
-                        className="flex h-[32px] cursor-pointer items-center justify-between"
-                        onClick={() => {
-                            if (!isSelected) {
-                                updateChannel(channel.source, channel);
-                            }
-                        }}
-                    >
-                        <div className="flex h-[24px] items-center gap-2">
-                            <Avatar
-                                className="mr-3 shrink-0 rounded-full border "
-                                src={channel.imageUrl}
-                                size={24}
-                                alt={channel.name}
-                            />
-                            <span className={classNames('font-bold', isSelected ? 'text-main' : 'text-secondary')}>
-                                {channel.name}
-                            </span>
-                        </div>
-                        {isSelected ? (
-                            <YesIcon width={40} height={40} className=" relative -right-[10px]" />
-                        ) : (
-                            <RadioDisableNoIcon width={20} height={20} className=" text-secondaryLine" />
-                        )}
-                    </div>
-                </Fragment>
-            );
-        })
     );
 
     return (
@@ -80,15 +97,8 @@ export function ChannelSearchPanel() {
             leaveTo="opacity-0 translate-y-1"
         >
             <Popover.Panel className=" absolute bottom-full right-0 flex w-[320px] -translate-y-3 flex-col gap-2 rounded-lg bg-bgModal p-3 text-[15px] shadow-popover dark:border dark:border-line dark:shadow-none">
-                <div className="relative flex flex-grow items-center rounded-xl bg-lightBg px-3 text-main">
-                    <SearchIcon width={18} height={18} className="shrink-0" />
-                    <SearchInput
-                        value={inputText}
-                        onChange={(ev) => setInputText(ev.target.value)}
-                        onClear={() => setInputText('')}
-                    />
-                </div>
-                <div className="channel-list flex min-h-[392px] flex-col gap-2">{ListBox}</div>
+                {InputBox}
+                {ListBox}
             </Popover.Panel>
         </Transition>
     );
