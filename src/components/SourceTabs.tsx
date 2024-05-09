c930f58764dee0d389ddaa0ec1d3ab5565bcaeff
Wukong Sun <swkatmask@gmail.com>
2024-05-09 16:11:47 +0800

    fix: fw-956 bookmark requires login

--- src/components/SourceTabs.tsx
+++ src/components/SourceTabs.tsx
@@ -4,7 +4,7 @@ import { usePathname, useSearchParams } from 'next/navigation.js';
 import { startTransition } from 'react';
 
 import { PageRoute, SearchType, Source } from '@/constants/enum.js';
-import { SORTED_HOME_SOURCES } from '@/constants/index.js';
+import { SORTED_HOME_SOURCES, SORTED_SOCIAL_SOURCES } from '@/constants/index.js';
 import { classNames } from '@/helpers/classNames.js';
 import { replaceSearchParams } from '@/helpers/replaceSearchParams.js';
 import { resolveSourceInURL } from '@/helpers/resolveSourceInURL.js';
@@ -44,51 +44,58 @@ export function SourceTabs() {
         );
     }
 
+    const sources = pathname === '/bookmark' ? SORTED_SOCIAL_SOURCES : SORTED_HOME_SOURCES;
+
     return (
         <div className="border-b border-line bg-primaryBottom px-4">
             <nav className="scrollable-tab -mb-px flex space-x-4" aria-label="Tabs">
-                {SORTED_HOME_SOURCES.filter((x) => {
-                    if (x !== Source.Article) return true;
-                    if (pathname === PageRoute.Home || (pathname === PageRoute.Following && !!fireflySession))
-                        return true;
-                    return false;
-                }).map((value) => (
-                    <li key={value} className="flex flex-1 list-none justify-center lg:flex-initial lg:justify-start">
-                        <a
-                            className={classNames(
-                                currentSource === value ? 'border-b-2 border-[#9250FF] text-main' : 'text-third',
-                                'h-[43px] px-4 text-center text-xl font-bold leading-[43px] hover:cursor-pointer hover:text-main',
-                                'md:h-[60px] md:py-[18px] md:leading-6',
-                            )}
-                            aria-current={currentSource === value ? 'page' : undefined}
-                            onClick={() =>
-                                startTransition(() => {
-                                    scrollTo(0, 0);
-                                    updateCurrentSource(value);
-                                    const type = searchParams.get('type') as SearchType;
-
-                                    if (type === SearchType.Channels && value === Source.Lens) {
-                                        updateSearchType(SearchType.Posts);
-                                        replaceSearchParams(
-                                            new URLSearchParams({
-                                                source: resolveSourceInURL(value),
-                                                type: SearchType.Posts,
-                                            }),
-                                        );
-                                    } else {
-                                        replaceSearchParams(
-                                            new URLSearchParams({
-                                                source: resolveSourceInURL(value),
-                                            }),
-                                        );
-                                    }
-                                })
-                            }
+                {sources
+                    .filter((x) => {
+                        if (x !== Source.Article) return true;
+                        if (pathname === PageRoute.Home || (pathname === PageRoute.Following && !!fireflySession))
+                            return true;
+                        return false;
+                    })
+                    .map((value) => (
+                        <li
+                            key={value}
+                            className="flex flex-1 list-none justify-center lg:flex-initial lg:justify-start"
                         >
-                            {resolveSourceName(value)}
-                        </a>
-                    </li>
-                ))}
+                            <a
+                                className={classNames(
+                                    currentSource === value ? 'border-b-2 border-[#9250FF] text-main' : 'text-third',
+                                    'h-[43px] px-4 text-center text-xl font-bold leading-[43px] hover:cursor-pointer hover:text-main',
+                                    'md:h-[60px] md:py-[18px] md:leading-6',
+                                )}
+                                aria-current={currentSource === value ? 'page' : undefined}
+                                onClick={() =>
+                                    startTransition(() => {
+                                        scrollTo(0, 0);
+                                        updateCurrentSource(value);
+                                        const type = searchParams.get('type') as SearchType;
+
+                                        if (type === SearchType.Channels && value === Source.Lens) {
+                                            updateSearchType(SearchType.Posts);
+                                            replaceSearchParams(
+                                                new URLSearchParams({
+                                                    source: resolveSourceInURL(value),
+                                                    type: SearchType.Posts,
+                                                }),
+                                            );
+                                        } else {
+                                            replaceSearchParams(
+                                                new URLSearchParams({
+                                                    source: resolveSourceInURL(value),
+                                                }),
+                                            );
+                                        }
+                                    })
+                                }
+                            >
+                                {resolveSourceName(value)}
+                            </a>
+                        </li>
+                    ))}
             </nav>
         </div>
     );
