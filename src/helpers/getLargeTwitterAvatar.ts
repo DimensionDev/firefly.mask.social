f5e4f9da561f82f496ae246888a4b384d5f71160
nuanyang233 <nuanyang233@gmail.com>
2024-05-11 02:56:43 +0800

    chore: ci

--- src/helpers/getLargeTwitterAvatar.ts
+++ src/helpers/getLargeTwitterAvatar.ts
@@ -1,6 +1,6 @@
-import { TWITTER_NORML_AVATAR } from '@/constants/regexp.js';
+import { TWITTER_NORMAL_AVATAR } from '@/constants/regexp.js';
 
 export function getLargeTwitterAvatar(url: string) {
-    if (!TWITTER_NORML_AVATAR.test(url)) return url;
+    if (!TWITTER_NORMAL_AVATAR.test(url)) return url;
     return url.replace(/_normal(\.\w+)$/, '_400x400$1');
 }
