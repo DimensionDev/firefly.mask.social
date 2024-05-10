9128fe3d471d717e45fd03e1341da772275335dc
Wukong Sun <swkatmask@gmail.com>
2024-05-10 16:40:34 +0800

    fix(Twitter): hide bookmark for now

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -56,12 +56,10 @@ export const SORTED_SEARCH_TYPE: Record<SocialSource, SearchType[]> = {
 export const SORTED_HOME_SOURCES = [Source.Farcaster, Source.Lens, Source.Article] as const;
 export const SORTED_PROFILE_SOURCES = [Source.Farcaster, Source.Lens, Source.Twitter, Source.Wallet];
 export const SORTED_SOCIAL_SOURCES = [Source.Farcaster, Source.Lens, Source.Twitter] as const;
-export const SORTED_BOOKMARK_SOURCES = compact([
-    Source.Farcaster,
-    Source.Lens,
-    Source.Twitter,
-    env.shared.NODE_ENV === 'development' ? Source.Article : null,
-]);
+export const SORTED_BOOKMARK_SOURCES =
+    env.shared.NODE_ENV === 'development'
+        ? [Source.Farcaster, Source.Lens, Source.Twitter, Source.Article]
+        : [Source.Farcaster, Source.Lens];
 export const SORTED_CHANNEL_SOURCES: SocialSource[] = [Source.Farcaster];
 export const SORTED_RESTECTION_TYPE = [RestrictionType.Everyone, RestrictionType.OnlyPeopleYouFollow];
 
