a25f7c81fdbac1a6fc31fbc1184b2cd658f85192
guanbinrui <guanbinrui@mask.io>
2024-05-10 19:49:26 +0800

    fix: type error

--- src/app/(normal)/bookmarks/page.tsx
+++ src/app/(normal)/bookmarks/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { Suspense } from 'react';
 
-import { ArticleBookmarkList } from '@/app/(normal)/bookmark/ArticleBookmarkList.js';
-import { BookmarkList } from '@/app/(normal)/bookmark/BookmarkList.js';
+import { ArticleBookmarkList } from '@/app/(normal)/bookmarks/ArticleBookmarkList.js';
+import { BookmarkList } from '@/app/(normal)/bookmarks/BookmarkList.js';
 import { Loading } from '@/components/Loading.js';
 import { type SocialSource, Source } from '@/constants/enum.js';
 import { useGlobalState } from '@/store/useGlobalStore.js';
