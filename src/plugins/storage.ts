03314c9c4400cfb9056d1ce0786740fc290ea1aa
dudu0506 <dudu0506@users.noreply.github.com>
2024-05-14 10:14:05 +0000

    chore: prettier

--- src/plugins/storage.ts
+++ src/plugins/storage.ts
@@ -3,66 +3,66 @@ const ls = typeof window === 'undefined' ? undefined : window.localStorage;
 const cacheFileName = 'opfs-storage-v1.txt';
 
 const getOPFSAccessHandle = async () => {
-  if (!hasOPFS) throw new Error('OPFS is not supported');
-  const opfsRoot = await navigator.storage.getDirectory();
-  const fileHandle = await opfsRoot.getFileHandle(cacheFileName, { create: true });
-  return fileHandle
-}
+    if (!hasOPFS) throw new Error('OPFS is not supported');
+    const opfsRoot = await navigator.storage.getDirectory();
+    const fileHandle = await opfsRoot.getFileHandle(cacheFileName, { create: true });
+    return fileHandle;
+};
 
 const writeToOPFS = async (content: string) => {
-  const accessHandle = await getOPFSAccessHandle();
+    const accessHandle = await getOPFSAccessHandle();
 
-  const encoder = new TextEncoder();
-  const encodedMessage = encoder.encode(content);
-  const writeBuffer = await accessHandle.createWritable();
-  await writeBuffer.write(encodedMessage);
+    const encoder = new TextEncoder();
+    const encodedMessage = encoder.encode(content);
+    const writeBuffer = await accessHandle.createWritable();
+    await writeBuffer.write(encodedMessage);
 
-  await writeBuffer.close();
-}
+    await writeBuffer.close();
+};
 const readFromOPFS = async () => {
-  const accessHandle = await getOPFSAccessHandle();
+    const accessHandle = await getOPFSAccessHandle();
 
-  const file = await accessHandle.getFile();
-  const fileContents = await file.text();
-  return fileContents;
-}
+    const file = await accessHandle.getFile();
+    const fileContents = await file.text();
+    return fileContents;
+};
 
 // Cache private data in OPFS. Such as user token, user info, etc.
 export class OPFSStorageProvider {
-  static async setItem(key: string, value: string) {
-    try {
-      const content = await readFromOPFS();
-      const data = JSON.parse(content || '{}');
-      data[key] = value;
-      await writeToOPFS(JSON.stringify(data));
-    } catch {
-      return ls?.setItem(key, value);
+    static async setItem(key: string, value: string) {
+        try {
+            const content = await readFromOPFS();
+            const data = JSON.parse(content || '{}');
+            data[key] = value;
+            await writeToOPFS(JSON.stringify(data));
+        } catch {
+            return ls?.setItem(key, value);
+        }
     }
-  }
-  static async getItem(key: string) {
-    try {
-      const content = await readFromOPFS();
-      const data = JSON.parse(content || '{}');
-      return data[key] ?? null;
-    } catch {
-      return ls?.getItem(key);
+    static async getItem(key: string) {
+        try {
+            const content = await readFromOPFS();
+            const data = JSON.parse(content || '{}');
+            return data[key] ?? null;
+        } catch {
+            return ls?.getItem(key);
+        }
     }
-  }
-  static async removeItem(key: string) {
-    try {
-      const content = await readFromOPFS();
-      const data = JSON.parse(content || '{}');
-      delete data[key];
-      await writeToOPFS(JSON.stringify(data));
-    } catch {
-      return ls?.removeItem(key);
+    static async removeItem(key: string) {
+        try {
+            const content = await readFromOPFS();
+            const data = JSON.parse(content || '{}');
+            delete data[key];
+            await writeToOPFS(JSON.stringify(data));
+        } catch {
+            return ls?.removeItem(key);
+        }
     }
-  }
-  static async clear() {
-    try {
-      await writeToOPFS('{}');
-    } catch {
-      return ls?.clear();
+    static async clear() {
+        try {
+            await writeToOPFS('{}');
+        } catch {
+            return ls?.clear();
+        }
     }
-  }
 }
