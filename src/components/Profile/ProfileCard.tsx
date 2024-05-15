f33077b0dd9e9f4713bdc91ad26c7c5d669c9d57
nuanyang233 <nuanyang233@gmail.com>
2024-05-15 18:30:00 +0800

    feat: profile card (#1171)
    * feat: profile card
    
    * chore: lingui extract
    
    ---------
    
    Co-authored-by: nuanyang233 <nuanyang233@users.noreply.github.com>

---
+++ src/components/Profile/ProfileCard.tsx
@@ -0,0 +1,121 @@
+import { plural, Trans } from '@lingui/macro';
+import { memo } from 'react';
+
+import { Avatar } from '@/components/Avatar.js';
+import { ClickableArea } from '@/components/ClickableArea.js';
+import { BioMarkup } from '@/components/Markup/BioMarkup.js';
+import { FollowButton } from '@/components/Profile/FollowButton.js';
+import { SocialSourceIcon } from '@/components/SocialSourceIcon.js';
+import { Source } from '@/constants/enum.js';
+import { Link } from '@/esm/Link.js';
+import { classNames } from '@/helpers/classNames.js';
+import { nFormatter } from '@/helpers/formatCommentCounts.js';
+import { getProfileUrl } from '@/helpers/getProfileUrl.js';
+import { resolveSourceInURL } from '@/helpers/resolveSourceInURL.js';
+import type { Profile } from '@/providers/types/SocialMedia.js';
+
+interface ProfileCardProps {
+    profile?: Profile;
+    loading?: boolean;
+}
+
+export const ProfileCard = memo<ProfileCardProps>(function ProfileCard({ profile, loading }) {
+    if (!profile) return null;
+
+    const url = getProfileUrl(profile);
+
+    if (loading) {
+        return (
+            <div className="h-[182px] w-[400px] rounded-2xl border border-secondaryLine bg-primaryBottom p-6">
+                <div className="animate-pulse">
+                    <div className="flex w-full gap-[10px]">
+                        <div className="h-20 w-20 rounded-full bg-slate-700" />
+                        <div className="flex flex-1 flex-col justify-between">
+                            <div className="h-3 w-[120px] rounded bg-slate-700" />
+                            <div className="h-3 w-[120px] rounded bg-slate-700" />
+                            <div className="h-3 w-[120px] rounded bg-slate-700" />
+                        </div>
+                    </div>
+                    <div className="mt-3 space-y-4">
+                        <div className="h-3 w-full rounded bg-slate-700" />
+                        <div className="h-3 w-full rounded bg-slate-700" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <ClickableArea className="flex  h-[238px] w-[400px] flex-col gap-y-3 rounded-2xl border border-secondaryLine bg-primaryBottom p-6">
+            <div className="flex gap-[10px]">
+                <Link href={url}>
+                    <Avatar
+                        src={profile.pfp}
+                        alt="avatar"
+                        size={80}
+                        className="h-20 w-20 cursor-pointer rounded-full"
+                    />
+                </Link>
+
+                <div className="flex flex-1 flex-col justify-between">
+                    <div className=" flex items-center gap-2">
+                        <Link href={url} className="cursor-pointer text-xl text-lightMain">
+                            {profile.displayName}
+                        </Link>
+                        <SocialSourceIcon source={profile.source} size={20} />
+                    </div>
+
+                    <Link href={url} className="cursor-pointer text-[15px] text-secondary">
+                        @{profile.handle}
+                    </Link>
+
+                    <div className=" flex justify-between gap-3 text-[15px]">
+                        <Link
+                            href={{
+                                pathname: `/profile/${profile?.profileId}/followers`,
+                                query: { source: resolveSourceInURL(profile.source) },
+                            }}
+                            className={classNames('gap-1 hover:underline', {
+                                'pointer-events-none':
+                                    profile.source !== Source.Farcaster && profile.source !== Source.Lens,
+                            })}
+                        >
+                            <span className=" font-bold text-lightMain">{nFormatter(profile.followerCount)} </span>
+                            <span className=" text-secondary">
+                                {plural(profile.followerCount, {
+                                    zero: 'Follower',
+                                    one: 'Follower',
+                                    other: 'Followers',
+                                })}
+                            </span>
+                        </Link>
+
+                        <Link
+                            href={{
+                                pathname: `/profile/${profile?.profileId}/following`,
+                                query: { source: resolveSourceInURL(profile.source) },
+                            }}
+                            className={classNames('gap-1 hover:underline', {
+                                'pointer-events-none':
+                                    profile.source !== Source.Farcaster && profile.source !== Source.Lens,
+                            })}
+                        >
+                            <span className=" font-bold text-lightMain">{nFormatter(profile.followingCount)} </span>
+                            <span className=" text-secondary">
+                                <Trans>Following</Trans>
+                            </span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+            <Link href={url}>
+                <BioMarkup className="mt-3 line-clamp-2 text-[15px] leading-[22px] text-lightMain">
+                    {profile.bio ?? '-'}
+                </BioMarkup>
+            </Link>
+
+            <FollowButton style={{ height: 40 }} className="w-full" profile={profile} />
+        </ClickableArea>
+    );
+});
---
+++ src/components/Profile/ProfileTippy.tsx
@@ -0,0 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
+import { memo, type PropsWithChildren } from 'react';
+
+import { ProfileCard } from '@/components/Profile/ProfileCard.js';
+import { type SocialSource, Source } from '@/constants/enum.js';
+import { Tippy } from '@/esm/Tippy.js';
+import { resolveSocialMediaProvider } from '@/helpers/resolveSocialMediaProvider.js';
+import { useIsMedium } from '@/hooks/useMediaQuery.js';
+
+export interface ProfileTippyProps extends PropsWithChildren {
+    identity: string;
+    source: SocialSource;
+}
+
+export const ProfileTippy = memo<ProfileTippyProps>(function ProfileTippy({ identity, source, children }) {
+    const isMedium = useIsMedium();
+
+    const { data: profile = null, isLoading } = useQuery({
+        enabled: !!identity && !!source && isMedium,
+        queryKey: ['profile', source, identity],
+        queryFn: async () => {
+            if (!identity || !source) return;
+            const provider = resolveSocialMediaProvider(source);
+            return source === Source.Lens ? provider.getProfileByHandle(identity) : provider.getProfileById(identity);
+        },
+    });
+
+    if (!isMedium || !children || !profile) return children;
+
+    return (
+        <Tippy
+            appendTo={() => document.body}
+            maxWidth={400}
+            className="tippy-card"
+            placement="bottom"
+            duration={200}
+            arrow={false}
+            trigger="mouseenter"
+            hideOnClick
+            interactive
+            content={<ProfileCard profile={profile} loading={isLoading} />}
+        >
+            <div>{children}</div>
+        </Tippy>
+    );
+});
