0da84f409a75bf8e96fdceecb9d2b239fde75576
guanbinrui <guanbinrui@mask.io>
2024-05-15 17:49:46 +0800

    fix: typo

---
+++ src/providers/types/Poll.ts
@@ -0,0 +1,36 @@
+import type { Pageable } from '@masknet/shared-base';
+
+export interface Poll {
+    pollId: string;
+    startAt: string;
+    expiredAt: string;
+    validInDays: number;
+    options: string[];
+}
+
+export interface Provider {
+    /**
+     * Creates a new poll
+     * @param poll
+     * @returns
+     */
+    createPoll: (poll: Poll) => Promise<void>;
+
+    /**
+     * Retrieves a poll by its id
+     * @param pollId
+     * @returns
+     */
+    getPollById: (pollId: string) => Promise<Poll>;
+
+    /**
+     * Deletes a poll by its id
+     */
+    deletePoll?: (pollId: string) => Promise<void>;
+
+    /**
+     * Retrieves all polls
+     * @returns
+     */
+    getPollsByProfileId?: (profileId: string) => Promise<Pageable<Poll>>;
+}
