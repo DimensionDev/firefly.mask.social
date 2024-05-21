e2158d0d3a58a50f4e120a620db58a9a4032249f
nuanyang233 <nuanyang233@gmail.com>
2024-05-21 16:31:12 +0800

    feat: profile card for avatar (#1202)
    * feat: profile card for avatar
    
    * chore: lingui extract
    
    ---------
    
    Co-authored-by: nuanyang233 <nuanyang233@users.noreply.github.com>

--- src/components/Profile/ProfileTippy.tsx
+++ src/components/Profile/ProfileTippy.tsx
@@ -6,19 +6,28 @@ import { type SocialSource, Source } from '@/constants/enum.js';
 import { Tippy } from '@/esm/Tippy.js';
 import { resolveSocialMediaProvider } from '@/helpers/resolveSocialMediaProvider.js';
 import { useIsMedium } from '@/hooks/useMediaQuery.js';
+import type { Profile } from '@/providers/types/SocialMedia.js';
 
 export interface ProfileTippyProps extends PropsWithChildren {
     identity: string;
     source: SocialSource;
     className?: string;
+    profile?: Profile;
 }
 
-export const ProfileTippy = memo<ProfileTippyProps>(function ProfileTippy({ identity, source, children, className }) {
+export const ProfileTippy = memo<ProfileTippyProps>(function ProfileTippy({
+    identity,
+    source,
+    children,
+    className,
+    profile: defaultProfile,
+}) {
     const isMedium = useIsMedium();
 
-    const { data: profile = null, isLoading } = useQuery({
+    const { data: profile, isLoading } = useQuery({
         enabled: !!identity && !!source && isMedium,
         queryKey: ['profile', source, identity],
+        initialData: defaultProfile,
         queryFn: async () => {
             if (!identity || !source) return;
             const provider = resolveSocialMediaProvider(source);
@@ -26,13 +35,12 @@ export const ProfileTippy = memo<ProfileTippyProps>(function ProfileTippy({ iden
         },
     });
 
-    if (!isMedium || !children || !profile) return children;
+    if (!isMedium || !children) return children;
 
     return (
         <Tippy
             appendTo={() => document.body}
             maxWidth={400}
-            offset={[-100, 0]}
             className="tippy-card"
             placement="bottom"
             duration={500}
