5082b806065f9188ad30d846945e12aca06b5468
guanbinrui <guanbinrui@mask.io>
2024-05-21 17:00:57 +0800

    fix: type errors

--- src/helpers/resolveRainbowKitLocale.ts
+++ src/helpers/resolveRainbowKitLocale.ts
@@ -3,10 +3,12 @@ import type { Locale as RainbowKitLocale } from '@rainbow-me/rainbowkit';
 
 import { Locale } from '@/types/index.js';
 
+// Learn more: https://www.rainbowkit.com/docs/localization#supported-languages
 export const resolveRainbowKitLocale = createLookupTableResolver<Locale, RainbowKitLocale>(
     {
         [Locale.en]: 'en',
         [Locale.zhHans]: 'zh-CN',
+        [Locale.zhHant]: 'zh-CN',
     },
     'en',
 );
