f873aad939cbbb3a1593d95e1ca5b5eabe2362d3
nuanyang233 <nuanyang233@gmail.com>
2024-05-21 16:18:22 +0800

    chore: reply review

--- src/components/Markup/MarkupLink/ChannelTag.tsx
+++ src/components/Markup/MarkupLink/ChannelTag.tsx
@@ -43,15 +43,10 @@ export const ChannelTag = memo<Omit<MarkupLinkProps, 'post'>>(function ChannelTa
             // If the cache be undefined, the query is first query.
             if (cache === undefined) {
                 const provider = resolveSocialMediaProvider(source);
+                const result = await provider.getChannelById(channelId);
+                addChannel(source, channelId, result ? result : null);
 
-                try {
-                    const result = await provider.getChannelById(channelId);
-                    addChannel(source, channelId, result);
-
-                    return result;
-                } catch {
-                    addChannel(source, channelId, null);
-                }
+                return result;
             }
 
             return cache;
