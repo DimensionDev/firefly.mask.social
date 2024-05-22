5d2ab2305a3179d4777ae1de470fbc798eb302ed
dudu0506 <dudu@mask.io>
2024-05-22 14:46:05 +0800

    refactor: refactor code structure

--- src/components/Poll/PollCard.tsx
+++ src/components/Poll/PollCard.tsx
@@ -1,17 +1,68 @@
 import { sumBy } from 'lodash-es';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
 import RightAnswerIcon from '@/assets/right-answer.svg';
 import { ClickableButton } from '@/components/ClickableButton.js';
 import { POLL_ACTION_ENABLED } from '@/constants/poll.js';
-import { classNames } from '@/helpers/classNames.js';
 import { enqueueErrorMessage } from '@/helpers/enqueueMessage.js';
 import { isSameProfile } from '@/helpers/isSameProfile.js';
 import { useCurrentProfile } from '@/hooks/useCurrentProfile.js';
+import { useIsLogin } from '@/hooks/useIsLogin.js';
+import type { PollOption } from '@/providers/types/Poll.js';
 import type { Post } from '@/providers/types/SocialMedia.js';
 
 interface PollCardProps {
     post: Post;
+}
+interface VoteButtonProps {
+    option: PollOption;
+    post: Post;
+}
+interface VoteResultProps {
+    option: PollOption;
+    totalVotes: number;
+    userChoice?: string;
+}
+
+function VoteButton ({ option, post }: VoteButtonProps) {
+    const isLogin = useIsLogin(post.source);
+
+    const handleVote = () => {
+        enqueueErrorMessage('Not implemented yet');
+    };
+
+    return (
+        <div className='mt-3'>
+            <ClickableButton
+                disabled={!isLogin}
+                className='h-10 leading-10 w-full text-center text-base font-bold text-lightMain rounded-[10px] border border-lightMain disabled:!cursor-default disabled:!opacity-100 hover:bg-secondaryMain'
+                onClick={handleVote}
+            >{option.label}</ClickableButton>
+        </div>
+    );
+}
+
+function VoteResult ({ option, totalVotes, userChoice }: VoteResultProps) {
+    const { label, votes = 0 } = option;
+    const currentRate = totalVotes ? parseFloat(((votes / totalVotes) * 100).toFixed(2)) : 0;
+
+    return (
+        <div className='relative h-10 mt-3'>
+            <div
+                className='absolute h-full bg-secondaryMain rounded-[10px]'
+                style={{ width: currentRate ? `${currentRate}%` : '20px'}}
+            />
+            <div className='absolute z-10 h-full w-full flex justify-between items-center pl-5 text-base font-bold text-lightMain'>
+                <span className="flex items-center gap-2">
+                    <span>{label}</span>
+                        {userChoice === label ? (
+                            <RightAnswerIcon className="mr-2" width={20} height={20} />
+                        ) : null}
+                    </span>
+                <span>{currentRate}%</span>
+            </div>
+        </div>
+    )
 }
 
 export function PollCard({ post }: PollCardProps) {
@@ -21,58 +72,25 @@ export function PollCard({ post }: PollCardProps) {
     const { poll } = post;
     if (!poll) return null;
 
-    const voteDisabled =
-        !POLL_ACTION_ENABLED[post.source] ||
-        post.poll?.votingStatus === 'closed' ||
-        !profile ||
-        isSameProfile(profile, post.author);
+    const totalVotes = sumBy(poll.options, (option) => option.votes ?? 0);
 
-    const toResultRate = (current: number) => {
-        const voteCount = sumBy(poll.options, (option) => option.votes ?? 0);
-        return voteCount ? parseFloat(((current / voteCount) * 100).toFixed(2)) : 0;
-    };
-    const handleVote = (optionLabel: string) => {
-        enqueueErrorMessage('Not implemented yet');
-    };
+    const showResultsOnly =
+        !POLL_ACTION_ENABLED[post.source] ||
+        poll.votingStatus === 'closed' ||
+        isSameProfile(profile, post.author) ||
+        !!userVote;
 
     return (
         <div>
-            {poll.options.map((option, index) => {
-                const rate = toResultRate(option.votes ?? 0);
-                const voteBarWidth = voteDisabled ? (rate > 0 ? `${rate}%` : '20px') : 0;
-                return (
-                    <div
-                        className={classNames(
-                            'relative mt-3 h-10 overflow-hidden rounded-[10px] border leading-10',
-                            voteDisabled ? 'border-transparent' : 'border-lightMain',
-                        )}
-                        key={index}
-                    >
-                        <div
-                            className="absolute left-0 top-0 h-full rounded-[10px] bg-lightBg transition-all delay-100 duration-[800] ease-in-out"
-                            style={{ width: voteBarWidth }}
-                        />
-                        <ClickableButton
-                            disabled={voteDisabled}
-                            className={classNames(
-                                'absolute left-0 top-0 z-10 flex h-full w-full items-center rounded-[10px] pl-5 pr-2 text-base font-bold text-lightMain disabled:cursor-default disabled:opacity-100',
-                                voteDisabled ? 'justify-between' : 'justify-center hover:bg-lightBg',
-                            )}
-                            onClick={() => {
-                                handleVote(option.label);
-                            }}
-                        >
-                            <span className="flex items-center gap-2">
-                                <span>{option.label}</span>
-                                {userVote === option.label ? (
-                                    <RightAnswerIcon className="mr-2" width={20} height={20} />
-                                ) : null}
-                            </span>
-                            {voteDisabled ? <span>{rate}%</span> : null}
-                        </ClickableButton>
-                    </div>
-                );
-            })}
+            {poll.options.map((option, index) => (
+                <Fragment key={index}>
+                    {showResultsOnly ? (
+                        <VoteResult option={option} totalVotes={totalVotes} userChoice={userVote} />
+                    ) : (
+                        <VoteButton option={option} post={post} />
+                    )}
+                </Fragment>
+            ))}
         </div>
     );
 }
