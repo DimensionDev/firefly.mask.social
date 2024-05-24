a2f7cc191d407a9cdd906e1d10f4dfd3834c388b
dudu0506 <dudu@mask.io>
2024-05-24 10:45:09 +0800

    fix: FW-1086 Discard inaccurate detection (#1237)

--- src/helpers/getLangNameFromLocal.ts
+++ src/helpers/getLangNameFromLocal.ts
@@ -3,6 +3,8 @@ import { find } from 'lodash-es';
 import { Language } from '@/services/translate.js';
 
 export const getLangNameFromLocal = (locale: string) => {
-    const matchedLang = find(Object.entries(Language), ([_, value]) => value === locale);
+    const matchedLang = find(Object.entries(Language), ([_, value]) => {
+        return `${value}`.startsWith(locale);
+    });
     return matchedLang?.[0];
 };
