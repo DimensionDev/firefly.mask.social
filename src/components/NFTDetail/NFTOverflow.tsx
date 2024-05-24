722eeae3a67d4f3572d6933fbd7653532af0f132
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-05-24 15:54:33 +0800

    fix: update channel (#1242)
    * fix: update channel
    
    * fix: eslint
    
    * chore: eslint
    
    ---------
    
    Co-authored-by: guanbinrui <guanbinrui@users.noreply.github.com>

--- src/components/NFTDetail/NFTOverflow.tsx
+++ src/components/NFTDetail/NFTOverflow.tsx
@@ -10,6 +10,7 @@ import LinkIcon from '@/assets/link-square.svg';
 import { ChainIcon } from '@/components/NFTDetail/ChainIcon.js';
 import { CopyButton } from '@/components/NFTDetail/CopyButton.js';
 import { Link } from '@/esm/Link.js';
+// eslint-disable-next-line no-restricted-imports
 import { resolveChain } from '@/maskbook/packages/web3-providers/src/SimpleHash/helpers.js';
 
 export interface NFTOverflowProps {
