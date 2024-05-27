2eba9fbd02af509c3db19e7ecbe4ffc6ee60d534
dudu0506 <dudu@mask.io>
2024-05-27 14:52:44 +0800

    fix: FW-1081 Dynamic display FF-garden (#1257)

--- src/hooks/useSearchChannel.ts
+++ src/hooks/useSearchChannel.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { compact, uniq } from 'lodash-es';
+import { compact, uniqBy } from 'lodash-es';
 import { useDebounce } from 'usehooks-ts';
 
 import { FF_GARDEN_CHANNEL, HOME_CHANNEL } from '@/constants/channel.js';
@@ -7,25 +7,38 @@ import { type SocialSource, Source } from '@/constants/enum.js';
 import { SORTED_CHANNEL_SOURCES } from '@/constants/index.js';
 import { resolveSocialMediaProvider } from '@/helpers/resolveSocialMediaProvider.js';
 
-async function searchChannels(source: SocialSource, keyword: string) {
+async function searchChannels(source: SocialSource, keyword: string, hasRedPacket: boolean) {
     const provider = resolveSocialMediaProvider(source);
     if (!keyword) {
         const defaultChannels = await provider.searchChannels('');
-        return source === Source.Farcaster
-            ? uniq(compact([HOME_CHANNEL, FF_GARDEN_CHANNEL, ...defaultChannels.data]))
-            : defaultChannels.data;
+        if (source === Source.Farcaster) {
+            return uniqBy(
+                compact([
+                    HOME_CHANNEL,
+                    hasRedPacket
+                        ? {
+                              ...FF_GARDEN_CHANNEL,
+                              followerCount: (await provider.getChannelById(FF_GARDEN_CHANNEL.id)).followerCount,
+                          }
+                        : null,
+                    ...defaultChannels.data,
+                ]),
+                'id',
+            );
+        }
+        return defaultChannels.data;
     }
     const response = await provider.searchChannels(keyword);
     return response.data;
 }
 
-export function useSearchChannels(keyword: string) {
+export function useSearchChannels(keyword: string, hasRedPacket: boolean) {
     const debouncedKeyword = useDebounce(keyword, 300);
     return useQuery({
-        queryKey: ['searchChannels', debouncedKeyword],
+        queryKey: ['searchChannels', debouncedKeyword, `${hasRedPacket}`],
         queryFn: async () => {
             const allSettled = await Promise.allSettled(
-                SORTED_CHANNEL_SOURCES.map((x) => searchChannels(x, debouncedKeyword)),
+                SORTED_CHANNEL_SOURCES.map((x) => searchChannels(x, debouncedKeyword, hasRedPacket)),
             );
             return allSettled.flatMap((x) => (x.status === 'fulfilled' ? x.value : []));
         },
