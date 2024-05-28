4dd40733f4bdc3969b249551d90ab8ae68109c16
LeifXu <ins@mask.io>
2024-05-28 16:56:38 +0800

    fix: double trans

--- src/components/VirtualList/VirtualListFooterBottomText.tsx
+++ src/components/VirtualList/VirtualListFooterBottomText.tsx
@@ -1,9 +1,9 @@
-import { t, Trans } from '@lingui/macro';
+import { t } from '@lingui/macro';
 
 export function VirtualListFooterBottomText() {
     return (
         <div className="flex items-center justify-center p-6 text-base text-secondary">
-            <Trans>{t`You've hit rock bottom.`}</Trans>
+            {t`You've hit rock bottom.`}
         </div>
     );
 }
