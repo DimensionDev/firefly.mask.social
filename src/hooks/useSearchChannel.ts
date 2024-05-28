4ab7ee976c17957955562676f819410e0ad01fd8
dudu0506 <dudu@mask.io>
2024-05-28 21:44:41 +0800

    feat: adjust channel search logic (#1274)
    * feat: adjust channel search logic
    
    https://mask.atlassian.net/browse/FW-1083
    https://mask.atlassian.net/browse/FW-1138
    
    * chore: prettier
    
    * fix: code review error
    
    * chore: prettier
    
    * fix: code review error
    
    ---------
    
    Co-authored-by: dudu0506 <dudu0506@users.noreply.github.com>

--- src/hooks/useSearchChannel.ts
+++ src/hooks/useSearchChannel.ts
@@ -6,11 +6,17 @@ import { FF_GARDEN_CHANNEL, HOME_CHANNEL } from '@/constants/channel.js';
 import { type SocialSource, Source } from '@/constants/enum.js';
 import { SORTED_CHANNEL_SOURCES } from '@/constants/index.js';
 import { resolveSocialMediaProvider } from '@/helpers/resolveSocialMediaProvider.js';
+import { useCurrentProfileAll } from '@/hooks/useCurrentProfileAll.js';
 
-async function searchChannels(source: SocialSource, keyword: string, hasRedPacket: boolean) {
+interface SearchExtraOptions {
+    hasRedPacket: boolean;
+    profileId?: string;
+}
+
+async function searchChannels(source: SocialSource, keyword: string, { hasRedPacket, profileId }: SearchExtraOptions) {
     const provider = resolveSocialMediaProvider(source);
-    if (!keyword) {
-        const defaultChannels = await provider.searchChannels('');
+    if (!keyword && profileId) {
+        const defaultChannels = await provider.getChannelsByProfileId(profileId);
         if (source === Source.Farcaster) {
             return uniqBy(
                 compact([
@@ -34,11 +40,15 @@ async function searchChannels(source: SocialSource, keyword: string, hasRedPacke
 
 export function useSearchChannels(keyword: string, hasRedPacket: boolean) {
     const debouncedKeyword = useDebounce(keyword, 300);
+    const profiles = useCurrentProfileAll();
+
     return useQuery({
         queryKey: ['searchChannels', debouncedKeyword, `${hasRedPacket}`],
         queryFn: async () => {
             const allSettled = await Promise.allSettled(
-                SORTED_CHANNEL_SOURCES.map((x) => searchChannels(x, debouncedKeyword, hasRedPacket)),
+                SORTED_CHANNEL_SOURCES.map((x) =>
+                    searchChannels(x, debouncedKeyword, { hasRedPacket, profileId: profiles[x]?.profileId }),
+                ),
             );
             return allSettled.flatMap((x) => (x.status === 'fulfilled' ? x.value : []));
         },
