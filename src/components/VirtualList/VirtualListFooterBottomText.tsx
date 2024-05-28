cf45ab11420c371ac5cd18ef548728f27996b5ce
LeifXu <ins@mask.io>
2024-05-28 16:25:24 +0800

    feat: adjust list bottom text

--- src/components/VirtualList/VirtualListFooterBottomText.tsx
+++ src/components/VirtualList/VirtualListFooterBottomText.tsx
@@ -1,9 +1,9 @@
-import { Trans } from '@lingui/macro';
+import { t, Trans } from '@lingui/macro';
 
 export function VirtualListFooterBottomText() {
     return (
         <div className="flex items-center justify-center p-6 text-base text-secondary">
-            <Trans>You&apos;ve hit rock bottom.</Trans>
+            <Trans>{t`You've hit rock bottom.`}</Trans>
         </div>
     );
 }
