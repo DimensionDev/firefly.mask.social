8c7c5d89e1bbc767981ed27db88e3259d8c1190d
dudu0506 <dudu0506@users.noreply.github.com>
2024-05-29 08:59:29 +0000

    chore: prettier

--- src/helpers/isValidContentToTranslate.ts
+++ src/helpers/isValidContentToTranslate.ts
@@ -1,6 +1,6 @@
-import { isNaN } from "lodash-es";
+import { isNaN } from 'lodash-es';
 
-import { trimify } from "@/helpers/trimify.js";
+import { trimify } from '@/helpers/trimify.js';
 
 export const isValidContentToTranslate = (content: string) => {
     if (!trimify(content) || !isNaN(+content)) return false;
