9a3ea0c40ce88576b0c30c22a21429dea6a2a980
dudu0506 <dudu@mask.io>
2024-05-29 16:56:30 +0800

    fix: optimize translate
    https://mask.atlassian.net/browse/FW-1127
    https://mask.atlassian.net/browse/FW-1145

---
+++ src/helpers/isValidContentToTranslate.ts
@@ -0,0 +1,8 @@
+import { isNaN } from "lodash-es";
+
+import { trimify } from "@/helpers/trimify.js";
+
+export const isValidContentToTranslate = (content: string) => {
+    if (!trimify(content) || !isNaN(+content)) return false;
+    return true;
+};
