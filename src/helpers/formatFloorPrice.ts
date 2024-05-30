c7333a5f527048497fb46b89db00dc13fdcbfd63
LeifXu <ins@mask.io>
2024-05-30 19:11:55 +0800

    feat: use big number

--- src/helpers/formatFloorPrice.ts
+++ src/helpers/formatFloorPrice.ts
@@ -1,10 +1,9 @@
-import { isLessThan } from '@masknet/web3-shared-base';
 import { BigNumber } from 'bignumber.js';
 
 import { removeTrailingZeros } from '@/helpers/removeTrailingZeros.js';
 
 export function formatFloorPrice(price: BigNumber.Value) {
-    if (isLessThan(price, 0.000001)) {
+    if (BigNumber(price).isLessThan(0.000001)) {
         return '< 0.000001';
     }
     return removeTrailingZeros(BigNumber(price).toFormat(6));
--- src/helpers/formatPercentage.ts
+++ src/helpers/formatPercentage.ts
@@ -1,4 +1,4 @@
-import { isLessThan } from '@masknet/web3-shared-base';
+import { BigNumber } from 'bignumber.js';
 
 import { removeTrailingZeros } from '@/helpers/removeTrailingZeros.js';
 
@@ -6,7 +6,7 @@ import { removeTrailingZeros } from '@/helpers/removeTrailingZeros.js';
  * @param value 0-1
  */
 export function formatPercentage(value: number) {
-    if (isLessThan(value, 0.0001)) {
+    if (BigNumber(value).isLessThan(0.0001)) {
         return '< 0.01%';
     }
     return `${removeTrailingZeros((value * 100).toFixed(2))}%`;
