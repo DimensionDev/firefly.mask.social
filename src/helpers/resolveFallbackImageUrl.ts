d0b666ba33245dd910fc2101d683c70a40a4102e
Wukong Sun <swkatmask@gmail.com>
2024-05-30 17:11:58 +0800

    fix: fw-1039 bookmark login (#1298)
    * fix: fw-1039 bookmark login
    
    * fixup! fix: fw-1039 bookmark login
    
    * fix: fw-1172 update query data for unbookmark
    
    * chore: lingui extract
    
    ---------
    
    Co-authored-by: swkatmask <swkatmask@users.noreply.github.com>

--- src/helpers/resolveFallbackImageUrl.ts
+++ src/helpers/resolveFallbackImageUrl.ts
@@ -2,11 +2,12 @@ import { createLookupTableResolver } from '@masknet/shared-base';
 
 import { type SocialSource, Source } from '@/constants/enum.js';
 
-export const resolveFallbackImageUrl = createLookupTableResolver<SocialSource, string>(
+export const resolveFallbackImageUrl = createLookupTableResolver<SocialSource | Source.Article, string>(
     {
         [Source.Farcaster]: '/image/farcaster-fallback.png',
         [Source.Lens]: '/image/lens-fallback.png',
         [Source.Twitter]: '/image/twitter-fallback.png',
+        [Source.Article]: '/image/article-fallback.png',
     },
     '',
 );
