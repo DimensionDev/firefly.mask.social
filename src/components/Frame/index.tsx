65c825ff35de00e0ce616b6fefccff2ee9162dd6
guanbinrui <guanbinrui@mask.io>
2024-05-30 18:38:55 +0800

    refactor: getNextFrame

--- src/components/Frame/index.tsx
+++ src/components/Frame/index.tsx
@@ -5,94 +5,117 @@ import { attemptUntil } from '@masknet/web3-shared-base';
 import { isValidDomain } from '@masknet/web3-shared-evm';
 import { useQuery } from '@tanstack/react-query';
 import { isUndefined } from 'lodash-es';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAsyncFn } from 'react-use';
 import urlcat from 'urlcat';
 
-import { Button } from '@/components/Frame/Button.js';
-import { Input } from '@/components/Frame/Input.js';
-import { Image } from '@/components/Image.js';
-import { Source } from '@/constants/enum.js';
+import { Card } from '@/components/Frame/Card.js';
 import { MAX_FRAME_SIZE_PER_POST } from '@/constants/index.js';
 import { enqueueErrorMessage } from '@/helpers/enqueueMessage.js';
 import { fetchJSON } from '@/helpers/fetchJSON.js';
 import { untilImageUrlLoaded } from '@/helpers/untilImageLoaded.js';
-import { ConfirmModalRef, LoginModalRef } from '@/modals/controls.js';
-import { farcasterSessionHolder } from '@/providers/farcaster/SessionHolder.js';
+import { ConfirmModalRef } from '@/modals/controls.js';
 import { HubbleSocialMediaProvider } from '@/providers/hubble/SocialMedia.js';
 import { ActionType, type Frame, type FrameButton, type LinkDigested } from '@/types/frame.js';
 import type { ResponseJSON } from '@/types/index.js';
 
-interface FrameUIProps {
-    frame: Frame;
-    readonly?: boolean;
-    loading?: boolean;
-    onButtonClick?: (button: FrameButton, input?: string) => Promise<void>;
-}
+async function getNextFrame(
+    postId: string,
+    frame: Frame,
+    latestFrame: Frame | null,
+    button: FrameButton,
+    input?: string,
+) {
+    try {
+        const confirm = () =>
+            ConfirmModalRef.openAndWaitForClose({
+                title: t`Leaving Firefly`,
+                content: (
+                    <div className=" text-main">
+                        <Trans>
+                            Please be cautious when connecting your wallet, as malicious websites may attempt to access
+                            your funds.
+                        </Trans>
+                    </div>
+                ),
+            });
 
-export function FrameUI({ frame, readonly = false, loading = false, onButtonClick }: FrameUIProps) {
-    const inputRef = useRef<HTMLInputElement>(null);
+        const post = async () => {
+            const url = urlcat('/api/frame', {
+                url: frame.url,
+                action,
+                'post-url': button.target || frame.postUrl || frame.url,
+            });
+            const packet = await HubbleSocialMediaProvider.generateFrameSignaturePacket(
+                postId,
+                frame,
+                button.index,
+                input,
+                // for initial frame should not provide state
+                latestFrame && frame.state ? frame.state : undefined,
+            );
 
-    return (
-        <div
-            className=" mt-4 w-full rounded-xl border border-line bg-bg p-2 text-sm"
-            onClick={(e) => e.stopPropagation()}
-        >
-            <div className="relative w-full">
-                {loading ? (
-                    <div
-                        className=" z10 absolute inset-0 overflow-hidden rounded-xl bg-white dark:bg-bg"
-                        style={{ boxShadow: '0px 0px 20px 0px rgba(0, 0, 0, 0.05)', backdropFilter: 'blur(4px)' }}
-                    />
-                ) : null}
-                <Image
-                    className="divider aspect-2 w-full rounded-xl object-cover"
-                    style={{
-                        aspectRatio: frame.aspectRatio?.replace(':', ' / '),
-                    }}
-                    unoptimized
-                    priority={false}
-                    src={frame.image.url}
-                    alt={frame.title}
-                    width={frame.image.width}
-                    height={frame.image.height}
-                />
-            </div>
-            {frame.input ? (
-                <div className="mt-2 flex">
-                    <Input input={frame.input} ref={inputRef} />
-                </div>
-            ) : null}
-            {frame.buttons.length ? (
-                <div className="mt-2 flex gap-2">
-                    {frame.buttons.map((button) => (
-                        <Button
-                            key={button.index}
-                            button={button}
-                            disabled={loading || readonly}
-                            onClick={async () => {
-                                if (readonly || loading) return;
+            return fetchJSON<ResponseJSON<LinkDigested | { redirectUrl: string }>>(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(packet),
+            });
+        };
 
-                                if (!farcasterSessionHolder.session && button.action === ActionType.Post) {
-                                    LoginModalRef.open({
-                                        source: Source.Farcaster,
-                                    });
-                                    return;
-                                }
+        switch (button.action) {
+            case ActionType.Post:
+                const postResponse = await post();
+                const nextFrame = postResponse.success ? (postResponse.data as LinkDigested).frame : null;
 
-                                const inputText = inputRef.current?.value;
-                                // there is only one input field in the frame
-                                // when a new frame arrives, clear the input field
-                                if (inputRef.current) inputRef.current.value = '';
+                if (!nextFrame) {
+                    enqueueErrorMessage(t`The frame server failed to process the request.`);
+                    return;
+                }
 
-                                await onButtonClick?.(button, inputText);
-                            }}
-                        />
-                    ))}
-                </div>
-            ) : null}
-        </div>
-    );
+                // in case the image loaded after loading state is set to false
+                if (nextFrame.image.url) {
+                    try {
+                        await untilImageUrlLoaded(nextFrame.image.url, AbortSignal.timeout(3_000));
+                    } catch {
+                        // ignore
+                    }
+                }
+
+                return nextFrame;
+            case ActionType.PostRedirect:
+                const postRedirectResponse = await post();
+                const redirectUrl = postRedirectResponse.success
+                    ? (postRedirectResponse.data as { redirectUrl: string }).redirectUrl
+                    : null;
+                if (!redirectUrl) {
+                    enqueueErrorMessage(t`The frame server failed to process the request.`);
+                    return;
+                }
+
+                if (await confirm()) openWindow(redirectUrl, '_blank');
+                return;
+            case ActionType.Link:
+                if (!button.target) return;
+                if (await confirm()) openWindow(button.target, '_blank');
+                return;
+            case ActionType.Mint:
+                enqueueErrorMessage(t`Mint button is not available yet.`);
+                return;
+            case ActionType.Transaction:
+                enqueueErrorMessage(t`Transaction button is not available yet.`);
+                return;
+            default:
+                safeUnreachable(button.action);
+                return;
+        }
+    } catch (error) {
+        enqueueErrorMessage(t`Something went wrong. Please try again.`, {
+            error,
+        });
+        throw error;
+    }
 }
 
 interface FrameProps {
@@ -149,102 +172,9 @@ export function Frame({ postId, urls, onData, children }: FrameProps) {
 
     const [{ loading: isLoadingNextFrame }, handleClick] = useAsyncFn(
         async (button: FrameButton, input?: string) => {
-            try {
-                if (!frame) return;
-
-                const { action, index, target } = button;
-
-                const confirm = () =>
-                    ConfirmModalRef.openAndWaitForClose({
-                        title: t`Leaving Firefly`,
-                        content: (
-                            <div className=" text-main">
-                                <Trans>
-                                    Please be cautious when connecting your wallet, as malicious websites may attempt to
-                                    access your funds.
-                                </Trans>
-                            </div>
-                        ),
-                    });
-
-                const post = async () => {
-                    const url = urlcat('/api/frame', {
-                        url: frame.url,
-                        action,
-                        'post-url': target || frame.postUrl || frame.url,
-                    });
-                    const packet = await HubbleSocialMediaProvider.generateFrameSignaturePacket(
-                        postId,
-                        frame,
-                        index,
-                        input,
-                        // for initial frame should not provide state
-                        latestFrame && frame.state ? frame.state : undefined,
-                    );
-
-                    return fetchJSON<ResponseJSON<LinkDigested | { redirectUrl: string }>>(url, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(packet),
-                    });
-                };
-
-                switch (action) {
-                    case ActionType.Post:
-                        const postResponse = await post();
-                        const nextFrame = postResponse.success ? (postResponse.data as LinkDigested).frame : null;
-
-                        if (!nextFrame) {
-                            enqueueErrorMessage(t`The frame server failed to process the request.`);
-                            return;
-                        }
-
-                        // in case the image loaded after loading state is set to false
-                        if (nextFrame.image.url) {
-                            try {
-                                await untilImageUrlLoaded(nextFrame.image.url, AbortSignal.timeout(3_000));
-                            } catch {
-                                // ignore
-                            }
-                        }
-
-                        setLatestFrame(nextFrame);
-                        break;
-                    case ActionType.PostRedirect:
-                        const postRedirectResponse = await post();
-                        const redirectUrl = postRedirectResponse.success
-                            ? (postRedirectResponse.data as { redirectUrl: string }).redirectUrl
-                            : null;
-                        if (!redirectUrl) {
-                            enqueueErrorMessage(t`The frame server failed to process the request.`);
-                            return;
-                        }
-
-                        if (await confirm()) openWindow(redirectUrl, '_blank');
-                        break;
-                    case ActionType.Link:
-                        if (!button.target) return;
-                        if (await confirm()) openWindow(button.target, '_blank');
-                        break;
-                    case ActionType.Mint:
-                        enqueueErrorMessage(t`Mint button is not available yet.`);
-                        break;
-                    case ActionType.Transaction:
-                        enqueueErrorMessage(t`Transaction button is not available yet.`);
-                        break;
-                    default:
-                        safeUnreachable(action);
-                        break;
-                }
-            } catch (error) {
-                enqueueErrorMessage(t`Something went wrong. Please try again.`, {
-                    error,
-                });
-                throw error;
-            }
-            return;
+            if (!frame) return;
+            const nextFrame = await getNextFrame(postId, frame, latestFrame, button, input);
+            if (nextFrame) setLatestFrame(nextFrame);
         },
         [frame, latestFrame, postId],
     );
@@ -253,5 +183,5 @@ export function Frame({ postId, urls, onData, children }: FrameProps) {
 
     if (error || !frame) return children;
 
-    return <FrameUI frame={frame} loading={isLoadingNextFrame} onButtonClick={handleClick} />;
+    return <Card frame={frame} loading={isLoadingNextFrame} onButtonClick={handleClick} />;
 }
