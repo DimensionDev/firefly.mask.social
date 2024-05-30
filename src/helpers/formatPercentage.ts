2eb6ed21cf26284116a2efbba1e9c181dcd3cc58
LeifXu <ins@mask.io>
2024-05-30 19:06:36 +0800

    feat: use `isLessThan` to compare

--- src/helpers/formatPercentage.ts
+++ src/helpers/formatPercentage.ts
@@ -1,10 +1,12 @@
+import { isLessThan } from '@masknet/web3-shared-base';
+
 import { removeTrailingZeros } from '@/helpers/removeTrailingZeros.js';
 
 /**
  * @param value 0-1
  */
 export function formatPercentage(value: number) {
-    if (value < 0.0001) {
+    if (isLessThan(value, 0.0001)) {
         return '< 0.01%';
     }
     return `${removeTrailingZeros((value * 100).toFixed(2))}%`;
