18a1a8b8141cce15f6989e420c15619883398474
guanbinrui <guanbinrui@mask.io>
2024-05-31 11:27:34 +0800

    chore: add types

--- src/types/frame.ts
+++ src/types/frame.ts
@@ -1,3 +1,5 @@
+import type { Abi, AbiItem } from 'viem';
+
 export type Index = 1 | 2 | 3 | 4;
 
 export enum ActionType {
@@ -51,4 +53,41 @@ export interface Frame {
 
 export interface LinkDigested {
     frame: Frame;
+}
+
+/**
+ * Supported chain IDs
+ */
+export enum ChainId {
+    Ethereum = 1,
+    Arbitrum = 42161,
+    Base = 8453,
+    Base_Sepolia = 84532,
+    Degen = 666666666,
+    Gnosis = 100,
+    Optimism = 10,
+    Zora = 7777777,
+}
+
+export enum MethodType {
+    ETH_SEND_TRANSACTION = 'eth_sendTransaction',
+}
+
+export interface TransactionParams {
+    //  JSON ABI which must include encoded function type and should include potential error types. Can be empty.
+    abi: AbiItem | AbiItem[];
+    to: `0x${string}`;
+    // value of ether to send with the transaction in wei. Optional.
+    value?: string;
+    data?: `0x${string}`;
+}
+
+export interface TransactionTargetResponse {
+    // a CAIP-2 chain ID to identify the tx network
+    chainId: `eip155:${ChainId}`;
+    method: MethodType;
+    // identifying client in calldata
+    // learn more: https://www.notion.so/warpcast/Frame-Transactions-Public-9d9f9f4f527249519a41bd8d16165f73?pvs=4#c1c3182208ce4ae4a7ffa72129b9795a
+    attribution?: boolean;
+    params: TransactionParams;
 }
