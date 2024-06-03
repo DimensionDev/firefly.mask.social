b1c7da9a1f44c0114d898d8bc751109fc60145a4
nuanyang233 <nuanyang233@gmail.com>
2024-06-03 18:00:17 +0800

    fix: bugfix for lens profile tab

--- src/components/ListInPage.tsx
+++ src/components/ListInPage.tsx
@@ -39,7 +39,8 @@ export function ListInPage<T = unknown, C = unknown>({
     const itemsRendered = useRef(false);
     const isLogin = useIsLogin(currentSocialSource);
 
-    const { data, hasNextPage, fetchNextPage, isFetchingNextPage, isFetching } = queryResult;
+    const { data, hasNextPage, fetchNextPage, isFetchingNextPage, isFetching, isLoading } = queryResult;
+
 
     const onEndReached = useCallback(async () => {
         if (!hasNextPage || isFetching || isFetchingNextPage) {
