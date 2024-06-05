4aaa552c41d5dc262871dc9a3491dd08d41e3b9a
Wukong Sun <swkatmask@gmail.com>
2024-06-05 16:58:31 +0800

    fix(Frame): fw-1222 button label overflow

--- src/components/Frame/Button.tsx
+++ src/components/Frame/Button.tsx
@@ -1,4 +1,5 @@
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
+import { useDetectOverflow } from '@masknet/theme';
 
 import { ClickableButton } from '@/components/ClickableButton.js';
 import { classNames } from '@/helpers/classNames.js';
@@ -11,13 +12,13 @@ interface Props {
 }
 
 export function Button({ button, disabled = false, onClick }: Props) {
+    const [overflow, ref] = useDetectOverflow();
     return (
         <ClickableButton
             className={classNames(
-                'flex flex-1 items-center justify-center rounded-md border border-line bg-white py-2 text-main dark:bg-darkBottom dark:text-white',
+                'box-border flex flex-1 items-center justify-center overflow-auto rounded-md border border-line bg-white p-2 text-main dark:bg-darkBottom dark:text-white',
                 {
-                    'hover:bg-bg': !disabled,
-                    'hover:cursor-pointer': !disabled,
+                    'hover:cursor-pointer hover:bg-bg': !disabled,
                 },
             )}
             disabled={disabled}
@@ -25,10 +26,13 @@ export function Button({ button, disabled = false, onClick }: Props) {
             onClick={() => {
                 if (!disabled) onClick?.();
             }}
+            title={overflow ? button.text : undefined}
         >
-            <span>{button.text}</span>
+            <span className="overflow-hidden text-ellipsis whitespace-nowrap" ref={ref}>
+                {button.text}
+            </span>
             {[ActionType.PostRedirect, ActionType.Link].includes(button.action) ? (
-                <ArrowTopRightOnSquareIcon className="ml-1" width={20} height={20} />
+                <ArrowTopRightOnSquareIcon className="ml-1 flex-shrink-0" width={20} height={20} />
             ) : null}
         </ClickableButton>
     );
