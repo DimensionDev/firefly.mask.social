abce1cf36e95c41625aac33ea5f6df9ae25b319e
LeifXu <ins@mask.io>
2024-06-05 22:41:58 +0800

    fix: replace `button` to `ClickableButton`

--- src/components/NFTDetail/ReportSpamButton.tsx
+++ src/components/NFTDetail/ReportSpamButton.tsx
@@ -3,18 +3,19 @@
 import { Trans } from '@lingui/macro';
 
 import ReportSpamIcon from '@/assets/report-spam.svg';
+import { ClickableButton } from '@/components/ClickableButton.js';
 import { useReportSpamNFT } from '@/hooks/useReportSpamNFT.js';
 
 export function ReportSpamButton(props: { collectionId: string }) {
     const { collectionId } = props;
     const [, reportSpamNFT] = useReportSpamNFT();
     return (
-        <button
+        <ClickableButton
             className="flex cursor-pointer select-none items-center gap-1 rounded-full border border-line bg-lightBg px-2 py-1 text-[10px] leading-[14px]"
             onClick={() => reportSpamNFT(collectionId)}
         >
             <ReportSpamIcon className="h-3 w-3" />
             <Trans>Report spam</Trans>
-        </button>
+        </ClickableButton>
     );
 }
