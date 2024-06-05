fc231f5772af258365904bba1c192e76568e4564
LeifXu <ins@mask.io>
2024-06-05 18:33:14 +0800

    fix: fix issue FW-1212

--- src/components/Compose/PostBy.tsx
+++ src/components/Compose/PostBy.tsx
@@ -20,7 +20,7 @@ export function PostBy(props: PostByProps) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0 translate-y-1"
         >
-            <Popover.Panel className="absolute bottom-full right-0 flex w-[280px] -translate-y-3 flex-col gap-2 rounded-lg bg-bgModal p-3 text-[15px] shadow-popover dark:border dark:border-line dark:shadow-none">
+            <Popover.Panel className="absolute bottom-full right-0 z-10 flex w-[280px] -translate-y-3 flex-col gap-2 rounded-lg bg-bgModal p-3 text-[15px] shadow-popover dark:border dark:border-line dark:shadow-none">
                 {SORTED_SOCIAL_SOURCES.map((source) => (
                     <PostByItem
                         key={source}
