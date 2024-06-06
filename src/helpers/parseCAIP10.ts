10d851716d0869dfa80ba07b9f436a4d83480952
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-06-06 19:22:35 +0800

    feat: mint tx (#1386)
    * refactor: parseCAIP10
    
    * chore: compose url from caip
    
    * chore: lingui extract
    
    * chore: resolveMintUrl
    
    * chore: prettier
    
    ---------
    
    Co-authored-by: guanbinrui <guanbinrui@users.noreply.github.com>

---
+++ src/helpers/parseCAIP10.ts
@@ -0,0 +1,24 @@
+import { getEnumAsArray } from '@masknet/kit';
+import { isValidAddress } from '@masknet/web3-shared-evm';
+
+import { ChainId } from '@/types/frame.js';
+
+export function parseCAIP10(caip10: string) {
+    if (!caip10.startsWith('eip155:')) throw new Error(`Invalid CAIP10 identifier: ${caip10}`);
+
+    const fragments = caip10.split(':');
+    const [chainId, address, ...parameters] = fragments.slice(1);
+
+    const chainIdParsed = Number.parseInt(chainId, 10);
+    if (isNaN(chainIdParsed) || chainIdParsed <= 0) throw new Error(`Invalid chain ID: ${chainId}`);
+    if (!getEnumAsArray(ChainId).find((x) => x.value === chainIdParsed))
+        throw new Error(`Unsupported chain ID: ${chainId}`);
+
+    const addressParsed = isValidAddress(address) ? address : undefined;
+
+    return {
+        chainId: chainIdParsed,
+        address: addressParsed,
+        parameters,
+    };
+}
