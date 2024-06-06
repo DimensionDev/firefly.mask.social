988ed5d6d43ef1d1a2b26af2dd5de63ce88b6ce2
dudu0506 <dudu@mask.io>
2024-06-06 23:36:14 +0800

    fix: build error

--- src/helpers/createSelector.ts
+++ src/helpers/createSelector.ts
@@ -1,5 +1,3 @@
-'use client';
-
 import type { StoreApi, UseBoundStore } from 'zustand';
 
 type WithSelectors<S> = S extends { getState: () => infer T } ? S & { use: { [K in keyof T]: () => T[K] } } : never;
