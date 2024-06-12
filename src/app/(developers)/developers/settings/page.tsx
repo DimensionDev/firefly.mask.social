f287bd7c5b05deb443b2dae8e7538f88fa30e7ea
guanbinrui <guanbinrui@mask.io>
2024-06-12 17:23:47 +0800

    refactor: ux

--- src/app/(developers)/developers/settings/page.tsx
+++ src/app/(developers)/developers/settings/page.tsx
@@ -5,7 +5,7 @@ import { t, Trans } from '@lingui/macro';
 import { Headline } from '@/app/(settings)/components/Headline.js';
 import { Section } from '@/app/(settings)/components/Section.js';
 import { CircleCheckboxIcon } from '@/components/CircleCheckboxIcon.js';
-import { ClickableButton } from '@/components/ClickableButton.js';
+import { ClickableArea } from '@/components/ClickableArea.js';
 import { useDeveloperSettings } from '@/store/useDeveloperSettings.js';
 
 export default function Settings() {
@@ -21,29 +21,31 @@ export default function Settings() {
                 <ul className="w-full">
                     {[
                         {
-                            title: 'Enable development API version',
-                            description: t`Switch to the development API version for testing new features.`,
+                            title: useDevelopmentAPI
+                                ? 'Enable production API version'
+                                : 'Enable development API version',
+                            description: useDevelopmentAPI
+                                ? t`Switch to the production API version for using stable features.`
+                                : t`Switch to the development API version for testing new features.`,
                         },
                     ].map((x, i) => {
                         return (
-                            <li
-                                className="mb-6 flex items-center justify-between border-b border-line pb-1 text-[18px] leading-[24px] text-main"
+                            <ClickableArea
+                                as="li"
+                                className="mb-6 flex cursor-pointer items-center justify-between border-b border-line pb-1 text-[18px] leading-[24px] text-main"
                                 key={i}
+                                onClick={() => {
+                                    updateUseDevelopmentAPI(!useDevelopmentAPI);
+                                }}
                             >
                                 <div className="flex-1">
                                     <h2 className="mb-2">{x.title}</h2>
                                     <p className="text-sm text-secondary">{x.description}</p>
                                 </div>
-                                <div className="">
-                                    <ClickableButton
-                                        onClick={() => {
-                                            updateUseDevelopmentAPI(!useDevelopmentAPI);
-                                        }}
-                                    >
-                                        <CircleCheckboxIcon checked={useDevelopmentAPI} />
-                                    </ClickableButton>
+                                <div>
+                                    <CircleCheckboxIcon checked={useDevelopmentAPI} />
                                 </div>
-                            </li>
+                            </ClickableArea>
                         );
                     })}
                 </ul>
