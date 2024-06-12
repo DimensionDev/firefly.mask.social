fea3b54203eb5c4ebf8af3ae8b573948e25209e9
LeifXu <ins@mask.io>
2024-06-13 00:15:15 +0800

    feat: follow button icon variant

--- src/components/Profile/FollowButton.tsx
+++ src/components/Profile/FollowButton.tsx
@@ -1,6 +1,8 @@
 import { t } from '@lingui/macro';
 import { memo, useState } from 'react';
 
+import FollowIcon from '@/assets/follow-bold.svg';
+import FollowedIcon from '@/assets/followed.svg';
 import { UnmuteProfileButton } from '@/components/Actions/UnmuteProfileButton.js';
 import { ClickableButton, type ClickableButtonProps } from '@/components/ClickableButton.js';
 import { classNames } from '@/helpers/classNames.js';
@@ -16,9 +18,15 @@ enum State {
 
 interface FollowButtonProps extends Omit<ClickableButtonProps, 'children'> {
     profile: Profile;
+    variant?: 'text' | 'icon';
 }
 
-export const FollowButton = memo(function FollowButton({ profile, className, ...rest }: FollowButtonProps) {
+export const FollowButton = memo(function FollowButton({
+    variant = 'text',
+    profile,
+    className,
+    ...rest
+}: FollowButtonProps) {
     const [hovering, setHovering] = useState(false);
     const isFollowing = !!profile.viewerContext?.following;
     const [loading, toggleFollow] = useToggleFollow(profile);
@@ -28,19 +36,28 @@ export const FollowButton = memo(function FollowButton({ profile, className, ...
     if (muted) {
         return <UnmuteProfileButton profile={profile} />;
     }
-    const buttonText = isFollowing ? (hovering && !loading ? t`Unfollow` : t`Following`) : t`Follow`;
+    const buttonText = {
+        text: isFollowing ? (hovering && !loading ? t`Unfollow` : t`Following`) : t`Follow`,
+        icon: isFollowing ? <FollowIcon className="h-4 w-4" /> : <FollowedIcon className="h-4 w-4" />,
+    }[variant];
+    const variantClassName = {
+        text: 'min-w-[100px]',
+        icon: 'w-8 max-w-8',
+    }[variant];
     const buttonState = isFollowing ? (hovering && !loading ? State.Unfollow : State.Following) : State.Follow;
 
     return (
         <ClickableButton
             className={classNames(
-                'flex h-8 min-w-[100px] items-center justify-center rounded-full px-2 text-[15px] font-semibold transition-all',
-                buttonState === State.Follow ? 'bg-main text-primaryBottom hover:opacity-80' : '',
-                buttonState === State.Following ? 'border-[1.5px] border-lightMain text-lightMain' : '',
-                buttonState === State.Unfollow
-                    ? 'border-[1.5px] border-danger border-opacity-50 bg-danger bg-opacity-20 text-danger'
-                    : '',
+                'flex h-8 items-center justify-center rounded-full px-2 text-[15px] font-semibold transition-all',
+                variantClassName,
                 className,
+                {
+                    'bg-main text-primaryBottom hover:opacity-80': buttonState === State.Follow,
+                    'border-[1.5px] border-lightMain text-lightMain': buttonState === State.Following,
+                    'border-[1.5px] border-danger border-opacity-50 bg-danger bg-opacity-20 text-danger':
+                        buttonState === State.Unfollow,
+                },
             )}
             {...rest}
             disabled={loading}
