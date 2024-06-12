aa664549444c8c2ac3e80a51e4b6fdd348cfa4a3
nuanyang233 <nuanyang233@gmail.com>
2024-06-12 16:57:20 +0800

    refactor: replace react-slide to swiper

--- src/app/(normal)/@modal/(.)post/[id]/photos/[index]/page.tsx
+++ src/app/(normal)/@modal/(.)post/[id]/photos/[index]/page.tsx
@@ -1,16 +1,16 @@
 'use client';
-
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/keyboard';
 
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import { EMPTY_LIST } from '@masknet/shared-base';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { compact } from 'lodash-es';
 import { useRouter } from 'next/navigation.js';
-import { useMemo, useRef } from 'react';
-import Slider from 'react-slick';
-import { useKeyPressEvent } from 'react-use';
+import { useMemo } from 'react';
+import { Keyboard, Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { PostActionsWithGrid } from '@/components/Actions/index.js';
 import { ClickableButton, type ClickableButtonProps } from '@/components/ClickableButton.js';
@@ -52,10 +52,6 @@ function CustomRightArrow(props: CustomArrowProps) {
 }
 
 export default function PreviewPhotoModal({ params: { id: postId, index }, searchParams: { source } }: Props) {
-    const slickRef = useRef<{
-        slickPrev: () => void;
-        slickNext: () => void;
-    }>(null);
     const router = useRouter();
     const isMedium = useIsMedium();
 
@@ -89,9 +85,6 @@ export default function PreviewPhotoModal({ params: { id: postId, index }, searc
         return imageAttachments;
     }, [post]);
 
-    useKeyPressEvent((ev) => ev.key === 'ArrowLeft', slickRef.current?.slickPrev);
-    useKeyPressEvent((ev) => ev.key === 'ArrowRight', slickRef.current?.slickNext);
-
     return (
         <Modal open backdrop={false} onClose={() => router.back()}>
             <div
@@ -102,34 +95,34 @@ export default function PreviewPhotoModal({ params: { id: postId, index }, searc
                     <CloseButton onClick={() => router.back()} />
                 </div>
                 <div className="flex w-full text-main">
-                    {/* Due to the react-slick type library not being updated yet,
-                        we can only ignore it for now to keep things working normally.
-                        https://github.com/akiran/react-slick/issues/2336
-                    */}
-                    {/* @ts-ignore */}
-                    <Slider
-                        ref={slickRef}
-                        infinite={false}
-                        vertical={false}
-                        swapToSlide
+                    <Swiper
+                        modules={[Navigation, Keyboard]}
+                        navigation={{
+                            prevEl: '.prev-button',
+                            nextEl: '.next-button',
+                        }}
+                        keyboard
                         initialSlide={Number(index) - 1}
-                        nextArrow={<CustomRightArrow />}
-                        prevArrow={<CustomLeftArrow />}
                     >
                         {images.map((x, key) => {
                             return (
-                                <Image
-                                    key={index}
-                                    src={x}
-                                    alt={x}
-                                    width={1000}
-                                    height={1000}
-                                    style={{ width: 'auto', height: 'auto' }}
-                                    className="max-h-[calc(100vh-110px)] object-contain max-md:h-[calc(calc(100vh-env(safe-area-inset-bottom)-env(safe-are-inset-top)-90px))] max-md:max-w-[calc(100%-30px)]"
-                                />
+                                <SwiperSlide key={key} className="flex">
+                                    <div>
+                                        <Image
+                                            key={index}
+                                            src={x}
+                                            alt={x}
+                                            width={1000}
+                                            height={1000}
+                                            className="max-h-[calc(100vh-110px)] w-full object-contain max-md:h-[calc(calc(100vh-env(safe-area-inset-bottom)-env(safe-are-inset-top)-90px))] max-md:max-w-[calc(100%-30px)]"
+                                        />
+                                    </div>
+                                </SwiperSlide>
                             );
                         })}
-                    </Slider>
+                        <CustomLeftArrow className="prev-button absolute left-[50px] top-[50%] z-[9999] max-md:hidden" />
+                        <CustomRightArrow className="next-button absolute right-[50px] top-[50%] z-[9999] max-md:hidden" />
+                    </Swiper>
                 </div>
                 <div className="absolute my-1 flex items-center justify-between bottom-safe">
                     {post ? <PostActionsWithGrid className="gap-8" post={post} disablePadding /> : null}
