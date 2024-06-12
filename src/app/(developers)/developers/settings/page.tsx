3ccc755fbcaede79d37e7b0ac4ecc05c35cdce11
guanbinrui <guanbinrui@mask.io>
2024-06-12 17:17:10 +0800

    chore: add settings page

--- src/app/(developers)/developers/settings/page.tsx
+++ src/app/(developers)/developers/settings/page.tsx
@@ -1,25 +1,53 @@
 'use client';
 
-import { Trans } from '@lingui/macro';
+import { t, Trans } from '@lingui/macro';
 
 import { Headline } from '@/app/(settings)/components/Headline.js';
 import { Section } from '@/app/(settings)/components/Section.js';
 import { CircleCheckboxIcon } from '@/components/CircleCheckboxIcon.js';
+import { ClickableButton } from '@/components/ClickableButton.js';
+import { useDeveloperSettings } from '@/store/useDeveloperSettings.js';
 
 export default function Settings() {
+    const { useDevelopmentAPI, updateUseDevelopmentAPI } = useDeveloperSettings();
+
     return (
         <Section>
             <Headline>
                 <Trans>General</Trans>
             </Headline>
 
-            <div className="mb-2 w-full">
-                <Trans>Enable the development API version for testing new features.</Trans>
-            </div>
-
-            <div className="mb-2 flex w-full flex-row gap-2">
-                <CircleCheckboxIcon checked />
-            </div>
+            {
+                <ul className="w-full">
+                    {[
+                        {
+                            title: 'Enable development API version',
+                            description: t`Switch to the development API version for testing new features.`,
+                        },
+                    ].map((x, i) => {
+                        return (
+                            <li
+                                className="mb-6 flex items-center justify-between border-b border-line pb-1 text-[18px] leading-[24px] text-main"
+                                key={i}
+                            >
+                                <div className="flex-1">
+                                    <h2 className="mb-2">{x.title}</h2>
+                                    <p className="text-sm text-secondary">{x.description}</p>
+                                </div>
+                                <div className="">
+                                    <ClickableButton
+                                        onClick={() => {
+                                            updateUseDevelopmentAPI(!useDevelopmentAPI);
+                                        }}
+                                    >
+                                        <CircleCheckboxIcon checked={useDevelopmentAPI} />
+                                    </ClickableButton>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            }
         </Section>
     );
 }
