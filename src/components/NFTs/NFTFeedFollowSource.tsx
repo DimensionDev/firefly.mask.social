add764a7987d54157b6fd2c90e332bdb0d6d47d0
LeifXu <ins@mask.io>
2024-06-19 21:35:29 +0800

    feat: truncate overflow text nft feed following source

--- src/components/NFTs/NFTFeedFollowSource.tsx
+++ src/components/NFTs/NFTFeedFollowSource.tsx
@@ -12,23 +12,23 @@ export function NFTFeedFollowSource({ sources }: { sources: FollowingNFT['follow
     if (!source) return null;
 
     return (
-        <div className="mb-3 flex items-center text-[15px] leading-6 text-second">
-            <StarIcon className="mr-2 h-4 w-4" />
+        <div className="mb-3 flex w-full items-center space-x-2 truncate text-[15px] leading-6 text-second">
+            <StarIcon className="h-4 w-4" />
             {[FollowingNFTSourceType.Farcaster, FollowingNFTSourceType.Lens, FollowingNFTSourceType.Twitter].includes(
                 source.type,
             ) ? (
                 <>
-                    {t`Following`}
+                    <div>{t`Following`}</div>
                     {
                         (
                             {
-                                [FollowingNFTSourceType.Lens]: <LensIcon className="mx-2 h-4 w-4" />,
-                                [FollowingNFTSourceType.Farcaster]: <FarcasterIcon className="mx-2 h-4 w-4" />,
-                                [FollowingNFTSourceType.Twitter]: <TwitterIcon className="mx-2 h-4 w-4" />,
+                                [FollowingNFTSourceType.Lens]: <LensIcon className="h-4 w-4" />,
+                                [FollowingNFTSourceType.Farcaster]: <FarcasterIcon className="h-4 w-4" />,
+                                [FollowingNFTSourceType.Twitter]: <TwitterIcon className="h-4 w-4" />,
                             } as { [key in FollowingNFTSourceType]?: ReactNode }
                         )[source.type]
                     }
-                    {source.handle ? `@${source.handle}` : null}
+                    {source.handle ? <div className="flex-1 truncate">@{source.handle}</div> : null}
                 </>
             ) : (
                 <>{FollowingNFTSourceType.Wallet === source.type ? t`Address on Watching Lists` : null}</>
