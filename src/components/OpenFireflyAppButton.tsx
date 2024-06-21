c37fe33a07968d0a12f378853b5461650b583123
guanbinrui <guanbinrui@dimension.im>
2024-06-21 21:36:32 +0800

    refactor: openAppScheme

--- src/components/OpenFireflyAppButton.tsx
+++ src/components/OpenFireflyAppButton.tsx
@@ -2,57 +2,20 @@
 
 import { useAsyncFn } from 'react-use';
 
-import { ClickableButton } from '@/components/ClickableButton.js';
+import { ClickableButton, type ClickableButtonProps } from '@/components/ClickableButton.js';
 import { env } from '@/constants/env.js';
-import { openAppIfInstalled } from '@/helpers/openAppIfInstalled.js';
+import { openAppScheme } from '@/helpers/openAppScheme.js';
+import { DeviceType } from '@/types/device.js';
 
-export type ComposedScheme = {
-    ios: string;
-    android: string;
-};
+interface OpenAppButtonProps extends ClickableButtonProps {}
 
-interface OpenAppButtonProps {
-    scheme: ComposedScheme;
-    downloadUrl: string;
-    children: React.ReactNode;
-    className?: string;
-    waitDuration?: number;
-}
-
-function OpenAppButton({ scheme, downloadUrl, waitDuration, children, className }: OpenAppButtonProps) {
+export function OpenFireflyAppButton(props: OpenAppButtonProps) {
     const [{ loading }, tryOpenApp] = useAsyncFn(async () => {
-        await openAppIfInstalled(scheme, downloadUrl, waitDuration);
+        await openAppScheme({
+            [DeviceType.Android]: env.external.NEXT_PUBLIC_FIREFLY_ANDROID_HOME,
+            [DeviceType.IOS]: env.external.NEXT_PUBLIC_FIREFLY_IOS_HOME,
+        });
     }, []);
 
-    return (
-        <ClickableButton className={className} onClick={tryOpenApp} disabled={loading}>
-            {children}
-        </ClickableButton>
-    );
-}
-
-interface OpenFireflyAppButtonProps {
-    children: React.ReactNode;
-    scheme?: Partial<ComposedScheme>;
-    className?: string;
-}
-
-export function OpenFireflyAppButton({
-    // TODO: Android doesn't have home url scheme
-    scheme = {
-        ios: env.external.NEXT_PUBLIC_FIREFLY_IOS_HOME,
-        android: env.external.NEXT_PUBLIC_FIREFLY_ANDROID_HOME,
-    },
-    className,
-    children,
-}: OpenFireflyAppButtonProps) {
-    const { android, ios } = scheme;
-    if (!ios || !android) {
-        return null;
-    }
-    return (
-        <OpenAppButton className={className} scheme={{ ios, android }} downloadUrl="https://5euxu.app.link/PHvNiyVemIb">
-            {children}
-        </OpenAppButton>
-    );
+    return <ClickableButton {...props} onClick={tryOpenApp} disabled={loading} />;
 }
