afc0f97f12dd2f706cbd4493532ffb22013ca3cb
LeifXu <ins@mask.io>
2024-06-28 16:17:05 +0800

    feat: multi chain selector

--- src/components/SideBar/ConnectWallet.tsx
+++ src/components/SideBar/ConnectWallet.tsx
@@ -1,58 +1,165 @@
 'use client';
 
-import { t } from '@lingui/macro';
-import { formatDomainName, formatEthereumAddress } from '@masknet/web3-shared-evm';
-import { useAccount, useEnsName } from 'wagmi';
+import { Trans } from '@lingui/macro';
+import { NetworkPluginID } from '@masknet/shared-base';
+import { useNetworkDescriptor } from '@masknet/web3-hooks-base';
+import { ChainId as EVMChainId, formatDomainName, formatEthereumAddress } from '@masknet/web3-shared-evm';
+import { ChainId as SolanaChainId, encodePublicKey, formatAddress } from '@masknet/web3-shared-solana';
+import { useAccountModal as useAccountModalEVM, useConnectModal as useConnectModalEVM } from '@rainbow-me/rainbowkit';
+import { useWallet as useSolanaWallet } from '@solana/wallet-adapter-react';
+import { useWalletModal as useConnectModalSolana } from '@solana/wallet-adapter-react-ui';
+import { useState } from 'react';
+import { useAccount as useEVMAccount, useEnsName } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
 
+import LineArrowUp from '@/assets/line-arrow-up.svg';
+import WalletSelectedIcon from '@/assets/wallet.selected.svg';
 import WalletIcon from '@/assets/wallet.svg';
+import { Image } from '@/components/Image.js';
 import { Tooltip } from '@/components/Tooltip.js';
 import { classNames } from '@/helpers/classNames.js';
 import { resolve } from '@/helpers/resolve.js';
 import { useMounted } from '@/hooks/useMounted.js';
-import { AccountModalRef, ConnectWalletModalRef } from '@/modals/controls.js';
+import { ConnectWalletModalRef } from '@/modals/controls.js';
 
 interface ConnectWalletProps {
     collapsed?: boolean;
 }
 
-export function ConnectWallet({ collapsed = false }: ConnectWalletProps) {
+export function ConnectWallet({ collapsed: sideBarCollapsed = false }: ConnectWalletProps) {
     const mounted = useMounted();
-    const account = useAccount();
 
-    const { data: ensName } = useEnsName({ address: account.address, chainId: mainnet.id });
+    const connectModalEVM = useConnectModalEVM();
+    const accountModalEVM = useAccountModalEVM();
+    const evmNetworkDescriptor = useNetworkDescriptor(NetworkPluginID.PLUGIN_EVM, EVMChainId.Mainnet);
+    const connectModalSolana = useConnectModalSolana();
+    const solanaNetworkDescriptor = useNetworkDescriptor(NetworkPluginID.PLUGIN_SOLANA, SolanaChainId.Mainnet);
 
-    const text = resolve(() => {
-        if (!account.isConnected || !account.address || !mounted) return t`Connect Wallet`;
-        if (ensName) return formatDomainName(ensName);
+    const evmAccount = useEVMAccount();
+    const solanaWallet = useSolanaWallet();
 
-        return formatEthereumAddress(account.address, 4);
-    });
+    const { data: ensName } = useEnsName({ address: evmAccount.address, chainId: mainnet.id });
+
+    const [collapsed, setCollapsed] = useState(false);
+
+    const chainTypes = [
+        {
+            icon: evmNetworkDescriptor?.icon,
+            label: resolve(() => {
+                if (!evmAccount.isConnected || !evmAccount.address || !mounted) return null;
+                if (ensName) return formatDomainName(ensName);
+                return formatEthereumAddress(evmAccount.address, 4);
+            }),
+            onOpenConnectModal: () => connectModalEVM.openConnectModal?.(),
+            onOpenAccountModal: () => accountModalEVM.openAccountModal?.(),
+            isConnected: evmAccount.isConnected,
+        },
+        {
+            icon: solanaNetworkDescriptor?.icon,
+            label: resolve(() => {
+                if (!solanaWallet.publicKey) return null;
+                const publicKey = encodePublicKey(solanaWallet.publicKey);
+                return formatAddress(publicKey, 4);
+            }),
+            onOpenConnectModal: () => connectModalSolana.setVisible(true),
+            onOpenAccountModal: () => connectModalSolana.setVisible(true),
+            isConnected: solanaWallet.connected,
+        },
+    ];
+
+    const activeType = chainTypes.find((type) => type.isConnected);
+    const text = activeType?.label ?? <Trans>Connect Wallet</Trans>;
+    const isConnected = !!activeType;
+
+    const icon = (
+        <div className="h-5 w-5 flex-shrink-0">
+            {collapsed ? <WalletSelectedIcon width={20} height={20} /> : <WalletIcon width={20} height={20} />}
+        </div>
+    );
 
     return (
         <div
             className={classNames(
-                'flex w-full items-center gap-x-3 overflow-hidden rounded-full text-xl hover:cursor-pointer hover:bg-bg md:w-auto',
-                collapsed ? 'p-2' : 'px-4 py-1',
+                'w-full space-y-3 rounded-md hover:bg-bg',
+                {
+                    'bg-lightBg': collapsed,
+                },
+                sideBarCollapsed ? 'p-2' : 'px-4 py-1',
             )}
-            onClick={() => {
-                account.isConnected ? AccountModalRef.open() : ConnectWalletModalRef.open();
+            onClick={(e) => {
+                e.stopPropagation();
             }}
         >
-            {collapsed ? (
-                <Tooltip content={account.address || text} placement="right">
-                    <WalletIcon className="flex-shrink-0" width={20} height={20} />
-                </Tooltip>
-            ) : (
-                <WalletIcon className="flex-shrink-0" width={20} height={20} />
-            )}
-            <span
-                className="overflow-hidden text-ellipsis py-1"
-                style={{ display: collapsed ? 'none' : 'inline' }}
-                title={account.address}
+            <div
+                className={classNames(
+                    'relative flex w-full cursor-pointer select-none items-center gap-x-3 overflow-hidden rounded-full text-xl',
+                )}
+                onClick={() => {
+                    if (isConnected) {
+                        setCollapsed((c) => !c);
+                        return;
+                    }
+                    ConnectWalletModalRef.open();
+                }}
             >
-                {text}
-            </span>
+                {sideBarCollapsed ? (
+                    <Tooltip content={text} placement="right">
+                        {icon}
+                    </Tooltip>
+                ) : (
+                    icon
+                )}
+                <span
+                    className={classNames('overflow-hidden text-ellipsis py-1', {
+                        'font-bold': collapsed,
+                    })}
+                    style={{ display: sideBarCollapsed ? 'none' : 'inline' }}
+                    onClick={(e) => {
+                        if (activeType?.isConnected) {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            activeType.onOpenAccountModal();
+                        }
+                    }}
+                >
+                    {text}
+                </span>
+                {isConnected ? (
+                    <LineArrowUp
+                        className={classNames('absolute right-0 top-1/2 -translate-y-1/2', {
+                            'rotate-180': collapsed,
+                        })}
+                    />
+                ) : null}
+            </div>
+            {isConnected && collapsed ? (
+                <>
+                    {chainTypes.map((type) => {
+                        if (type === activeType) return null;
+                        return (
+                            <button
+                                onClick={type.onOpenConnectModal}
+                                className="flex w-full flex-row items-center gap-3 py-1 text-xl font-bold leading-6"
+                            >
+                                <Image
+                                    src={type.icon ?? ''}
+                                    alt="chain-icon"
+                                    width={20}
+                                    height={20}
+                                    className="h-5 w-5"
+                                />
+                                {type.isConnected ? (
+                                    <span>{type.label}</span>
+                                ) : (
+                                    <span>
+                                        <Trans>Connect Wallet</Trans>
+                                    </span>
+                                )}
+                            </button>
+                        );
+                    })}
+                </>
+            ) : null}
         </div>
     );
 }
--- src/modals/ConnectWalletModal.tsx
+++ src/modals/ConnectWalletModal.tsx
@@ -1,13 +1,87 @@
-import type { SingletonModalRefCreator } from '@masknet/shared-base';
-import { useConnectModal } from '@rainbow-me/rainbowkit';
+import { Trans } from '@lingui/macro';
+import { NetworkPluginID, type SingletonModalRefCreator } from '@masknet/shared-base';
+import { useSingletonModal } from '@masknet/shared-base-ui';
+import { useNetworkDescriptor } from '@masknet/web3-hooks-base';
+import { ChainId as EVMChainId } from '@masknet/web3-shared-evm';
+import { ChainId as SolanaChainId } from '@masknet/web3-shared-solana';
+import { useConnectModal as useConnectModalEVM } from '@rainbow-me/rainbowkit';
+import { useWalletModal as useConnectModalSolana } from '@solana/wallet-adapter-react-ui';
 import { forwardRef } from 'react';
 
-import { useRainbowModal } from '@/hooks/useRainbowModal.js';
+import { CloseButton } from '@/components/CloseButton.js';
+import { Image } from '@/components/Image.js';
+import { Modal } from '@/components/Modal.js';
+import { Popover } from '@/components/Popover.js';
+import { useIsMedium } from '@/hooks/useMediaQuery.js';
 
 export const ConnectWalletModal = forwardRef<SingletonModalRefCreator>(function ConnectWalletModal(_, ref) {
-    const { openConnectModal, connectModalOpen } = useConnectModal();
+    const connectModalEVM = useConnectModalEVM();
+    const connectModalSolana = useConnectModalSolana();
+    const evmNetworkDescriptor = useNetworkDescriptor(NetworkPluginID.PLUGIN_EVM, EVMChainId.Mainnet);
+    const solanaNetworkDescriptor = useNetworkDescriptor(NetworkPluginID.PLUGIN_SOLANA, SolanaChainId.Mainnet);
 
-    useRainbowModal(openConnectModal, connectModalOpen, ref);
+    const [open, dispatch] = useSingletonModal(ref, {});
 
-    return null;
+    const isMedium = useIsMedium();
+
+    function onClose() {
+        dispatch?.close();
+    }
+
+    const chainTypes = [
+        {
+            icon: evmNetworkDescriptor?.icon,
+            label: <Trans>EVM</Trans>,
+            onOpen: () => connectModalEVM.openConnectModal?.(),
+        },
+        {
+            icon: solanaNetworkDescriptor?.icon,
+            label: <Trans>Solana</Trans>,
+            onOpen: () => connectModalSolana.setVisible(true),
+        },
+    ];
+
+    const content = (
+        <div className="grid grid-cols-1 gap-3 p-4 text-sm font-bold leading-5 text-second md:grid-cols-2">
+            {chainTypes.map((chainType) => {
+                return (
+                    <button
+                        className="flex flex-col items-center gap-2 rounded-md px-4 py-6 hover:bg-lightBg hover:text-main"
+                        onClick={() => {
+                            chainType.onOpen();
+                            onClose();
+                        }}
+                    >
+                        <Image src={chainType.icon ?? ''} width={48} height={48} alt="evm" className="h-12 w-12" />
+                        <div>{chainType.label}</div>
+                    </button>
+                );
+            })}
+        </div>
+    );
+
+    if (!isMedium) {
+        return (
+            <Popover open={open} onClose={onClose}>
+                {content}
+            </Popover>
+        );
+    }
+
+    return (
+        <Modal open={open} onClose={onClose}>
+            <div className="transform rounded-[12px] bg-primaryBottom transition-all">
+                <div
+                    className="relative inline-flex items-center justify-center gap-2 rounded-t-[12px] p-4 text-center md:h-[56px] md:w-[600px]"
+                    style={{ background: 'var(--m-modal-title-bg)' }}
+                >
+                    <CloseButton onClick={() => dispatch?.close()} className="absolute left-4 top-4" />
+                    <div className="text-lg font-bold leading-6 text-main">
+                        <Trans>Connect Wallet</Trans>
+                    </div>
+                </div>
+                {content}
+            </div>
+        </Modal>
+    );
 });
