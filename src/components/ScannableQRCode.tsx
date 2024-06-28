52a0e115a1e7ef34805f8330a2f223f22a586eea
dudu0506 <dudu@mask.io>
2024-06-28 18:16:26 +0800

    feat: update QR code background
    https://mask.atlassian.net/browse/FW-1514

--- src/components/ScannableQRCode.tsx
+++ src/components/ScannableQRCode.tsx
@@ -13,13 +13,9 @@ export function ScannableQRCode(props: Props) {
     const { url, scanned, countdown } = props;
     return (
         <div className="relative flex items-center justify-center">
-            <QRCode
-                className={classNames({
-                    'blur-md': countdown === 0 || scanned,
-                })}
-                value={url}
-                size={360}
-            />
+            <div className={classNames('rounded-2xl bg-white p-4', countdown === 0 || scanned ? 'blur-md' : '')}>
+                <QRCode value={url} size={328} />
+            </div>
             {countdown === 0 ? (
                 <ReloadIcon className="absolute inset-0 m-auto text-white" width={48} height={48} />
             ) : null}
