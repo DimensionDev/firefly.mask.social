ff56dfcd29b451383e687773c15e6606c2e01350
Wukong Sun <swkatmask@gmail.com>
2024-07-01 12:31:37 +0800

    fix: click on symbol tag on mobile (#1640)

--- src/app/(normal)/token/[symbol]/page.tsx
+++ src/app/(normal)/token/[symbol]/page.tsx
@@ -1,7 +1,5 @@
 'use client';
-import ComeBackIcon from '@/assets/comeback.svg';
 import { TokenDetail } from '@/components/TokenProfile/TokenDetail.js';
-import { useComeBack } from '@/hooks/useComeback.js';
 
 interface Props {
     params: {
@@ -11,16 +9,5 @@ interface Props {
 
 export default function TokenPage({ params }: Props) {
     const { symbol } = params;
-    const comeback = useComeBack();
-    return (
-        <>
-            <div className="sticky top-0 z-30 flex h-[60px] items-center justify-between border-b border-line bg-primaryBottom px-4">
-                <div className="flex items-center gap-7">
-                    <ComeBackIcon className="cursor-pointer text-lightMain" onClick={comeback} />
-                    <span className="text-xl font-black text-lightMain">${symbol}</span>
-                </div>
-            </div>
-            <TokenDetail symbol={symbol} />
-        </>
-    );
+    return <TokenDetail symbol={symbol} />;
 }
