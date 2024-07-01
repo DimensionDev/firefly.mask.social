bf33940d0d23d81c0d298e4d30864843c64c199f
Wukong Sun <swkatmask@gmail.com>
2024-07-01 18:53:38 +0800

    fix: fw-1547 adjust buttons in nft detail page

--- src/components/NFTDetail/DownloadImageButton.tsx
+++ src/components/NFTDetail/DownloadImageButton.tsx
@@ -16,13 +16,13 @@ export function DownloadImageButton({ url }: { url: string }) {
 
     return (
         <a
-            className="flex cursor-pointer select-none items-center gap-1 rounded-full border border-line bg-lightBg px-2 py-1 text-[10px] leading-[14px]"
+            className="flex cursor-pointer select-none items-center gap-1 rounded-full border border-line bg-lightBg px-2 py-1 font-inter text-xs leading-[14px] hover:bg-primaryBottom"
             href={blobUrl}
             target="_blank"
             download={url}
         >
             <DownloadIcon className="h-3 w-3" />
-            <Trans>Download Image</Trans>
+            <Trans>Download image</Trans>
         </a>
     );
 }
