0419be89700cba281b15b3a7628c09b6b31efd30
guanbinrui <guanbinrui@mask.io>
2024-07-02 17:31:23 +0800

    fix: ci

--- src/components/Blink/index.tsx
+++ src/components/Blink/index.tsx
@@ -3,7 +3,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
 
-import { ActionContainer } from '@/components/Blink/ActionContainer.jsx';
+import { ActionContainer } from '@/components/Blink/index.js';
 import { BlinkLoader } from '@/providers/blink/Loader.js';
 import type { Action } from '@/providers/types/Blink.js';
 
