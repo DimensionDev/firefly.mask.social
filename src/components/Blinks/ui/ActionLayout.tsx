686014ee1c64e8930358b5dc82dee10f3d55d63c
LeifXu <ins@mask.io>
2024-07-02 12:52:39 +0800

    fix: fix issues

--- src/components/Blinks/ui/ActionLayout.tsx
+++ src/components/Blinks/ui/ActionLayout.tsx
@@ -109,7 +109,7 @@ export function ActionLayout({
                 <div className="text-left text-[15px] font-semibold text-main">{title}</div>
                 <div className="whitespace-pre-wrap text-left text-[15px] text-sm text-main">{description}</div>
                 {disclaimer ? <div>{disclaimer}</div> : null}
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col">
                     {buttons && buttons.length > 0 ? (
                         <ul className="flex flex-wrap items-center gap-3">
                             {buttons?.map((it, index) => (
