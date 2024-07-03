6963dba015bbc10dd167b4a4b1eaeffcbb42cc1b
guanbinrui <guanbinrui@mask.io>
2024-07-03 16:50:28 +0800

    fix: ci warning

--- src/helpers/removeAtEnd.ts
+++ src/helpers/removeAtEnd.ts
@@ -1,13 +1,12 @@
 export function removeAtEnd(content: string, fragment: string): string {
-    if (fragment && content) {
-        content = content.trimEnd();
-        const indexOfUrl = content.indexOf(fragment);
-        if (indexOfUrl === content.length - fragment.length) {
-            return content?.replace(fragment, '');
-        } else {
-            return content;
-        }
+    content = content.trimEnd();
+
+    const indexOfUrl = content.indexOf(fragment);
+    if (indexOfUrl === -1) return content;
+
+    if (indexOfUrl === content.length - fragment.length) {
+        return content?.replace(fragment, '');
     } else {
-        return content ?? '';
+        return content;
     }
 }
