2bca9f92b2253956c044e5e30f37c608bbc0fad4
guanbinrui <guanbinrui@mask.io>
2024-07-04 23:35:29 +0800

    refactor: remove unused code

--- src/providers/firefly/SessionHolder.ts
+++ src/providers/firefly/SessionHolder.ts
@@ -1,32 +1,8 @@
 import { fetchJSON } from '@/helpers/fetchJSON.js';
 import { SessionHolder } from '@/providers/base/SessionHolder.js';
-import type { FarcasterSession } from '@/providers/farcaster/Session.js';
-import { farcasterSessionHolder } from '@/providers/farcaster/SessionHolder.js';
 import type { FireflySession } from '@/providers/firefly/Session.js';
-import type { LensSession } from '@/providers/lens/Session.js';
-import { lensSessionHolder } from '@/providers/lens/SessionHolder.js';
 
 class FireflySessionHolder extends SessionHolder<FireflySession> {
-    withLensSession<K extends (host: LensSession | null, self: FireflySession | null) => unknown>(
-        callback: K,
-        required = false,
-    ) {
-        return callback(
-            required ? lensSessionHolder.sessionRequired : lensSessionHolder.session,
-            this.session,
-        ) as ReturnType<K>;
-    }
-
-    withFarcasterSession<K extends (host: FarcasterSession | null, self: FireflySession | null) => unknown>(
-        callback: K,
-        required = false,
-    ) {
-        return callback(
-            required ? farcasterSessionHolder.sessionRequired : farcasterSessionHolder.session,
-            this.session,
-        ) as ReturnType<K>;
-    }
-
     override fetch<T>(url: string, options?: RequestInit, required = false) {
         return this.internalSession || required
             ? fetchJSON<T>(url, {
