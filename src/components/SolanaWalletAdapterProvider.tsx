4b3dfe1b744ac50051d210532dc94cc9d53eca3c
LeifXu <ins@mask.io>
2024-07-08 22:28:56 +0800

    feat: hidden some solana wallets

--- src/components/SolanaWalletAdapterProvider.tsx
+++ src/components/SolanaWalletAdapterProvider.tsx
@@ -7,20 +7,13 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 import { type Adapter, WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import {
-    CoinbaseWalletAdapter,
-    PhantomWalletAdapter,
-    SolflareWalletAdapter,
-    WalletConnectWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter, WalletConnectWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { type PropsWithChildren } from 'react';
 
 import { env } from '@/constants/env.js';
 
 const wallets: Adapter[] = [
     new PhantomWalletAdapter(),
-    new SolflareWalletAdapter(),
-    new CoinbaseWalletAdapter(),
     new WalletConnectWalletAdapter({
         options: {
             projectId: env.external.NEXT_PUBLIC_W3M_PROJECT_ID,
