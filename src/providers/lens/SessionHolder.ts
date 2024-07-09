d27ab840dfd6094a99c38af72869cd50331666eb
guanbinrui <guanbinrui@mask.io>
2024-07-09 16:23:28 +0800

    refactor: lens session holder

--- src/providers/lens/SessionHolder.ts
+++ src/providers/lens/SessionHolder.ts
@@ -1,14 +1,28 @@
+import { LensClient as LensClientSDK } from '@lens-protocol/client';
 import { createLensSDK, LocalStorageProvider, setLensCredentials } from '@/helpers/createLensSDK.js';
 import { SessionHolder } from '@/providers/base/SessionHolder.js';
 import type { LensSession } from '@/providers/lens/Session.js';
 
 export class LensSessionHolder extends SessionHolder<LensSession> {
+    private lensClientSDK: LensClientSDK | null = null;
+
     get sdk() {
-        return createLensSDK(new LocalStorageProvider());
+        if (!this.lensClientSDK) {
+            this.lensClientSDK = createLensSDK(new LocalStorageProvider());
+        }
+        return this.lensClientSDK;
     }
 
     override resumeSession(session: LensSession) {
-        if (session.refreshToken) setLensCredentials(localStorage, session);
+        if (session.refreshToken) {
+            const storage = new LocalStorageProvider();
+
+            // overwrite lens credentials in local storage
+            setLensCredentials(storage, session);
+
+            // renew the sdk instance, since it could possess the old credentials
+            this.lensClientSDK = createLensSDK(storage);
+        }
         super.resumeSession(session);
     }
 }
