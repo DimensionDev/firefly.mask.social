9bb3c1a3e781de70fe529bdcae4b3e106f5e4bdb
Wukong Sun <swkatmask@gmail.com>
2024-07-09 18:00:59 +0800

    fix: fw-1658 avoid icon shrinking in profile settings (#1755)
    * chore: bump version to 5.5.0
    
    * fix: fw-1658 avoid icon shrinking in profile settings
    
    add reorder animation
    
    ---------
    
    Co-authored-by: guanbinrui <guanbinrui@mask.io>

--- src/components/CircleCheckboxIcon.tsx
+++ src/components/CircleCheckboxIcon.tsx
@@ -1,27 +1,24 @@
 import DisableNoIcon from '@/assets/disable-no.svg';
 import YesIcon from '@/assets/yes.svg';
+import { classNames } from '@/helpers/classNames.js';
 
 interface Props extends React.HTMLProps<SVGAElement> {
     size?: number;
     checked: boolean;
 }
 
-export function CircleCheckboxIcon({ size = 40, checked, ...props }: Props) {
+export function CircleCheckboxIcon({ size = 40, checked, className, ...props }: Props) {
+    if (checked) return <YesIcon width={size} height={size} {...props} className={className} />;
+
     return (
-        <>
-            {checked ? (
-                <YesIcon width={size} height={size} {...props} />
-            ) : (
-                <div
-                    className="flex items-center justify-center"
-                    style={{
-                        width: size,
-                        height: size,
-                    }}
-                >
-                    <DisableNoIcon width={size / 2} height={size / 2} {...props} />
-                </div>
-            )}
-        </>
+        <div
+            className={classNames('flex items-center justify-center', className)}
+            style={{
+                width: size,
+                height: size,
+            }}
+        >
+            <DisableNoIcon width={size / 2} height={size / 2} {...props} />
+        </div>
     );
 }
