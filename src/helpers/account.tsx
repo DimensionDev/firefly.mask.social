eeadaedb27cc20c656fc56af7dded02b1869fdf5
guanbinrui <guanbinrui@mask.io>
2024-07-09 16:18:14 +0800

    fix: overwrite sessions

--- src/helpers/account.tsx
+++ src/helpers/account.tsx
@@ -39,9 +39,10 @@ async function updateState(accounts: Account[], overwrite = false) {
     // remove all accounts if overwrite is true
     if (overwrite) {
         SORTED_SOCIAL_SOURCES.forEach((source) => {
-            const state = getProfileState(source);
+            const { state, sessionHolder } = getContext(source);
             state.resetCurrentAccount();
             state.updateAccounts([]);
+            sessionHolder.removeSession();
         });
     }
 
