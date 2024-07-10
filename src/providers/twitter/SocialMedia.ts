15de351b842219d973a8fcde633b594a4ba7d732
Wukong Sun <swkatmask@gmail.com>
2024-07-10 11:23:56 +0800

    fix(Twitter): fw-1672 update query data for blocking/unblocking profile

--- src/providers/twitter/SocialMedia.ts
+++ src/providers/twitter/SocialMedia.ts
@@ -12,6 +12,7 @@ import urlcat from 'urlcat';
 
 import { Source } from '@/constants/enum.js';
 import { NotImplementedError } from '@/constants/error.js';
+import { SetQueryDataForBlockProfile } from '@/decorators/SetQueryDataForBlockProfile.js';
 import { SetQueryDataForFollowProfile } from '@/decorators/SetQueryDataForFollowProfile.js';
 import { formatTweetsPage, tweetV2ToPost } from '@/helpers/formatTwitterPost.js';
 import { formatTwitterProfile } from '@/helpers/formatTwitterProfile.js';
@@ -31,6 +32,7 @@ import {
 import type { ResponseJSON } from '@/types/index.js';
 
 @SetQueryDataForFollowProfile(Source.Twitter)
+@SetQueryDataForBlockProfile(Source.Twitter)
 class TwitterSocialMedia implements Provider {
     async unmirrorPost(postId: string, authorId?: number | undefined): Promise<void> {
         const response = await twitterSessionHolder.fetch<ResponseJSON<void>>(`/api/twitter/unretweet/${postId}`, {
