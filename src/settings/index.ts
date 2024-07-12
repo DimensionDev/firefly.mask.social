1059476c1d15fde5f142b8c4b2a4d6939578c805
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-07-13 01:42:07 +0800

    refactor: developer settings store (#1790)

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,9 +1,9 @@
 import { FIREFLY_DEV_ROOT_URL, FIREFLY_ROOT_URL } from '@/constants/index.js';
-import { useDeveloperSettings } from '@/store/useDeveloperSettings.js';
+import { useDeveloperSettingsState } from '@/store/useDeveloperSettingsStore.js';
 
 class Settings {
     get FIREFLY_ROOT_URL() {
-        return useDeveloperSettings.getState().useDevelopmentAPI ? FIREFLY_DEV_ROOT_URL : FIREFLY_ROOT_URL;
+        return useDeveloperSettingsState.getState().useDevelopmentAPI ? FIREFLY_DEV_ROOT_URL : FIREFLY_ROOT_URL;
     }
 }
 
