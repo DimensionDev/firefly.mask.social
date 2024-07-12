effe1fa868376cd890ac5251974ea95ed06517f2
guanbinrui <guanbinrui@users.noreply.github.com>
2024-07-12 03:07:44 +0000

    chore: eslint

--- src/helpers/getCurrentSourceFromUrl.ts
+++ src/helpers/getCurrentSourceFromUrl.ts
@@ -1,10 +1,5 @@
-import type { StateSnapshot } from 'react-virtuoso';
-import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
-import { immer } from 'zustand/middleware/immer';
 
 import { Source } from '@/constants/enum.js';
-import { createSelectors } from '@/helpers/createSelector.js';
 import { resolveSourceFromUrl } from '@/helpers/resolveSource.js';
 
 export function getCurrentSourceFromUrl() {
