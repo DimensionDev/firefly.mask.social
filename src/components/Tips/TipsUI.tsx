06f6af6193d01bd1324594fc3aa4194e7c491ee8
dudu0506 <dudu@mask.io>
2024-07-16 12:12:51 +0800

    fix: tips ui

--- src/components/Tips/TipsUI.tsx
+++ src/components/Tips/TipsUI.tsx
@@ -1,6 +1,5 @@
 import { t } from '@lingui/macro';
 import { memo, useMemo } from 'react';
-import { useAccount } from 'wagmi';
 
 import { SendWithEVM, SendWithSolana } from '@/components/Tips/SendTipsButton.js';
 import { TipsModalHeader } from '@/components/Tips/TipsModalHeader.js';
@@ -10,13 +9,12 @@ import { NetworkType } from '@/constants/enum.js';
 import { TipsContext } from '@/hooks/useTipsContext.js';
 
 export const TipsUI = memo(function TipsUI() {
-    const account = useAccount();
     const { token, receiver, amount, handle, isSending, update } = TipsContext.useContainer();
 
     const { RE_MATCH_WHOLE_AMOUNT, RE_MATCH_FRACTION_AMOUNT } = useMemo(
         () => ({
-            RE_MATCH_FRACTION_AMOUNT: new RegExp(`^\\.\\d{0,${token?.decimals}}$`),
-            RE_MATCH_WHOLE_AMOUNT: new RegExp(`^\\d*\\.?\\d{0,${token?.decimals}}$`),
+            RE_MATCH_FRACTION_AMOUNT: new RegExp(`^\\.\\d{0,${token?.decimals ?? 18}}$`),
+            RE_MATCH_WHOLE_AMOUNT: new RegExp(`^\\d*\\.?\\d{0,${token?.decimals ?? 18}}$`),
         }),
         [token?.decimals],
     );
@@ -34,20 +32,20 @@ export const TipsUI = memo(function TipsUI() {
         <>
             <TipsModalHeader title={receiver ? t`Tip to @${handle || receiver.displayName}` : undefined} />
             <div className="font-bold">
-                <WalletSelectorEntry disabled={!account.isConnected || isSending} />
+                <WalletSelectorEntry disabled={isSending} />
                 <div className="mt-3 flex gap-x-3">
                     <div className="h-10 flex-1 rounded-2xl bg-lightBg">
                         <input
                             className="h-full w-full border-none bg-transparent text-center outline-none focus:outline-none focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50"
-                            placeholder={token ? t`Max: ${token.balance}` : t`Please select token.`}
+                            placeholder={token ? t`Max: ${token.balance}` : t`Enter amount`}
                             value={amount}
                             autoComplete="off"
                             spellCheck="false"
                             onChange={handleAmountChange}
-                            disabled={!account.isConnected || !token || isSending}
+                            disabled={isSending}
                         />
                     </div>
-                    <TokenSelectorEntry disabled={!account.isConnected || isSending} />
+                    <TokenSelectorEntry disabled={isSending} />
                 </div>
                 {receiver ? receiver.networkType === NetworkType.Ethereum ? <SendWithEVM /> : <SendWithSolana /> : null}
             </div>
