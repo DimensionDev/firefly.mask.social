1b9843c6fd36878e7c96bad196cde33c0fb9c2ca
Wukong Sun <swkatmask@gmail.com>
2024-07-16 16:44:09 +0800

    fixup! refactor: use a slim versioin maskbook

--- src/mask/background-worker/plugin-register.ts
+++ src/mask/background-worker/plugin-register.ts
@@ -1,3 +1,3 @@
-import '@masknet/plugin-debugger/register';
+// import '@masknet/plugin-debugger/register';
 import '@masknet/plugin-redpacket/register';
 import '@masknet/plugin-snapshot/register';
