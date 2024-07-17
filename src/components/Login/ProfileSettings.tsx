6faae6b3c86c6994a40161b0becc7f016b2dbf75
dudu0506 <dudu@mask.io>
2024-07-17 18:23:21 +0800

    fix: show all lens profiles (#1832)
    * fix: show all lens profiles
    
    https://mask.atlassian.net/browse/FW-1785
    
    * chore: prettier
    
    ---------
    
    Co-authored-by: dudu0506 <dudu0506@users.noreply.github.com>

--- src/components/Login/ProfileSettings.tsx
+++ src/components/Login/ProfileSettings.tsx
@@ -4,16 +4,21 @@ import { Trans } from '@lingui/macro';
 import { Reorder } from 'framer-motion';
 import { noop, sortBy } from 'lodash-es';
 import { usePathname } from 'next/navigation.js';
-import { useMount } from 'react-use';
+import { useAsync, useMount } from 'react-use';
 import urlcat from 'urlcat';
 
+import LoadingIcon from '@/assets/loading.svg';
 import { CircleCheckboxIcon } from '@/components/CircleCheckboxIcon.js';
 import { ClickableButton } from '@/components/ClickableButton.js';
 import { ProfileAvatar } from '@/components/ProfileAvatar.js';
 import { ProfileName } from '@/components/ProfileName.js';
+import { queryClient } from '@/configs/queryClient.js';
+import { config } from '@/configs/wagmiClient.js';
 import { PageRoute, type SocialSource, Source } from '@/constants/enum.js';
+import { EMPTY_LIST } from '@/constants/index.js';
 import { switchAccount } from '@/helpers/account.js';
 import { getProfileState } from '@/helpers/getProfileState.js';
+import { getWalletClientRequired } from '@/helpers/getWalletClientRequired.js';
 import { isRoutePathname } from '@/helpers/isRoutePathname.js';
 import { isSameProfile } from '@/helpers/isSameProfile.js';
 import { resolveProfileId } from '@/helpers/resolveProfileId.js';
@@ -21,6 +26,7 @@ import { resolveSourceInURL } from '@/helpers/resolveSourceInURL.js';
 import { useProfileStore } from '@/hooks/useProfileStore.js';
 import { useUpdateParams } from '@/hooks/useUpdateParams.js';
 import { LoginModalRef, LogoutModalRef } from '@/modals/controls.js';
+import { LensSocialMediaProvider } from '@/providers/lens/SocialMedia.js';
 import { useProfileTabState } from '@/store/useProfileTabStore.js';
 
 interface ProfileSettingsProps {
@@ -38,6 +44,24 @@ export function ProfileSettings({ source, onClose }: ProfileSettingsProps) {
     const isMyProfilePage =
         !!profileTab.isMyProfile && (isPureProfilePage || isRoutePathname(pathname, PageRoute.Profile));
 
+    const { value: wrappedAccounts, loading } = useAsync(async () => {
+        if (source !== Source.Lens) return accounts;
+        const { account } = await getWalletClientRequired(config);
+        const profiles = await queryClient.fetchQuery({
+            queryKey: ['lens', 'profiles', account.address],
+            staleTime: 1000 * 60 * 10,
+            queryFn: async () => {
+                if (!account.address) return EMPTY_LIST;
+                return LensSocialMediaProvider.getProfilesByAddress(account.address);
+            },
+        });
+        if (!profiles?.length) return accounts;
+        return profiles.map((profile) => ({
+            ...accounts.find((account) => isSameProfile(account.profile, profile)),
+            profile,
+        }));
+    }, [source, accounts]);
+
     useMount(() => {
         getProfileState(source).refreshAccounts();
     });
@@ -45,7 +69,7 @@ export function ProfileSettings({ source, onClose }: ProfileSettingsProps) {
     if (!currentProfile) return null;
 
     /* the selected account goes first */
-    const sortedAccounts = sortBy(accounts, (x) => (isSameProfile(x.profile, currentProfile) ? -1 : 0));
+    const sortedAccounts = sortBy(wrappedAccounts, (x) => (isSameProfile(x.profile, currentProfile) ? -1 : 0));
 
     return (
         <div className="flex flex-col overflow-x-hidden bg-primaryBottom md:w-[290px] md:rounded-2xl md:border md:border-line">
@@ -53,7 +77,7 @@ export function ProfileSettings({ source, onClose }: ProfileSettingsProps) {
                 values={sortedAccounts}
                 draggable="false"
                 onReorder={noop}
-                className="no-scrollbar max-h-[calc(62.5px*3.5)] overflow-auto md:max-h-[calc(72px*3.5)]"
+                className="no-scrollbar max-h-[calc(62.5px*4.5)] overflow-auto md:max-h-[calc(72px*7.5)]"
             >
                 {sortedAccounts.map((account) => (
                     <Reorder.Item
@@ -64,7 +88,13 @@ export function ProfileSettings({ source, onClose }: ProfileSettingsProps) {
                         <ClickableButton
                             className="flex w-full min-w-0 items-center justify-between gap-3 rounded px-2 py-2 hover:bg-bg md:rounded-none md:px-5"
                             onClick={async () => {
-                                await switchAccount(account);
+                                if (!account.session) {
+                                    return LoginModalRef.open({
+                                        source,
+                                        options: { expectedProfile: account.profile },
+                                    });
+                                }
+                                await switchAccount({ ...account, session: account.session });
                                 if (
                                     isMyProfilePage &&
                                     profileTab.source === source &&
@@ -89,6 +119,11 @@ export function ProfileSettings({ source, onClose }: ProfileSettingsProps) {
                         </ClickableButton>
                     </Reorder.Item>
                 ))}
+                {loading ? (
+                    <div className="flex h-20 items-center justify-center">
+                        <LoadingIcon className="animate-spin" width={24} height={24} />
+                    </div>
+                ) : null}
             </Reorder.Group>
 
             <hr className="mb-3 border-b border-t-0 border-line" />
