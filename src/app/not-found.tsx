b665160aad15e4d15ed3a17b0db16ce89905c181
Wukong Sun <swkatmask@gmail.com>
2024-07-22 11:04:08 +0800

    fix: adjust 404 page style
    fw-1836
    fw-1834

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,13 @@
 import { t, Trans } from '@lingui/macro';
 
 import { BaseNotFound } from '@/components/BaseNotFound.js';
+import { SuggestedChannels } from '@/components/Channel/SuggestedChannels.js';
 import { IfPathname } from '@/components/IfPathname.js';
+import { LinkCloud } from '@/components/LinkCloud.js';
 import { AsideSearchBar } from '@/components/Search/SearchBar.js';
 import { SearchFilter } from '@/components/Search/SearchFilter.js';
+import { SuggestedFollowsCard } from '@/components/SuggestedFollows/SuggestedFollowsCard.js';
+import { Source } from '@/constants/enum.js';
 import { Link } from '@/esm/Link.js';
 import { createPageTitleSSR } from '@/helpers/createPageTitle.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
@@ -17,7 +21,7 @@ export async function generateMetadata() {
 export default function NotFound() {
     return (
         <>
-            <BaseNotFound className="w-[888px] md:pl-[289px]">
+            <BaseNotFound className="min-h-[100vh] flex-grow md:pl-[289px]">
                 <div className="mt-11 text-sm font-bold">
                     <Trans>The page could not be found.</Trans>
                 </div>
@@ -25,13 +29,20 @@ export default function NotFound() {
                     <Trans>Back to home</Trans>
                 </Link>
             </BaseNotFound>
-            <aside className="top-0 z-[1] hidden h-full w-96 px-4 md:sticky lg:block">
+            <aside className="top-0 z-[1] hidden h-full w-96 flex-shrink-0 px-4 md:sticky lg:block">
                 <IfPathname isNotOneOf={['/settings']}>
                     <AsideSearchBar />
                 </IfPathname>
-                <IfPathname isOneOf={['/search']}>
-                    <SearchFilter />
-                </IfPathname>
+                <div className="no-scrollbar flex flex-1 flex-col gap-6 overflow-auto">
+                    <IfPathname isOneOf={['/search']}>
+                        <SearchFilter />
+                    </IfPathname>
+                    <IfPathname isNotOneOf={['/']} exact>
+                        <SuggestedFollowsCard />
+                        <SuggestedChannels source={Source.Farcaster} />
+                    </IfPathname>
+                    <LinkCloud />
+                </div>
             </aside>
         </>
     );
