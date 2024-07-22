cb2132c6cdae974cf58d6ac1fe6271714124d20a
guanbinrui <guanbinrui@mask.io>
2024-07-23 06:36:38 +0800

    fix: err msg

--- src/helpers/getFrameErrorMessage.ts
+++ src/helpers/getFrameErrorMessage.ts
@@ -4,6 +4,11 @@ export function getFrameErrorMessage(error: string) {
     const parsed = parseJSON(error as string);
     if (!parsed) return error;
 
+    const errorIsMessage = parsed as { error: string } | undefined;
+    if (typeof errorIsMessage === 'object' && typeof errorIsMessage.error === 'string') {
+        return errorIsMessage.error;
+    }
+
     const errorWithMessage = parsed as { message: string } | undefined;
     if (typeof errorWithMessage === 'object' && typeof errorWithMessage.message === 'string') {
         return errorWithMessage.message;
