678d6b7330d609929a3c013022cafee653c1bc6b
dudu0506 <dudu@mask.io>
2024-07-22 16:47:27 +0800

    fix: judge native token
    https://mask.atlassian.net/browse/FW-1848

--- src/providers/ethereum/isNativeToken.ts
+++ src/providers/ethereum/isNativeToken.ts
@@ -2,14 +2,14 @@ import { isNativeTokenAddress } from '@masknet/web3-shared-evm';
 import { find } from 'lodash-es';
 import { isAddress } from 'viem';
 
-import { DEBANK_CHAINS } from '@/constants/chain.js';
+import { chains } from '@/configs/wagmiClient.js';
 import type { Token } from '@/providers/types/Transfer.js';
 
 export function isNativeToken(token: Token) {
     // It is a native token when token.id is not an address
     if (!isAddress(token.id)) {
-        const chain = find(DEBANK_CHAINS, (chain) => chain.community_id === token.chainId);
-        return !!chain && chain.native_token_id === token.id;
+        const chain = find(chains, (chain) => chain.id === token.chainId);
+        return !!chain && chain.nativeCurrency.symbol === token.symbol;
     } else {
         return isNativeTokenAddress(token.id);
     }
