2d395d197c2ae2f7287b6b1d45b57afbad5934a6
LeifXu <167842553+LeifXu@users.noreply.github.com>
2024-07-24 14:34:38 +0800

    feat: FW-1878 move communities to more (#1946)
    * feat: FW-1878 move communities to more
    
    * chore: lingui extract
    
    ---------
    
    Co-authored-by: LeifXu <LeifXu@users.noreply.github.com>

--- src/app/(settings)/settings/more/page.tsx
+++ src/app/(settings)/settings/more/page.tsx
@@ -4,8 +4,15 @@ import { t, Trans } from '@lingui/macro';
 
 import { DocumentCard } from '@/app/(settings)/components/DocumentCard.js';
 import { Headline } from '@/app/(settings)/components/Headline.js';
+import { LinkCard } from '@/app/(settings)/components/LinkCard.js';
+import { Subtitle } from '@/app/(settings)/components/Subtitle.js';
+import DiscordIcon from '@/assets/discord.svg';
 import DocumentsIcon from '@/assets/documents.svg';
+import FireflyRoundIcon from '@/assets/firefly.round.svg';
+import MaskRoundIcon from '@/assets/mask.round.svg';
 import SecurityIcon from '@/assets/security.svg';
+import TelegramIcon from '@/assets/telegram.svg';
+import XLightIcon from '@/assets/x-light.svg';
 import { useNavigatorTitle } from '@/hooks/useNavigatorTitle.js';
 
 export default function More() {
@@ -31,6 +38,68 @@ export default function More() {
                     },
                 ].map((document) => (
                     <DocumentCard key={document.href} {...document} />
+                ))}
+            </div>
+
+            <Subtitle>
+                <Trans>Firefly</Trans>
+            </Subtitle>
+
+            <div className="flex w-full flex-col gap-4">
+                {[
+                    {
+                        content: t`Follow @thefireflyapp on X`,
+                        link: 'https://x.com/intent/user?screen_name=thefireflyapp',
+                        logo: XLightIcon,
+                    },
+                    {
+                        content: t`Visit firefly.social`,
+                        link: 'https://firefly.social',
+                        logo: FireflyRoundIcon,
+                    },
+                    {
+                        content: t`Join our Discord`,
+                        link: 'https://discord.com/invite/pufMbBGQZN',
+                        logo: DiscordIcon,
+                    },
+                    {
+                        content: t`Join our Telegram`,
+                        link: 'https://t.me/+mz9T_4YOYhoyYmYx',
+                        logo: TelegramIcon,
+                    },
+                ].map(({ content, link, logo }) => (
+                    <LinkCard key={link} content={content} link={link} logo={logo} />
+                ))}
+            </div>
+
+            <Subtitle>
+                <Trans>Mask Network</Trans>
+            </Subtitle>
+
+            <div className="flex w-full flex-col gap-4">
+                {[
+                    {
+                        content: t`Follow @realMaskNetwork on X`,
+                        link: 'https://x.com/intent/user?screen_name=realMaskNetwork',
+                        logo: XLightIcon,
+                    },
+                    {
+                        content: t`Visit mask.io`,
+                        link: 'https://mask.io',
+                        logo: MaskRoundIcon,
+                    },
+                    {
+                        content: t`Join our Discord`,
+                        link: 'https://discord.com/invite/4SVXvj7',
+                        logo: DiscordIcon,
+                    },
+                    {
+                        content: t`Join our Telegram`,
+                        link: 'https://t.me/maskbook_group#telegram',
+                        logo: TelegramIcon,
+                    },
+                ].map(({ content, link, logo }) => (
+                    <LinkCard key={link} content={content} link={link} logo={logo} />
                 ))}
             </div>
         </div>
