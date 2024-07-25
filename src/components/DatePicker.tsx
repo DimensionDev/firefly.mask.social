63a22b6cee34eb07161fdeed8da138f7fe1c2314
nuanyang233 <nuanyang233@gmail.com>
2024-07-25 18:23:58 +0800

    fix: replace popover to tippy (#1978)

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,11 +1,11 @@
-import { Popover, Transition } from '@headlessui/react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { type DateCalendarProps } from '@mui/x-date-pickers/DateCalendar';
 import dayjs from 'dayjs';
-import { Fragment, memo, type PropsWithChildren } from 'react';
+import { memo, type PropsWithChildren,useState } from 'react';
 
+import { Tippy } from '@/esm/Tippy.js';
 import { classNames } from '@/helpers/classNames.js';
 
 interface DatePickerProps extends PropsWithChildren<DateCalendarProps<dayjs.Dayjs>> {
@@ -21,43 +21,43 @@ export const DatePicker = memo<DatePickerProps>(function DatePicker({
     panelClassName,
     ...props
 }) {
+    const [visible, setVisible] = useState(false);
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Popover as="div" className={classNames('relative', containerClassName)}>
-                {({ close }) => (
-                    <>
-                        <Popover.Button className="w-full">
-                            <span className={className}>{children}</span>
-                        </Popover.Button>
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-200"
-                            enterFrom="opacity-0 translate-y-1"
-                            enterTo="opacity-100"
-                            leave="transition ease-in duration-150"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0 translate-y-1"
-                        >
-                            <Popover.Panel
-                                static
-                                className={classNames(
-                                    'absolute bottom-full left-0 z-50 flex flex-col gap-2 rounded-lg bg-lightBottom shadow-popover dark:border dark:border-line dark:bg-darkBottom dark:shadow-none',
-                                    panelClassName,
-                                )}
-                            >
-                                <DateCalendar
-                                    {...props}
-                                    classes={{ root: 'custom-date-picker' }}
-                                    onChange={(...args) => {
-                                        close();
-                                        props.onChange?.(...args);
-                                    }}
-                                />
-                            </Popover.Panel>
-                        </Transition>
-                    </>
-                )}
-            </Popover>
+            <Tippy
+                visible={visible}
+                onClickOutside={() => setVisible(false)}
+                appendTo={() => document.body}
+                content={
+                    <div
+                        className={classNames(
+                            'rounded-lg bg-lightBottom shadow-popover dark:border dark:border-line dark:bg-darkBottom dark:shadow-none',
+                            panelClassName,
+                        )}
+                    >
+                        <DateCalendar
+                            {...props}
+                            classes={{ root: 'custom-date-picker' }}
+                            onChange={(...args) => {
+                                close();
+                                props.onChange?.(...args);
+                            }}
+                        />
+                    </div>
+                }
+                className="tippy-card"
+                placement="bottom-end"
+                duration={500}
+                delay={500}
+                arrow={false}
+                trigger="click"
+                hideOnClick
+                interactive
+            >
+                <div className="w-full" onClick={() => setVisible(!visible)}>
+                    <span className={className}>{children}</span>
+                </div>
+            </Tippy>
         </LocalizationProvider>
     );
 });
