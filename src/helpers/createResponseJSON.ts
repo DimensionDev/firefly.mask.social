558a4358ed484b0b1162563ac9541ab149729d39
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-07-25 19:01:45 +0800

    fix: no dynamic headers (#1986)

--- src/helpers/createResponseJSON.ts
+++ src/helpers/createResponseJSON.ts
@@ -1,11 +1,10 @@
 import { getReasonPhrase, StatusCodes } from 'http-status-codes';
 
-export function createResponseJSON(data: unknown, init?: ResponseInit) {
+export function createResponseJSON(data: unknown, init?: Omit<ResponseInit, 'headers'>) {
     const status = init?.status ?? StatusCodes.OK;
 
     return Response.json(data, {
         status,
         statusText: getReasonPhrase(status),
-        headers: init?.headers,
     });
 }
