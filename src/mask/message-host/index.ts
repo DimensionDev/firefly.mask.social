c454b28a41bc947413e05c64c762e02d609a24d2
Wukong Sun <swkatmask@gmail.com>
2024-07-26 10:45:33 +0800

    refactor: use a slim version maskbook (#1817)
    * refactor: use a slim versioin maskbook
    
    * fixup! refactor: use a slim versioin maskbook
    
    * refactor: remove snapshot
    
    ---------
    
    Co-authored-by: guanbinrui <guanbinrui@mask.io>

--- src/mask/message-host/index.ts
+++
@@ -1,8 +0,0 @@
-import type { DebuggerMessages } from '@masknet/plugin-debugger/messages';
-import type { PluginMessageEmitter } from '@masknet/plugin-infra';
-
-export let PluginDebuggerMessages: PluginMessageEmitter<DebuggerMessages> | undefined;
-
-export function setPluginDebuggerMessages(messages: PluginMessageEmitter<DebuggerMessages>) {
-    PluginDebuggerMessages = messages;
-}
