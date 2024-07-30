6b6a88c238db192ca519cb1d529e3993781704fa
LeifXu <167842553+LeifXu@users.noreply.github.com>
2024-07-30 22:40:34 +0800

    feat: follow back (#2028)

--- src/components/Profile/FollowButton.tsx
+++ src/components/Profile/FollowButton.tsx
@@ -21,6 +21,7 @@ interface FollowButtonProps extends Omit<ClickableButtonProps, 'children'> {
     profile: Profile;
     variant?: 'text' | 'icon';
     hasMutedButton?: boolean;
+    hasFollowBack?: boolean;
 }
 
 export const FollowButton = memo(function FollowButton({
@@ -28,6 +29,7 @@ export const FollowButton = memo(function FollowButton({
     profile,
     className,
     hasMutedButton = true,
+    hasFollowBack = true,
     ...rest
 }: FollowButtonProps) {
     const [hovering, setHovering] = useState(false);
@@ -36,7 +38,7 @@ export const FollowButton = memo(function FollowButton({
     const muted = useIsProfileMuted(profile, hasMutedButton);
 
     const isFollowing = !!profile.viewerContext?.following;
-    const isFollowingBy = !!profile.viewerContext?.followedBy;
+    const isFollowingBy = hasFollowBack && !!profile.viewerContext?.followedBy;
     const buttonText = useMemo(() => {
         if (variant === 'text') {
             if (isFollowing) return hovering && !loading ? t`Unfollow` : t`Following`;
