0d252776c7ad04d7cb36bc3a6688f448acc022d6
Wukong Sun <swkatmask@gmail.com>
2024-08-02 12:46:58 +0800

    fixup! refactor: warping a span inside tooltip might make layout unexpected (#2050)

--- src/components/Compose/SchedulePostEntryButton.tsx
+++ src/components/Compose/SchedulePostEntryButton.tsx
@@ -1,6 +1,6 @@
 import { Trans } from '@lingui/macro';
 import dayjs from 'dayjs';
-import { type HTMLProps, useCallback } from 'react';
+import { forwardRef, type HTMLProps, useCallback } from 'react';
 
 import ScheduleIcon from '@/assets/schedule.svg';
 import { SchedulePostSettings } from '@/components/Compose/SchedulePostSettings.js';
@@ -13,45 +13,51 @@ interface SchedulePostEntryButtonProps extends HTMLProps<HTMLDivElement> {
     showText?: boolean;
 }
 
-export function SchedulePostEntryButton({ className, showText }: SchedulePostEntryButtonProps) {
-    const isMedium = useIsMedium();
-    const { scheduleTime } = useComposeScheduleStateStore();
+export const SchedulePostEntryButton = forwardRef<HTMLDivElement, SchedulePostEntryButtonProps>(
+    function SchedulePostEntryButton({ className, showText }: SchedulePostEntryButtonProps, ref) {
+        const isMedium = useIsMedium();
+        const { scheduleTime } = useComposeScheduleStateStore();
 
-    const handleClick = useCallback(() => {
-        const action = scheduleTime ? 'update' : 'create';
+        const handleClick = useCallback(() => {
+            const action = scheduleTime ? 'update' : 'create';
 
-        if (isMedium) {
-            SchedulePostModalRef.open({
-                action,
-            });
-        } else {
-            DraggablePopoverRef.open({
-                content: <SchedulePostSettings action={action} onClose={() => DraggablePopoverRef.close()} />,
-                enableOverflow: false,
-            });
+            if (isMedium) {
+                SchedulePostModalRef.open({
+                    action,
+                });
+            } else {
+                DraggablePopoverRef.open({
+                    content: <SchedulePostSettings action={action} onClose={() => DraggablePopoverRef.close()} />,
+                    enableOverflow: false,
+                });
+            }
+        }, [scheduleTime, isMedium]);
+
+        if (showText) {
+            return (
+                <div
+                    className="mb-3 flex items-center gap-[10px] text-[13px] text-second"
+                    onClick={handleClick}
+                    ref={ref}
+                >
+                    <ScheduleIcon className={classNames('cursor-pointer', className)} />
+                    <span>
+                        <Trans>
+                            Will send on{' '}
+                            <span>
+                                {dayjs(scheduleTime).format('ddd, MMM DD, YYYY')} at{' '}
+                                <span>{dayjs(scheduleTime).format('hh:mm A')}</span>
+                            </span>
+                        </Trans>
+                    </span>
+                </div>
+            );
         }
-    }, [scheduleTime, isMedium]);
 
-    if (showText) {
         return (
-            <div className="mb-3 flex items-center gap-[10px] text-[13px] text-second" onClick={handleClick}>
-                <ScheduleIcon className={classNames('cursor-pointer', className)} />
-                <span>
-                    <Trans>
-                        Will send on{' '}
-                        <span>
-                            {dayjs(scheduleTime).format('ddd, MMM DD, YYYY')} at{' '}
-                            <span>{dayjs(scheduleTime).format('hh:mm A')}</span>
-                        </span>
-                    </Trans>
-                </span>
+            <div className="flex items-center gap-[10px] text-[13px] text-second" ref={ref}>
+                <ScheduleIcon className={classNames('cursor-pointer', className)} onClick={handleClick} />
             </div>
         );
-    }
-
-    return (
-        <div className="flex items-center gap-[10px] text-[13px] text-second">
-            <ScheduleIcon className={classNames('cursor-pointer', className)} onClick={handleClick} />
-        </div>
-    );
-}
+    },
+);
