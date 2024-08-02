a1dd0f3206579caa501135c4fecca85bb853ecd5
Wukong Sun <swkatmask@gmail.com>
2024-08-02 14:44:03 +0800

    fix: use the original qrcode (#2067)
    * fix: use the original qrcode
    
    * fixup! refactor: warping a span inside tooltip might make layout unexpected (#2050)
    
    * fixup! fix: use the original qrcode

--- src/components/ScannableQRCode.tsx
+++ src/components/ScannableQRCode.tsx
@@ -1,4 +1,4 @@
-import { QRCode } from 'react-qrcode-logo';
+import QRCode from 'react-qr-code';
 
 import ReloadIcon from '@/assets/reload.svg';
 import { classNames } from '@/helpers/classNames.js';
@@ -13,13 +13,8 @@ export function ScannableQRCode(props: Props) {
     const { url, scanned, countdown } = props;
     return (
         <div className="relative flex items-center justify-center">
-            <div
-                className={classNames(
-                    'overflow-hidden rounded-2xl bg-white shadow-[0_0_20px_0_rgba(0,0,0,0.05)] backdrop-blur',
-                    countdown === 0 || scanned ? 'blur-md' : '',
-                )}
-            >
-                <QRCode value={url} ecLevel="H" size={238} quietZone={16} eyeRadius={150} qrStyle="dots" />
+            <div className={classNames('rounded-2xl bg-white p-4', countdown === 0 || scanned ? 'blur-md' : '')}>
+                <QRCode value={url} size={238} />
             </div>
             {countdown === 0 ? (
                 <ReloadIcon className="absolute inset-0 m-auto text-white" width={80} height={80} />
