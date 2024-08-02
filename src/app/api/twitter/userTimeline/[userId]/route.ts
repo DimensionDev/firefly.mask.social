0d35a9bc0ebf5bfa2c330a2f2f71292925073f8e
LeifXu <ins@mask.io>
2024-08-02 22:36:56 +0800

    fix: FW-1997

--- src/app/api/twitter/userTimeline/[userId]/route.ts
+++ src/app/api/twitter/userTimeline/[userId]/route.ts
@@ -21,7 +21,7 @@ export const GET = compose<(request: NextRequest, context?: NextRequestContext) 
         const queryParams = getSearchParamsFromRequestWithZodObject(request, Pageable);
 
         const client = await createTwitterClientV2(request);
-        const { data, errors } = await client.v2.userTimeline(userId, {
+        const { data } = await client.v2.userTimeline(userId, {
             ...TWITTER_TIMELINE_OPTIONS,
             pagination_token: queryParams.cursor ? queryParams.cursor : undefined,
             max_results: queryParams.limit,
