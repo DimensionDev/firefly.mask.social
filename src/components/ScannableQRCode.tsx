6d8501bdb084217ac4a34a9aec7fe4c4d8f124ad
Wukong Sun <swkatmask@gmail.com>
2024-08-02 14:14:21 +0800

    fixup! fix: use the original qrcode

--- src/components/ScannableQRCode.tsx
+++ src/components/ScannableQRCode.tsx
@@ -13,12 +13,7 @@ export function ScannableQRCode(props: Props) {
     const { url, scanned, countdown } = props;
     return (
         <div className="relative flex items-center justify-center">
-            <div
-                className={classNames(
-                    'overflow-hidden rounded-2xl bg-white shadow-[0_0_20px_0_rgba(0,0,0,0.05)] backdrop-blur',
-                    countdown === 0 || scanned ? 'blur-md' : '',
-                )}
-            >
+            <div className={classNames('rounded-2xl bg-white p-4', countdown === 0 || scanned ? 'blur-md' : '')}>
                 <QRCode value={url} size={238} />
             </div>
             {countdown === 0 ? (
