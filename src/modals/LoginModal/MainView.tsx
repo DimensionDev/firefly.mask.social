7e17fd80b615ddd80b12d2aa8e3402ce6bb5aa17
Wukong Sun <swkatmask@gmail.com>
2024-08-02 10:57:09 +0800

    fixup! feat: fw-1646 refine compose ui

--- src/modals/LoginModal/MainView.tsx
+++ src/modals/LoginModal/MainView.tsx
@@ -3,7 +3,6 @@ import { useRouter } from '@tanstack/react-router';
 import urlcat from 'urlcat';
 
 import { LoginButton } from '@/components/Login/LoginButton.js';
-import { LoginFirefly } from '@/components/Login/LoginFirefly.js';
 import { FarcasterSignType, Source } from '@/constants/enum.js';
 import { SORTED_SOCIAL_SOURCES } from '@/constants/index.js';
 import { resolveSourceInURL } from '@/helpers/resolveSourceInURL.js';
@@ -15,7 +14,7 @@ export function MainView() {
     return (
         <div className="flex flex-col rounded-[12px] bg-primaryBottom md:w-[500px]">
             <div className="flex w-full flex-col md:gap-3 md:p-6 md:pt-0">
-                <LoginFirefly />
+                {/* <LoginFirefly /> */}
                 <p className="text-center text-xs leading-4 text-second">
                     <Trans>Or login with any social account</Trans>
                 </p>
