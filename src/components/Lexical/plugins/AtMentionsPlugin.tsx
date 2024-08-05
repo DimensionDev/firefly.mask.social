cc791a31b1f0639210a68bc19bf47ad1e11b8eeb
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-08-05 12:18:12 +0800

    refactor: types (#2079)

--- src/components/Lexical/plugins/AtMentionsPlugin.tsx
+++ src/components/Lexical/plugins/AtMentionsPlugin.tsx
@@ -170,7 +170,7 @@ export function MentionsPlugin(): JSX.Element | null {
     const ref = useRef<HTMLDivElement>(null);
     const [open, setOpen] = useState(false);
     const currentProfileAll = useCurrentProfileAll();
-    const isUpdatingMentionTag = useRef<boolean>(false);
+    const isUpdatingMentionTag = useRef(false);
     const matchedNodeCache = useRef<AutoLinkNode | null>(null);
 
     const { availableSources } = useCompositePost();
