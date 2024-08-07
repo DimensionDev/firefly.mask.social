e7a479392e0036c3d331844440afe0ec6c01b964
Wukong Sun <swkatmask@gmail.com>
2024-08-07 14:53:40 +0800

    fix: hide login firefly qrcode on mobile (#2143)
    * fix: hide login firefly qrcode on mobile
    
    * fix: radio on button

--- src/modals/LoginModal/MainView.tsx
+++ src/modals/LoginModal/MainView.tsx
@@ -7,18 +7,24 @@ import { LoginFirefly } from '@/components/Login/LoginFirefly.js';
 import { FarcasterSignType, Source } from '@/constants/enum.js';
 import { SORTED_SOCIAL_SOURCES } from '@/constants/index.js';
 import { resolveSourceInURL } from '@/helpers/resolveSourceInURL.js';
+import { useIsMedium } from '@/hooks/useMediaQuery.js';
 
 export function MainView() {
     const router = useRouter();
     const { history } = router;
+    const isMedium = useIsMedium();
 
     return (
         <div className="flex flex-col rounded-[12px] bg-primaryBottom md:w-[500px]">
             <div className="flex w-full flex-col md:gap-3 md:p-6 md:pt-0">
-                <LoginFirefly />
-                <p className="text-center text-xs leading-4 text-second">
-                    <Trans>Or login with any social account</Trans>
-                </p>
+                {isMedium ? (
+                    <>
+                        <LoginFirefly />
+                        <p className="text-center text-xs leading-4 text-second">
+                            <Trans>Or login with any social account</Trans>
+                        </p>
+                    </>
+                ) : null}
                 <div className="flex w-full flex-col md:flex-row md:gap-3">
                     {SORTED_SOCIAL_SOURCES.map((source) => (
                         <LoginButton
