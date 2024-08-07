c28f010b37ddc3fdab70d0cd31de7609870768f6
Wukong Sun <swkatmask@gmail.com>
2024-08-07 09:46:33 +0800

    fix: fw-2029 chain icon

--- src/components/TokenProfile/useChainInfo.ts
+++ src/components/TokenProfile/useChainInfo.ts
@@ -6,6 +6,6 @@ import type { Runtime } from '@/providers/types/Trending.js';
 
 export function useChainInfo(runtime: Runtime | undefined, chainId: number | undefined) {
     const chain = useNetworkDescriptor(NetworkPluginID.PLUGIN_EVM, chainId);
-    if (runtime === 'ethereum' && chain) return chain;
+    if (chain) return chain;
     return chainInfos.find((x) => x.runtime === runtime);
 }
