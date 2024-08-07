583ffd189da7e50d305b3840fb584eb6904feba8
guanbinrui <guanbinrui@mask.io>
2024-08-07 16:41:27 +0800

    fix: max cdn no more support twmoji

--- src/helpers/loadTwemojiUrls.ts
+++ src/helpers/loadTwemojiUrls.ts
@@ -2,12 +2,16 @@ import { parse } from 'twemoji-parser';
 
 import { fetchText } from '@/helpers/fetchText.js';
 
+function fixMaxCDN(url: string) {
+    return url.replace('https://twemoji.maxcdn.com/v/latest/svg/', 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/');
+}
+
 export async function loadTwemojiUrls(content: string) {
     const allSettled = await Promise.allSettled(
         parse(content).map(async (emoji) => {
             if (emoji.type !== 'emoji' || !emoji.url) return [];
 
-            const text = await fetchText(emoji.url, {
+            const text = await fetchText(fixMaxCDN(emoji.url), {
                 cache: 'force-cache',
             });
             if (!text) return [];
