de0bda1e48cd4d6f35dfe8e18af489fd341caf1a
guanbinrui <guanbinrui@users.noreply.github.com>
2024-08-08 01:50:58 +0000

    chore: prettier

--- src/helpers/isSameFireflyIdentity.ts
+++ src/helpers/isSameFireflyIdentity.ts
@@ -1,10 +1,10 @@
-import { Source } from "@/constants/enum.js";
-import { isSameAddress } from "@/helpers/isSameAddress.js";
-import type { FireflyIdentity, FireflyProfile } from "@/providers/types/Firefly.js";
+import { Source } from '@/constants/enum.js';
+import { isSameAddress } from '@/helpers/isSameAddress.js';
+import type { FireflyIdentity, FireflyProfile } from '@/providers/types/Firefly.js';
 
 export function isSameFireflyIdentity(identity?: FireflyIdentity, otherIdentity?: FireflyIdentity) {
     if (!identity || !otherIdentity) return false;
-    
+
     if (identity.source === Source.Wallet || otherIdentity.source === Source.Wallet) {
         return isSameAddress(identity.id, otherIdentity.id);
     }
@@ -14,5 +14,8 @@ export function isSameFireflyIdentity(identity?: FireflyIdentity, otherIdentity?
 
 export function isSameFireflyProfile(profile?: FireflyProfile, otherProfile?: FireflyProfile) {
     if (!profile || !otherProfile) return false;
-    return isSameFireflyIdentity({source: profile.source, id: profile.identity}, {source: otherProfile.source, id: otherProfile.identity});
+    return isSameFireflyIdentity(
+        { source: profile.source, id: profile.identity },
+        { source: otherProfile.source, id: otherProfile.identity },
+    );
 }
