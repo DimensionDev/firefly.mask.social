6ecfb611e1f6f3a1dcdc7e6f55d199376de3f95e
guanbinrui <guanbinrui@mask.io>
2024-08-08 23:07:05 +0800

    refactor: code stlye

--- src/services/getAllPlatformProfileFromFirefly.ts
+++ src/services/getAllPlatformProfileFromFirefly.ts
@@ -5,14 +5,15 @@ import { fireflySessionHolder } from '@/providers/firefly/SessionHolder.js';
 import type { WalletProfileResponse } from '@/providers/types/Firefly.js';
 import { settings } from '@/settings/index.js';
 
+const QUERY_KEY_MAP: { [key in Source]?: string } = {
+    [Source.Lens]: 'lensHandle',
+    [Source.Farcaster]: 'fid',
+    [Source.Wallet]: 'walletAddress',
+    [Source.Twitter]: 'twitterId',
+};
+
 export async function getAllPlatformProfileFromFirefly(source: Source, identity: string) {
-    const queryKeyMap: { [key in Source]?: string } = {
-        [Source.Lens]: 'lensHandle',
-        [Source.Farcaster]: 'fid',
-        [Source.Wallet]: 'walletAddress',
-        [Source.Twitter]: 'twitterId',
-    };
-    const queryKey = queryKeyMap[source] ?? '';
+    const queryKey = QUERY_KEY_MAP[source] ?? '';
 
     const url = urlcat(settings.FIREFLY_ROOT_URL, '/v2/wallet/profile', queryKey ? { [`${queryKey}`]: identity } : {});
 
