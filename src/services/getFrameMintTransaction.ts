bedd5afa4201fd219d97dc2fcc51b9435c50fc19
guanbinrui <guanbinrui@mask.io>
2024-08-09 16:52:35 +0800

    refactor: remove error

--- src/services/getFrameMintTransaction.ts
+++ src/services/getFrameMintTransaction.ts
@@ -1,11 +1,10 @@
 import type { SendTransactionParameters } from 'viem';
 
-import { NotImplementedError } from '@/constants/error.js';
 import type { Frame, FrameButton } from '@/types/frame.js';
 
 export async function getFrameMintTransaction(
     frame: Frame,
     button: FrameButton,
 ): Promise<SendTransactionParameters | null> {
-    throw new NotImplementedError();
+    return null;
 }
