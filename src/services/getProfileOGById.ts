6d1d11a2aaf40b7b6b5c3e06afd75145305be5f5
LeifXu <ins@mask.io>
2024-08-09 15:29:50 +0800

    fix: wallet profile

--- src/services/getProfileOGById.ts
+++ src/services/getProfileOGById.ts
@@ -7,8 +7,8 @@ import { getProfileUrl } from '@/helpers/getProfileUrl.js';
 import { resolveSocialSource } from '@/helpers/resolveSource.js';
 import { getProfileById } from '@/services/getProfileById.js';
 
-export async function getProfileOGById(sourceInUrl: SocialSourceInURL, profileId: string) {
-    const profile = await getProfileById(resolveSocialSource(sourceInUrl), profileId).catch(() => null);
+export async function getProfileOGById(source: SocialSourceInURL, profileId: string) {
+    const profile = await getProfileById(resolveSocialSource(source), profileId).catch(() => null);
     if (!profile) return createSiteMetadata();
 
     const images = [
