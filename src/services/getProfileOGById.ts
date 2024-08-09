adad98db0d8f6a5de24fdc45b1405a83788e54d3
LeifXu <ins@mask.io>
2024-08-09 15:16:11 +0800

    fix: wallet profile

--- src/services/getProfileOGById.ts
+++ src/services/getProfileOGById.ts
@@ -1,39 +1,44 @@
 import urlcat from 'urlcat';
 
-import type { SocialSource, SocialSourceInURL } from '@/constants/enum.js';
+import type { SocialSourceInURL } from '@/constants/enum.js';
 import { SITE_URL } from '@/constants/index.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 import { getProfileUrl } from '@/helpers/getProfileUrl.js';
-import { resolveSource } from '@/helpers/resolveSource.js';
+import { narrowToSocialSourceInURL } from '@/helpers/narrowToSocialSource.js';
+import { resolveSocialSource } from '@/helpers/resolveSource.js';
 import { getProfileById } from '@/services/getProfileById.js';
 
 export async function getProfileOGById(sourceInUrl: SocialSourceInURL, profileId: string) {
-    const source = resolveSource(sourceInUrl);
-    const profile = await getProfileById(source as SocialSource, profileId).catch(() => null);
-    if (!profile) return createSiteMetadata();
+    try {
+        const source = resolveSocialSource(narrowToSocialSourceInURL(sourceInUrl));
+        const profile = await getProfileById(source, profileId).catch(() => null);
+        if (!profile) return createSiteMetadata();
 
-    const images = [
-        {
-            url: profile.pfp,
-        },
-    ];
+        const images = [
+            {
+                url: profile.pfp,
+            },
+        ];
 
-    const title = `@${profile.handle} in Firefly`;
-    const description = profile.bio ?? '';
+        const title = `@${profile.handle} in Firefly`;
+        const description = profile.bio ?? '';
 
-    return createSiteMetadata({
-        openGraph: {
-            type: 'profile',
-            url: urlcat(SITE_URL, getProfileUrl(profile)),
-            title,
-            description,
-            images,
-        },
-        twitter: {
-            card: 'summary',
-            title,
-            description,
-            images,
-        },
-    });
+        return createSiteMetadata({
+            openGraph: {
+                type: 'profile',
+                url: urlcat(SITE_URL, getProfileUrl(profile)),
+                title,
+                description,
+                images,
+            },
+            twitter: {
+                card: 'summary',
+                title,
+                description,
+                images,
+            },
+        });
+    } catch {
+        return createSiteMetadata();
+    }
 }
