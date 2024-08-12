b878fc726f22259a93c88589e5174d3ec7eb0062
dudu0506 <dudu@mask.io>
2024-08-12 15:26:09 +0800

    fix: optimize compose ui (#2225)

--- src/components/Compose/PostBy.tsx
+++ src/components/Compose/PostBy.tsx
@@ -34,7 +34,7 @@ export function PostBy(props: PostByProps) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0 translate-y-1"
         >
-            <Popover.Panel className="absolute bottom-full right-0 z-10 flex w-[280px] -translate-y-3 flex-col gap-2 rounded-lg bg-bgModal p-3 text-[15px] shadow-popover dark:border dark:border-line dark:shadow-none">
+            <Popover.Panel className="no-scrollbar absolute bottom-full right-0 z-10 flex max-h-[275px] w-[280px] -translate-y-3 flex-col gap-2 overflow-y-auto rounded-lg bg-bgModal p-3 text-[15px] shadow-popover dark:border dark:border-line dark:shadow-none md:max-h-[370px]">
                 {SORTED_SOCIAL_SOURCES.map((source, index) => (
                     <PostByItem key={source} source={source} disabled={postByDisabled[index]} />
                 ))}
