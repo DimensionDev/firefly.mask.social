576a1da87297fccd2c741bcaa5308245729322d4
Wukong Sun <swkatmask@gmail.com>
2024-08-15 21:22:13 +0800

    fix: fw-2110 adjust profile list item

--- src/components/ProfileInList.tsx
+++ src/components/ProfileInList.tsx
@@ -28,29 +28,45 @@ export function ProfileInList({ profile, noFollowButton, listKey, index }: Profi
 
     const setScrollIndex = useGlobalState.use.setScrollIndex();
 
+    const handleClickOnLink = () => {
+        if (listKey && !isUndefined(index)) setScrollIndex(listKey, index);
+    };
+
+    const profileUrl = getProfileUrl(profile);
+
     return (
-        <div className="flex-start flex cursor-pointer overflow-auto border-b border-secondaryLine p-3 hover:bg-bg dark:border-line">
-            <Link
-                onClick={() => {
-                    if (listKey && !isUndefined(index)) setScrollIndex(listKey, index);
-                }}
-                className="flex-start flex flex-1 gap-3 overflow-auto"
-                href={getProfileUrl(profile)}
-            >
+        <div className="flex-start flex gap-3 overflow-auto border-b border-secondaryLine p-3 hover:bg-bg dark:border-line">
+            <Link onClick={handleClickOnLink} className="shrink-0" href={profileUrl}>
                 <Avatar
-                    className="shrink-0 rounded-full border"
+                    className="rounded-full border"
                     src={profile.pfp}
                     size={isSmall ? 40 : 44}
                     alt={profile.displayName}
                 />
-
-                <div className="flex-start flex flex-1 flex-col overflow-auto">
-                    <p className="flex-start flex items-center text-sm font-bold leading-5">
-                        <span className="mr-2 truncate text-xl">{profile.displayName}</span>
-                        <SocialSourceIcon className="shrink-0" source={profile.source} />
-                    </p>
-                    {profile.handle ? <p className="text-sm text-secondary">@{profile.handle}</p> : null}
-                    {profile.bio ? (
+            </Link>
+            <div className="min-w-0 flex-grow">
+                <div className="flex-start flex gap-3">
+                    <div className="flex-start flex flex-1 flex-col overflow-auto">
+                        <p className="flex items-center gap-2 text-sm font-bold leading-5">
+                            <Link className="truncate text-xl" href={profileUrl}>
+                                {profile.displayName}
+                            </Link>
+                            <SocialSourceIcon className="shrink-0" source={profile.source} />
+                        </p>
+                        {profile.handle ? (
+                            <Link
+                                className="self-start text-sm text-secondary"
+                                href={profileUrl}
+                                onClick={handleClickOnLink}
+                            >
+                                @{profile.handle}
+                            </Link>
+                        ) : null}
+                    </div>
+                    {!noFollowButton && !isCurrentProfile(profile) ? <FollowButton profile={profile} /> : null}
+                </div>
+                {profile.bio ? (
+                    <Link href={profileUrl} onClick={handleClickOnLink}>
                         <BioMarkup
                             className="mt-1.5 truncate text-sm"
                             components={overrideComponents}
@@ -58,12 +74,8 @@ export function ProfileInList({ profile, noFollowButton, listKey, index }: Profi
                         >
                             {profile.bio}
                         </BioMarkup>
-                    ) : null}
-                </div>
-            </Link>
-
-            <div className="ml-2 flex-shrink-0">
-                {!noFollowButton && !isCurrentProfile(profile) ? <FollowButton profile={profile} /> : null}
+                    </Link>
+                ) : null}
             </div>
         </div>
     );
