8e0f50cea17576b2635b6134690506e3b8fbb29e
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-08-15 13:42:16 +0800

    refactor: viewer id (#2280)

--- src/services/addPostViews.ts
+++ src/services/addPostViews.ts
@@ -1,15 +1,14 @@
 import { bom } from '@/helpers/bom.js';
-import { hydrateLeafwatchViewerId } from '@/store/useLeafwatchPersistStore.js';
+import { useLeafwatchPersistStore } from '@/store/useLeafwatchPersistStore.js';
 
 export function addPostViews(id: string) {
     const controller = bom.navigator?.serviceWorker.controller;
-    const viewerId = hydrateLeafwatchViewerId();
 
     if (id && controller) {
         controller.postMessage({
             type: 'PUBLICATION_VISIBLE',
             id,
-            viewerId,
+            viewerId: useLeafwatchPersistStore.getState().hydrateLeafwatchViewerId(),
         });
     }
 }
