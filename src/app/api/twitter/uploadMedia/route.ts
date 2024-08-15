5f4406b2c93b83355958c0dbde2f711fd016bbfc
guanbinrui <guanbinrui@mask.io>
2024-08-15 15:06:48 +0800

    refactor: code style

--- src/app/api/twitter/uploadMedia/route.ts
+++ src/app/api/twitter/uploadMedia/route.ts
@@ -27,11 +27,11 @@ export const POST = compose<(request: NextRequest) => Promise<Response>>(
     async (request) => {
         const formData = await request.formData();
         const file = formData.get('file') as File | null;
+        if (!file) throw new MalformedError('file not found');
         const options = (
             formData.get('options') ? parseJSON(formData.get('options') as string) : null
         ) as Partial<UploadMediaV1Params> | null;
         const parsedOptions = options ? UploadSchema.safeParse(options) : undefined;
-        if (!file) throw new MalformedError('file not found');
         if (parsedOptions && !parsedOptions.success) throw new Error(parsedOptions.error.message);
 
         const client = await createTwitterClientV2(request);
