ccaf5fde5066ab36c45a564a98d088a12befb043
dudu0506 <dudu@mask.io>
2024-08-15 22:22:44 +0800

    feat: support long video on x (#2277)

--- src/services/uploadToTwitter.ts
+++ src/services/uploadToTwitter.ts
@@ -1,23 +1,29 @@
+import type { UploadMediaV1Params } from 'twitter-api-v2';
+
+import { getVideoDuration } from '@/helpers/getVideoDuration.js';
 import { twitterSessionHolder } from '@/providers/twitter/SessionHolder.js';
 
 interface UploadMediaResponse {
     data: {
-        media_id: number;
+        media_id: string;
         media_id_string: string;
     };
 }
 
 export interface TwitterMediaResponse {
     file: File;
-    media_id: number;
+    media_id: string;
     media_id_string: string;
 }
 
-export async function uploadToTwitter(files: File[]): Promise<TwitterMediaResponse[]> {
+export async function uploadToTwitter(
+    uploads: Array<{ file: File; options?: Partial<UploadMediaV1Params> }>,
+): Promise<TwitterMediaResponse[]> {
     const medias = await Promise.all(
-        files.map((x) => {
+        uploads.map(({ file, options = {} }) => {
             const formData = new FormData();
-            formData.append('file', x);
+            formData.append('file', file);
+            formData.append('options', JSON.stringify(options));
 
             return twitterSessionHolder.fetch<UploadMediaResponse>('/api/twitter/uploadMedia', {
                 method: 'POST',
@@ -26,8 +32,14 @@ export async function uploadToTwitter(files: File[]): Promise<TwitterMediaRespon
         }),
     );
     return medias.map((x, i) => ({
-        file: files[i],
+        file: uploads[i].file,
         media_id: x.data.media_id,
         media_id_string: x.data.media_id_string,
     }));
+}
+
+export async function uploadVideoToTwitter(file: File): Promise<TwitterMediaResponse[]> {
+    const duration = await getVideoDuration(file);
+
+    return uploadToTwitter([{ file, options: { longVideo: duration > 140 } }]);
 }
