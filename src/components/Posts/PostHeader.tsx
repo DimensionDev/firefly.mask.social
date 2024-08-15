caaa3376c456fb185069f55b29f86a2fc80b5a8c
dudu0506 <dudu@mask.io>
2024-08-15 22:21:46 +0800

    fix: post ui (#2285)

--- src/components/Posts/PostHeader.tsx
+++ src/components/Posts/PostHeader.tsx
@@ -44,7 +44,7 @@ export const PostHeader = memo<PostHeaderProps>(function PostHeader({
     const isDetailPage = isRoutePathname(pathname, '/post/:detail', true);
 
     const identity = useFireflyIdentity(post.source, post.author.profileId);
-    const newLine = isSmall || (isDetailPage && !isQuote && !isComment);
+    const newLine = !isQuote && (isSmall || (isDetailPage && !isComment));
 
     const handle = (
         <ProfileTippy identity={identity}>
@@ -59,7 +59,7 @@ export const PostHeader = memo<PostHeaderProps>(function PostHeader({
     );
 
     return (
-        <div className="flex items-start gap-3">
+        <div className={classNames('flex gap-3', isQuote ? 'items-center' : 'items-start')}>
             <ProfileTippy identity={identity}>
                 <Link
                     href={profileLink}
@@ -87,6 +87,7 @@ export const PostHeader = memo<PostHeaderProps>(function PostHeader({
                     className={classNames('flex flex-1 items-center overflow-hidden', {
                         'max-w-[calc(100%-40px-28px-24px)]': !isQuote && !isMyPost,
                         'max-w-[calc(100%-40px-28px)]': !isQuote && isMyPost,
+                        'max-w-[calc(100%-40px)]': isQuote,
                     })}
                 >
                     <ProfileTippy identity={identity}>
@@ -98,21 +99,23 @@ export const PostHeader = memo<PostHeaderProps>(function PostHeader({
                             {post.author.displayName}
                         </Link>
                     </ProfileTippy>
-                    {post.author.isPowerUser ? <PowerUserIcon className="mr-2" width={16} height={16} /> : null}
+                    {post.author.isPowerUser ? (
+                        <PowerUserIcon className={newLine ? '' : 'mr-2'} width={16} height={16} />
+                    ) : null}
                     {newLine ? null : handle}
-                    {post.timestamp && (!newLine || isQuote) ? (
+                    {post.timestamp && (isComment || isQuote || !isDetailPage) ? (
                         <>
-                            <span className="mx-1 leading-5">·</span>
-                            <span className="whitespace-nowrap text-[15px] leading-5 text-secondary">
+                            <span className="mx-1 leading-5 text-secondary">·</span>
+                            <span className="whitespace-nowrap text-xs leading-5 text-secondary md:text-[13px]">
                                 <TimestampFormatter time={post.timestamp} />
                             </span>
-                            <span className="mx-1 leading-5">·</span>
+                            <span className="mx-1 leading-5 text-secondary">·</span>
                         </>
                     ) : null}
                     {isSendFromFirefly(post) ? (
-                        <FireflyAvatarIcon fontSize={15} width={15} height={15} className="mr-1 inline" />
+                        <FireflyAvatarIcon fontSize={15} width={15} height={15} className="mr-1 inline shrink-0" />
                     ) : null}
-                    <SocialSourceIcon source={post.source} size={15} />
+                    <SocialSourceIcon className="shrink-0" source={post.source} size={15} />
                 </div>
                 {newLine ? <div>{handle}</div> : null}
             </div>
