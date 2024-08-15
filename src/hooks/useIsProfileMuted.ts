e62d7cb8d6dfe8eab9ff73f38cea862c71feccf4
dudu0506 <dudu@mask.io>
2024-08-15 16:30:43 +0800

    fix: update query for mute (#2284)

--- src/hooks/useIsProfileMuted.ts
+++ src/hooks/useIsProfileMuted.ts
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { queryClient } from '@/configs/queryClient.js';
-import { Source } from '@/constants/enum.js';
 import { resolveSourceInURL } from '@/helpers/resolveSourceInURL.js';
 import { useIsLogin } from '@/hooks/useIsLogin.js';
 import { FireflySocialMediaProvider } from '@/providers/firefly/SocialMedia.js';
@@ -9,10 +8,9 @@ import type { Profile } from '@/providers/types/SocialMedia.js';
 
 export function useIsProfileMuted(profile: Profile, enabled = true) {
     const isLogin = useIsLogin();
-    const profileId = profile?.source === Source.Farcaster ? +profile.profileId : profile.profileId;
     const { data } = useQuery({
         enabled: !!profile && isLogin && enabled,
-        queryKey: ['profile-is-muted', profile.source, profileId],
+        queryKey: ['profile-is-muted', profile.source, profile.profileId],
         staleTime: 600_000,
         queryFn: () => {
             return FireflySocialMediaProvider.isProfileMuted(resolveSourceInURL(profile.source), profile.profileId);
