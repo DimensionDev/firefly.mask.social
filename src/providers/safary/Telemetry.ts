9d6e51a7e4d2bf4d7b9afd741424f46afc7aaa50
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-08-15 14:32:24 +0800

    feat: setup Telemetry (#2271)
    * chore: add SafaryTelemetryAPI
    
    * chore: eslint
    
    * chore: define events
    
    * refactor: type
    
    * fix: vitest type
    
    * refactor: type safe
    
    * chore: eslint
    
    * refactor: type
    
    * feat: impl captureEvent
    
    * refactor: adjust msg
    
    * fix: type
    
    * chore: public parameters
    
    * chore: account id
    
    * chore: update public params
    
    * refactor: code style
    
    * chore: prefix
    
    * refactor: event name
    
    * refactor: code style
    
    ---------
    
    Co-authored-by: guanbinrui <guanbinrui@users.noreply.github.com>

---
+++ src/providers/safary/Telemetry.ts
@@ -0,0 +1,86 @@
+import { ChainId } from '@masknet/web3-shared-solana';
+import { getClient } from '@wagmi/core';
+
+import { config } from '@/configs/wagmiClient.js';
+import { NODE_ENV, STATUS } from '@/constants/enum.js';
+import { env } from '@/constants/env.js';
+import { NotImplementedError } from '@/constants/error.js';
+import { bom } from '@/helpers/bom.js';
+import { resolveWalletAdapter } from '@/providers/solana/resolveWalletAdapter.js';
+import type { Events, Safary } from '@/providers/types/Safary.js';
+import { Provider } from '@/providers/types/Telemetry.js';
+import { useDeveloperSettingsState } from '@/store/useDeveloperSettingsStore.js';
+import { useFireflyStateStore } from '@/store/useProfileStore.js';
+
+function formatParameter(key: string, value: unknown): [string, unknown] {
+    if (typeof value === 'boolean') {
+        return [key, value === true ? 'Y' : 'N'];
+    }
+    return [key, value];
+}
+
+class SafaryTelemetry extends Provider<Events, never> {
+    private get sdk() {
+        if (typeof bom.window?.safary === 'undefined') return null;
+        return bom.window.safary as Safary;
+    }
+
+    getPublicParameters() {
+        const evmClient = getClient(config);
+        const solanaAdaptor = resolveWalletAdapter();
+
+        return {
+            public_ua: bom.navigator?.userAgent,
+            public_href: bom.location?.href,
+
+            public_evm_address: evmClient?.account?.address,
+            public_evm_chain_id: evmClient?.chain.id,
+            public_solana_chain_id: ChainId.Mainnet,
+            public_solana_address: solanaAdaptor.publicKey?.toBase58(),
+
+            public_account_id: useFireflyStateStore.getState().currentProfileSession?.profileId,
+            public_use_development_api: useDeveloperSettingsState.getState().useDevelopmentAPI,
+        };
+    }
+
+    override async captureEvent<T extends keyof Events>(name: T, parameters: Events[T]['parameters']): Promise<void> {
+        if (env.external.NEXT_PUBLIC_TELEMETRY === STATUS.Disabled) {
+            return;
+        }
+
+        if (env.shared.NODE_ENV === NODE_ENV.Development) {
+            console.info('[safary] capture event:', name, parameters);
+            return;
+        }
+
+        if (!this.sdk) {
+            console.error('[safary] safary SDK not available. failed to capture event:', name, parameters);
+            return;
+        }
+
+        try {
+            const publicParameters = this.getPublicParameters();
+            const formattedParameters = Object.fromEntries(
+                Object.entries(parameters).map(([key, value]) => formatParameter(key, value)),
+            );
+
+            await this.sdk.track({
+                eventType: name,
+                eventName: name.replaceAll(/_/g, ' '),
+                parameters: {
+                    ...publicParameters,
+                    ...formattedParameters,
+                } as unknown as Events[T]['parameters'],
+            });
+        } catch (error) {
+            console.error('[safary] failed to capture event:', name, parameters);
+            throw error;
+        }
+    }
+
+    override async captureException(): Promise<void> {
+        throw new NotImplementedError();
+    }
+}
+
+export const SafaryTelemetryProvider = new SafaryTelemetry();
---
+++ src/providers/types/Telemetry.ts
@@ -0,0 +1,68 @@
+export enum EventType {
+    Debug = 'debug',
+    Access = 'access',
+    Exit = 'exit',
+    Interact = 'interact',
+}
+
+export enum ExceptionType {
+    Error = 'Error',
+}
+
+export enum EventId {
+    SEND_CROSS_POST_SUCCESS = 'send_cross_post_success',
+    SEND_TIPS_SUCCESS = 'send_tips_success',
+    CREATE_SCHEDULED_POST = 'create_scheduled_post',
+    CREATE_LUCKY_DROP_SUCCESS = 'create_lucky_drop_success',
+    CREATE_POLL_SUCCESS = 'create_poll_success',
+
+    // farcaster
+    FARCASTER_SEND_POST_SUCCESS = 'farcaster_send_post_success',
+    FARCASTER_LIKE_SUCCESS = 'farcaster_like_success',
+    FARCASTER_REPLY_SUCCESS = 'farcaster_reply_success',
+    FARCASTER_RECAST_SUCCESS = 'farcaster_recast_success',
+    FARCASTER_QUOTE_SUCCESS = 'farcaster_quote_success',
+    FARCASTER_SHARE_SUCCESS = 'farcaster_share_success',
+    FARCASTER_FOLLOW_SUCCESS = 'farcaster_follow_success',
+    FARCASTER_BOOKMARK_SUCCESS = 'farcaster_bookmark_success',
+
+    // lens
+    LENS_SEND_POST_SUCCESS = 'lens_send_post_success',
+    LENS_LIKE_SUCCESS = 'lens_like_success',
+    LENS_MIRROR_SUCCESS = 'lens_mirror_success',
+    LENS_QUOTE_SUCCESS = 'lens_quote_success',
+    LENS_SHARE_SUCCESS = 'lens_share_success',
+    LENS_FOLLOW_SUCCESS = 'lens_follow_success',
+    LENS_BOOKMARK_SUCCESS = 'lens_bookmark_success',
+
+    // twitter
+    X_SEND_POST_SUCCESS = 'x_send_post_success',
+    X_REPLY_SUCCESS = 'x_reply_success',
+    X_LIKE_SUCCESS = 'x_like_success',
+    X_QUOTE_SUCCESS = 'x_quote_success',
+    X_REPOST_SUCCESS = 'x_repost_success',
+    X_SHARE_SUCCESS = 'x_share_success',
+    X_FOLLOW_SUCCESS = 'x_follow_success',
+    X_BOOKMARK_SUCCESS = 'x_bookmark_success',
+}
+
+export enum ExceptionId {}
+
+export interface Event {
+    type: EventType;
+    // bypassing the type check
+    parameters: {};
+}
+
+export interface Exception {
+    type: ExceptionType;
+    error: Error;
+}
+
+export abstract class Provider<
+    Events extends Record<EventId, Event>,
+    Exceptions extends Record<ExceptionId, Exception>,
+> {
+    abstract captureEvent<T extends EventId>(name: EventId, parameters: Events[T]['parameters']): Promise<void>;
+    abstract captureException<T extends ExceptionId>(name: ExceptionId, error: Exceptions[T]['error']): Promise<void>;
+}
