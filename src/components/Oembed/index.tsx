ccd8f0a30fa7c792e3034c6e9215d89c140b0a29
LeifXu <ins@mask.io>
2024-08-16 20:11:27 +0800

    fix: post link type is mirror

--- src/components/Oembed/index.tsx
+++ src/components/Oembed/index.tsx
@@ -32,6 +32,7 @@ export const OembedLayout = memo<{ data: LinkDigested; post?: Post }>(function O
     } = props;
 
     if (!og.title) return null;
+    if (payload?.type === 'Post' && post?.type === 'Mirror') return null;
     if (payload?.type === 'Post' && payload.id === post?.postId) return null;
 
     const type = payload?.type;
