e111592d29cbcfd0373ffe5c1c5f43d83a5ed49c
dudu0506 <dudu@mask.io>
2024-08-16 18:16:22 +0800

    fix: deprecate m3u8 on x (#2302)
    * fix: deprecate m3u8 on x
    
    * chore: eslint
    
    ---------
    
    Co-authored-by: dudu0506 <dudu0506@users.noreply.github.com>
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>

--- src/helpers/formatTwitterMedia.ts
+++ src/helpers/formatTwitterMedia.ts
@@ -5,14 +5,10 @@ import type { MediaObjectV2, MediaVariantsV2 } from 'twitter-api-v2';
 import { type Attachment } from '@/providers/types/SocialMedia.js';
 
 /**
- * use m3u8 link first if available
- * then use mp4 link with highest bitrate
+ * use mp4 link with highest bitrate
  * TODO: Maybe we can choose video on network speed
  */
 function getBestVideoUrl(variants: MediaVariantsV2[]) {
-    const m3u8Variant = variants.find((v) => v.content_type === 'application/x-mpegURL');
-    if (m3u8Variant?.url) return m3u8Variant.url;
-
     const mp4Variants = variants.filter((v) => v.content_type === 'video/mp4');
     return last(mp4Variants)?.url ?? first(variants)?.url;
 }
