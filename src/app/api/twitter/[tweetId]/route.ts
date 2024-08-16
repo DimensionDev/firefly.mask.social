96ea2e2f74caa17c38433f563ec2ba685fe7a5bb
LeifXu <ins@mask.io>
2024-08-16 23:20:17 +0800

    fix: retweet post lost some data

--- src/app/api/twitter/[tweetId]/route.ts
+++ src/app/api/twitter/[tweetId]/route.ts
@@ -39,10 +39,30 @@ export const GET = compose<(request: NextRequest, context?: NextRequestContext) 
         if (!tweetId) throw new MalformedError('tweetId not found');
 
         const client = await createTwitterClientV2(request);
-        const { data, includes, errors } = await client.v2.singleTweet(tweetId, {
+        const {
+            data,
+            includes = {},
+            errors,
+        } = await client.v2.singleTweet(tweetId, {
             ...TWITTER_TIMELINE_OPTIONS,
         });
         if (errors?.length) console.error('[twitter] v2.singleTweet', errors);
+
+        // The retweeted post may not receive attachment
+        const retweeted = data.referenced_tweets?.find((tweet) => tweet.type === 'retweeted');
+        if (retweeted) {
+            const tweet = includes?.tweets?.find((x) => x.id === retweeted.id);
+            if (tweet) {
+                const result = await client.v2.singleTweet(tweet.id, {
+                    expansions: ['attachments.media_keys'],
+                    'media.fields': ['media_key', 'height', 'width', 'type', 'url', 'preview_image_url', 'variants'],
+                });
+                data.attachments = tweet?.attachments;
+                data.note_tweet = tweet.note_tweet;
+                data.text = tweet.text;
+                if (result.includes?.media) includes.media = result.includes.media;
+            }
+        }
 
         return createSuccessResponseJSON(tweetV2ToPost(data, includes));
     },
