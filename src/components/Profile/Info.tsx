7fbd0bcc682f24264b355cb438dedc6fd3614b8f
dudu0506 <dudu@mask.io>
2024-08-19 11:56:50 +0800

    fix: profile name overflow

--- src/components/Profile/Info.tsx
+++ src/components/Profile/Info.tsx
@@ -60,6 +60,7 @@ export function Info({ profile }: InfoProps) {
 
     const followingCount = profile.followingCount || 0;
     const followerCount = profile.followerCount || 0;
+    const showAction = profile && isMedium && !reached;
 
     return (
         <div className="flex gap-3 p-3">
@@ -74,12 +75,19 @@ export function Info({ profile }: InfoProps) {
                 <SocialSourceIcon className="rounded-full" source={source} size={80} />
             )}
 
-            <div className="relative flex flex-1 flex-col">
-                <div className="flex flex-col">
-                    <div className="flex items-center gap-2">
-                        <span className="text-xl font-black text-lightMain">{profile.displayName}</span>
-                        <SocialSourceIcon className="mr-auto" source={source} size={20} />
-                        {profile && isMedium && !reached ? <ProfileAction profile={profile} /> : null}
+            <div className="relative flex w-[calc(100%-80px-12px)] flex-col">
+                <div className="flex w-full flex-col">
+                    <div className="flex w-full items-center gap-2">
+                        <span
+                            className={classNames('truncate text-clip text-xl font-black text-lightMain', {
+                                'max-w-[calc(100%-152px-20px-24px)]': showAction,
+                                'max-w-[calc(100%-20px-16px)]': !showAction,
+                            })}
+                        >
+                            {profile.displayName}
+                        </span>
+                        <SocialSourceIcon className="mr-auto shrink-0" source={source} size={20} />
+                        {showAction ? <ProfileAction profile={profile} /> : null}
                     </div>
                     <span className="text-[15px] text-secondary">@{profile.handle}</span>
                 </div>
