916e9e1536f3ff1c444f213f4f20f48d6d806a42
dudu0506 <dudu@mask.io>
2024-08-20 18:20:40 +0800

    feat: suggest profiles

--- src/components/AsideTitle.tsx
+++ src/components/AsideTitle.tsx
@@ -1,12 +1,14 @@
 import React, { memo } from 'react';
 
+import { classNames } from '@/helpers/classNames.js';
+
 interface AsideTitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
     children?: React.ReactNode;
 }
 
-export const AsideTitle = memo(function AsideTitle({ children, ...props }: AsideTitleProps) {
+export const AsideTitle = memo(function AsideTitle({ children, className, ...props }: AsideTitleProps) {
     return (
-        <h2 className="p-4 text-lg font-bold leading-none" {...props}>
+        <h2 className={classNames('p-4 text-lg font-bold leading-none', className)} {...props}>
             {children}
         </h2>
     );
