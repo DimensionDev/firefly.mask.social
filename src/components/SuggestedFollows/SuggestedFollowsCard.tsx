3d26b7fa6d1ac8f4dcbcc8ce480e9497a2c8dd18
dudu0506 <dudu@mask.io>
2024-08-20 21:41:00 +0800

    feat: suggest profiles (#2335)
    * feat: suggest profiles
    
    * chore: lingui extract
    
    ---------
    
    Co-authored-by: dudu0506 <dudu0506@users.noreply.github.com>

--- src/components/SuggestedFollows/SuggestedFollowsCard.tsx
+++ src/components/SuggestedFollows/SuggestedFollowsCard.tsx
@@ -1,56 +1,42 @@
 'use client';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-coverflow';
 
 import { Trans } from '@lingui/macro';
 import { useQuery } from '@tanstack/react-query';
+import { shuffle } from 'lodash-es';
 import { useMemo } from 'react';
+import { Autoplay, EffectCoverflow } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import urlcat from 'urlcat';
 
 import LoadingIcon from '@/assets/loading.svg';
 import { AsideTitle } from '@/components/AsideTitle.js';
-import { ProfileCell } from '@/components/Profile/ProfileCell.js';
+import { ProfileSlide } from '@/components/SuggestedFollows/ProfileSlide.js';
 import { DiscoverType, PageRoute, Source, SourceInURL } from '@/constants/enum.js';
 import { Link } from '@/esm/Link.js';
 import { isSocialSource } from '@/helpers/isSocialSource.js';
-import { type Pageable, type PageIndicator } from '@/helpers/pageable.js';
 import { resolveSourceInURL } from '@/helpers/resolveSourceInURL.js';
 import { useCurrentProfileAll } from '@/hooks/useCurrentProfile.js';
 import { FarcasterSocialMediaProvider } from '@/providers/farcaster/SocialMedia.js';
 import { LensSocialMediaProvider } from '@/providers/lens/SocialMedia.js';
-import type { Profile } from '@/providers/types/SocialMedia.js';
 import { useGlobalState } from '@/store/useGlobalStore.js';
-
-async function filterFollowedProfiles(queryCallback: (indicator?: PageIndicator) => Promise<Pageable<Profile>>) {
-    let result = await queryCallback();
-    let data: Profile[] = [];
-    let sliceIndex = 0;
-    while (data.length < 3 && result.data.length - sliceIndex > 0) {
-        const sliceEndIndex = 3 - data.length;
-        const newData = result.data
-            .slice(sliceIndex, sliceEndIndex)
-            .filter((item) => !item.viewerContext?.blocking && !item.viewerContext?.following);
-        sliceIndex = sliceIndex + sliceEndIndex;
-        data = [...data, ...newData];
-        if (data.length < 3 && result.data.length - sliceIndex <= 0 && result.nextIndicator) {
-            result = await queryCallback(result.nextIndicator as PageIndicator);
-            sliceIndex = 0;
-        }
-    }
-    return data;
-}
 
 export function SuggestedFollowsCard() {
     const currentSource = useGlobalState.use.currentSource();
     const profileAll = useCurrentProfileAll();
-    const { data: farcasterData, isLoading: isLoadingFarcaster } = useQuery({
-        queryKey: ['suggested-follows-lite', Source.Farcaster],
-        queryFn() {
-            return filterFollowedProfiles(FarcasterSocialMediaProvider.getSuggestedFollows);
-        },
-    });
-    const { data: lensData, isLoading: isLoadingLens } = useQuery({
-        queryKey: ['suggested-follows-lite', Source.Lens],
-        queryFn() {
-            return filterFollowedProfiles(LensSocialMediaProvider.getSuggestedFollows);
+    const { data: suggestedFollows, isLoading } = useQuery({
+        queryKey: ['suggested-follows-lite'],
+        staleTime: 1000 * 60 * 2,
+        queryFn: async () => {
+            const farcasterData = (await FarcasterSocialMediaProvider.getSuggestedFollows())?.data;
+            const lensData = (await LensSocialMediaProvider.getSuggestedFollows())?.data;
+            return shuffle(
+                [...(farcasterData ?? []), ...(lensData ?? [])].filter(
+                    (item) => !item.viewerContext?.blocking && !item.viewerContext?.following,
+                ),
+            );
         },
     });
 
@@ -75,47 +61,55 @@ export function SuggestedFollowsCard() {
         });
     }, [currentSource, profileAll.Farcaster, profileAll.Lens]);
 
-    if (!profileAll.Farcaster && !profileAll.Lens) {
-        return null;
+    if (!profileAll.Farcaster && !profileAll.Lens) return null;
+
+    if (isLoading) {
+        return (
+            <div className="flex h-[268px] w-full shrink-0 items-center justify-center">
+                <LoadingIcon width={16} height={16} className="animate-spin" />
+            </div>
+        );
     }
 
-    const loadingEl = (
-        <div className="flex h-[180px] w-full items-center justify-center">
-            <LoadingIcon width={16} height={16} className="animate-spin" />
-        </div>
-    );
+    if (!suggestedFollows?.length) return null;
 
     return (
-        <div className="rounded-lg border border-line dark:border-0 dark:bg-lightBg">
-            <AsideTitle>
-                <Trans>Suggested Follows</Trans>
+        <div>
+            <AsideTitle className="flex items-center justify-between !px-3">
+                <span className="text-xl">
+                    <Trans>You might like</Trans>
+                </span>
+                <Link href={showMoreUrl} className="text-[15px] text-lightHighlight">
+                    <Trans>More</Trans>
+                </Link>
             </AsideTitle>
-            <div className="flex w-full flex-col">
-                {isLoadingFarcaster && isLoadingLens ? (
-                    <div className="flex h-[360px] w-full items-center justify-center">
-                        <LoadingIcon width={16} height={16} className="animate-spin" />
-                    </div>
-                ) : (
-                    <>
-                        {!isLoadingFarcaster
-                            ? farcasterData?.map((profile) => (
-                                  <ProfileCell key={profile.profileId} profile={profile} source={profile.source} />
-                              ))
-                            : loadingEl}
-                        {!isLoadingLens
-                            ? lensData?.map((profile) => (
-                                  <ProfileCell key={profile.profileId} profile={profile} source={profile.source} />
-                              ))
-                            : loadingEl}
-                    </>
-                )}
+            <div>
+                <Swiper
+                    initialSlide={suggestedFollows.length > 2 ? 1 : 0}
+                    effect={'coverflow'}
+                    grabCursor
+                    centeredSlides
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: false,
+                    }}
+                    pagination
+                    autoplay={{ delay: 2000 }}
+                    modules={[Autoplay, EffectCoverflow]}
+                >
+                    {suggestedFollows.map((profile, key) => (
+                        <SwiperSlide className="!h-[208px] !w-[164px]" key={key}>
+                            <div className="py-3">
+                                <ProfileSlide profile={profile} />
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
-            <Link
-                href={showMoreUrl}
-                className="flex px-4 py-2 text-[15px] font-bold leading-[24px] text-lightHighlight"
-            >
-                <Trans>Show more</Trans>
-            </Link>
         </div>
     );
 }
