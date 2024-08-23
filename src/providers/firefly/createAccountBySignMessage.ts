5000296155ba6b2d8bbd80915af7eea3625ba59a
guanbinrui <guanbinrui@mask.io>
2024-08-23 12:52:16 +0800

    feat: impl createAccountBySignMessage

---
+++ src/providers/firefly/createAccountBySignMessage.ts
@@ -0,0 +1,48 @@
+import urlcat from 'urlcat';
+
+import { config } from '@/configs/wagmiClient.js';
+import { createDummyProfileFromFireflyAccountId } from '@/helpers/createDummyProfile.js';
+import { fetchJSON } from '@/helpers/fetchJSON.js';
+import { getWalletClientRequired } from '@/helpers/getWalletClientRequired.js';
+import { resolveFireflyResponseData } from '@/helpers/resolveFireflyResponseData.js';
+import { FireflySession } from '@/providers/firefly/Session.js';
+import type { MessageToSignResponse, WalletLoginResponse } from '@/providers/types/Firefly.js';
+import { settings } from '@/settings/index.js';
+
+async function createSession(signal?: AbortSignal) {
+    const client = await getWalletClientRequired(config);
+    const address = client.account.address.toLowerCase();
+
+    const url = urlcat(settings.FIREFLY_ROOT_URL, '/v1/wallet/messageToSign', {
+        address,
+    });
+    const response = await fetchJSON<MessageToSignResponse>(url, {
+        method: 'GET',
+        signal,
+    });
+    const { message } = resolveFireflyResponseData(response);
+
+    const loginUrl = urlcat(settings.FIREFLY_ROOT_URL, '/v3/auth/wallet/login');
+
+    const loginResponse = await fetchJSON<WalletLoginResponse>(loginUrl, {
+        method: 'POST',
+        body: JSON.stringify({
+            address,
+            signedMessage: message,
+            signature: await client.signMessage({
+                message,
+            }),
+        }),
+    });
+
+    const { accessToken, accountId } = resolveFireflyResponseData(loginResponse);
+    return new FireflySession(accountId, accessToken, null);
+}
+
+export async function createAccountBySignMessage(signal?: AbortSignal) {
+    const session = await createSession(signal);
+    return {
+        session,
+        profile: createDummyProfileFromFireflyAccountId(session.profileId),
+    };
+}
