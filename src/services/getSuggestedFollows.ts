f640c5e2d766ae38ee119f31cb04c4ff11526cd8
dudu0506 <dudu@mask.io>
2024-08-26 16:06:14 +0800

    fix: fixed suggest profiles total (#2407)

--- src/services/getSuggestedFollows.ts
+++ src/services/getSuggestedFollows.ts
@@ -1,16 +1,34 @@
 import { type SocialSource, Source } from '@/constants/enum.js';
-import { EMPTY_LIST } from '@/constants/index.js';
-import { createIndicator, createPageable, type PageIndicator } from '@/helpers/pageable.js';
+import { type Pageable, type PageIndicator } from '@/helpers/pageable.js';
 import { resolveSocialMediaProvider } from '@/helpers/resolveSocialMediaProvider.js';
 import type { Profile } from '@/providers/types/SocialMedia.js';
 
-export async function getSuggestedFollows(source: SocialSource, indicator?: PageIndicator, retry = 3) {
-    if (source === Source.Twitter) return createPageable<Profile>(EMPTY_LIST, createIndicator(undefined));
-    const provider = resolveSocialMediaProvider(source);
-    const results = await provider.getSuggestedFollows(indicator);
-    if (!results.data?.length && results.nextIndicator && retry) {
-        return getSuggestedFollows(source, results.nextIndicator as PageIndicator, retry - 1);
+async function getProfilesWithFixedTotal(
+    queryCallback: (indicator?: PageIndicator) => Promise<Pageable<Profile>>,
+    total: number,
+) {
+    let data: Profile[] = [];
+    let indicator: PageIndicator | undefined;
+    let retry = 10;
+
+    while (data.length < total && retry > 0) {
+        const result = await queryCallback(indicator);
+        const newData =
+            result.data?.filter((item) => !item.viewerContext?.blocking && !item.viewerContext?.following) ?? [];
+        data = [...data, ...newData].slice(0, total);
+        retry -= 1;
+        if (data.length < total && result.nextIndicator) {
+            indicator = result.nextIndicator as PageIndicator;
+        } else {
+            break;
+        }
     }
 
-    return results;
+    return data;
+}
+
+export async function getSuggestedFollows(source: SocialSource) {
+    if (source === Source.Twitter) return [];
+    const provider = resolveSocialMediaProvider(source);
+    return getProfilesWithFixedTotal(provider.getSuggestedFollows, 50);
 }
