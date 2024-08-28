9d0ff4d3c8bd4f9e28af7accda17c8891f095b48
guanbinrui <guanbinrui@mask.io>
2024-08-28 09:45:25 +0800

    chore: add reportFarcasterSigner

---
+++ src/services/reportFarcasterSigner.ts
@@ -0,0 +1,20 @@
+/* cspell:disable */
+
+import urlcat from 'urlcat';
+
+import type { FarcasterSession } from "@/providers/farcaster/Session.js";
+import { settings } from '@/settings/index.js';
+import { fireflySessionHolder } from '@/providers/firefly/SessionHolder.js';
+
+export async function reportFarcasterSigner(session: FarcasterSession) {
+    const url = urlcat(settings.FIREFLY_ROOT_URL, '/v1/farcaster_account/upSignerConfig');
+    await fireflySessionHolder.fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            fid: session.profileId,
+            signerPublickey: '',
+            signerPrivatekey: session.token,
+        })
+    })
+}
+
