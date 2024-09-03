f04d97534d44cd2eee3b2f357f8dd7ed0d8665ec
guanbinrui <guanbinrui@dimension.im>
2024-09-03 11:50:09 +0800

    refactor: remove restore confirmation

--- src/components/Login/LoginStatusBar.tsx
+++ src/components/Login/LoginStatusBar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { delay } from '@masknet/kit';
-import { useState } from 'react';
 import { useAsyncFn } from 'react-use';
 
 import { ProfileAvatarAdd } from '@/components/Login/ProfileAvatarAdd.js';
@@ -9,10 +8,8 @@ import { ProfileAvatarInteractive } from '@/components/Login/ProfileAvatarIntera
 import { config } from '@/configs/wagmiClient.js';
 import { type SocialSource, Source } from '@/constants/enum.js';
 import { SORTED_SOCIAL_SOURCES } from '@/constants/index.js';
-import { restoreCurrentAccounts } from '@/helpers/account.js';
 import { classNames } from '@/helpers/classNames.js';
 import { getWalletClientRequired } from '@/helpers/getWalletClientRequired.js';
-import { useAbortController } from '@/hooks/useAbortController.js';
 import { useCurrentProfileAll } from '@/hooks/useCurrentProfile.js';
 import { LoginModalRef } from '@/modals/controls.js';
 import { useNavigatorState } from '@/store/useNavigatorStore.js';
@@ -26,19 +23,7 @@ export function LoginStatusBar({ collapsed = false }: LoginStatusBarProps) {
 
     const currentProfileAll = useCurrentProfileAll();
 
-    const controller = useAbortController();
-    const [selectedSource, setSelectedSource] = useState<SocialSource>();
-
     const [{ loading }, onLogin] = useAsyncFn(async (source: SocialSource) => {
-        setSelectedSource(source);
-
-        try {
-            const confirmed = await restoreCurrentAccounts(controller.current.signal);
-            if (confirmed) return;
-        } catch {
-            // if any error occurs, we will just proceed with the login
-        }
-
         updateSidebarOpen(false);
         await delay(300);
         if (source === Source.Lens) await getWalletClientRequired(config);
@@ -59,12 +44,7 @@ export function LoginStatusBar({ collapsed = false }: LoginStatusBarProps) {
 
             {SORTED_SOCIAL_SOURCES.map((x) =>
                 !currentProfileAll[x] ? (
-                    <ProfileAvatarAdd
-                        key={x}
-                        source={x}
-                        loading={loading ? x === selectedSource : false}
-                        onClick={() => onLogin(x)}
-                    />
+                    <ProfileAvatarAdd key={x} source={x} loading={loading} onClick={() => onLogin(x)} />
                 ) : null,
             )}
         </div>
