507058a8af74f8ea48736fa6b319e01f6e7b72b0
guanbinrui <guanbinrui@dimension.im>
2024-09-03 11:38:04 +0800

    refactor: code style

--- src/hooks/useAsyncStatus.ts
+++ src/hooks/useAsyncStatus.ts
@@ -3,18 +3,16 @@ import { SORTED_SOCIAL_SOURCES } from '@/constants/index.js';
 import { useProfileStoreAll } from '@/hooks/useProfileStore.js';
 import { useGlobalState } from '@/store/useGlobalStore.js';
 
-export function useAsyncStatusAll() {
+export function useAsyncStatusAll(status = AsyncStatus.Pending) {
     const store = useProfileStoreAll();
     const { asyncStatus } = useGlobalState();
 
-    return SORTED_SOCIAL_SOURCES.some(
-        (x) => store[x].status === AsyncStatus.Pending || asyncStatus[x] === AsyncStatus.Pending,
-    );
+    return SORTED_SOCIAL_SOURCES.some((x) => store[x].status === status || asyncStatus[x] === status);
 }
 
-export function useAsyncStatus(source: SocialSource) {
+export function useAsyncStatus(source: SocialSource, status = AsyncStatus.Pending) {
     const store = useProfileStoreAll();
     const { asyncStatus } = useGlobalState();
 
-    return store[source].status === AsyncStatus.Pending || asyncStatus[source] === AsyncStatus.Pending;
+    return store[source].status === status || asyncStatus[source] === status;
 }
