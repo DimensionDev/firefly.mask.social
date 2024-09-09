ea3b9b96465912654d8579a3e076dd989cb8f5d0
guanbinrui <guanbinrui@dimension.im>
2024-09-09 10:57:10 +0800

    refactor: rename file

--- src/components/TokenProfile/useChainInfo.ts
+++ src/components/TokenProfile/useChainInfo.ts
@@ -1,11 +1,105 @@
 import { useNetworkDescriptor } from '@masknet/web3-hooks-base';
 
 import { NetworkPluginID } from '@/constants/enum.js';
-import { chainInfos } from '@/providers/coingecko/chain-infos.js';
 import type { Runtime } from '@/providers/types/Trending.js';
+
+interface Chain {
+    name: string;
+    runtime: Runtime;
+    /** url of the icon*/
+    icon: string;
+}
+
+const CHAINS: Chain[] = [
+    {
+        runtime: 'solana',
+        name: 'Solana',
+        icon: new URL('../../assets/chains/solana.png', import.meta.url).href,
+    },
+    {
+        runtime: 'polkadot',
+        name: 'Polkadot',
+        icon: 'https://coin-images.coingecko.com/coins/images/12171/large/polkadot.png',
+    },
+    {
+        runtime: 'stellar',
+        name: 'Stellar',
+        icon: 'https://coin-images.coingecko.com/coins/images/100/large/Stellar_symbol_black_RGB.png',
+    },
+    {
+        runtime: 'near-protocol',
+        name: 'Near',
+        icon: 'https://coin-images.coingecko.com/coins/images/20582/large/aurora.jpeg',
+    },
+    {
+        runtime: 'hedera-hashgraph',
+        name: 'HashGraph',
+        icon: 'https://coin-images.coingecko.com/coins/images/3688/large/hbar.png',
+    },
+    {
+        runtime: 'zksync',
+        name: 'ZKSync',
+        icon: 'https://assets.coingecko.com/asset_platforms/images/121/small/zksync.jpeg?1706606814',
+    },
+    {
+        runtime: 'tron',
+        name: 'Tron',
+        icon: 'https://coin-images.coingecko.com/coins/images/1094/large/tron-logo.png',
+    },
+    {
+        runtime: 'arbitrum-one',
+        name: 'Arbitrum-One',
+        icon: new URL('../../assets/chains/arbitrum-one.png', import.meta.url).href,
+    },
+    {
+        runtime: 'polygon-pos',
+        name: 'Polygon-PoS',
+        icon: 'https://coin-images.coingecko.com/coins/images/4713/large/polygon.png',
+    },
+    {
+        runtime: 'flow',
+        name: 'Flow',
+        icon: new URL('../../assets/chains/flow.png', import.meta.url).href,
+    },
+    {
+        runtime: 'celo',
+        name: 'Celo',
+        icon: 'https://coin-images.coingecko.com/coins/images/11090/large/InjXBNx9_400x400.jpg',
+    },
+    {
+        runtime: 'the-open-network',
+        name: 'Ton',
+        icon: new URL('../../assets/chains/ton.png', import.meta.url).href,
+    },
+    {
+        runtime: 'algorand',
+        name: 'Algorand',
+        icon: 'https://assets.coingecko.com/asset_platforms/images/3/small/algorand_logo_mark_black.png?1706606710',
+    },
+    {
+        runtime: 'optimistic-ethereum',
+        name: 'Optimistic-Ethereum',
+        icon: new URL('../../assets/chains/optimism.png', import.meta.url).href,
+    },
+    {
+        runtime: 'avalanche',
+        name: 'Avalanche',
+        icon: new URL('../../assets/chains/avalanche.png', import.meta.url).href,
+    },
+    {
+        runtime: 'base',
+        name: 'Base',
+        icon: new URL('../../assets/chains/base.png', import.meta.url).href,
+    },
+    {
+        runtime: 'kava',
+        name: 'Kava',
+        icon: 'https://assets.coingecko.com/asset_platforms/images/2/small/kava.jpeg?1707096364',
+    },
+];
 
 export function useChainInfo(runtime: Runtime | undefined, chainId: number | undefined) {
     const chain = useNetworkDescriptor(NetworkPluginID.PLUGIN_EVM, chainId);
     // runtime is more accurate than chainId
-    return chainInfos.find((x) => x.runtime === runtime) || chain;
+    return CHAINS.find((x) => x.runtime === runtime) || chain;
 }
