33dd9d546e074947f096d15cc93675ac3cd97ea6
guanbinrui <guanbinrui@dimension.im>
2024-09-09 10:57:10 +0800

    refactor: rename file

--- src/components/TokenProfile/useChainInfo.ts
+++ src/components/TokenProfile/useChainInfo.ts
@@ -1,7 +1,7 @@
 import { useNetworkDescriptor } from '@masknet/web3-hooks-base';
 
 import { NetworkPluginID } from '@/constants/enum.js';
-import { chainInfos } from '@/providers/coingecko/chain-infos.js';
+import { chainInfos } from '@/providers/coingecko/chains.js';
 import type { Runtime } from '@/providers/types/Trending.js';
 
 export function useChainInfo(runtime: Runtime | undefined, chainId: number | undefined) {
