c3d00a6e81907cc728611e1a065c8987239a4f05
nuanyang233 <nuanyang233@gmail.com>
2024-09-13 21:22:40 +0800

    fix: bugfix for lens handle support (#2566)

--- src/helpers/getLensHandleFromMentionTitle.ts
+++ src/helpers/getLensHandleFromMentionTitle.ts
@@ -1,6 +1,5 @@
 const MATCH_LENS_HANDLE_A_RE = /^@lens\/([^\s/@]+)$/i; // e.g., @lens/handle
 const MATCH_LENS_HANDLE_B_RE = /^@([^\s/@]+)\.lens$/i; // e.g., @handle.lens
-const MATCH_LENS_HANDLE_C_RE = /^@([^\s/@]+)$/i; // e.g., @handle
 
 /**
  * Examples of mention titles in the returned post:
@@ -13,6 +12,5 @@ const MATCH_LENS_HANDLE_C_RE = /^@([^\s/@]+)$/i; // e.g., @handle
 export function getLensHandleFromMentionTitle(mentionTitle: string) {
     if (MATCH_LENS_HANDLE_A_RE.test(mentionTitle)) return mentionTitle.replace(MATCH_LENS_HANDLE_A_RE, '$1');
     if (MATCH_LENS_HANDLE_B_RE.test(mentionTitle)) return mentionTitle.replace(MATCH_LENS_HANDLE_B_RE, '$1');
-    if (MATCH_LENS_HANDLE_C_RE.test(mentionTitle)) return mentionTitle.replace(MATCH_LENS_HANDLE_C_RE, '$1');
     return;
 }
