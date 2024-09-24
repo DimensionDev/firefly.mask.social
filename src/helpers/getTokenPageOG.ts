39cc33932209dfef5cc5804e6fe2636db1e8b19d
LeifXu <ins@mask.io>
2024-09-24 12:19:34 +0800

    feat: token page og

--- src/helpers/getTokenPageOG.ts
+++ src/helpers/getTokenPageOG.ts
@@ -21,7 +21,7 @@ export async function getTokenPageOG(symbol: string) {
             url: resolveTokenPageUrl(symbol),
         },
         twitter: {
-            card: 'summary_large_image',
+            card: 'summary',
             title,
             description,
             images,
