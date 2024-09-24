d90ceecb7f8f7fef61447e153f7942dc7b3b59ed
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-09-24 14:47:18 +0800

    refactor: reduce helpers (#2620)
    * refactor: reduce helpers
    
    * chore: eslint
    
    ---------
    
    Co-authored-by: guanbinrui <guanbinrui@users.noreply.github.com>

--- src/helpers/parseEngagementUrl.ts
+++ src/helpers/parseEngagementUrl.ts
@@ -1,6 +1,12 @@
-import { isEngagementType } from '@/helpers/isEngagementType.js';
+import { EngagementType } from '@/constants/enum.js';
 import { isSocialSource } from '@/helpers/isSocialSource.js';
 import { resolveSourceFromUrlNoFallback } from '@/helpers/resolveSource.js';
+
+function isEngagementType(value: string): value is EngagementType {
+    return [EngagementType.Mirrors, EngagementType.Quotes, EngagementType.Likes, EngagementType.Recasts].includes(
+        value as EngagementType,
+    );
+}
 
 export function parseOldEngagementUrl(url: URL) {
     if (!url.pathname.startsWith('/post')) return null;
--- src/helpers/parseProfileUrl.ts
+++ src/helpers/parseProfileUrl.ts
@@ -1,5 +1,6 @@
 import { Source } from '@/constants/enum.js';
 import { isFollowCategory } from '@/helpers/isFollowCategory.js';
+import { isProfilePageSource } from '@/helpers/isProfilePageSource.js';
 import { isSocialProfileCategory } from '@/helpers/isSocialProfileCategory.js';
 import { isSocialSource } from '@/helpers/isSocialSource.js';
 import { isWalletProfileCategory } from '@/helpers/isWalletProfileCategory.js';
@@ -23,4 +24,14 @@ export function parseProfileUrl(pathname: string) {
             : null;
     }
     return null;
+}
+
+export function parseOldProfileUrl(url: URL) {
+    if (!url.pathname.startsWith('/profile')) return null;
+    const source = resolveSourceFromUrlNoFallback(url.searchParams.get('source'));
+    if (!source || !isProfilePageSource(source)) return null;
+    const [, , id, ...end] = url.pathname.split('/');
+    if (end.length) return null;
+    if (!id) return null;
+    return { source, id };
 }
