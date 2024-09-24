889c18a18ee00d0f3f527b60720e9c63e67ae42d
nuanyang233 <nuanyang233@gmail.com>
2024-09-24 17:34:41 +0800

    fix: post collect (#2626)
    * fix: post collect
    
    * chore: lingui extract
    
    ---------
    
    Co-authored-by: nuanyang233 <nuanyang233@users.noreply.github.com>

--- src/components/Profile/ArticleList.tsx
+++ src/components/Profile/ArticleList.tsx
@@ -4,6 +4,7 @@ import { ListInPage } from '@/components/ListInPage.js';
 import { getArticleItemContent } from '@/components/VirtualList/getArticleItemContent.js';
 import { ScrollListKey, Source } from '@/constants/enum.js';
 import { createIndicator } from '@/helpers/pageable.js';
+import { useIsWalletMuted } from '@/hooks/useIsWalletMuted.js';
 import { FireflyArticleProvider } from '@/providers/firefly/Article.js';
 
 interface ArticleListProps {
@@ -21,6 +22,8 @@ export function ArticleList({ address }: ArticleListProps) {
         select: (data) => data.pages.flatMap((x) => x.data),
     });
 
+    const { data: isMuted = false } = useIsWalletMuted(address);
+
     return (
         <ListInPage
             key={address}
@@ -29,7 +32,11 @@ export function ArticleList({ address }: ArticleListProps) {
                 listKey: `${ScrollListKey.Profile}:${Source.Article}:${address}`,
                 computeItemKey: (index, article) => `${article.id}-${index}`,
                 itemContent: (index, article) =>
-                    getArticleItemContent(index, article, `${ScrollListKey.Profile}:${Source.Article}:${address}`),
+                    getArticleItemContent(
+                        index,
+                        { ...article, author: { ...article.author, isMuted } },
+                        `${ScrollListKey.Profile}:${Source.Article}:${address}`,
+                    ),
             }}
             NoResultsFallbackProps={{
                 className: 'md:pt-[228px] max-md:py-20',
--- src/hooks/useNavigatorTitle.ts
+++ src/hooks/useNavigatorTitle.ts
@@ -1,16 +1,16 @@
 'use client';
 
-import { useMount } from 'react-use';
+import { useEffect } from 'react';
 
 import { useNavigatorState } from '@/store/useNavigatorStore.js';
 
 export function useNavigatorTitle(title: string) {
     const updateTitle = useNavigatorState.use.updateTitle();
 
-    useMount(() => {
+    useEffect(() => {
         if (title) updateTitle(title);
         return () => {
             updateTitle('');
         };
-    });
+    }, [title, updateTitle]);
 }
