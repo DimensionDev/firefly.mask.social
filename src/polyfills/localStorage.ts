c6e228ecb007d935d4d5668495978565b0999d3a
guanbinrui <guanbinrui@mask.io>
2024-09-25 12:55:18 +0200

    chore: polyfill ls

---
+++ src/polyfills/localStorage.ts
@@ -0,0 +1,29 @@
+try {
+    if (typeof window !== 'undefined') {
+        window.localStorage = window.localStorage || (() => {
+            const storage = new Map()
+            return {
+                get length() {
+                    return storage.size
+                },
+                getItem(key: string) {
+                    return storage.get(key)
+                },
+                setItem(key: string, value: string) {
+                    return storage.set(key, value)
+                },
+                removeItem(key: string) {
+                    return storage.delete(key)
+                },
+                key(index: number) {
+                    return Array.from(storage.keys())[index]
+                },
+                clear() {
+                    storage.clear()
+                },
+            }
+        })()
+    }
+} catch (error) {
+    console.error(`[polyfill localStorage]: ${error}`)
+}
