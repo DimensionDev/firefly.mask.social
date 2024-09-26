af277c0855dac00fc7b5b99cbc65211bf40d9318
guanbinrui <guanbinrui@dimension.im>
2024-09-26 12:05:07 +0800

    fix: mv to the top most

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 /* cspell:disable */
 
+// eslint-disable-next-line simple-import-sort/imports
+import '@/polyfills/index.js';
+
 import '@dialectlabs/blinks/index.css';
 import '@/app/globals.css';
-import '@/polyfills/index.js';
 
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { Inter } from 'next/font/google';
