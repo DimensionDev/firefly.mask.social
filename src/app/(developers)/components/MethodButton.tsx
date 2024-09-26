96485c4ace878ebe891d228d32c429dd758ea0a2
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-09-26 17:19:34 +0800

    refactor: remove no return (#2653)
    * refactor: remove no return
    
    * refactor: error msg

--- src/app/(developers)/components/MethodButton.tsx
+++ src/app/(developers)/components/MethodButton.tsx
@@ -45,20 +45,16 @@ export function MethodButton({ item }: Props) {
                     break;
                 }
                 case SupportedMethod.SHARE:
-                    fireflyBridgeProvider.request(SupportedMethod.SHARE, { text: SITE_NAME }, true);
+                    fireflyBridgeProvider.request(SupportedMethod.SHARE, { text: SITE_NAME });
                     break;
                 case SupportedMethod.COMPOSE:
-                    fireflyBridgeProvider.request(
-                        SupportedMethod.COMPOSE,
-                        {
-                            text: SITE_NAME,
-                            platform: Platform.FARCASTER,
-                        },
-                        true,
-                    );
+                    fireflyBridgeProvider.request(SupportedMethod.COMPOSE, {
+                        text: SITE_NAME,
+                        platform: Platform.FARCASTER,
+                    });
                     break;
                 case SupportedMethod.BACK:
-                    fireflyBridgeProvider.request(SupportedMethod.BACK, {}, true);
+                    fireflyBridgeProvider.request(SupportedMethod.BACK, {});
                     break;
                 case SupportedMethod.BACK:
                     fireflyBridgeProvider.request(SupportedMethod.BACK, {});
@@ -68,7 +64,9 @@ export function MethodButton({ item }: Props) {
                     break;
             }
         } catch (error) {
-            enqueueErrorMessage(getSnackbarMessageFromError(error, 'Failed to call method'));
+            enqueueErrorMessage(getSnackbarMessageFromError(error, 'Failed to call method'), {
+                error,
+            });
             throw error;
         }
     });
