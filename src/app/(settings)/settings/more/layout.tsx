6e23271c646d69233a4c0993c0107548f68a9e8f
nuanyang233 <nuanyang233@gmail.com>
2024-09-26 11:03:42 +0800

    chore: reply review

--- src/app/(settings)/settings/more/layout.tsx
+++ src/app/(settings)/settings/more/layout.tsx
@@ -1,11 +1,11 @@
 import { t } from '@lingui/macro';
 
-import { createPageTitle, createPageTitleSSR } from '@/helpers/createPageTitle.js';
+import { createPageTitleSSR } from '@/helpers/createPageTitle.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 
 export async function generateMetadata() {
     return createSiteMetadata({
-        title: createPageTitle(createPageTitleSSR(t`More`)),
+        title: createPageTitleSSR(t`More`),
     });
 }
 
