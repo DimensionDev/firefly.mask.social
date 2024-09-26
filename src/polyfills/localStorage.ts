9af0660e1ccf3d922da10b4c7fc73e5459cc4e7c
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-09-26 14:43:33 +0800

    fix: set ls (#2645)

--- src/polyfills/localStorage.ts
+++ src/polyfills/localStorage.ts
@@ -1,33 +1,37 @@
 try {
     if (typeof window !== 'undefined') {
-        function createStorage() {
-            const storage = new Map();
-            return {
-                get length() {
-                    return storage.size;
-                },
-                getItem(key: string) {
-                    return storage.get(key);
-                },
-                setItem(key: string, value: string) {
-                    return storage.set(key, value);
-                },
-                removeItem(key: string) {
-                    return storage.delete(key);
-                },
-                key(index: number) {
-                    return Array.from(storage.keys())[index];
-                },
-                clear() {
-                    storage.clear();
-                },
-            };
-        }
-
         if (!window.localStorage) {
-            console.info('[polyfill localStorage]: created');
+            function createStorage() {
+                const storage = new Map();
+                return {
+                    get length() {
+                        return storage.size;
+                    },
+                    getItem(key: string) {
+                        return storage.get(key);
+                    },
+                    setItem(key: string, value: string) {
+                        return storage.set(key, value);
+                    },
+                    removeItem(key: string) {
+                        return storage.delete(key);
+                    },
+                    key(index: number) {
+                        return Array.from(storage.keys())[index];
+                    },
+                    clear() {
+                        storage.clear();
+                    },
+                };
+            }
 
-            window.localStorage = createStorage();
+            // for firefly android webview the localStorage is not writable
+            Object.defineProperty(window, 'localStorage', {
+                value: createStorage(),
+                configurable: true,
+                enumerable: true,
+                writable: true,
+            });
         }
     }
 } catch (error) {
