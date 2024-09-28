eaee5a3e9da4c91db4004f479c0229b0cb9c4477
LeifXu <167842553+LeifXu@users.noreply.github.com>
2024-09-29 01:07:46 +0800

    feat: cz host redirect (#2703)
    * feat: cz host redirect
    
    * chore: eslint
    
    * feat: cz modal open
    
    * chore: eslint
    
    ---------
    
    Co-authored-by: LeifXu <LeifXu@users.noreply.github.com>

--- src/app/(activity)/activity/cz/layout.tsx
+++ src/app/(activity)/activity/cz/layout.tsx
@@ -1,13 +1,24 @@
 'use client';
 
 import { useRouter } from 'next/navigation.js';
-import type { PropsWithChildren } from 'react';
+import { type PropsWithChildren, useEffect } from 'react';
+import urlcat from 'urlcat';
 
 import { ActivityContextProvider } from '@/components/CZ/ActivityContext.js';
+import { SITE_URL } from '@/constants/index.js';
 import { ActivityClaimSuccessModalRef } from '@/modals/controls.js';
+import { fireflyBridgeProvider } from '@/providers/firefly/Bridge.js';
 
 export default function Layout({ children }: PropsWithChildren) {
     const router = useRouter();
+
+    useEffect(() => {
+        if (window.location.hostname === 'cz.firefly.social' && !fireflyBridgeProvider.supported) {
+            window.location.href = urlcat(SITE_URL, '/farcaster/trending', {
+                modal: 'cz',
+            });
+        }
+    }, []);
 
     return (
         <ActivityContextProvider
