01072b33575da25027fc238e1b85e0b4e02e825e
LeifXu <ins@mask.io>
2024-09-29 00:50:58 +0800

    feat: cz host redirect

--- src/app/(activity)/activity/cz/layout.tsx
+++ src/app/(activity)/activity/cz/layout.tsx
@@ -1,13 +1,23 @@
 'use client';
 
 import { useRouter } from 'next/navigation.js';
-import type { PropsWithChildren } from 'react';
+import { type PropsWithChildren, useEffect } from 'react';
 
 import { ActivityContextProvider } from '@/components/CZ/ActivityContext.js';
 import { ActivityClaimSuccessModalRef } from '@/modals/controls.js';
+import { fireflyBridgeProvider } from '@/providers/firefly/Bridge.js';
+import { SITE_URL } from '@/constants/index.js';
 
 export default function Layout({ children }: PropsWithChildren) {
     const router = useRouter();
+
+    useEffect(() => {
+        if (window.location.host === 'cz.firefly.social' && !fireflyBridgeProvider.supported) {
+            const url = new URL(SITE_URL);
+            url.searchParams.set('modal', 'cz');
+            window.location.href = url.toString();
+        }
+    }, []);
 
     return (
         <ActivityContextProvider
--- src/components/CZ/ActivityModal.tsx
+++ src/components/CZ/ActivityModal.tsx
@@ -1,4 +1,6 @@
-import { forwardRef, useState } from 'react';
+'use client';
+
+import { forwardRef, useEffect, useState } from 'react';
 
 import { ActivityClaimSuccessDialog } from '@/components/CZ/ActivityClaimSuccessDialog.js';
 import { ActivityDialog } from '@/components/CZ/ActivityDialog.js';
@@ -7,6 +9,12 @@ import type { SingletonModalRefCreator } from '@/libs/SingletonModal.js';
 
 export const ActivityModal = forwardRef<SingletonModalRefCreator>(function ActivityModal(_, ref) {
     const [open, dispatch] = useSingletonModal(ref);
+    useEffect(() => {
+        const searchParams = new URLSearchParams(window.location.search);
+        if (searchParams.get('modal') === 'cz') {
+            dispatch?.open();
+        }
+    }, []);
     return <ActivityDialog open={open} onClose={() => dispatch?.close()} />;
 });
 
