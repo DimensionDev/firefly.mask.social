e2282211ad332c49fba9acb01ae161c07740b1a5
nuanyang233 <nuanyang233@gmail.com>
2024-09-30 15:50:51 +0800

    chore: bookmarks title (#2737)
    * chore: bookmarks title
    
    * chore: prettier
    
    ---------
    
    Co-authored-by: nuanyang233 <nuanyang233@users.noreply.github.com>

--- src/app/(normal)/bookmarks/[source]/page.tsx
+++ src/app/(normal)/bookmarks/[source]/page.tsx
@@ -5,10 +5,13 @@ import { BookmarkList } from '@/app/(normal)/bookmarks/BookmarkList.js';
 import { Loading } from '@/components/Loading.js';
 import { type BookmarkSource, Source, SourceInURL } from '@/constants/enum.js';
 import { resolveSource } from '@/helpers/resolveSource.js';
+import { useNavigatorTitle } from '@/hooks/useNavigatorTitle.js';
+import { t } from '@lingui/macro';
 
 export default function Page({ params }: { params: { source: SourceInURL } }) {
     const source = resolveSource(params.source) as BookmarkSource;
 
+    useNavigatorTitle(t`Bookmarks`);
     return (
         <Suspense fallback={<Loading />}>
             {source === Source.Article ? <ArticleBookmarkList /> : <BookmarkList source={source} />}
