2521159fac58bcf3e28856b951264af0f3bddc05
nuanyang233 <nuanyang233@gmail.com>
2024-09-30 16:20:27 +0800

    fix: avoid duplicate error

--- src/app/(normal)/error.tsx
+++ src/app/(normal)/error.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { usePathname } from 'next/navigation.js';
+
 import { ErrorHandler } from '@/components/ErrorHandler.js';
+import { isRoutePathname } from '@/helpers/isRoutePathname.js';
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+    const pathname = usePathname();
+    const isProfilePage = isRoutePathname(pathname, '/profile/:source');
+    if(isProfilePage) return
     return <ErrorHandler error={error} reset={reset} />;
 }
