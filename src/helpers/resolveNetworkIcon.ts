7ce8fb28f885044b0b74d8e2ab3a365cc4fec275
guanbinrui <guanbinrui@mask.io>
2024-10-11 10:44:11 +0800

    fix: path

--- src/helpers/resolveNetworkIcon.ts
+++ src/helpers/resolveNetworkIcon.ts
@@ -5,11 +5,11 @@ import { NetworkType } from '@/constants/enum.js';
 export function resolveNetworkIcon(networkType: NetworkType, isDarkMode: boolean) {
     switch (networkType) {
         case NetworkType.Solana:
-            return new URL(`../../assets/chains/solana.png`, import.meta.url).href;
+            return new URL(`../assets/chains/solana.png`, import.meta.url).href;
         case NetworkType.Ethereum:
             return isDarkMode
-                ? new URL('../../assets/chains/ethereum.dark.png', import.meta.url).href
-                : new URL('../../assets/chains/ethereum.light.png', import.meta.url).href;
+                ? new URL('../assets/chains/ethereum.dark.png', import.meta.url).href
+                : new URL('../assets/chains/ethereum.light.png', import.meta.url).href;
         default:
             safeUnreachable(networkType);
             return null;
