898f72921664b5c328e0326cacf5d95b0715c8f4
LeifXu <ins@mask.io>
2024-10-14 15:52:06 +0800

    fix: `parseOldNftUrl`

--- src/helpers/parseOldNftUrl.ts
+++ src/helpers/parseOldNftUrl.ts
@@ -1,9 +1,9 @@
-import { parseChainId } from '@/helpers/parseChainId.js';
+import type { ChainId } from '@masknet/web3-shared-evm';
 
 export function parseOldNftUrl(url: URL) {
     if (!/^\/nft\/0x[a-fA-F0-9]{40}/.test(url.pathname)) return null;
     const [, , address, tokenId] = url.pathname.split('/');
-    const chainId = parseChainId(url.searchParams.get('chainId') ?? '1');
+    const chainId = parseInt(url.searchParams.get('chainId') ?? '1') as ChainId;
     if (!chainId) return null;
     if (tokenId) {
         return {
