845dbf7c12fc4ebf1f9eb16445032e7043a19f52
Wukong Sun <swkatmask@gmail.com>
2024-10-14 11:45:39 +0800

    refactor: fw-2503 move close button to left side (#2795)

--- src/modals/CollectPostModal.tsx
+++ src/modals/CollectPostModal.tsx
@@ -29,16 +29,15 @@ export const CollectPostModal = forwardRef<SingletonModalRefCreator<CollectPostM
             <Modal disableDialogClose onClose={() => dispatch?.close()} open={open}>
                 <ClickableArea className="relative w-[432px] max-w-[90vw] rounded-xl bg-lightBottom shadow-popover transition-all dark:bg-darkBottom dark:text-gray-950">
                     <div className="inline-flex h-14 w-full items-center justify-center gap-2 rounded-t-[12px] p-4">
-                        <div className="relative h-6 w-6" />
-                        <div className="shrink grow basis-0 text-center text-lg font-bold leading-snug text-main">
-                            <Trans>Collect Post</Trans>
-                        </div>
-
                         <CloseButton
                             onClick={() => {
                                 dispatch?.close();
                             }}
                         />
+                        <div className="shrink grow basis-0 text-center text-lg font-bold leading-snug text-main">
+                            <Trans>Collect Post</Trans>
+                        </div>
+                        <div className="relative h-6 w-6" />
                     </div>
                     {props?.post ? <PostCollect post={props?.post} onClose={() => dispatch?.close()} /> : null}
                 </ClickableArea>
