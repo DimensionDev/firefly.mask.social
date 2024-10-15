36839618faf2248ab19d994a3da852c210675cbc
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-10-15 22:55:36 +0800

    feat: add bridge methods for events (#2833)
    * chore: impl bridge methods for events
    
    * chore: update desc
    
    * refactor: desc

--- src/app/(developers)/developers/bridge/page.tsx
+++ src/app/(developers)/developers/bridge/page.tsx
@@ -18,7 +18,7 @@ const items: Item[] = [
         type: 'method',
         name: SupportedMethod.GET_SUPPORTED_METHODS,
         title: 'Get Supported Methods',
-        description: 'Get supported methods from firefly app.',
+        description: 'Get supported bridge methods.',
     },
     {
         type: 'method',
@@ -42,13 +42,31 @@ const items: Item[] = [
         type: 'method',
         name: SupportedMethod.GET_WALLET_ADDRESS,
         title: 'Get Wallet Address',
-        description: 'Get wallet address from firefly app.',
+        description: 'Get connected wallet address from firefly app.',
     },
     {
         type: 'method',
         name: SupportedMethod.CONNECT_WALLET,
         title: 'Connect Wallet',
-        description: 'Connect wallet from firefly app.',
+        description: 'Connect a wallet',
+    },
+    {
+        type: 'method',
+        name: SupportedMethod.BIND_WALLET,
+        title: 'Bind Wallet',
+        description: 'Bind a wallet',
+    },
+    {
+        type: 'method',
+        name: SupportedMethod.FOLLOW_TWITTER_USER,
+        title: 'Follow Twitter User',
+        description: 'Follow a twitter user.',
+    },
+    {
+        type: 'method',
+        name: SupportedMethod.UPDATE_NAVIGATOR_BAR,
+        title: 'Update Navigator Bar',
+        description: 'Update title on navigator bar.',
     },
     {
         type: 'method',
