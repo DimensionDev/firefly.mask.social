95f09e4086a3ef5e9444b81c50d23bf7a58e116f
nuanyang233 <nuanyang233@gmail.com>
2024-10-21 17:39:02 +0800

    chore: ui

--- src/components/SourceTabs/index.tsx
+++ src/components/SourceTabs/index.tsx
@@ -13,9 +13,12 @@ interface SourceTabsProps<S extends Source> {
 export function SourceTabs<S extends Source>({ sources, source, href }: SourceTabsProps<S>) {
     return (
         <div
-            className={
-                'no-scrollbar sticky top-[53px] z-40 w-full overflow-x-auto overflow-y-hidden border-b border-line bg-primaryBottom px-4 md:top-0'
-            }
+            className={classNames(
+                'no-scrollbar sticky top-[54px] z-40 w-full overflow-x-auto overflow-y-hidden border-b border-line bg-primaryBottom px-4 md:top-0',
+                {
+                    'top-[53px]': IS_APPLE && IS_SAFARI,
+                },
+            )}
         >
             <nav className="flex space-x-4 text-xl" aria-label="Tabs">
                 {sources.map((x) => (
