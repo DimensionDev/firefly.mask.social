efa4c9e8856d7b5425e2451cc82d89c556845be7
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-10-22 15:04:31 +0800

    chore: remove zerion

--- src/configs/wagmiClient.ts
+++ src/configs/wagmiClient.ts
@@ -112,14 +112,12 @@ createAppKit({
               'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
               '18388be9ac2d02726dbac9777c96efaac06d744b2f6d580fccdd4127a6d01fd1', // Rabby
               '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709', // OKX Wallet
-              'ecc4036f814562b41a5268adc86270fba1365471402006302e70169465b7ac18', // Zerion
           ]
         : [
               'network.particle', // Firefly Wallet,
               'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase
               '18388be9ac2d02726dbac9777c96efaac06d744b2f6d580fccdd4127a6d01fd1', // Rabby
               '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709', // OKX Wallet
-              'ecc4036f814562b41a5268adc86270fba1365471402006302e70169465b7ac18', // Zerion
           ],
 });
 
