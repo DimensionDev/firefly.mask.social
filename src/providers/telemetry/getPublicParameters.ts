c75ab4d9cfa2f9ca30b71cd2f6ee5f26bea13cc3
guanbinrui <guanbinrui@mask.io>
2024-10-23 21:15:55 +0800

    fix: sol addr

--- src/providers/telemetry/getPublicParameters.ts
+++ src/providers/telemetry/getPublicParameters.ts
@@ -6,10 +6,11 @@ import { bom } from '@/helpers/bom.js';
 import { resolveWalletAdapter } from '@/providers/solana/resolveWalletAdapter.js';
 import { useDeveloperSettingsState } from '@/store/useDeveloperSettingsStore.js';
 import { useFireflyStateStore } from '@/store/useProfileStore.js';
+import { runInSafe } from '@/helpers/runInSafe.js';
 
 export function getPublicParameters(eventId: string, previousEventId: string | null) {
     const evmClient = getClient(config);
-    const solanaAdaptor = resolveWalletAdapter();
+    const solanaAdaptor = runInSafe(() => resolveWalletAdapter());
     return {
         public_uuid: eventId,
         public_previous_uuid: previousEventId,
@@ -20,7 +21,7 @@ export function getPublicParameters(eventId: string, previousEventId: string | n
         public_evm_address: evmClient?.account?.address,
         public_evm_chain_id: evmClient?.chain.id,
         public_solana_chain_id: ChainId.Mainnet,
-        public_solana_address: solanaAdaptor.publicKey?.toBase58(),
+        public_solana_address: solanaAdaptor?.publicKey?.toBase58(),
 
         public_account_id: useFireflyStateStore.getState().currentProfileSession?.profileId,
         public_use_development_api: useDeveloperSettingsState.getState().useDevelopmentAPI,
