bb725e77b7fd2064dae4c030ab80ebe2e4847e54
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-10-24 17:54:22 +0800

    fix: evm address (#2927)

--- src/providers/telemetry/getPublicParameters.ts
+++ src/providers/telemetry/getPublicParameters.ts
@@ -1,5 +1,5 @@
 import { ChainId } from '@masknet/web3-shared-solana';
-import { getClient } from '@wagmi/core';
+import { getAccount } from '@wagmi/core';
 
 import { config } from '@/configs/wagmiClient.js';
 import { bom } from '@/helpers/bom.js';
@@ -14,8 +14,9 @@ import {
 } from '@/store/useProfileStore.js';
 
 export function getPublicParameters(eventId: string, previousEventId: string | null) {
-    const evmClient = getClient(config);
+    const evmAccount = runInSafe(() => getAccount(config));
     const solanaAdaptor = runInSafe(() => resolveWalletAdapter());
+
     return {
         public_uuid: eventId,
         public_previous_uuid: previousEventId,
@@ -23,8 +24,8 @@ export function getPublicParameters(eventId: string, previousEventId: string | n
         public_ua: bom.navigator?.userAgent,
         public_href: bom.location?.href,
 
-        public_evm_address: evmClient?.account?.address,
-        public_evm_chain_id: evmClient?.chain.id,
+        public_evm_address: evmAccount?.address,
+        public_evm_chain_id: evmAccount?.chainId,
         public_solana_chain_id: ChainId.Mainnet,
         public_solana_address: solanaAdaptor?.publicKey?.toBase58(),
 
