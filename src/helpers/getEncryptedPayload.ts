9aaeb730ebddb9456d6cf4543d8074e97f7d9350
Wukong Sun <swkatmask@gmail.com>
2024-10-24 17:37:18 +0800

    fix: fw-2671 liking post flickers UI (#2926)
    set critical hook deps

--- src/helpers/getEncryptedPayload.ts
+++ src/helpers/getEncryptedPayload.ts
@@ -1,15 +1,14 @@
 import { compact, first } from 'lodash-es';
 
-import type { Post } from '@/providers/types/SocialMedia.js';
+import type { Attachment } from '@/providers/types/SocialMedia.js';
 import { steganographyDecodeImage } from '@/services/steganography.js';
 
 export type EncryptedPayload = readonly [string | Uint8Array, '1' | '2', string | null];
 
-export function getEncryptedPayloadFromText(post: Post): EncryptedPayload | undefined {
-    const raw = post.metadata.content?.content;
-    if (!raw) return;
+export function getEncryptedPayloadFromText(text: string | undefined): EncryptedPayload | undefined {
+    if (!text) return;
 
-    const matched = raw.match(/(?:.*)PostData_(v1|v2)=(.*)/);
+    const matched = text.match(/(?:.*)PostData_(v1|v2)=(.*)/);
     if (!matched) return;
 
     const [, version, payload] = matched;
@@ -19,17 +18,19 @@ export function getEncryptedPayloadFromText(post: Post): EncryptedPayload | unde
     return;
 }
 
-export async function getEncryptedPayloadFromImageAttachment(post: Post): Promise<EncryptedPayload | undefined> {
-    const result =
-        post.metadata.content?.attachments?.map(async (attachment) => {
-            if (attachment.type !== 'Image') return;
-            if (!attachment.uri) return;
+export async function getEncryptedPayloadFromImageAttachment(
+    attachments: Attachment[] | undefined,
+): Promise<EncryptedPayload | undefined> {
+    if (!attachments) return undefined;
+    const result = attachments.map(async (attachment) => {
+        if (attachment.type !== 'Image') return;
+        if (!attachment.uri) return;
 
-            const decoded = await steganographyDecodeImage(attachment.uri);
-            if (!decoded) return;
+        const decoded = await steganographyDecodeImage(attachment.uri);
+        if (!decoded) return;
 
-            return [decoded, '2', attachment.uri] as EncryptedPayload;
-        }) ?? [];
+        return [decoded, '2', attachment.uri] as EncryptedPayload;
+    });
 
     const allSettled = await Promise.allSettled(result);
     return first(compact(allSettled.map((x) => (x.status === 'fulfilled' ? x.value : null))));
