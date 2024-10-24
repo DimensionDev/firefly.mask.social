53160ee8463645d81b4434ed2b07e77d1435a9e8
dudu0506 <dudu@mask.io>
2024-10-24 17:18:41 +0800

    feat: search users on x

---
+++ src/app/api/twitter/user/search/route.ts
@@ -0,0 +1,59 @@
+import { isObject } from 'lodash-es';
+import { NextRequest } from 'next/server.js';
+import urlcat from 'urlcat';
+import { z } from 'zod';
+
+import { TWITTER_USER_OPTIONS } from '@/constants/twitter.js';
+import { compose } from '@/helpers/compose.js';
+import { createSuccessResponseJSON } from '@/helpers/createResponseJSON.js';
+import { createTwitterClientV2 } from '@/helpers/createTwitterClientV2.js';
+import { getSearchParamsFromRequestWithZodObject } from '@/helpers/getSearchParamsFromRequestWithZodObject.js';
+import { withRequestErrorHandler } from '@/helpers/withRequestErrorHandler.js';
+import { withTwitterRequestErrorHandler } from '@/helpers/withTwitterRequestErrorHandler.js';
+import type { NextRequestContext } from '@/types/index.js';
+
+const SearchPageable = z.object({
+    query: z.string().min(1),
+    cursor: z.string().optional(),
+    limit: z.coerce
+        .number()
+        .default(25)
+        .refine((value) => {
+            if (value) z.coerce.number().int().min(1).parse(value);
+            return true;
+        }),
+});
+
+export const GET = compose<(request: NextRequest, context?: NextRequestContext) => Promise<Response>>(
+    withRequestErrorHandler({ throwError: true }),
+    withTwitterRequestErrorHandler,
+    async (request) => {
+        const queryParams = getSearchParamsFromRequestWithZodObject(request, SearchPageable);
+
+        const userFields = TWITTER_USER_OPTIONS['user.fields'];
+        const url = urlcat('users/search', {
+            query: queryParams.query,
+            'user.fields': Array.isArray(userFields) ? userFields.join(',') : undefined,
+            next_token: queryParams.cursor ? queryParams.cursor : undefined,
+            max_results: queryParams.limit,
+        });
+        const client = await createTwitterClientV2(request);
+
+        let data = {};
+        try {
+            data = await client.v2.get(url);
+        } catch (error) {
+            if (isObject(error)) {
+                return createSuccessResponseJSON({
+                    error: JSON.stringify(error),
+                });
+            } else {
+                return createSuccessResponseJSON({
+                    error,
+                });
+            }
+        }
+
+        return createSuccessResponseJSON(data);
+    },
+);
