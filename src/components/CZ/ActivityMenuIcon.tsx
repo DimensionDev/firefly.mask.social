d208ff2e8a11318d1b696227ae3df46856ad53e3
LeifXu <ins@mask.io>
2024-10-25 18:06:04 +0800

    feat: cz landing page

--- src/components/CZ/ActivityMenuIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { Trans } from '@lingui/macro';
-
-export function ActivityMenuIcon() {
-    return (
-        <div className="h-5 w-5 text-xl leading-5">
-            <Trans>🎉</Trans>
-        </div>
-    );
-}
