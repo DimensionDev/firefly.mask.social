95ab4e5a228068d554b4a0a29a5f64ab1aad9001
LeifXu <167842553+LeifXu@users.noreply.github.com>
2024-10-26 00:08:20 +0800

    feat: bridge login params (#2952)

--- src/components/Activity/hooks/useLoginInActivity.ts
+++ src/components/Activity/hooks/useLoginInActivity.ts
@@ -13,6 +13,7 @@ export function useLoginInActivity() {
         if (fireflyBridgeProvider.supported) {
             const result = await fireflyBridgeProvider.request(SupportedMethod.LOGIN, {
                 platform: Platform.TWITTER,
+                activity: window.location.href,
             });
             await queryFireflyBridgeAuthorization.refetch();
             if (result === 'true') {
