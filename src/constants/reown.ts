0bc0ddd8e8c6b40a3ca65f7566c74c00dc315567
Wukong Sun <swkatmask@gmail.com>
2024-10-28 19:22:18 +0800

    fix: fw-2624 update feature wallets (#2969)

--- src/constants/reown.ts
+++ src/constants/reown.ts
@@ -6,6 +6,7 @@ export enum WalletId {
     MetaMask = 'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
     Rabby = '18388be9ac2d02726dbac9777c96efaac06d744b2f6d580fccdd4127a6d01fd1',
     OKX = '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709',
+    Rainbow = '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369',
 
     Injected = 'injected',
 }
