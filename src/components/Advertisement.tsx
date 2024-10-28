238a0ea00e919de714c47a20184a549cd5e1d864
guanbinrui <guanbinrui@dimension.im>
2024-10-28 12:05:05 +0800

    refactor: swiper

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -9,6 +9,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { ClickableArea } from '@/components/ClickableArea.js';
 import { Image } from '@/components/Image.js';
+import { NoSSR } from '@/components/NoSSR.js';
 import { AdFunctionType, AdvertisementType } from '@/constants/enum.js';
 import { Link } from '@/esm/Link.js';
 import { fetchJSON } from '@/helpers/fetchJSON.js';
@@ -29,59 +30,61 @@ export async function Advertisement() {
 
     return (
         <div>
-            <Swiper
-                className="ff-advertisement"
-                pagination
-                loop
-                modules={[Autoplay, Pagination, Navigation]}
-                autoplay={{ delay: 8000 }}
-                spaceBetween={50}
-            >
-                {data.map((ad, index) => (
-                    <SwiperSlide key={index} className="w-96">
-                        {ad.type === AdvertisementType.Link && ad.link ? (
-                            <Link href={ad.link} target="_blank">
-                                <Image
-                                    className="w-full cursor-pointer rounded-xl"
-                                    alt={ad.link}
-                                    src={ad.image}
-                                    width={346}
-                                    height={130}
-                                />
-                            </Link>
-                        ) : (
-                            <ClickableArea
-                                onClick={() => {
-                                    switch (ad.function) {
-                                        case AdFunctionType.OpenScan:
-                                            LoginModalRef.open();
-                                            break;
-                                        case AdFunctionType.OpenActivity:
-                                            // native
-                                            if (fireflyBridgeProvider.supported) {
-                                                openWindow('https://cz.firefly.social/');
-                                            } else {
-                                                ActivityModalRef.open();
-                                            }
-                                            break;
-                                        default:
-                                            safeUnreachable(ad.function);
-                                            break;
-                                    }
-                                }}
-                            >
-                                <Image
-                                    className="w-full cursor-pointer"
-                                    alt={ad.function}
-                                    src={ad.image}
-                                    width={346}
-                                    height={130}
-                                />
-                            </ClickableArea>
-                        )}
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            <NoSSR>
+                <Swiper
+                    className="ff-advertisement"
+                    pagination
+                    loop
+                    modules={[Autoplay, Pagination, Navigation]}
+                    autoplay={{ delay: 8000 }}
+                    spaceBetween={50}
+                >
+                    {data.map((ad, index) => (
+                        <SwiperSlide key={index} className="w-96">
+                            {ad.type === AdvertisementType.Link && ad.link ? (
+                                <Link href={ad.link} target="_blank">
+                                    <Image
+                                        className="w-full cursor-pointer rounded-xl"
+                                        alt={ad.link}
+                                        src={ad.image}
+                                        width={346}
+                                        height={130}
+                                    />
+                                </Link>
+                            ) : (
+                                <ClickableArea
+                                    onClick={() => {
+                                        switch (ad.function) {
+                                            case AdFunctionType.OpenScan:
+                                                LoginModalRef.open();
+                                                break;
+                                            case AdFunctionType.OpenActivity:
+                                                // native
+                                                if (fireflyBridgeProvider.supported) {
+                                                    openWindow('https://cz.firefly.social/');
+                                                } else {
+                                                    ActivityModalRef.open();
+                                                }
+                                                break;
+                                            default:
+                                                safeUnreachable(ad.function);
+                                                break;
+                                        }
+                                    }}
+                                >
+                                    <Image
+                                        className="w-full cursor-pointer"
+                                        alt={ad.function}
+                                        src={ad.image}
+                                        width={346}
+                                        height={130}
+                                    />
+                                </ClickableArea>
+                            )}
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </NoSSR>
         </div>
     );
 }
