50fec673812f308ba24e49f4061188a68f45267e
LeifXu <167842553+LeifXu@users.noreply.github.com>
2024-10-31 15:29:09 +0800

    feat: activity ended dialog (#3019)

--- src/app/(event)/event/[name]/page.tsx
+++ src/app/(event)/event/[name]/page.tsx
@@ -1,9 +1,11 @@
 import { ActivityElex24Provider } from '@/components/Activity/ActivityElex24/ActivityElex24Context.js';
+import { ActivityEndedDialog } from '@/components/Activity/ActivityEndedDialog.js';
 import { ActivityHeader } from '@/components/Activity/ActivityHeader.js';
 import { ActivityNavigationBar } from '@/components/Activity/ActivityNavigationBar.js';
 import { ActivityTasks } from '@/components/Activity/ActivityTasks/index.js';
 import { setupLocaleForSSR } from '@/i18n/index.js';
 import { FireflyActivityProvider } from '@/providers/firefly/Activity.js';
+import { ActivityStatus } from '@/providers/types/Firefly.js';
 
 export default async function Page({
     params: { name },
@@ -21,6 +23,7 @@ export default async function Page({
                 <ActivityNavigationBar>{data.title}</ActivityNavigationBar>
                 <ActivityHeader data={data} />
                 <ActivityTasks data={data} name={name} />
+                {data.status === ActivityStatus.Ended ? <ActivityEndedDialog data={data} /> : null}
             </div>
         </ActivityElex24Provider>
     );
