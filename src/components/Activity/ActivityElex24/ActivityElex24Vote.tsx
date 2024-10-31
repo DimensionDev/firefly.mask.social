fbc6e0660d9aa2cabfcc07ecff17ca0904e9688d
LeifXu <ins@mask.io>
2024-10-31 15:46:20 +0800

    feat: activity i18n text

--- src/components/Activity/ActivityElex24/ActivityElex24Vote.tsx
+++ src/components/Activity/ActivityElex24/ActivityElex24Vote.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Trans } from '@lingui/macro';
 import { useContext } from 'react';
 
 import TickSquareIcon from '@/assets/tick-square.svg';
@@ -14,12 +15,12 @@ export function ActivityElex24Vote() {
     const options = [
         {
             icon: '/image/activity/elex24/trump-basic.png',
-            name: 'Donald Trump',
+            name: <Trans>Donald Trump</Trans>,
             value: ActivityElex24VoteOption.Trump,
         },
         {
             icon: '/image/activity/elex24/harris-basic.png',
-            name: 'Kamala Harris',
+            name: <Trans>Kamala Harris</Trans>,
             value: ActivityElex24VoteOption.Harris,
         },
     ];
@@ -30,7 +31,7 @@ export function ActivityElex24Vote() {
                 const selected = vote === option.value || option.value === data?.ext?.vote;
                 return (
                     <button
-                        key={option.name}
+                        key={option.value}
                         className="flex h-12 rounded-2xl bg-bg p-3 text-sm font-semibold leading-6 disabled:bg-success/10 disabled:dark:bg-success/20"
                         disabled={selected}
                         onClick={() => {
@@ -38,7 +39,7 @@ export function ActivityElex24Vote() {
                             setVote(option.value);
                         }}
                     >
-                        <Image src={option.icon} alt={option.name} width={24} height={24} className="mr-2 h-6 w-6" />
+                        <Image src={option.icon} alt={option.value} width={24} height={24} className="mr-2 h-6 w-6" />
                         <span>{option.name}</span>
                         {selected ? <TickSquareIcon className="ml-auto h-6 w-6 shrink-0 text-success" /> : null}
                     </button>
