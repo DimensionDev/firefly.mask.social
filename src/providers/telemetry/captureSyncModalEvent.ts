eca6cce9f3a921731bfcd5cb7c587fdc0e9eb085
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-11-06 13:12:27 +0800

    fix: sync modal parameters (#3079)

--- src/providers/telemetry/captureSyncModalEvent.ts
+++ src/providers/telemetry/captureSyncModalEvent.ts
@@ -2,8 +2,10 @@ import { runInSafeAsync } from '@/helpers/runInSafe.js';
 import { TelemetryProvider } from '@/providers/telemetry/index.js';
 import { EventId } from '@/providers/types/Telemetry.js';
 
-export function captureSyncModalEvent(confirmed: boolean) {
+export function captureSyncModalEvent(accountId: string, confirmed: boolean) {
     return runInSafeAsync(() => {
-        return TelemetryProvider.captureEvent(confirmed ? EventId.TOKEN_SYNC_USE_YES : EventId.TOKEN_SYNC_USE_NO, {});
+        return TelemetryProvider.captureEvent(confirmed ? EventId.TOKEN_SYNC_USE_YES : EventId.TOKEN_SYNC_USE_NO, {
+            firefly_account_id: accountId,
+        });
     });
 }
