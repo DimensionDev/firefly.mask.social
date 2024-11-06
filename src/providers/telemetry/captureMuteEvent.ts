5d988cf7b1206a835e1c8440e6670dfdde35946c
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-11-06 13:59:15 +0800

    chore: add captureMuteEvent (#3080)
    * chore: add captureMuteEvent
    
    * refactor: against
    
    * chore: capture mute events

---
+++ src/providers/telemetry/captureMuteEvent.ts
@@ -0,0 +1,25 @@
+import type { Address } from 'viem';
+
+import { UnreachableError } from '@/constants/error.js';
+import { createLookupTableResolver } from '@/helpers/createLookupTableResolver.js';
+import { runInSafeAsync } from '@/helpers/runInSafe.js';
+import { TelemetryProvider } from '@/providers/telemetry/index.js';
+import type { Channel, Profile } from '@/providers/types/SocialMedia.js';
+import { EventId } from '@/providers/types/Telemetry.js';
+
+const resolveEventId = createLookupTableResolver(
+    {
+        all: EventId.MUTE_ALL_SUCCESS,
+        mute: EventId.MUTE_SUCCESS,
+        unmute: EventId.UNMUTE_SUCCESS,
+    },
+    (action) => {
+        throw new UnreachableError('Invalid action', action);
+    },
+);
+
+export function captureMuteEvent(action: 'all' | 'mute' | 'unmute', against: Profile | Channel | Address) {
+    return runInSafeAsync(async () => {
+        return TelemetryProvider.captureEvent(resolveEventId(action), {});
+    });
+}
