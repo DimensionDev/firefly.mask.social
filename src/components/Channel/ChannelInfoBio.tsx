6ceeb2a7bf1fbe4b9251c70f8ec8759a9714d6d4
nuanyang233 <nuanyang233@gmail.com>
2024-11-08 21:44:59 +0800

    chore: style

--- src/components/Channel/ChannelInfoBio.tsx
+++ src/components/Channel/ChannelInfoBio.tsx
@@ -3,18 +3,10 @@
 import { BioMarkup } from '@/components/Markup/BioMarkup.js';
 import { Source } from '@/constants/enum.js';
 import { classNames } from '@/helpers/classNames.js';
-import { useIsMedium } from '@/hooks/useMediaQuery.js';
 
 export function ChannelInfoBio({ description }: { description?: string }) {
-    const isMedium = useIsMedium();
-
     return (
-        <BioMarkup
-            className={classNames('text-medium', {
-                '-ml-[60px]': !isMedium,
-            })}
-            source={Source.Farcaster}
-        >
+        <BioMarkup className={classNames('text-medium max-md:-ml-[60px]')} source={Source.Farcaster}>
             {description ?? '-'}
         </BioMarkup>
     );
