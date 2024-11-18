976f727d9e6fbdbb8db9547c590ba9d1422a92ba
dudu0506 <dudu@mask.io>
2024-11-18 20:00:46 +0800

    fix: search ui bugs (#3217)

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -5,6 +5,7 @@ import LeftArrowIcon from '@/assets/left-arrow.svg';
 import { ClickableButton } from '@/components/ClickableButton.js';
 import { Tooltip } from '@/components/Tooltip.js';
 import { classNames } from '@/helpers/classNames.js';
+import { useIsMedium } from '@/hooks/useMediaQuery.js';
 
 interface BackButtonProps extends HTMLProps<HTMLButtonElement> {
     size?: number;
@@ -12,17 +13,25 @@ interface BackButtonProps extends HTMLProps<HTMLButtonElement> {
 }
 
 export function BackButton({ size = 24, tooltip = t`Back`, ref, ...rest }: BackButtonProps) {
-    return (
+    const isMedium = useIsMedium();
+
+    const button = (
+        <ClickableButton {...rest} className={classNames('rounded hover:bg-lightBg', rest.className)}>
+            <LeftArrowIcon
+                className={classNames('text-main', {
+                    'cursor-pointer': !rest.disabled,
+                })}
+                width={size}
+                height={size}
+            />
+        </ClickableButton>
+    );
+
+    return isMedium ? (
         <Tooltip content={tooltip} placement="top">
-            <ClickableButton {...rest} className={classNames('rounded hover:bg-lightBg', rest.className)}>
-                <LeftArrowIcon
-                    className={classNames('text-main', {
-                        'cursor-pointer': !rest.disabled,
-                    })}
-                    width={size}
-                    height={size}
-                />
-            </ClickableButton>
+            {button}
         </Tooltip>
+    ) : (
+        button
     );
 }
