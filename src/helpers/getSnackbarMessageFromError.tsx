40111fb51fbb38964712b03595e04c2fc7f9be24
guanbinrui <guanbinrui@mask.io>
2024-11-26 10:51:54 +0800

    refactor: getSnackbarMessageFromError

--- src/helpers/getSnackbarMessageFromError.tsx
+++ src/helpers/getSnackbarMessageFromError.tsx
@@ -1,4 +1,4 @@
-import { t, Trans } from '@lingui/macro';
+import { Trans } from '@lingui/macro';
 import { ClientError } from 'graphql-request';
 import { first } from 'lodash-es';
 import type { SnackbarMessage } from 'notistack';
@@ -6,40 +6,12 @@ import { EstimateGasExecutionError, UserRejectedRequestError } from 'viem';
 
 import { SnackbarErrorMessage } from '@/components/SnackbarErrorMessage.js';
 import { FarcasterInvalidSignerKey, FetchError, UserRejectionError } from '@/constants/error.js';
-import { enqueueWarningMessage } from '@/helpers/enqueueMessage.js';
 import { getErrorMessageFromFetchError } from '@/helpers/getErrorMessageFromFetchError.js';
 
 type SolanaError = {
     code: number;
     message: string;
 };
-function captureWarningMessageFromError(error: unknown) {
-    let currentError = error;
-    const visited = new Set();
-
-    // For solana wallet adapter
-    const message = 'user rejected the request';
-    if (
-        error instanceof Error &&
-        (error.message.includes(message) ||
-            ('error' in error && (error.error as SolanaError).message?.includes(message)))
-    ) {
-        enqueueWarningMessage(t`The user rejected the request.`);
-        return true;
-    }
-
-    // UserRejectedRequestError from viem
-    while (currentError instanceof Error && !visited.has(currentError)) {
-        visited.add(currentError);
-        if (currentError instanceof UserRejectedRequestError) {
-            enqueueWarningMessage(t`The user rejected the request.`);
-            return true;
-        }
-        currentError = currentError.cause;
-    }
-
-    return false;
-}
 
 /**
  * Get a snackbar message from an error.
@@ -48,10 +20,6 @@ function captureWarningMessageFromError(error: unknown) {
  * @returns
  */
 export function getSnackbarMessageFromError(error: unknown, fallback: string): SnackbarMessage {
-    if (captureWarningMessageFromError(error)) {
-        return '';
-    }
-
     if (error instanceof ClientError) {
         const message = first(error.response.errors)?.message;
         if (message) return message;
@@ -85,6 +53,42 @@ export function getSnackbarMessageFromError(error: unknown, fallback: string): S
                 message={<Trans>The user canceled the operation.</Trans>}
             />
         );
+    }
+
+    {
+        // For solana wallet adapter
+        const message = 'user rejected the request';
+        if (
+            error instanceof Error &&
+            (error.message.includes(message) ||
+                ('error' in error && (error.error as SolanaError).message?.includes(message)))
+        ) {
+            return (
+                <SnackbarErrorMessage
+                    title={<Trans>Rejected</Trans>}
+                    message={<Trans>The user rejected the request.</Trans>}
+                />
+            );
+        }
+    }
+
+    {
+        let currentError = error;
+        const visited = new Set();
+
+        // UserRejectedRequestError from viem
+        while (currentError instanceof Error && !visited.has(currentError)) {
+            visited.add(currentError);
+            if (currentError instanceof UserRejectedRequestError) {
+                return (
+                    <SnackbarErrorMessage
+                        title={<Trans>Rejected</Trans>}
+                        message={<Trans>The user rejected the request.</Trans>}
+                    />
+                );
+            }
+            currentError = currentError.cause;
+        }
     }
 
     return fallback;
