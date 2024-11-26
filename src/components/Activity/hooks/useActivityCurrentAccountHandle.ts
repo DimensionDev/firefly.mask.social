db8888f6583ba6c478bb9b7adeb483109d9931e2
LeifXu <ins@mask.io>
2024-11-26 22:57:07 +1030

    feat: frensgiving event

--- src/components/Activity/hooks/useActivityCurrentAccountHandle.ts
+++ src/components/Activity/hooks/useActivityCurrentAccountHandle.ts
@@ -1,5 +1,4 @@
 import { safeUnreachable } from '@masknet/kit';
-import { useMemo } from 'react';
 
 import { useActivityWalletProfiles } from '@/components/Activity/hooks/useActivityWalletProfiles.js';
 import { type SocialSource, Source } from '@/constants/enum.js';
@@ -9,20 +8,18 @@ import { fireflyBridgeProvider } from '@/providers/firefly/Bridge.js';
 export function useActivityCurrentAccountHandle(source: SocialSource) {
     const profile = useCurrentProfile(source);
     const { data: profiles } = useActivityWalletProfiles();
-    return useMemo(() => {
-        if (fireflyBridgeProvider.supported) {
-            switch (source) {
-                case Source.Twitter:
-                    return profiles?.twitterProfiles?.[0]?.handle;
-                case Source.Lens:
-                    return profiles?.lensProfilesV3?.[0]?.fullHandle;
-                case Source.Farcaster:
-                    return profiles?.farcasterProfiles?.[0]?.username;
-                default:
-                    safeUnreachable(source);
-                    return;
-            }
+    if (fireflyBridgeProvider.supported) {
+        switch (source) {
+            case Source.Twitter:
+                return profiles?.twitterProfiles?.[0]?.handle;
+            case Source.Lens:
+                return profiles?.lensProfilesV3?.[0]?.fullHandle;
+            case Source.Farcaster:
+                return profiles?.farcasterProfiles?.[0]?.username;
+            default:
+                safeUnreachable(source);
+                return;
         }
-        return profile?.handle;
-    }, [profile, profiles, source]);
+    }
+    return profile?.handle;
 }
