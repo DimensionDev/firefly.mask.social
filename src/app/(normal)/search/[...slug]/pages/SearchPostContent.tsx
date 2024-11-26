6740d04b23412f25999908bdcc75f18cc51640c4
dudu0506 <dudu@mask.io>
2024-11-26 09:15:46 +0800

    fix: search ui for mobile mode (#3283)

--- src/app/(normal)/search/[...slug]/pages/SearchPostContent.tsx
+++ src/app/(normal)/search/[...slug]/pages/SearchPostContent.tsx
@@ -4,19 +4,14 @@ import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 import { compact } from 'lodash-es';
 
 import { ListInPage } from '@/components/ListInPage.js';
-import { SinglePost } from '@/components/Posts/SinglePost.js';
 import { Empty } from '@/components/Search/Empty.js';
 import { SearchSources } from '@/components/Search/SearchSources.js';
+import { getPostItemContent } from '@/components/VirtualList/getPostItemContent.js';
 import { ScrollListKey } from '@/constants/enum.js';
 import { narrowToSocialSource } from '@/helpers/narrowToSocialSource.js';
 import { createIndicator } from '@/helpers/pageable.js';
 import { resolveSocialMediaProvider } from '@/helpers/resolveSocialMediaProvider.js';
-import type { Post } from '@/providers/types/SocialMedia.js';
 import { useSearchStateStore } from '@/store/useSearchStore.js';
-
-const getSearchItemContent = (post: Post, index: number, listKey: string) => {
-    return <SinglePost key={post.postId} post={post} listKey={listKey} index={index} />;
-};
 
 export function SearchPostContent() {
     const { searchKeyword, searchType, source } = useSearchStateStore();
@@ -53,7 +48,7 @@ export function SearchPostContent() {
                 VirtualListProps={{
                     listKey,
                     computeItemKey: (index, post) => `${post.postId}_${index}`,
-                    itemContent: (index, post) => getSearchItemContent(post, index, listKey),
+                    itemContent: (index, post) => getPostItemContent(index, post, listKey),
                 }}
                 NoResultsFallbackProps={{
                     message: <Empty keyword={searchKeyword} />,
