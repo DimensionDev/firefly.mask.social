79786be02ace9c1c78463c279e7879eae21f5a1e
Wukong Sun <swkatmask@gmail.com>
2024-11-26 19:11:30 +0800

    fix: fw-2942 login idle accounts (#3299)

--- src/app/(settings)/components/AccountCard.tsx
+++ src/app/(settings)/components/AccountCard.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { t, Trans } from '@lingui/macro';
+import { useQuery } from '@tanstack/react-query';
 import { useAsyncFn } from 'react-use';
+import { useAccount } from 'wagmi';
 
 import DisconnectIcon from '@/assets/disconnect.svg';
 import LoadingIcon from '@/assets/loading.svg';
@@ -9,14 +11,19 @@ import { ClickableButton } from '@/components/ClickableButton.js';
 import { ProfileAvatar } from '@/components/ProfileAvatar.js';
 import { ProfileName } from '@/components/ProfileName.js';
 import { Tooltip } from '@/components/Tooltip.js';
-import type { SocialSource } from '@/constants/enum.js';
+import { config } from '@/configs/wagmiClient.js';
+import { type SocialSource, Source } from '@/constants/enum.js';
+import { EMPTY_LIST } from '@/constants/index.js';
 import { enqueueErrorMessage, enqueueSuccessMessage } from '@/helpers/enqueueMessage.js';
+import { getProfileState } from '@/helpers/getProfileState.js';
 import { getSnackbarMessageFromError } from '@/helpers/getSnackbarMessageFromError.js';
+import { getWalletClientRequired } from '@/helpers/getWalletClientRequired.js';
 import { isSameProfile } from '@/helpers/isSameProfile.js';
 import { useConnectedAccounts } from '@/hooks/useConnectedAccounts.js';
 import { useCurrentProfile } from '@/hooks/useCurrentProfile.js';
 import { useProfileStoreAll } from '@/hooks/useProfileStore.js';
 import { DisconnectFireflyAccountModalRef, LoginModalRef, LogoutModalRef } from '@/modals/controls.js';
+import { LensSocialMediaProvider } from '@/providers/lens/SocialMedia.js';
 import type { Account } from '@/providers/types/Account.js';
 import { switchAccount } from '@/services/account.js';
 
@@ -78,16 +85,38 @@ export function AccountCard({ source }: AccountCardProps) {
         }
     }, []);
 
+    const account = useAccount();
+    const { data: profiles = EMPTY_LIST } = useQuery({
+        enabled: source === Source.Lens,
+        queryKey: ['lens', 'profiles', account.address],
+        queryFn: async () => {
+            try {
+                const { account } = await getWalletClientRequired(config);
+                const profiles = await LensSocialMediaProvider.getProfilesByAddress(account.address);
+                return profiles ?? EMPTY_LIST;
+            } catch {
+                return EMPTY_LIST;
+            }
+        },
+        select: (profiles) => {
+            if (!profiles) return EMPTY_LIST;
+            const { accounts } = getProfileState(Source.Lens);
+            return profiles
+                .filter((x) => !accounts.some((y) => isSameProfile(x, y.profile)))
+                .map((x) => ({ profile: x, session: null }));
+        },
+    });
+
     return (
         <div className="flex w-full flex-col gap-4">
-            {accounts.map((account) => (
+            {[...accounts, ...profiles].map((account) => (
                 <div
                     key={account.profile.profileId}
                     className="inline-flex h-[63px] w-full items-center justify-start gap-3 rounded-lg bg-white bg-bottom px-3 py-2 shadow-primary backdrop-blur dark:bg-bg"
                 >
                     <ProfileAvatar profile={account.profile} size={36} />
                     <ProfileName profile={account.profile} />
-                    <DisconnectButton account={account} />
+                    {account.session ? <DisconnectButton account={account} /> : null}
                     {isSameProfile(account.profile, profile) && account.session ? (
                         <ClickableButton
                             className="text-medium font-bold leading-none text-main"
