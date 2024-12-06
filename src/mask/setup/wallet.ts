29c80f423dd2c32f0978b073db925bfa8909f440
guanbinrui <guanbinrui@mask.io>
2024-12-06 19:31:32 +0800

    chore: impl context

--- src/mask/setup/wallet.ts
+++ src/mask/setup/wallet.ts
@@ -1,15 +1,21 @@
-import { ChainId as EVMChainId } from '@masknet/web3-shared-evm';
 import { ChainId as SolanaChainId } from '@masknet/web3-shared-solana';
 
 import { AsyncStatus } from '@/constants/enum.js';
-import { createConstantSubscription } from '@/helpers/subscription.js';
+import { createConstantSubscription, SubscriptionDebug } from '@/helpers/subscription.js';
 import { initWallet, type WalletAPI } from '@/mask/bindings/index.js';
 import { useGlobalState } from '@/store/useGlobalStore.js';
+import { config } from '@/configs/wagmiClient.js';
 
 const WalletIO: WalletAPI.IOContext = {
     EVM: {
-        chainId: createConstantSubscription(EVMChainId.Mainnet),
-        account: createConstantSubscription('0x66b57885E8E9D84742faBda0cE6E3496055b012d'),
+        chainId: SubscriptionDebug({
+            getCurrentValue: () => config.getClient().chain.id,
+            subscribe: (sub) => config.subscribe((s) => s, sub),
+        }),
+        account: SubscriptionDebug({
+            getCurrentValue: () => config.getClient().account?.address ?? '',
+            subscribe: (sub) => config.subscribe((s) => s, sub),
+        }),
     },
     Solana: {
         chainId: createConstantSubscription(SolanaChainId.Mainnet),
