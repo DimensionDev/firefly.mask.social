f7d3de35a6762ebc36be22b520ce666d82654d01
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2024-12-06 23:40:48 +0800

    fix: send rp tx (#3419)
    * fix: send rp tx
    
    * chore: sync submodule - maskbook

--- src/mask/setup/wallet.ts
+++ src/mask/setup/wallet.ts
@@ -1,5 +1,6 @@
+import { EthereumMethodType, type RequestArguments } from '@masknet/web3-shared-evm';
 import { ChainId as SolanaChainId } from '@masknet/web3-shared-solana';
-import { getAccount, getChainId } from 'wagmi/actions';
+import { getAccount, getChainId, sendTransaction, waitForTransactionReceipt } from 'wagmi/actions';
 
 import { config } from '@/configs/wagmiClient.js';
 import { AsyncStatus } from '@/constants/enum.js';
@@ -17,6 +18,28 @@ const WalletIO: WalletAPI.IOContext = {
             getCurrentValue: () => getAccount(config).address ?? '',
             subscribe: (sub) => config.subscribe((s) => s, sub),
         }),
+        request: async <T>(requestArguments: RequestArguments) => {
+            const sendRequest = async () => {
+                switch (requestArguments.method) {
+                    case EthereumMethodType.ETH_SEND_TRANSACTION:
+                        const transaction: { chainId: string | number } = requestArguments.params[0];
+                        const hash = await sendTransaction(config, {
+                            ...transaction,
+                            chainId:
+                                typeof transaction.chainId === 'string'
+                                    ? Number.parseInt(transaction.chainId, 16)
+                                    : transaction.chainId,
+                        });
+                        await waitForTransactionReceipt(config, {
+                            hash,
+                        });
+                        return hash;
+                    default:
+                        throw new Error(`Method ${requestArguments.method} is not supported`);
+                }
+            };
+            return (await sendRequest()) as T;
+        },
     },
     Solana: {
         chainId: createConstantSubscription(SolanaChainId.Mainnet),
