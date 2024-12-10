3d8488f2d43a7aecbf3bdba62bd4382d6711e4e0
dudu0506 <dudu@mask.io>
2024-12-10 12:07:53 +0800

    feat: compose fetching nft bookmark status

--- src/hooks/useHasBookmarked.ts
+++ src/hooks/useHasBookmarked.ts
@@ -1,16 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
+import { skipToken, useQuery } from '@tanstack/react-query';
 
 import type { BookmarkType, FireflyPlatform } from '@/constants/enum.js';
 import { FireflySocialMediaProvider } from '@/providers/firefly/SocialMedia.js';
 
-export function useHasBookmarked(platform: FireflyPlatform, id: string, postType?: BookmarkType) {
+export function useHasBookmarked(platform: FireflyPlatform, id: string, postType?: BookmarkType, disabled?: boolean) {
     return useQuery({
         queryKey: ['has-bookmarked', platform, id],
         staleTime: 1000 * 60 * 5,
-        queryFn: async () => {
-            const data = await FireflySocialMediaProvider.getBookmarksByIds(platform, [id], postType);
+        enabled: !disabled,
+        queryFn: disabled
+            ? skipToken
+            : async () => {
+                  const data = await FireflySocialMediaProvider.getBookmarksByIds(platform, [id], postType);
 
-            return data[0]?.post_id === id && !!data[0].has_book_marked;
-        },
+                  return data[0]?.post_id === id && !!data[0].has_book_marked;
+              },
     });
 }
