fe3578d26a5805852ce8dbbcd0bf516712f67502
LeifXu <167842553+LeifXu@users.noreply.github.com>
2024-12-10 13:27:23 +1030

    feat: gasless to claim of red packet (#3429)
    * feat: free gas fees to claim of red packet
    
    * feat: free gas fees to claim of red packet
    
    * feat: free gas fees to claim of red packet
    
    * feat: firefly red packet query key
    
    * Update src/mask/plugins/red-packet/hooks/useClaimCallback.ts
    
    ---------
    
    Co-authored-by: guanbinrui <52657989+guanbinrui@users.noreply.github.com>

---
+++ src/mask/plugins/red-packet/hooks/useCheckSponsorableGasFee.ts
@@ -0,0 +1,18 @@
+import { NetworkPluginID } from '@masknet/shared-base';
+import { useQuery } from '@tanstack/react-query';
+
+import type { Web3Helper } from '@/maskbook/packages/web3-helpers/src/index.js';
+import { FireflyRedPacket } from '@/providers/red-packet/index.js';
+
+export function useCheckSponsorableGasFee<T extends NetworkPluginID>(
+    chainId: Web3Helper.Definition[T]['ChainId'],
+    walletAddress?: string,
+) {
+    return useQuery({
+        queryKey: ['firefly-red-packet-sponsorable-gas-fee', walletAddress, chainId],
+        queryFn() {
+            if (!walletAddress || !chainId) return false;
+            return FireflyRedPacket.checkGasFreeStatus(walletAddress, chainId);
+        },
+    });
+}
