a5a29aaaad290e6c7fa9de36f3f4a01ba2794da6
guanbinrui <guanbinrui@mask.io>
2024-12-12 20:44:19 +0800

    refactor: rm wc adaptor

--- src/providers/solana/resolveSolanaWalletAdapter.ts
+++ src/providers/solana/resolveSolanaWalletAdapter.ts
@@ -1,11 +1,9 @@
 /* cspell:disable */
 
-import { type SignerWalletAdapter, WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { type SignerWalletAdapter } from '@solana/wallet-adapter-base';
 import { CoinbaseWalletAdapter, CoinbaseWalletName } from '@solana/wallet-adapter-coinbase';
 import { PhantomWalletAdapter, PhantomWalletName } from '@solana/wallet-adapter-phantom';
-import { WalletConnectWalletAdapter, WalletConnectWalletName } from '@solana/wallet-adapter-walletconnect';
 
-import { env } from '@/constants/env.js';
 import { UnreachableError } from '@/constants/error.js';
 import { createLookupTableResolver } from '@/helpers/createLookupTableResolver.js';
 
@@ -13,12 +11,6 @@ export const resolveSolanaWalletAdapter = createLookupTableResolver(
     {
         [PhantomWalletName]: new PhantomWalletAdapter(),
         [CoinbaseWalletName]: new CoinbaseWalletAdapter(),
-        [WalletConnectWalletName]: new WalletConnectWalletAdapter({
-            options: {
-                projectId: env.external.NEXT_PUBLIC_W3M_PROJECT_ID,
-            },
-            network: WalletAdapterNetwork.Mainnet,
-        }),
     } as unknown as Record<string, SignerWalletAdapter>,
     (walletName: string) => {
         throw new UnreachableError('Solana wallet', walletName);
