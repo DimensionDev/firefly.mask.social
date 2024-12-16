47e20cd01add3ecd6d490b97051c526a4e41009c
LeifXu <ins@mask.io>
2024-12-16 16:37:50 +1030

    fix: gasless params to claim red packet

--- src/mask/plugins/red-packet/hooks/useClaimCallback.ts
+++ src/mask/plugins/red-packet/hooks/useClaimCallback.ts
@@ -1,14 +1,18 @@
 import { useRedPacketConstants } from '@masknet/web3-shared-evm';
+import { useCallback } from 'react';
 import { useAsyncFn } from 'react-use';
 import { type Address } from 'viem';
 import { getChainId, switchChain, writeContract } from 'wagmi/actions';
 
 import { config } from '@/configs/wagmiClient.js';
 import type { SocialSource } from '@/constants/enum.js';
+import { resolveRedPacketPlatformType } from '@/helpers/resolveRedPacketPlatformType.js';
+import { runInSafeAsync } from '@/helpers/runInSafe.js';
 import { waitForEthereumTransaction } from '@/helpers/waitForEthereumTransaction.js';
 import { useChainContext } from '@/hooks/useChainContext.js';
 import { HappyRedPacketV4ABI } from '@/mask/bindings/constants.js';
 import { EVMChainResolver } from '@/mask/bindings/index.js';
+import { useCurrentClaimProfile } from '@/mask/plugins/red-packet/hooks/useCurrentClaimProfile.js';
 import { useSignedMessage } from '@/mask/plugins/red-packet/hooks/useSignedMessage.js';
 import { FireflyRedPacket } from '@/providers/red-packet/index.js';
 import type { RedPacketJSONPayload } from '@/providers/red-packet/types.js';
@@ -30,17 +34,32 @@ export function useClaimCallback(
     const { HAPPY_RED_PACKET_ADDRESS_V4: redpacketContractAddress } = useRedPacketConstants(chainId);
 
     const { refetch } = useSignedMessage(account, payload, source);
+    const { data: me } = useCurrentClaimProfile(source);
+    const tryToClaimWithSponsor = useCallback(async (): Promise<string | undefined> => {
+        return runInSafeAsync(async () => {
+            const sponsorable = await FireflyRedPacket.checkGasFreeStatus(chainId, account);
+            if (!sponsorable || !me?.profileId) return;
+            return FireflyRedPacket.claimForGasFree(rpid, account, {
+                needLensAndFarcasterHandle: true,
+                platform: resolveRedPacketPlatformType(source),
+                profileId: me.profileId,
+                handle: me?.handle,
+                lensToken: me?.lensToken,
+                farcasterMessage: me?.farcasterMessage as HexString,
+                farcasterSigner: me?.farcasterSigner as HexString,
+                farcasterSignature: me?.farcasterSignature as HexString,
+            });
+        });
+    }, []);
 
     return useAsyncFn(async () => {
         const globalChainId = getChainId(config);
         if (globalChainId !== chainId) await switchChain(config, { chainId });
-
         if (!redpacketContractAddress || !rpid) return;
-        const sponsorable = await FireflyRedPacket.checkGasFreeStatus(chainId, account);
-        if (sponsorable) {
-            const hash = await FireflyRedPacket.claimForGasFree(rpid, account);
-            return hash;
-        }
+
+        const claimWithSponsorHash = await tryToClaimWithSponsor();
+        if (claimWithSponsorHash) return claimWithSponsorHash;
+
         const { data: signedMsg } = await refetch();
         if (!signedMsg) return;
 
