00ded37c9b1f749ca629f487d2495a84792eb87b
LeifXu <ins@mask.io>
2024-12-19 18:32:40 +1030

    feat: disable lens and x when searching for profile posts

--- src/components/Activity/ActivityTasks/index.tsx
+++ src/components/Activity/ActivityTasks/index.tsx
@@ -22,7 +22,7 @@ export function ActivityTasks({ name, data }: Props) {
             return <ActivityFrensgivingTasks data={data} />;
         case 'pengu':
             return (
-                <ActivityMobileOnly disabled>
+                <ActivityMobileOnly>
                     <ActivityPenguTasks data={data} />
                 </ActivityMobileOnly>
             );
