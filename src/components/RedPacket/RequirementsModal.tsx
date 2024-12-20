1bc20e8007bb96a72cbcb90ef50f6defa1d1bd5f
Wukong Sun <swkatmask@gmail.com>
2024-12-20 09:57:14 +0800

    fix: fw-3096 verify and claim (#3525)
    * fix: fw-3096 verify and claim
    
    * fix: fetch image as png

--- src/components/RedPacket/RequirementsModal.tsx
+++ src/components/RedPacket/RequirementsModal.tsx
@@ -1,15 +1,10 @@
 import { t, Trans } from '@lingui/macro';
 import { getEnumAsArray } from '@masknet/kit';
-import { useRedPacketConstants } from '@masknet/web3-shared-evm';
 import { useQueries } from '@tanstack/react-query';
-import { last, sortBy } from 'lodash-es';
+import { sortBy } from 'lodash-es';
 import { Fragment, useMemo } from 'react';
-import { useAsyncFn } from 'react-use';
-import type { Address } from 'viem';
-import { readContract } from 'wagmi/actions';
 
 import AddUser from '@/assets/add-user.svg';
-import CircleSuccessIcon from '@/assets/circle-success.svg';
 import CloseSquareIcon from '@/assets/close-square.svg';
 import Comment from '@/assets/comment-rp.svg';
 import Like from '@/assets/like.svg';
@@ -21,23 +16,12 @@ import { ActionButton } from '@/components/ActionButton.js';
 import { CloseButton } from '@/components/IconButton.js';
 import { Loading } from '@/components/Loading.js';
 import { Modal } from '@/components/Modal.js';
-import { queryClient } from '@/configs/queryClient.js';
-import { config } from '@/configs/wagmiClient.js';
 import { Link } from '@/esm/Link.js';
 import { classNames } from '@/helpers/classNames.js';
-import { enqueueErrorMessage, enqueueSuccessMessage } from '@/helpers/enqueueMessage.js';
-import { formatBalance } from '@/helpers/formatBalance.js';
 import { resolveRedPacketPlatformType } from '@/helpers/resolveRedPacketPlatformType.js';
-import { useChainContext } from '@/hooks/useChainContext.js';
-import { HappyRedPacketV4ABI } from '@/mask/bindings/constants.js';
 import { EVMExplorerResolver, NFTScanNonFungibleTokenEVM } from '@/mask/bindings/index.js';
 import { MentionLink } from '@/mask/plugins/red-packet/components/Requirements/MentionLink.js';
-import { useClaimCallback } from '@/mask/plugins/red-packet/hooks/useClaimCallback.js';
-import { useClaimStrategyStatus } from '@/mask/plugins/red-packet/hooks/useClaimStrategyStatus.js';
-import { useCurrentClaimProfile } from '@/mask/plugins/red-packet/hooks/useCurrentClaimProfile.js';
-import { ConfirmModalRef } from '@/modals/controls.js';
-import { FireflyRedPacket } from '@/providers/red-packet/index.js';
-import { FireflyRedPacketAPI, type RedPacketJSONPayload } from '@/providers/red-packet/types.js';
+import { FireflyRedPacketAPI } from '@/providers/red-packet/types.js';
 import type { Post } from '@/providers/types/SocialMedia.js';
 
 interface NFTListProps {
@@ -84,14 +68,6 @@ function ResultIcon({ result }: { result: boolean }) {
     );
 }
 
-interface RequirementsModalProps {
-    open: boolean;
-    onClose: () => void;
-    payload: RedPacketJSONPayload;
-    post: Post;
-    showResults: boolean;
-}
-
 const IconMap: Record<
     FireflyRedPacketAPI.PostReactionKind,
     React.FunctionComponent<React.SVGAttributes<SVGElement>>
@@ -111,87 +87,37 @@ export const TitleMap = {
     collect: t`NFT holder`,
 };
 
-export function RequirementsModal({ open, onClose, payload, post, showResults }: RequirementsModalProps) {
-    const { account } = useChainContext();
-
-    const { data, isLoading, isFetching, refetch } = useClaimStrategyStatus(payload, post.source, () => {
-        enqueueErrorMessage(t`Oops... Network issue. Please try again`);
-    });
-    const { data: currentClaimProfile } = useCurrentClaimProfile(post.source);
+interface RequirementsModalProps {
+    open: boolean;
+    post: Post;
+    claimStrategyStatus?: FireflyRedPacketAPI.ClaimStrategyStatus[];
+    showResults: boolean;
+    isVerifying: boolean;
+    isClaiming: boolean;
+    onClose: () => void;
+    onVerifyAndClaim: () => void;
+}
+export function RequirementsModal({
+    open,
+    post,
+    showResults,
+    claimStrategyStatus,
+    isVerifying,
+    isClaiming,
+    onClose,
+    onVerifyAndClaim,
+}: RequirementsModalProps) {
     const requirements = useMemo(() => {
         const orders = getEnumAsArray(FireflyRedPacketAPI.StrategyType).map((x) => x.value);
-        return sortBy(data?.data.claimStrategyStatus, (x) =>
-            orders.indexOf(x.type as FireflyRedPacketAPI.StrategyType),
-        );
-    }, [data?.data.claimStrategyStatus]);
-
-    const [{ loading: claimLoading }, claimCallback] = useClaimCallback(account, payload, post.source);
-
-    const { HAPPY_RED_PACKET_ADDRESS_V4: redpacketContractAddress } = useRedPacketConstants(payload.chainId);
-
-    const [{ loading }, handleClick] = useAsyncFn(async () => {
-        const { data } = await refetch();
-        if (!data?.data.canClaim) {
-            enqueueErrorMessage(t`Oops... Not all the requirements have been meet`);
-            return;
-        }
-
-        const hash = await claimCallback();
-        if (hash && currentClaimProfile?.profileId && currentClaimProfile.handle) {
-            await FireflyRedPacket.finishClaiming(
-                payload.rpid,
-                currentClaimProfile.platform,
-                currentClaimProfile.profileId,
-                currentClaimProfile?.handle,
-                hash,
-            );
-        }
-
-        await queryClient.refetchQueries({
-            queryKey: ['red-packet', 'claim', payload.rpid],
-        });
-
-        const availability = (await readContract(config, {
-            abi: HappyRedPacketV4ABI,
-            functionName: 'check_availability',
-            address: redpacketContractAddress as Address,
-            args: [payload.rpid],
-            account: account as Address,
-        })) as [string, bigint, bigint, bigint, boolean, bigint];
-
-        const claimed_amount = last(availability) as bigint;
-
-        const amount = formatBalance(claimed_amount.toString(), payload.token?.decimals, { significant: 2 });
-        ConfirmModalRef.open({
-            title: t`Lucky Drop`,
-            content: (
-                <div className="flex h-[276px] w-[388px] flex-col items-center">
-                    <CircleSuccessIcon width={90} height={90} />
-                    <div className="mt-3 text-xl font-bold leading-6 text-success">
-                        <Trans>Congratulations!</Trans>
-                    </div>
-                    <div className="mt-10 text-base font-bold leading-5 text-main">
-                        <Trans>
-                            Your claimed {amount} {payload.token?.symbol}.
-                        </Trans>
-                    </div>
-                </div>
-            ),
-            modalClass: 'md:w-auto',
-            enableConfirmButton: true,
-            variant: 'normal',
-            confirmButtonText: t`Share`,
-        });
-
-        enqueueSuccessMessage(t`Claimed lucky drop with ${amount} ${payload.token?.symbol} successfully`);
-        onClose();
-    }, [claimCallback, currentClaimProfile]);
+        return sortBy(claimStrategyStatus, (x) => orders.indexOf(x.type as FireflyRedPacketAPI.StrategyType));
+    }, [claimStrategyStatus]);
 
     return (
         <Modal open={open} onClose={onClose}>
             <div className="flex min-h-[344px] min-w-[476px] transform flex-col rounded-xl bg-primaryBottom transition-all">
                 <div className="flex items-center justify-center gap-2 rounded-t-xl p-4">
                     <CloseButton
+                        className="h-6 w-6 shrink-0"
                         onClick={() => {
                             onClose();
                         }}
@@ -202,7 +128,7 @@ export function RequirementsModal({ open, onClose, payload, post, showResults }:
                     <div className="relative h-6 w-6" />
                 </div>
                 <div className="flex flex-1 flex-col gap-2 rounded-b-xl bg-primaryBottom px-6 pb-4 pt-6">
-                    {isLoading && !requirements.length ? (
+                    {isVerifying && !requirements.length ? (
                         <Loading className="!min-h-[200px]" />
                     ) : (
                         requirements.flatMap((status) => {
@@ -216,9 +142,9 @@ export function RequirementsModal({ open, onClose, payload, post, showResults }:
                                         className={classNames(
                                             'flex items-center gap-x-[10px] rounded-lg px-2 py-4 text-base leading-[18px]',
                                             {
-                                                'bg-bg': showResults && isFetching,
+                                                'bg-bg': showResults && isVerifying,
                                                 'bg-success/10 dark:bg-success/20':
-                                                    showResults && !isFetching && status.result,
+                                                    showResults && !isVerifying && status.result,
                                             },
                                         )}
                                         key={FireflyRedPacketAPI.StrategyType.profileFollow}
@@ -232,7 +158,7 @@ export function RequirementsModal({ open, onClose, payload, post, showResults }:
                                         </span>
 
                                         {showResults ? (
-                                            isFetching && !status.result ? (
+                                            isVerifying && !status.result ? (
                                                 <LoadingIcon
                                                     className="animate-spin text-secondary"
                                                     width={24}
@@ -273,9 +199,9 @@ export function RequirementsModal({ open, onClose, payload, post, showResults }:
                                                 className={classNames(
                                                     'flex items-center gap-x-[10px] rounded-lg px-2 py-4 text-base leading-[18px]',
                                                     {
-                                                        'bg-bg': showResults && isFetching,
+                                                        'bg-bg': showResults && isVerifying,
                                                         'bg-success/10 dark:bg-success/20':
-                                                            showResults && !isFetching && condition.value,
+                                                            showResults && !isVerifying && condition.value,
                                                     },
                                                 )}
                                                 key={condition.key}
@@ -285,7 +211,7 @@ export function RequirementsModal({ open, onClose, payload, post, showResults }:
                                                     {TitleMap[condition.key]}
                                                 </span>
                                                 {showResults ? (
-                                                    isFetching && !condition.value ? (
+                                                    isVerifying && !condition.value ? (
                                                         <LoadingIcon
                                                             className="animate-spin text-secondary"
                                                             width={24}
@@ -306,9 +232,9 @@ export function RequirementsModal({ open, onClose, payload, post, showResults }:
                                         className={classNames(
                                             'flex items-center gap-x-[10px] rounded-lg px-2 py-4 text-base leading-[18px]',
                                             {
-                                                'bg-bg': showResults && isFetching,
+                                                'bg-bg': showResults && isVerifying,
                                                 'bg-success/10 dark:bg-success/20':
-                                                    showResults && !isFetching && status.result,
+                                                    showResults && !isVerifying && status.result,
                                             },
                                         )}
                                         key={status.type}
@@ -320,7 +246,7 @@ export function RequirementsModal({ open, onClose, payload, post, showResults }:
                                             </Trans>
                                         </span>
                                         {showResults ? (
-                                            isFetching && !status.result ? (
+                                            isVerifying && !status.result ? (
                                                 <LoadingIcon
                                                     className="animate-spin text-secondary"
                                                     width={24}
@@ -341,9 +267,9 @@ export function RequirementsModal({ open, onClose, payload, post, showResults }:
                     {showResults ? (
                         <ActionButton
                             className="w-full flex-none"
-                            onClick={handleClick}
-                            loading={loading || claimLoading}
-                            disabled={isFetching}
+                            onClick={onVerifyAndClaim}
+                            loading={isClaiming || isVerifying}
+                            disabled={isVerifying}
                         >
                             <Trans>Verify and Claim</Trans>
                         </ActionButton>
