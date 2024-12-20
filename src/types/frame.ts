50a8389841d77ff4fec42c001a859363a23ffbba
guanbinrui <guanbinrui@mask.io>
2024-12-20 22:25:23 +0800

    refactor: config

--- src/types/frame.ts
+++ src/types/frame.ts
@@ -183,6 +183,58 @@ export interface FarcasterManifest {
     triggers?: TriggerConfig[];
 }
 
+/**
+ * The embedded frame configuration.
+ */
+export interface FrameV2 {
+    x_url: string;
+    x_manifest?: FarcasterManifest;
+
+    // Frame spec version. Required.
+    // Example: "next"
+    version: Omit<string, 'next'> | 'next';
+
+    // Frame image.
+    // Max 512 characters.
+    // Image must be 3:2 aspect ratio and less than 10 MB.
+    // Example: "https://yoink.party/img/start.png"
+    imageUrl: string;
+
+    // Button attributes
+    button: {
+        // Button text.
+        // Max length of 32 characters.
+        // Example: "Yoink Flag"
+        title: string;
+
+        // Action attributes
+        action: {
+            // Action type. Must be "launch_frame".
+            type: 'launch_frame';
+
+            // App name
+            // Max length of 32 characters.
+            // Example: "Yoink!"
+            name: string;
+
+            // Frame launch URL.
+            // Max 512 characters.
+            // Example: "https://yoink.party/"
+            url: string;
+
+            // Splash image URL.
+            // Max 512 characters.
+            // Image must be 200x200px and less than 1MB.
+            // Example: "https://yoink.party/img/splash.png"
+            splashImageUrl: string;
+
+            // Hex color code.
+            // Example: "#eeeee4"
+            splashBackgroundColor: string;
+        };
+    };
+}
+
 // didn't find the type definition for Mention
 type Mention = unknown;
 
@@ -269,56 +321,14 @@ export interface DirectCastEmbedLaunchContext {
     type: 'direct_cast_embed';
 }
 
-/**
- * The embedded frame configuration.
- */
-export interface FrameV2 {
-    x_url: string;
-
-    // Frame spec version. Required.
-    // Example: "next"
-    version: Omit<string, 'next'> | 'next';
-
-    // Frame image.
-    // Max 512 characters.
-    // Image must be 3:2 aspect ratio and less than 10 MB.
-    // Example: "https://yoink.party/img/start.png"
-    imageUrl: string;
-
-    // Button attributes
-    button: {
-        // Button text.
-        // Max length of 32 characters.
-        // Example: "Yoink Flag"
-        title: string;
-
-        // Action attributes
-        action: {
-            // Action type. Must be "launch_frame".
-            type: 'launch_frame';
-
-            // App name
-            // Max length of 32 characters.
-            // Example: "Yoink!"
-            name: string;
-
-            // Frame launch URL.
-            // Max 512 characters.
-            // Example: "https://yoink.party/"
-            url: string;
-
-            // Splash image URL.
-            // Max 512 characters.
-            // Image must be 200x200px and less than 1MB.
-            // Example: "https://yoink.party/img/splash.png"
-            splashImageUrl: string;
-
-            // Hex color code.
-            // Example: "#eeeee4"
-            splashBackgroundColor: string;
-        };
-    };
-}
+export type FrameContext =
+    | CastLaunchContext
+    | NotificationLaunchContext
+    | ComposerLaunchContext
+    | ChannelLaunchContext
+    | UserLaunchContext
+    | LinkLaunchContext
+    | DirectCastEmbedLaunchContext;
 
 export type ReadyAction = (
     options: Partial<{
@@ -368,7 +378,16 @@ type AddFrameResult =
 export type AddFrameAction = () => Promise<AddFrameResult>;
 
 // Feature: Primary Button
-export type SetPrimaryButton = (options: { text: string; enabled?: boolean; hidden?: boolean }) => Promise<void>;
+export type SetPrimaryButtonAction = (options: { text: string; enabled?: boolean; hidden?: boolean }) => Promise<void>;
+
+export type FrameAction =
+    | ReadyAction
+    | OpenExternalUrlAction
+    | CloseAction
+    | RequestAuthTokenAction
+    | AddFrameAction
+    | SetPrimaryButtonAction;
+
 // #endregion
 
 export type Frame = FrameV1 | FrameV2;
