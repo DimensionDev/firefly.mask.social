efff3ba0081690f016b7f0b2271289a322517cca
dudu0506 <dudu@mask.io>
2024-12-20 22:03:07 +0800

    fix: hide follwers count for farcaster profile (#3546)
    * fix: hide follwers count for farcaster profile
    
    https://mask.atlassian.net/browse/FW-3120
    
    * chore: code maintenance [bot]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/components/FollowInList.tsx
+++ src/components/FollowInList.tsx
@@ -1,6 +1,13 @@
 import { ProfileInList } from '@/components/ProfileInList.js';
+import { type SocialSource, Source } from '@/constants/enum.js';
 import type { Profile } from '@/providers/types/SocialMedia.js';
 
-export function getFollowInList(index: number, profile: Profile) {
-    return <ProfileInList profile={profile} key={`${profile.profileId}-${index}`} />;
+export function getFollowInList(index: number, profile: Profile, source?: SocialSource) {
+    return (
+        <ProfileInList
+            profile={profile}
+            key={`${profile.profileId}-${index}`}
+            hideFollowers={source === Source.Farcaster}
+        />
+    );
 }
