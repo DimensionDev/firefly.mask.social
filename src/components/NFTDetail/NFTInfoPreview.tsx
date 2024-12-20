66e6c5e94a14c9d9978d78e6caf02010c28e75f8
LeifXu <ins@mask.io>
2024-12-20 23:32:08 +1030

    feat: nft video player

--- src/components/NFTDetail/NFTInfoPreview.tsx
+++ src/components/NFTDetail/NFTInfoPreview.tsx
@@ -9,24 +9,23 @@ import { NFTImage } from '@/components/NFTImage.js';
 import { classNames } from '@/helpers/classNames.js';
 import type { SimpleHash } from '@/providers/simplehash/type.js';
 
-export function NFTInfoPreview({
-    name,
-    imageURL,
+function NFTVideo({
     video,
+    imageURL,
 }: {
     imageURL: string;
     video?: {
         properties: SimpleHash.VideoProperties;
         url: string;
     };
-    name: string;
 }) {
     const iconClassName = 'size-4 text-primaryBottom';
     const [isStarted, setIsStarted] = useState(false);
     const [isPlaying, setIsPlaying] = useState(false);
     const ref = useRef<HTMLVideoElement | null>(null);
+
     return (
-        <div className="relative h-full w-full max-w-[250px] overflow-hidden rounded-[20px] object-cover shadow-lightS3">
+        <>
             {!isStarted || !isPlaying ? (
                 <ClickableButton
                     className="absolute left-1/2 top-1/2 flex size-8 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-lg bg-black/25"
@@ -37,8 +36,7 @@ export function NFTInfoPreview({
                 >
                     <PlayIcon className={iconClassName} />
                 </ClickableButton>
-            ) : null}
-            {isPlaying ? (
+            ) : isPlaying ? (
                 <ClickableButton
                     className="absolute left-0 top-0 z-10 flex h-full w-full cursor-pointer items-center justify-center opacity-0 duration-100 hover:opacity-100"
                     onClick={() => {
@@ -74,6 +72,27 @@ export function NFTInfoPreview({
                         setIsPlaying(false);
                     }}
                 />
+            ) : null}
+        </>
+    );
+}
+
+export function NFTInfoPreview({
+    name,
+    imageURL,
+    video,
+}: {
+    imageURL: string;
+    video?: {
+        properties: SimpleHash.VideoProperties;
+        url: string;
+    };
+    name: string;
+}) {
+    return (
+        <div className="relative h-full w-full max-w-[250px] overflow-hidden rounded-[20px] object-cover shadow-lightS3">
+            {video ? (
+                <NFTVideo video={video} imageURL={imageURL} />
             ) : (
                 <NFTImage width={250} height={250} src={imageURL} alt={name} className="h-full w-full object-cover" />
             )}
