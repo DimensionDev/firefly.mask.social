74c77adbb725e781513c292254b2fe57168cb55d
Wukong Sun <swkatmask@gmail.com>
2024-12-20 17:41:23 +0800

    fix: fw-3090 fw-3121 custom cover file type

--- src/modals/RedPacketModal/ConfirmView.tsx
+++ src/modals/RedPacketModal/ConfirmView.tsx
@@ -17,7 +17,7 @@ import { ActionButton } from '@/components/ActionButton.js';
 import { RedPacketEnvelope } from '@/components/RedPacket/RedPacketEnvelope.js';
 import { Tab, Tabs } from '@/components/Tabs/index.js';
 import { Tooltip } from '@/components/Tooltip.js';
-import { ALLOWED_IMAGES_MIMES, EMPTY_LIST } from '@/constants/index.js';
+import { ALLOWED_COVER_MIMES, EMPTY_LIST } from '@/constants/index.js';
 import { classNames } from '@/helpers/classNames.js';
 import { enqueueErrorMessage } from '@/helpers/enqueueMessage.js';
 import { formatAddress } from '@/helpers/formatAddress.js';
@@ -234,7 +234,7 @@ export function ConfirmView() {
     const handleTabChange = useCallback(
         async (tab: 'default' | 'custom') => {
             if (tab === 'custom') {
-                const files = await selectFiles(ALLOWED_IMAGES_MIMES.join(', '));
+                const files = await selectFiles(ALLOWED_COVER_MIMES.join(', '));
                 if (files.length === 0) return;
                 const created = await createTheme(files[0]);
                 if (!created) return;
@@ -344,7 +344,7 @@ export function ConfirmView() {
                                     <div
                                         className="flex cursor-pointer justify-center gap-3 text-sm text-highlight"
                                         onClick={async () => {
-                                            const files = await selectFiles(ALLOWED_IMAGES_MIMES.join(', '));
+                                            const files = await selectFiles(ALLOWED_COVER_MIMES.join(', '));
                                             if (files.length === 0) return;
                                             await createTheme(files[0]);
                                         }}
