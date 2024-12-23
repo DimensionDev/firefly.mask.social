18668a5898ae844cb437f945fd6d7f009aa0375a
LeifXu <167842553+LeifXu@users.noreply.github.com>
2024-12-23 12:32:17 +1030

    feat: activity mobile only ssr (#3559)
    * feat: activity mobile only ssr
    
    * feat: activity pengu text
    
    * chore: code maintenance [bot]
    
    * Update src/locales/zh-Hans/messages.po
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/components/Activity/ActivityTasks/ActivityPenguTasks.tsx
+++ src/components/Activity/ActivityTasks/ActivityPenguTasks.tsx
@@ -145,7 +145,7 @@ Submit here ${shareUrl}
                         source={Source.Twitter}
                     >
                         <p className="pt-4 text-sm font-normal leading-6">
-                            <Trans>*Note: Each NFT can only be used once.</Trans>
+                            <Trans>*Note: Each NFT can only be submitted once.</Trans>
                         </p>
                     </ActivityPremiumConditionList>
                 </div>
