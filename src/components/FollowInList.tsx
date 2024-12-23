f5b039f8bf20ad2eff2e1391fd6d0f125906c16c
dudu0506 <dudu@mask.io>
2024-12-23 16:39:47 +0800

    feat: batch fetch profiles in following list (#3563)

--- src/components/FollowInList.tsx
+++ src/components/FollowInList.tsx
@@ -1,13 +1,6 @@
 import { ProfileInList } from '@/components/ProfileInList.js';
-import { type SocialSource, Source } from '@/constants/enum.js';
 import type { Profile } from '@/providers/types/SocialMedia.js';
 
-export function getFollowInList(index: number, profile: Profile, source?: SocialSource) {
-    return (
-        <ProfileInList
-            profile={profile}
-            key={`${profile.profileId}-${index}`}
-            hideFollowers={source === Source.Farcaster}
-        />
-    );
+export function getFollowInList(index: number, profile: Profile) {
+    return <ProfileInList profile={profile} key={`${profile.profileId}-${index}`} />;
 }
