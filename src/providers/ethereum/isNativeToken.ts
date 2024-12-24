495c08fafde7bf18e0c1d3af420c6e1c7f7f77fd
nuanyang233 <nuanyang233@gmail.com>
2024-12-24 19:09:04 +0800

    chore: hack for aurora native token (#3581)

--- src/providers/ethereum/isNativeToken.ts
+++ src/providers/ethereum/isNativeToken.ts
@@ -1,6 +1,7 @@
 import { isNativeTokenAddress } from '@masknet/web3-shared-evm';
 import { find } from 'lodash-es';
 import { isAddress } from 'viem';
+import { aurora } from 'wagmi/chains';
 
 import { chains } from '@/configs/wagmiClient.js';
 import type { Token } from '@/providers/types/Transfer.js';
@@ -9,6 +10,8 @@ export function isNativeToken(token: Token) {
     // It is a native token when token.id is not an address
     if (!isAddress(token.id)) {
         const chain = find(chains, (chain) => chain.id === token.chainId);
+        // hack for aurora, the debank id is different with wagmi.
+        if (token.id === 'aurora' && chain?.id === aurora.id) return true;
         return !!chain && chain.nativeCurrency.symbol === token.symbol;
     } else {
         return isNativeTokenAddress(token.id);
