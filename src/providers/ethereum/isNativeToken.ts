8cf052aeffb4fdac26a0577dc89804448125ca56
Wukong Sun <swkatmask@gmail.com>
2024-12-24 22:11:30 +0800

    fixup! fix: detect debank native token (#3582)

--- src/providers/ethereum/isNativeToken.ts
+++ src/providers/ethereum/isNativeToken.ts
@@ -9,7 +9,7 @@ export function isNativeToken(token: Token) {
     if (!isAddress(token.id)) {
         // according to https://docs.cloud.debank.com/en/readme/api-pro-reference/chain
         // id of native token is the same as chain id
-        return DEBANK_CHAIN_TO_CHAIN_ID_MAP[token.chain] === token.chainId;
+        return DEBANK_CHAIN_TO_CHAIN_ID_MAP[token.id] === token.chainId;
     } else {
         return isNativeTokenAddress(token.id);
     }
