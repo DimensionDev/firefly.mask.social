f667cea6cba0725f3b620421f10640ed40ec7334
guanbinrui <guanbinrui@mask.io>
2025-01-03 08:56:38 +0800

    fix: frame height

--- src/modals/FrameViewerModal/index.tsx
+++ src/modals/FrameViewerModal/index.tsx
@@ -74,7 +74,7 @@ export const FrameViewerModal = forwardRef<SingletonModalRefCreator<FrameViewerM
 
         return (
             <Modal disableDialogClose open={open} onClose={() => dispatch?.close()}>
-                <div className="relative flex h-[695px] w-[424px] flex-col overflow-hidden rounded-xl">
+                <div className="relative flex h-[755px] w-[424px] flex-col overflow-hidden rounded-xl">
                     <div className="flex h-[60px] flex-1 items-center justify-between bg-lightBg px-4 py-3 text-black dark:bg-fireflyBrand dark:text-white">
                         <div className="cursor-pointer">
                             <CloseButton onClick={() => dispatch?.close()} />
